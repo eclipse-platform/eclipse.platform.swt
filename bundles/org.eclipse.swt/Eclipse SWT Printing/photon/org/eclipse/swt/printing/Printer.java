31339801855a013a4f150de1231111e05e5f4887
Silenio Quarti <ssq>
2001-05-14 18:25:01 +0000

    Printing stubs for Photon

---
+++ bundles/org.eclipse.swt/Eclipse SWT Printing/photon/org/eclipse/swt/printing/Printer.java
@@ -0,0 +1,81 @@
+package org.eclipse.swt.printing;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.internal.*;
+import org.eclipse.swt.internal.photon.*; 
+
+public final class Printer extends Device {
+	PrinterData data;
+	int printContext, xScreen, xDrawable;
+	int defaultFontList;
+
+public static PrinterData[] getPrinterList() {
+	PrinterData printerList[] = new PrinterData[0];
+	return printerList;
+}
+
+static PrinterData getDefaultPrinterData() {
+	/* Use the first printer in the list as the default */
+	PrinterData[] list = getPrinterList();
+	if (list.length == 0) {
+		/* no printers */
+		SWT.error(SWT.ERROR_NO_HANDLES);
+	}
+	return list[0];
+}
+
+public Printer() {
+	this(getDefaultPrinterData());
+}
+
+public Printer(PrinterData data) {
+	super(data);
+}
+
+
+public int internal_new_GC(GCData data) {
+	return 0;
+}
+
+public void internal_dispose_GC(int xGC, GCData data) {
+}
+
+public boolean startJob(String jobName) {
+	return true;
+}
+
+public void endJob() {
+}
+
+public void cancelJob() {
+}
+
+public boolean startPage() {
+	return true;
+}
+
+public void endPage() {
+}
+
+public Point getDPI() {
+	return new Point(0, 0);
+}
+
+public Rectangle getBounds() {
+	return null;
+}
+
+public Rectangle getClientArea() {
+	return null;
+}
+
+public Rectangle computeTrim(int x, int y, int width, int height) {
+	return new Rectangle(0,0,0,0);
+}
+
+public PrinterData getPrinterData() {
+	return data;
+}
+
+}
