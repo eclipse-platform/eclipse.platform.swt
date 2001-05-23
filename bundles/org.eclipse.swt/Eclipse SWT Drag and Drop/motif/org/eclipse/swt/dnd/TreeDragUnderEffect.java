0c3b3268b515acb26b65212dcaa923b6ebe99e7b
Jeff Brown <jbrown>
2001-05-23 22:15:42 +0000

    Javado

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/motif/org/eclipse/swt/dnd/TreeDragUnderEffect.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/motif/org/eclipse/swt/dnd/TreeDragUnderEffect.java
@@ -1,13 +1,13 @@
 package org.eclipse.swt.dnd;
 
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.widgets.*;
-
 /*
  * Licensed Materials - Property of IBM,
  * (c) Copyright IBM Corp. 1998, 2000  All Rights Reserved
  */
- 
+
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+
 class TreeDragUnderEffect extends DragUnderEffect {
 
 	private Tree tree;
