2fc8e87f1a77e973a5cca4979755074c733213e3
Steve Northover <wadman>
2001-05-25 20:27:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Button.java
@@ -547,11 +547,11 @@ int widgetStyle () {
 	if ((style & SWT.LEFT) != 0) bits |= OS.BS_LEFT;
 	if ((style & SWT.CENTER) != 0) bits |= OS.BS_CENTER;
 	if ((style & SWT.RIGHT) != 0) bits |= OS.BS_RIGHT;
-	if ((style & SWT.PUSH) != 0) return bits | OS.BS_PUSHBUTTON;
+	if ((style & SWT.PUSH) != 0) return bits | OS.BS_PUSHBUTTON | OS.WS_TABSTOP;
 	if ((style & SWT.CHECK) != 0) return bits | OS.BS_CHECKBOX;
 	if ((style & SWT.RADIO) != 0) return bits | OS.BS_RADIOBUTTON;
 	if ((style & SWT.TOGGLE) != 0) return bits | OS.BS_PUSHLIKE | OS.BS_CHECKBOX;
-	return bits | OS.BS_PUSHBUTTON;
+	return bits | OS.BS_PUSHBUTTON | OS.WS_TABSTOP;
 }
 
 byte [] windowClass () {
