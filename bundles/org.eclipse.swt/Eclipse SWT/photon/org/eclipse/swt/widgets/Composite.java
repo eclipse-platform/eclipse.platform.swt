afdee057a49570355580d6ca3191459802b66504
Silenio Quarti <ssq>
2001-05-26 21:12:44 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/photon/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/photon/org/eclipse/swt/widgets/Composite.java
@@ -252,6 +252,7 @@ void resizeClientArea (int width, int height) {
 void setBounds (int x, int y, int width, int height, boolean move, boolean resize) {
 	if (resize) resizeClientArea (width, height);
 	super.setBounds (x, y, width, height, move, resize);
+	if (layout != null) layout (false);
 }
 
 public void setLayout (Layout layout) {
