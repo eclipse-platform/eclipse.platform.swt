5f020286f99747a4aa3be9274102d1550f59dacd
Veronika Irvine <torres>
2001-05-31 16:06:20 +0000

    build 116

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/Callback.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/Callback.java
@@ -36,7 +36,7 @@ public class Callback {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	public static int MINOR_VERSION = 115;
+	public static int MINOR_VERSION = 116;
 	
 	/**
 	 * SWT revision number (must be >= 0)
