f6c35a5cd899f4ade4d4491ef81fbb513fda39bd
Veronika Irvine <torres>
2001-06-14 15:36:49 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/graphics/Device.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/graphics/Device.java
@@ -77,6 +77,9 @@ public abstract class Device implements Drawable {
 		} catch (Throwable e) {}
 	}	
 
+/* 
+* TEMPORARY CODE 
+*/
 static Device getDevice () {
 	if (DeviceFinder != null) DeviceFinder.run();
 	Device device = CurrentDevice;
