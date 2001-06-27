044c66e0b8813c302d4225d54bce4a23dd6a98a0
Steve Northover <wadman>
2001-06-27 18:25:40 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Display.java
@@ -1379,10 +1379,11 @@ public boolean readAndDispatch () {
 	if (status != 0) {
 		if ((status & OS.XtIMTimer) != 0) {
 			OS.XtAppProcessEvent (xtContext, OS.XtIMTimer);
+			status = OS.XtAppPending (xtContext);
 		}
 		if ((status & OS.XtIMAlternateInput) != 0) {
 			OS.XtAppProcessEvent (xtContext, OS.XtIMAlternateInput);
-//			runAsyncMessages ();
+			status = OS.XtAppPending (xtContext);
 		}
 		if ((status & OS.XtIMXEvent) != 0) {
 			OS.XtAppNextEvent (xtContext, xEvent);
