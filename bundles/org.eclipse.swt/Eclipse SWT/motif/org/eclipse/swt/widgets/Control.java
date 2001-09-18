e43eb9c5e299ed9ea8560b4ae686437d38c1b11d
Grant Gayed <ggayed>
2001-09-18 14:37:14 +0000

    remove previous change

--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Control.java
@@ -1689,16 +1689,17 @@ void sendKeyEvent (int type, XKeyEvent xEvent) {
 	Event event = new Event ();
 	event.time = xEvent.time;
 	setKeyState (event, xEvent);
-	Control control = this;
-	if ((state & CANVAS) != 0) {
-		if ((style & SWT.NO_FOCUS) != 0) {
-			Display display = getDisplay ();
-			control = display.getFocusControl ();
-		}
-	}
-	if (control != null) {
-		control.postEvent (type, event);
-	}
+	postEvent (type,event);
+//	Control control = this;
+//	if ((state & CANVAS) != 0) {
+//		if ((style & SWT.NO_FOCUS) != 0) {
+//			Display display = getDisplay ();
+//			control = display.getFocusControl ();
+//		}
+//	}
+//	if (control != null) {
+//		control.postEvent (type, event);
+//	}
 }
 void sendMouseEvent (int type, int button, XInputEvent xEvent) {
 	Event event = new Event ();
