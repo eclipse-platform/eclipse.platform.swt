cc072dee2d2f5ba69bd99e0eaca43919cb605f60
Veronika Irvine <torres>
2001-09-26 15:15:31 +0000

    build 1.0 137

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/Callback.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/Callback.java
@@ -36,7 +36,7 @@ public class Callback {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	public static int MINOR_VERSION = 136;
+	public static int MINOR_VERSION = 137;
 	
 	/**
 	 * SWT revision number (must be >= 0)
--- bundles/org.eclipse.swt/Eclipse SWT/photon/library/makefile.mak
+++ bundles/org.eclipse.swt/Eclipse SWT/photon/library/makefile.mak
@@ -4,7 +4,7 @@
 # All Rights Reserved.
 
 maj_ver=1
-min_ver=136
+min_ver=137
 revision=0
 
 #assumes IVE_HOME is set in the environment
