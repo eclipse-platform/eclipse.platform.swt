2f020c0782dc1bb6c6cd07bcd17191986a8899b9
Carolyn MacLeod <carolyn>
2002-04-01 22:49:06 +0000

    Added new coolbar features

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/CoolBarTab.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/CoolBarTab.java
@@ -6,6 +6,8 @@ import org.eclipse.swt.layout.*;
 import org.eclipse.swt.events.*;
 
 class CoolBarTab extends Tab {
+	Button dropDownButton, lockedButton;
+
 	/* Example widgets and group that contains them */
 	CoolBar coolBar;
 	CoolItem pushItem, dropDownItem, radioItem, checkItem;
@@ -23,6 +25,24 @@ class CoolBarTab extends Tab {
 	}
 
 	/**
+	 * Creates the "Display" group.
+	 */
+	void createDisplayGroup () {
+		super.createDisplayGroup ();
+	
+		/* Create display controls specific to this example */
+		lockedButton = new Button (displayGroup, SWT.CHECK);
+		lockedButton.setText (ControlExample.getResourceString("Locked"));
+	
+		/* Add the listeners */
+		lockedButton.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				setWidgetLocked ();
+			}
+		});
+	}
+	
+	/**
 	 * Creates the "Example" group.
 	 */
 	void createExampleGroup () {
@@ -37,11 +57,12 @@ class CoolBarTab extends Tab {
 	 * Creates the "Example" widgets.
 	 */
 	void createExampleWidgets () {
-		int style = 0;
+		int style = 0, itemStyle = 0;
 
 		/* Compute the widget style */
 		int toolBarStyle = SWT.FLAT;
 		if (borderButton.getSelection()) style |= SWT.BORDER;
+		if (dropDownButton.getSelection()) itemStyle |= SWT.DROP_DOWN;
 	
 		/*
 		* Create the example widgets.
@@ -66,11 +87,13 @@ class CoolBarTab extends Tab {
 		item = new ToolItem (toolBar, SWT.PUSH);
 		item.setImage (instance.images[ControlExample.ciOpenFolder]);
 		item.setToolTipText ("SWT.PUSH");		
-		pushItem = new CoolItem (coolBar, SWT.NULL);
+		pushItem = new CoolItem (coolBar, itemStyle);
 		pushItem.setControl (toolBar);
 		Point pushSize = toolBar.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		pushSize = pushItem.computeSize(pushSize.x, pushSize.y);
 		pushItem.setSize(pushSize);
+		pushItem.setMinimumWidth(item.getWidth());
+        pushItem.addSelectionListener (new CoolItemDropDownSelectionListener());
 				
 		/* create the dropdown toolbar */
 		toolBar = new ToolBar (coolBar, toolBarStyle);
@@ -82,11 +105,13 @@ class CoolBarTab extends Tab {
 		item.setImage (instance.images[ControlExample.ciClosedFolder]);
 		item.setToolTipText ("SWT.DROP_DOWN");
 		item.addSelectionListener (new DropDownSelectionListener());
-		dropDownItem = new CoolItem (coolBar, SWT.NULL);
+		dropDownItem = new CoolItem (coolBar, itemStyle);
 		dropDownItem.setControl (toolBar);
 		Point dropSize = toolBar.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		dropSize = dropDownItem.computeSize(dropSize.x, dropSize.y);
 		dropDownItem.setSize(dropSize);
+		dropDownItem.setMinimumWidth(item.getWidth());
+        dropDownItem.addSelectionListener (new CoolItemDropDownSelectionListener());
 				
 		/* create the radio button toolbar */
 		toolBar = new ToolBar (coolBar, toolBarStyle);
@@ -99,11 +124,13 @@ class CoolBarTab extends Tab {
 		item = new ToolItem (toolBar, SWT.RADIO);
 		item.setImage (instance.images[ControlExample.ciClosedFolder]);
 		item.setToolTipText ("SWT.RADIO");
-		radioItem = new CoolItem (coolBar, SWT.NULL);
+		radioItem = new CoolItem (coolBar, itemStyle);
 		radioItem.setControl (toolBar);
 		Point radioSize = toolBar.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		radioSize = radioItem.computeSize(radioSize.x, radioSize.y);
 		radioItem.setSize(radioSize);
+		radioItem.setMinimumWidth(item.getWidth());
+        radioItem.addSelectionListener (new CoolItemDropDownSelectionListener());
 		
 		/* create the check button toolbar */
 		toolBar = new ToolBar (coolBar, toolBarStyle);
@@ -119,11 +146,13 @@ class CoolBarTab extends Tab {
 		item = new ToolItem (toolBar, SWT.CHECK);
 		item.setImage (instance.images[ControlExample.ciTarget]);
 		item.setToolTipText ("SWT.CHECK");
-		checkItem = new CoolItem (coolBar, SWT.NULL);
+		checkItem = new CoolItem (coolBar, itemStyle);
 		checkItem.setControl (toolBar);
 		Point checkSize = toolBar.computeSize(SWT.DEFAULT, SWT.DEFAULT);
 		checkSize = checkItem.computeSize(checkSize.x, checkSize.y);
 		checkItem.setSize(checkSize);
+		checkItem.setMinimumWidth(item.getWidth());
+        checkItem.addSelectionListener (new CoolItemDropDownSelectionListener());
 		
 		/* if we have saved state, restore it */
 		if (order != null) {
@@ -160,6 +189,17 @@ class CoolBarTab extends Tab {
 		/* Create the extra widget */
 		borderButton = new Button (styleGroup, SWT.CHECK);
 		borderButton.setText ("SWT.BORDER");
+		Group itemGroup = new Group(styleGroup, SWT.NONE);
+		itemGroup.setLayout (new GridLayout ());
+		itemGroup.setLayoutData (new GridData (GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_FILL));
+		itemGroup.setText(ControlExample.getResourceString("Item_Styles"));
+		dropDownButton = new Button (itemGroup, SWT.CHECK);
+		dropDownButton.setText ("SWT.DROP_DOWN");
+		dropDownButton.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				recreateExampleWidgets ();
+			}
+		});
 	}
 	
 	/**
@@ -195,6 +235,15 @@ class CoolBarTab extends Tab {
 	void setExampleWidgetState () {
 		super.setExampleWidgetState ();
 		borderButton.setSelection ((coolBar.getStyle () & SWT.BORDER) != 0);
+		dropDownButton.setSelection ((coolBar.getItem(0).getStyle () & SWT.DROP_DOWN) != 0);
+		setWidgetLocked ();
+	}
+	
+	/**
+	 * Sets the header visible state of the "Example" widgets.
+	 */
+	void setWidgetLocked () {
+		coolBar.setLocked (lockedButton.getSelection ());
 	}
 	
 	/**
@@ -202,7 +251,7 @@ class CoolBarTab extends Tab {
 	 * and opens/closes a menu when appropriate.
 	 */
 	class DropDownSelectionListener extends SelectionAdapter {
-		private Menu    menu = null;
+		private Menu menu = null;
 		private boolean visible = false;
 		
 		public void widgetSelected(SelectionEvent event) {
@@ -264,6 +313,104 @@ class CoolBarTab extends Tab {
 		private void setMenuVisible(boolean visible) {
 			menu.setVisible(visible);
 			this.visible = visible;
+		}
+	}
+
+	/**
+	 * Listens to widgetSelected() events on SWT.DROP_DOWN type CoolItems
+	 * and opens/closes a menu when appropriate.
+	 */
+	class CoolItemDropDownSelectionListener extends SelectionAdapter {
+		private Menu menu = null;
+		
+		public void widgetSelected(SelectionEvent event) {
+			/**
+			 * A selection event will be fired when the cool item
+			 * is selected by its gripper or if the drop down arrow
+			 * (or 'chevron') is selected. Examine the event detail
+			 * to determine where the widget was selected.
+			 */
+			if (event.detail == SWT.ARROW) {
+				/* If the popup menu is already up (i.e. user pressed arrow twice),
+				 * then dispose it.
+				 */
+				if (menu != null) {
+					menu.dispose();
+					menu = null;
+					return;
+				}
+				
+				/* Get the cool item and convert its bounds to display coordinates. */
+				CoolItem coolItem = (CoolItem) event.widget;
+				Rectangle itemBounds = coolItem.getBounds ();
+				Point pt = coolBar.toDisplay(new Point (itemBounds.x, itemBounds.y));
+				itemBounds.x = pt.x;
+				itemBounds.y = pt.y;
+				
+				/* Get the toolbar from the cool item. */
+				ToolBar toolBar = (ToolBar) coolItem.getControl ();
+				ToolItem[] tools = toolBar.getItems ();
+				int toolCount = tools.length;
+								
+				/* Convert the bounds of each tool item to display coordinates,
+				 * and determine which ones are past the bounds of the cool item.
+				 */
+				int i = 0;
+				while (i < toolCount) {
+					Rectangle toolBounds = tools[i].getBounds ();
+					pt = toolBar.toDisplay(new Point(toolBounds.x, toolBounds.y));
+					toolBounds.x = pt.x;
+					toolBounds.y = pt.y;
+			  		Rectangle intersection = itemBounds.intersection (toolBounds);
+			  		if (!intersection.equals (toolBounds)) break;
+			  		i++;
+				}
+				
+				/* Create a pop-up menu with items for each of the hidden buttons. */
+				menu = new Menu (coolBar);
+				for (int j = i; j < toolCount; j++) {
+					ToolItem tool = tools[j];
+					Image image = tool.getImage();
+					if (image == null) {
+						new MenuItem (menu, SWT.SEPARATOR);
+					} else {
+						if ((tool.getStyle() & SWT.DROP_DOWN) != 0) {
+							MenuItem menuItem = new MenuItem (menu, SWT.CASCADE);
+							menuItem.setImage(image);
+							Menu m = new Menu(menu);
+							menuItem.setMenu(m);
+							for (int k = 0; k < 9; ++k) {
+								String text = ControlExample.getResourceString("DropDownData_" + k);
+								if (text.length() != 0) {
+									MenuItem mi = new MenuItem(m, SWT.NONE);
+									mi.setText(text);
+								} else {
+									new MenuItem(m, SWT.SEPARATOR);
+								}
+							}
+						} else {
+							MenuItem menuItem = new MenuItem (menu, SWT.NONE);
+							menuItem.setImage(image);
+						}
+						/* Application code to perform the action for the menu item would go here. */
+
+					}
+				}
+				
+				/* Display the pop-up menu at the lower left corner of the arrow button.
+				 * Dispose the menu when the user is done with it.
+				 */
+				menu.setLocation (event.x, event.y);
+				menu.setVisible (true);
+				Display display = coolBar.getDisplay ();
+				while (menu.isVisible ()) {
+					if (!display.readAndDispatch ()) display.sleep ();
+				}
+				if (menu != null) {
+					menu.dispose ();
+					menu = null;
+				}
+			}
 		}
 	}
 }
