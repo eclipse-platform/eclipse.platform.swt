59d68781d2f381676820fceedcb569528c53c05a
Mike Wilson <mcq>
2002-04-05 19:28:21 +0000

    updated class javadoc

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Combo.java
@@ -39,7 +39,7 @@ import org.eclipse.swt.events.*;
  * <dd>DefaultSelection, Modify, Selection</dd>
  * </dl>
  * <p>
- * Note: Only one of the styles DROP_DOWN, READ_ONLY, and SIMPLE 
+ * Note: Only one of the styles DROP_DOWN and SIMPLE 
  * may be specified.
  * </p><p>
  * IMPORTANT: This class is <em>not</em> intended to be subclassed.
