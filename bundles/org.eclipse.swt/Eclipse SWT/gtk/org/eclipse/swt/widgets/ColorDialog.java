1617f86437dc438e368de40428b11023d2e7a7bd
Boris Shingarov <bshingar>
2002-04-24 21:20:12 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ColorDialog.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ColorDialog.java
@@ -78,14 +78,7 @@ public ColorDialog (Shell parent, int style) {
 	super (parent, style);
 	checkSubclass ();
 }
-int cancelFunc (int widget, int callData) {
-	OS.gtk_widget_destroy (callData);
-	return 0;
-}
-int destroyFunc (int widget, int colorInfo) {
-	OS.gtk_main_quit ();
-	return 0;
-}
+
 /**
  * Returns the currently selected color in the receiver.
  *
@@ -95,13 +88,6 @@ int destroyFunc (int widget, int colorInfo) {
  */
 public RGB getRGB () {
 	return rgb;
-}
-int okFunc (int widget, int callData) {
-	double [] color = new double [4];
-	OS.gtk_color_selection_get_color (OS.GTK_COLOR_SELECTION_COLORSEL(callData), color);
-	rgb = new RGB ((int)(color [0] * 256), (int)(color [1] * 256), (int)(color [2] * 256));
-	OS.gtk_widget_destroy (callData);
-	return 0;
 }
 /**
  * Makes the receiver visible and brings it to the front
@@ -120,34 +106,23 @@ public RGB open () {
 	byte [] buffer = Converter.wcsToMbcs (null, title, true);
 	int handle = OS.gtk_color_selection_dialog_new (buffer);
 	if (parent!=null) {
-		OS.gtk_window_set_modal(handle, true);
 		OS.gtk_window_set_transient_for(handle, parent.topHandle());
 	}
-	OS.gtk_widget_hide (OS.GTK_COLOR_SELECTION_OK_BUTTON(handle));
 	if (rgb != null) {
 		double [] color = new double [4];
 		color [0] = (double)rgb.red / 256;
 		color [1] = (double)rgb.green / 256;
 		color [2] = (double)rgb.blue / 256;
-		OS.gtk_color_selection_set_color (OS.GTK_COLOR_SELECTION_COLORSEL(handle), color);
+		OS.gtk_color_selection_set_color (OS.GTK_COLOR_SELECTION_DIALOG_COLORSEL(handle), color);
 	}
-	Callback destroyCallback = new Callback (this, "destroyFunc", 2);
-	int destroyFunc = destroyCallback.getAddress ();
-	byte [] destroy = Converter.wcsToMbcs (null, "destroy", true);
-	OS.gtk_signal_connect (handle, destroy, destroyFunc, handle);
-	byte [] clicked = Converter.wcsToMbcs (null, "clicked", true);
-	Callback okCallback = new Callback (this, "okFunc", 2);
-	int okFunc = okCallback.getAddress ();
-	Callback cancelCallback = new Callback (this, "cancelFunc", 2);
-	int cancelFunc = cancelCallback.getAddress ();
-	OS.gtk_signal_connect (OS.GTK_COLOR_SELECTION_OK_BUTTON(handle), clicked, okFunc, handle);
-	OS.gtk_signal_connect (OS.GTK_COLOR_SELECTION_CANCEL_BUTTON(handle), clicked, cancelFunc, handle);
 	rgb = null;
-	OS.gtk_widget_show_now (handle);
-	OS.gtk_main ();
-	destroyCallback.dispose ();
-	okCallback.dispose ();
-	cancelCallback.dispose ();
+	int response = OS.gtk_dialog_run(handle);
+	if (response == OS.GTK_RESPONSE_OK) {
+		double [] color = new double [4];
+		OS.gtk_color_selection_get_color (OS.GTK_COLOR_SELECTION_DIALOG_COLORSEL(handle), color);
+		rgb = new RGB ((int)(color [0] * 256), (int)(color [1] * 256), (int)(color [2] * 256));
+	}
+	OS.gtk_widget_destroy(handle);
 	return rgb;
 }
 /**
