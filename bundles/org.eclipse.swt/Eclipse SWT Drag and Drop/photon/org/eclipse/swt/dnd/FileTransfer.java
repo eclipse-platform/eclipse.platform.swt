2c7d380be2e321a7928b1ce1264ec8b852b6e69d
Silenio Quarti <silenio>
2002-05-16 23:27:07 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/photon/org/eclipse/swt/dnd/FileTransfer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/photon/org/eclipse/swt/dnd/FileTransfer.java
@@ -4,7 +4,10 @@ package org.eclipse.swt.dnd;
  * (c) Copyright IBM Corp. 2000, 2001.
  * All Rights Reserved
  */
- 
+  
+import org.eclipse.swt.internal.Converter;
+import org.eclipse.swt.internal.photon.OS;
+
 /**
  * The <code>FileTransfer</code> class is used to transfer files in a drag and drop operation.
  */
@@ -43,7 +46,17 @@ public static FileTransfer getInstance () {
  *        with the platform specific format of the data
  */
 public void javaToNative(Object object, TransferData transferData) {
-	DND.error(org.eclipse.swt.SWT.ERROR_NOT_IMPLEMENTED);
+	if (object == null || !(object instanceof String[])) return;
+	byte [] data = new byte[0];
+	String[] filenames = (String[])object;
+	for (int i = 0; i < filenames.length; i++) {
+		byte [] buffer = Converter.wcsToMbcs (null, filenames[i], true);		
+		byte [] temp = data;
+		data = new byte[ data.length + buffer.length];
+		System.arraycopy(temp, 0, data, 0, temp.length);
+		System.arraycopy(buffer, 0, data, data.length - buffer.length, buffer.length);
+	}
+	super.javaToNative(data, transferData);
 }
 /**
  * Converts a platform specific representation of a list of file names to a Java array of String.
@@ -53,8 +66,24 @@ public void javaToNative(Object object, TransferData transferData) {
  *         otherwise null
  */
 public Object nativeToJava(TransferData transferData) {
-	DND.error(org.eclipse.swt.SWT.ERROR_NOT_IMPLEMENTED);
-	return null;
+	if (transferData.pData == 0 || !(isSupportedType(transferData))) return null;
+	int size = transferData.length;
+	if (size == 0) return null;
+	byte[] buffer = new byte[size];
+	OS.memmove(buffer, transferData.pData, size);
+	String[] filenames = new String[0];
+	int lastMark = 0;
+	for (int i = 0; i < buffer.length; i++) {
+		if ( buffer[i] == 0 ) {
+			String s = new String(buffer, lastMark, i - lastMark );
+			String[] temp = filenames;
+			filenames = new String[ filenames.length + 1];
+			System.arraycopy(temp, 0, filenames, 0 , temp.length);		
+			filenames[ filenames.length - 1] = s;
+			lastMark = i + 1;
+		}
+	}
+	return filenames;
 }
 protected String[] getTypeNames(){
 	return new String[]{TYPENAME};
