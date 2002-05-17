c094648ffc8a72b873846910c5c303ece3eabf93
Grant Gayed <ggayed>
2002-05-17 16:05:45 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -17,7 +17,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 39;
+	static int MINOR_VERSION = 40;
 	
 	/**
 	 * SWT revision number (must be >= 0)
--- bundles/org.eclipse.swt/Eclipse SWT/common/library/make_common.mak
+++ bundles/org.eclipse.swt/Eclipse SWT/common/library/make_common.mak
@@ -1,4 +1,4 @@
 maj_ver=2
-min_ver=039
+min_ver=040
 bld_num=0
-comma_ver=2,0,3,9
+comma_ver=2,0,4,0
