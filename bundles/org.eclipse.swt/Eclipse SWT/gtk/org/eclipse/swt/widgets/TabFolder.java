3e0fbc94829ccd56d23e0eb7edfd051721be3e19
Silenio Quarti <silenio>
2002-05-27 21:42:53 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TabFolder.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TabFolder.java
@@ -139,14 +139,14 @@ public Point computeSize (int wHint, int hHint, boolean changed) {
 	} else {
 		size = minimumSize ();
 	}
+	Rectangle trim = computeTrim (0, 0, size.x, size.y);
+	size.x = trim.width;  size.y = trim.height;
 	if (size.x == 0) size.x = DEFAULT_WIDTH;
 	if (size.y == 0) size.y = DEFAULT_HEIGHT;
 	if (wHint != SWT.DEFAULT) size.x = wHint;
 	if (hHint != SWT.DEFAULT) size.y = hHint;
 	width = Math.max (width, size.x);
 	height = Math.max (height, size.y);
-	Rectangle trim = computeTrim (0, 0, width, height);
-	width = trim.width;  height = trim.height;
 	return new Point (width, height);
 }
 
