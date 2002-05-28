643c8a72b95aea7aef5cc3f99f76de67e13053f2
Veronika Irvine <veronika>
2002-05-28 14:24:24 +0000

    Clipboard should be available as long as the disply is available.

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/Clipboard.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/Clipboard.java
@@ -5,12 +5,11 @@ package org.eclipse.swt.dnd;
  * All Rights Reserved
  */
 
-import org.eclipse.swt.*;
-import org.eclipse.swt.internal.*;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.internal.Converter;
 import org.eclipse.swt.internal.gtk.GtkSelectionData;
-import org.eclipse.swt.internal.gtk.GtkTargetEntry;
 import org.eclipse.swt.internal.gtk.OS;
-import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.widgets.Display;
 
 /**
  * IMPORTANT: This class is <em>not</em> intended to be subclassed.
@@ -18,17 +17,8 @@ import org.eclipse.swt.widgets.*;
 public class Clipboard {
 	
 	Display display;
-	Callback getFunc;
-	Callback clearFunc;
 	int pGtkClipboard;
-	boolean onClipboard = false;
 	int pGtkPrimary;
-	boolean onPrimary = false;
-	
-	/* Data is not flushed to the clipboard immediately.
-	 * This class will remember the data and provide it when requested. */
-	Object[] data;
-	Transfer[] dataTypes;
 
 public Clipboard(Display display) {	
 	checkSubclass ();
@@ -42,26 +32,10 @@ public Clipboard(Display display) {
 		SWT.error(SWT.ERROR_THREAD_INVALID_ACCESS);
 	}
 	this.display = display;
-	getFunc = new Callback( this, "getFunc", 4);
-	clearFunc = new Callback( this, "clearFunc", 2);
 	pGtkClipboard = OS.gtk_clipboard_get(OS.GDK_NONE);
 	byte[] buffer = Converter.wcsToMbcs(null, "PRIMARY", true);
 	int primary = OS.gdk_atom_intern(buffer, false);
 	pGtkPrimary = OS.gtk_clipboard_get(primary);
-}
-
-int clearFunc(int clipboard,int user_data_or_owner){
-	if (clipboard == pGtkClipboard) {
-		onClipboard = false;
-	}
-	if (clipboard == pGtkPrimary) {
-		onPrimary = false;
-	}
-	if (!onClipboard && !onPrimary) {	
-		data = null;
-		dataTypes = null;
-	}
-	return 1;
 }
 
 protected void checkSubclass () {
@@ -74,21 +48,9 @@ protected void checkSubclass () {
 
 public void dispose () {
 	if (pGtkClipboard == 0) return;
-	if (onPrimary) OS.gtk_clipboard_clear(pGtkPrimary);
-	if (onClipboard) OS.gtk_clipboard_clear(pGtkClipboard);
-	// Uncommenting the following line causes a segfault on shutdown
-//	OS.g_free(pGtkClipboard);
 	pGtkClipboard = 0;
-	// Uncommenting the following line causes a segfault on shutdown
-//	OS.g_free(pGtkPrimary);
 	pGtkPrimary = 0;
 	display = null;
-	if (getFunc != null ) getFunc.dispose();
-	getFunc = null;
-	if (clearFunc != null) clearFunc.dispose();
-	clearFunc = null;
-	data = null;
-	dataTypes = null;
 }
 
 public Object getContents(Transfer transfer) {
@@ -123,82 +85,16 @@ public Object getContents(Transfer transfer) {
 	return result;
 }
 
-/**
- * This function provides the data to the clipboard on request.
- * When this clipboard is disposed, the data will no longer be available.
- */
-int getFunc( int clipboard, int selection_data, int info, int user_data_or_owner){
-	if (selection_data == 0) return 0;
-	GtkSelectionData selectionData = new GtkSelectionData();
-	OS.memmove(selectionData, selection_data, GtkSelectionData.sizeof);
-	TransferData tdata = new TransferData();
-	tdata.type = selectionData.target;
-	int index = -1;
-	for (int i = 0; i < dataTypes.length; i++) {
-		if (dataTypes[i].isSupportedType(tdata)) {
-			index = i;
-			break;
-		}
-	}
-	if (index == -1) return 0;
-	dataTypes[index].javaToNative(data[index], tdata);
-	OS.gtk_selection_data_set(selection_data, tdata.type, tdata.format, tdata.pValue, tdata.length);	
-	return 1;
-}
-
 public void setContents(Object[] data, Transfer[] dataTypes){
 	if (data == null || dataTypes == null || data.length != dataTypes.length) {
 		DND.error(SWT.ERROR_INVALID_ARGUMENT);
 	}
 	if (display.isDisposed() ) DND.error(DND.ERROR_CANNOT_SET_CLIPBOARD);
-	if (dataTypes.length == 0) return;	
-	
-	if (onClipboard) {
-		OS.gtk_clipboard_clear(pGtkClipboard);
+	if (dataTypes.length == 0) return;
+	ClipboardProxy proxy = ClipboardProxy._getInstance(display);
+	if (!proxy.setData(data, dataTypes)) {
+		DND.error(DND.ERROR_CANNOT_SET_CLIPBOARD);
 	}
-	if (onPrimary) {
-		OS.gtk_clipboard_clear(pGtkPrimary);
-	}
-		
-	GtkTargetEntry[] entries = new  GtkTargetEntry [0];
-	for (int i = 0; i < dataTypes.length; i++) {
-		Transfer transfer = dataTypes[i];
-		int[] typeIds = transfer.getTypeIds();
-		String[] typeNames = transfer.getTypeNames();
-		for (int j = 0; j < typeIds.length; j++) {
-			GtkTargetEntry	entry = new GtkTargetEntry();						
-			entry.info = typeIds[j];
-			byte[] buffer = Converter.wcsToMbcs(null, typeNames[j], true);
-			int pName = OS.g_malloc(buffer.length);
-			OS.memmove(pName, buffer, buffer.length);
-			entry.target = pName;
-			GtkTargetEntry[] tmp = new GtkTargetEntry [entries.length + 1];
-			System.arraycopy(entries, 0, tmp, 0, entries.length);
-			tmp[entries.length] = entry;
-			entries = tmp;				
-		}	
-	}
-	
-	int pTargetsList = OS.g_malloc(GtkTargetEntry.sizeof * entries.length);
-	int offset = 0;
-	for (int i = 0; i < entries.length; i++) {
-		OS.memmove(pTargetsList + offset, entries[i], GtkTargetEntry.sizeof);
-		offset += GtkTargetEntry.sizeof;
-	}
-
-	this.data = data;
-	this.dataTypes = dataTypes;
-
-	onPrimary = OS.gtk_clipboard_set_with_data(pGtkPrimary, pTargetsList, entries.length, getFunc.getAddress(), clearFunc.getAddress(), 0);
-	onClipboard = OS.gtk_clipboard_set_with_data(pGtkClipboard, pTargetsList, entries.length, getFunc.getAddress(), clearFunc.getAddress(), 0);
-	
-	for (int i = 0; i < entries.length; i++) {
-		GtkTargetEntry entry = entries[i];
-		if( entry.target != 0) OS.g_free(entry.target);
-	}
-	if (pTargetsList != 0) OS.g_free(pTargetsList);
-	
-	if (!onClipboard && !onPrimary) DND.error(DND.ERROR_CANNOT_SET_CLIPBOARD);
 }
 /*
  * Note: getAvailableTypeNames is a tool for writing a Transfer sub-class only.  It should
