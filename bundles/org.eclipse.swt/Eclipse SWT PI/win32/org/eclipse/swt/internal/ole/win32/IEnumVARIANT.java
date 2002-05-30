f2ea892d12a43d80fb5336099fb262b68a70ecc6
Carolyn MacLeod <carolyn>
2002-05-30 20:58:55 +0000

    Completion of fix for 15575.

---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/ole/win32/IEnumVARIANT.java
@@ -0,0 +1,11 @@
+package org.eclipse.swt.internal.ole.win32;
+
+/*
+ * (c) Copyright IBM Corp. 2000, 2001.
+ * All Rights Reserved
+ */
+public class IEnumVARIANT extends IEnum {
+public IEnumVARIANT(int address) {
+	super(address);
+}
+}
