0aedbb4556525ba14c27249953c01a4a12eb8f42
Veronika Irvine <veronika>
2002-06-07 19:19:44 +0000

    fix for 17494

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Clipboard.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Clipboard.java
@@ -11,7 +11,10 @@ import org.eclipse.swt.internal.ole.win32.*;
 import org.eclipse.swt.widgets.*;
 
 /**
- * IMPORTANT: This class is <em>not</em> intended to be subclassed.
+ * The <code>Clipboard</code> provides a mechanism for copying data from one
+ * application to another or within an application.
+ * 
+ * <p>IMPORTANT: This class is <em>not</em> intended to be subclassed.</p>
  */
 public class Clipboard {
 
@@ -45,9 +48,36 @@ protected void checkSubclass () {
 		DND.error (SWT.ERROR_INVALID_SUBCLASS);
 	}
 }
+
+/**
+ * Disposes of the operating system resources associated with the clipboard. 
+ * The data will still be available on the system clipboard after the dispose 
+ * method is called.  
+ * 
+ * <p>NOTE: On some platforms the data will not be available once the application
+ * has exited or the display has been disposed.</p>
+ */
 public void dispose () {
 	this.Release();
 }
+
+/**
+ *  Get the data from the clipboard.
+ * 
+ *  <code><pre>
+ * 	Clipboard clipboard = new Clipboard(display);
+ *		TextTransfer textTransfer = TextTransfer.getInstance();
+ *		String textData = (String)clipboard.getContents(textTransfer);
+ * 	if (textData != null) System.out.println("Text is "+textData);
+ *		RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+ *  	String rtfData = (String)clipboard.getContents(rtfTransfer);
+ * 	if (rtfData != null) System.out.println("RTF Text is "+rtfData);
+ *		clipboard.dispose();
+ * </code></pre>
+ * 
+ * @param transfer the transfer agent for the type of data being requested
+ * @return the data obtained from the clipboard or null if no data of this type is available
+ */
 public Object getContents(Transfer transfer) {
 	int[] ppv = new int[1];
 	int retries = 0;
@@ -75,14 +105,28 @@ public Object getContents(Transfer transfer) {
 	return transfer.nativeToJava(match);
 }
 /**
- * Set the data onto the clipboard.  NOTE: On some platforms, the data is
- * immediately flushed to the clipboard but on some platforms it is provided
- * upon request.  As a result, if the application modifes the data Object it has set 
- * on the clipboard, that modification may or may not be available when the 
- * object is subsequently pasted.
+ * Set the data onto the clipboard.  
  * 
+ * <p>NOTE: On some platforms, the data is immediately copied to the system
+ * clipboard but on other platforms it is provided upon request.  As a result, if the 
+ * application modifes the data object it has set on the clipboard, that modification 
+ * may or may not be available when the data is subsequently requested.</p>
+ *
+ * <p>The following snippet shows text and RTF text being set on the clipboard:</p>
+ * 
+ * <code><pre>
+ * 	Clipboard clipboard = new Clipboard(display);
+ *		String textData = "Hello World";
+ *		String rtfData = "{\\rtf1\\b\\i Hello World}";
+ *		TextTransfer textTransfer = TextTransfer.getInstance();
+ *		RTFTransfer rtfTransfer = RTFTransfer.getInstance();
+ *		clipboard.setContents(new Object[]{textData, rtfData}, new Transfer[]{textTransfer, rtfTransfer});
+ *		clipboard.dispose();
+ * </code></pre>
+ *
  * @param data the data to be set in the clipboard
- * @param dataTypes the corresponding transfer agent for data
+ * @param dataTypes the transfer agents that will convert the data to its platform 
+ * specific format; each entry in the data array must have a corresponding dataType
  * 
  * @exception IllegalArgumentException <ul>
  *    <li>ERROR_NULL_ARGUMENT - if data is null or datatypes is null 
@@ -250,10 +294,16 @@ private int Release() {
 	return refCount;
 }
 
-/*
- * Note: getAvailableTypeNames is a tool for writing a Transfer sub-class only.  It should
- * NOT be used within an application because it provides platform specfic 
- * information.
+/**
+ * Returns a platform specific list of the data types currently available on the 
+ * system clipboard.
+ * 
+ * <p>Note: <code>getAvailableTypeNames</code> is a tool for writing a Transfer 
+ * sub-class only.  It should NOT be used within an application because it provides 
+ * platform specific information.</p>
+ * 
+ * @returns a platform specific list of the data types currently available on the 
+ * system clipboard
  */
 public String[] getAvailableTypeNames() {	
 	int[] ppv = new int[1];
