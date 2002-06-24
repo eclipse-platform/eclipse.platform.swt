1028a3d4bbded681d3c8beccf77e696ee68e5325
Andre Weinand <aweinand>
2002-06-24 09:08:45 +0000

    removed unecessary files and updated copyright notice

--- bundles/org.eclipse.swt/Eclipse SWT PI/carbon/library/launch_swt.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/library/launch_swt.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#********************************************************************** 
+# Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+# This file is made available under the terms of the Common Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/cpl-v10.html
+# 
+# Andre Weinand, OTI - Initial version
+#********************************************************************** 
+
 # 
 # This script wrappers a SWT based Java application on the fly as an
 # application bundle and launches it.
