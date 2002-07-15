70ff5457307f157aef0fd877ea31da01003dfc42
Veronika Irvine <veronika>
2002-07-15 17:54:22 +0000

    fix set/getChecked javadoc

--- bundles/org.eclipse.swt/Eclipse SWT/emulated/treetable/org/eclipse/swt/widgets/TableItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/emulated/treetable/org/eclipse/swt/widgets/TableItem.java
@@ -332,7 +332,7 @@ public Rectangle getBounds(int index) {
  * and false otherwise.  When the parent does not have
  * the <code>CHECK style, return false.
  *
- * @return the checked state
+ * @return the checked state of the checkbox
  *
  * @exception SWTException <ul>
  *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
@@ -418,9 +418,9 @@ public Color getForeground(){
 /**
  * Returns <code>true</code> if the receiver is grayed,
  * and false otherwise. When the parent does not have
- * the <code>CHECK style, return false.
+ * the <code>CHECK</code> style, return false.
  *
- * @return the grayed state
+ * @return the grayed state of the checkbox
  *
  * @exception SWTException <ul>
  *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
@@ -1214,9 +1214,10 @@ void setTrimmedText(String label, int columnIndex) {
 	}
 }
 /**
- * Sets the checked state of the receiver.
+ * Sets the checked state of the checkbox for this item.  This state change 
+ * only applies if the Table was created with the SWT.CHECK style.
  *
- * @param checked the new checked state
+ * @param checked the new checked state of the checkbox
  *
  * @exception SWTException <ul>
  *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
@@ -1228,9 +1229,10 @@ public void setChecked(boolean checked) {
 	super.setChecked(checked);
 }
 /**
- * Sets the grayed state of the receiver.
+ * Sets the grayed state of the checkbox for this item.  This state change 
+ * only applies if the Table was created with the SWT.CHECK style.
  *
- * @param checked the new grayed state
+ * @param grayed the new grayed state of the checkbox
  *
  * @exception SWTException <ul>
  *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
