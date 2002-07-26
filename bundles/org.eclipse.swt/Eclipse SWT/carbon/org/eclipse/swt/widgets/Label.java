d6b025633a28920ae40301a1b987f0b923d26e74
Andre Weinand <aweinand>
2002-07-26 15:29:10 +0000

    adapted drawing to new GC.focus stuff

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Label.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Label.java
@@ -247,45 +247,50 @@ boolean mnemonicMatch (char key) {
 int processPaint (Object callData) {
 	if ((style & SWT.SEPARATOR) != 0) return 0;
 	
-	int hndl= topHandle();
-	Display display= getDisplay();
-	
-	MacRect bounds= new MacRect();
-	OS.GetControlBounds(hndl, bounds.getData());
-
-	int w= bounds.getWidth();
-	int h= bounds.getHeight();
-	int borderWidth = (style & SWT.BORDER) != 0 ? 1 : 0;
-
 	GC gc= new GC(this);
-	gc.carbon_focus();
+	MacControlEvent me= (MacControlEvent) callData;
+	Rectangle r= gc.carbon_focus(me.getDamageRegionHandle());
 	
-	gc.fillRectangle(0, 0, w, h);
+	if (! r.isEmpty()) {
+		
+		MacRect bounds= new MacRect();
+		int hndl= topHandle();
+		OS.GetControlBounds(hndl, bounds.getData());
 	
-	if (image != null) {
-		Rectangle imageBounds= image.getBounds();
-		gc.drawImage(image, (w-imageBounds.width) / 2, (h-imageBounds.height) / 2);
-	} else {
-		int sHandle= OS.CFStringCreateWithCharacters(MacUtil.removeMnemonics(text));
-		boolean wrap= (style & SWT.WRAP) != 0;
-		short just= 0;
-		if ((style & SWT.RIGHT) != 0)
-			just= 2;
-		else if ((style & SWT.CENTER) != 0)
-			just= 1;
-		if (OS.IsControlEnabled(handle))
-			OS.RGBForeColor(0x000000);
-		else
-			OS.RGBForeColor(0x808080);
-		gc.installFont();
-		bounds.set(bounds.getX()+borderWidth, bounds.getY()+borderWidth, w-2*borderWidth, h-2*borderWidth);
-		OS.DrawThemeTextBox(sHandle, OS.kThemeCurrentPortFont, OS.kThemeStateActive, wrap, bounds.getData(), just, 0);
-		OS.CFRelease(sHandle);
-	}
-	
-	if (borderWidth > 0) {
-		gc.setForeground(display.getSystemColor(SWT.COLOR_GRAY));
-		gc.drawRectangle(0, 0, w-1, h-1);
+		int w= bounds.getWidth();
+		int h= bounds.getHeight();
+		int borderWidth = (style & SWT.BORDER) != 0 ? 1 : 0;
+		
+		gc.fillRectangle(0, 0, r.width, r.height);
+		
+		if (image != null) {
+			Rectangle imageBounds= image.getBounds();
+			gc.drawImage(image, (w-imageBounds.width) / 2, (h-imageBounds.height) / 2);
+		} else {
+			int sHandle= OS.CFStringCreateWithCharacters(MacUtil.removeMnemonics(text));
+			boolean wrap= (style & SWT.WRAP) != 0;
+			short just= 0;
+			if ((style & SWT.RIGHT) != 0)
+				just= 2;
+			else if ((style & SWT.CENTER) != 0)
+				just= 1;
+			if (OS.IsControlEnabled(handle))
+				OS.RGBForeColor(0x000000);
+			else
+				OS.RGBForeColor(0x808080);
+			gc.installFont();
+			bounds.set(borderWidth, borderWidth, w-2*borderWidth, h-2*borderWidth);
+			OS.DrawThemeTextBox(sHandle, OS.kThemeCurrentPortFont, OS.kThemeStateActive, wrap, bounds.getData(), just, 0);
+			OS.CFRelease(sHandle);
+		}
+		
+		if (borderWidth > 0) {
+			gc.setForeground(getDisplay().getSystemColor(SWT.COLOR_GRAY));
+			gc.drawRectangle(0, 0, w-1, h-1);
+		}
+		
+		// AW: debugging
+		//gc.drawRectangle(0, 0, r.width-1, r.height-1);
 	}
 	
 	gc.carbon_unfocus();
