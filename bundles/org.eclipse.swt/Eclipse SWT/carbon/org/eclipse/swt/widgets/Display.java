28df2931b46ceabb00c00de6880dd2e2dbcd7b35
Andre Weinand <aweinand>
2002-08-07 16:56:36 +0000

    added comment

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
@@ -707,7 +707,7 @@ public Control getFocusControl () {
  * </ul>
  */
 public int getIconDepth () {
-	return 8;
+	return 8;	// we don't support direct icons yet
 }
 /**
  * Returns an array containing all shells which have not been
