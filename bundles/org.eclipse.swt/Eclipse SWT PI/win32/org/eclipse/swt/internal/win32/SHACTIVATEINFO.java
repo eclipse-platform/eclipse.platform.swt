036d7f05f41364bcc1a654d1ac3e8a6d76af565d
Christophe Cornu <ccornu>
2002-09-17 16:10:01 +0000

    struct uses bit fields

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/SHACTIVATEINFO.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/SHACTIVATEINFO.java
@@ -9,9 +9,9 @@ package org.eclipse.swt.internal.win32;
 public class SHACTIVATEINFO {
 	public int cbSize;
 	public int hwndLastFocus;
-	public int fSipUp;
-	public int fSipOnDeactivation;
-	public int fActive;
-	public int fReserved;
+	public int fSipUp; // :1
+	public int fSipOnDeactivation; // :1
+	public int fActive; // :1
+	public int fReserved; // :29
 	public static final int sizeof = 12;
 }
