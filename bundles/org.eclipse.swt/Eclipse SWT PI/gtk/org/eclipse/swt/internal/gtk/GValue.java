2fee5243669df95de1d1bab7415daa0719146cd7
Boris Shingarov <bshingar>
2002-09-20 20:38:58 +0000

    Initial

---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/GValue.java
@@ -0,0 +1,5 @@
+package org.eclipse.swt.internal.gtk;
+
+public class GValue {
+	public static final int sizeof = OS.GValue_sizeof();
+}
---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/GtkTreeIter.java
@@ -0,0 +1,5 @@
+package org.eclipse.swt.internal.gtk;
+
+public class GtkTreeIter {
+	public static final int sizeof = OS.GtkTreeIter_sizeof();
+}
