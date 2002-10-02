8a95d19703afb57d6decbe7efefbba4ee57ed253
Boris Shingarov <bshingar>
2002-10-02 22:12:06 +0000

    Possible segfault in getItem(Point)

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -556,6 +556,7 @@ public TableItem getItem (Point pt) {
 	int[] column = new int[1];
 	int clientY = pt.y - getHeaderHeight();
 	if (!OS.gtk_tree_view_get_path_at_pos(handle, pt.x, clientY, path, column, null, null)) return null;
+	if (path[0]==0) return null;
 	int indexPtr = OS.gtk_tree_path_get_indices(path[0]);
 	if (indexPtr==0) return null;
 	int[] indices = new int[1];
