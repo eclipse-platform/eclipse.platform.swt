061c6830201262a3bc8f3b5f7912c468b2d23091
Boris Shingarov <bshingar>
2002-10-04 18:28:31 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
@@ -164,8 +164,6 @@ public Rectangle getBounds (int index) {
 public boolean getChecked () {
 	checkWidget();
 	if ((parent.style & SWT.CHECK) == 0) return false;
-	int row = parent.indexOf (this);
-	if (row == -1) return false;
 	int[] ptr = new int[1];
 	OS.gtk_tree_model_get(parent.modelHandle, handle, 2*parent.MAX_COLUMNS, ptr, -1);
 	return ptr[0] != 0;
