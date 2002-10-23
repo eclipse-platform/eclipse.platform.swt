22a645e239757397b89adaaef9c4ca51e1b63d74
Silenio Quarti <silenio>
2002-10-23 15:41:33 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -638,7 +638,7 @@ public void removeAll () {
 	checkWidget ();
 	OS.gtk_tree_store_clear (modelHandle);
 	for (int i=0; i<items.length; i++) {
-		TreeItem item = items[i];
+		TreeItem item = items [i];
 		if (item != null && !item.isDisposed ()) item.releaseResources ();
 	}
 	items = new TreeItem[4];
