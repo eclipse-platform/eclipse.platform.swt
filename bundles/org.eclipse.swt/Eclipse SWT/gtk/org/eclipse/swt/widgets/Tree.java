b1fa1fe3a9c194c31c22688c6c35b4fdd2bd06e9
Veronika Irvine <veronika>
2002-10-28 21:56:48 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -386,7 +386,7 @@ public int getItemHeight () {
 	GdkRectangle rect = new GdkRectangle ();
 	int path = OS.gtk_tree_path_new_from_string (Converter.wcsToMbcs (null, "0", true));
 	int column = OS.gtk_tree_view_get_column (handle, 0);
-	OS.gtk_tree_view_get_cell_area (parent.handle, path, column, rect);
+	OS.gtk_tree_view_get_cell_area (handle, path, column, rect);
 	OS.gtk_tree_path_free (path);
 	return rect.height;
 }
