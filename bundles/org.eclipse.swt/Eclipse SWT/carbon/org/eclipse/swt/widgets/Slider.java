0f8f4db6e904a3b5b78a56b282c81fb4ce1a8550
Steve Northover <steve>
2002-11-04 22:07:38 +0000

    *** empty log message ***

---
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Slider.java
@@ -0,0 +1,215 @@
+package org.eclipse.swt.widgets;
+
+/*
+ * Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+ * This file is made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ */
+ 
+import org.eclipse.swt.internal.carbon.OS;
+import org.eclipse.swt.internal.carbon.Rect;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+
+public class Slider extends Control {
+	boolean dragging;
+	int increment = 1;
+	int pageIncrement = 10;
+
+
+public Slider (Composite parent, int style) {
+	super (parent, checkStyle (style));
+}
+
+public void addSelectionListener(SelectionListener listener) {
+	checkWidget();
+	if (listener == null) error (SWT.ERROR_NULL_ARGUMENT);
+	TypedListener typedListener = new TypedListener(listener);
+	addListener(SWT.Selection,typedListener);
+	addListener(SWT.DefaultSelection,typedListener);
+}
+
+static int checkStyle (int style) {
+	return checkBits (style, SWT.HORIZONTAL, SWT.VERTICAL, 0, 0, 0, 0);
+}
+
+int actionProc (int theControl, int partCode) {
+	Event event = new Event ();
+	int value = OS.GetControl32BitValue (handle);
+    switch (partCode) {
+	    case OS.kControlUpButtonPart:
+			value -= increment;
+	        event.detail = SWT.ARROW_UP;
+	        break;
+	    case OS.kControlPageUpPart:
+			value -= pageIncrement;
+	        event.detail = SWT.PAGE_UP;
+	        break;
+	    case OS.kControlPageDownPart:
+			value += pageIncrement;
+	        event.detail = SWT.PAGE_DOWN;
+	        break;
+	    case OS.kControlDownButtonPart:
+			value += increment;
+	        event.detail = SWT.ARROW_DOWN;
+	        break;
+	    case OS.kControlIndicatorPart:
+	    	dragging = true;
+			event.detail = SWT.DRAG;
+	        break;
+		default:
+			return 0;
+	}
+	OS.SetControl32BitValue (handle, value);
+	sendEvent (SWT.Selection, event);
+	return 0;
+}
+
+public Point computeSize (int wHint, int hHint, boolean changed) {
+	checkWidget();
+	Rect rect = new Rect ();
+	OS.GetControlBounds (handle, rect);
+	int width = rect.right - rect.left;
+	int height = rect.bottom - rect.top;
+	boolean horizontal = width > height;
+	short [] base = new short [1];
+	OS.GetBestControlRect (handle, rect, base);
+	width = rect.right - rect.left;
+	height = rect.bottom - rect.top;
+	if ((style & SWT.HORIZONTAL) != 0) {
+		if (!horizontal) height = width;
+		width = height * 10;
+	} else {
+		if (horizontal) width = height;
+		height = width * 10;
+	}
+	if (wHint != SWT.DEFAULT) width = wHint;
+	if (hHint != SWT.DEFAULT) height = hHint;
+	return new Point (width, height);
+}
+
+void createHandle () {
+	Display display = getDisplay ();
+	int [] outControl = new int [1];
+	int window = OS.GetControlOwner (parent.handle);
+	OS.CreateScrollBarControl (window, null, 0, 0, 100, 10, true, display.actionProc, outControl);
+	if (outControl [0] == 0) error (SWT.ERROR_NO_HANDLES);
+	handle = outControl [0];
+	OS.HIViewAddSubview (parent.handle, handle);
+	OS.HIViewSetZOrder (handle, OS.kHIViewZOrderBelow, 0);
+}
+
+public int getIncrement () {
+	checkWidget();
+    return increment;
+}
+
+public int getMaximum () {
+	checkWidget();
+    return OS.GetControl32BitMaximum (handle);
+}
+
+public int getMinimum () {
+	checkWidget();
+    return OS.GetControl32BitMinimum (handle);
+}
+
+public int getPageIncrement () {
+	checkWidget();
+    return pageIncrement;
+}
+
+public int getSelection () {
+	checkWidget();
+    return OS.GetControl32BitValue (handle);
+}
+
+public int getThumb () {
+	checkWidget();
+    return OS.GetControlViewSize (handle);
+}
+
+int kEventMouseDown (int nextHandler, int theEvent, int userData) {
+	int status = super.kEventMouseDown (nextHandler, theEvent, userData);
+	if (status == OS.noErr) return status;
+	dragging = false;
+	status = OS.CallNextEventHandler (nextHandler, theEvent);
+	if (dragging) {
+		Event event = new Event ();
+		sendEvent (SWT.Selection, event);
+	}
+	dragging = false;
+	return status;
+}
+
+public void removeSelectionListener(SelectionListener listener) {
+	checkWidget();
+	if (listener == null) error (SWT.ERROR_NULL_ARGUMENT);
+	if (eventTable == null) return;
+	eventTable.unhook(SWT.Selection, listener);
+	eventTable.unhook(SWT.DefaultSelection,listener);
+}
+
+public void setIncrement (int value) {
+	checkWidget();
+	if (value < 1) return;
+	increment = value;
+}
+
+public void setMaximum (int value) {
+	checkWidget();
+	if (value < 0) return;
+	int minimum = OS.GetControl32BitMinimum (handle);
+	int viewSize = OS.GetControlViewSize (handle);
+	if (value - minimum - viewSize < 0) return;
+	OS.SetControl32BitMaximum (handle, value);
+}
+
+public void setMinimum (int value) {
+	checkWidget();
+	if (value < 0) return;
+	int maximum = OS.GetControl32BitMinimum (handle);
+	int viewSize = OS.GetControlViewSize (handle);
+	if (maximum - value - viewSize < 0) return;
+	OS.SetControl32BitMinimum (handle, value);
+}
+
+public void setPageIncrement (int value) {
+	checkWidget();
+	if (value < 1) return;
+	pageIncrement = value;
+}
+
+public void setSelection (int value) {
+	checkWidget();
+	if (value < 0) return;
+	OS.SetControl32BitValue (handle, value);
+}
+
+public void setThumb (int value) {
+	checkWidget();
+	if (value < 1) return;
+    OS.SetControlViewSize (handle, value);
+}
+
+public void setValues (int selection, int minimum, int maximum, int thumb, int increment, int pageIncrement) {
+	checkWidget();
+	if (selection < 0) return;
+	if (minimum < 0) return;
+	if (maximum < 0) return;
+	if (thumb < 1) return;
+	if (maximum - minimum - thumb < 0) return;
+	if (increment < 1) return;
+	if (pageIncrement < 1) return;
+	OS.SetControl32BitMinimum (handle, minimum);
+	OS.SetControl32BitMaximum (handle, maximum);
+	OS.SetControlViewSize (handle, thumb);
+	OS.SetControl32BitValue (handle, selection);
+	increment= increment;
+	pageIncrement= pageIncrement;
+}
+
+}
