bf3689fc436c01f5bd358f92126e9bea9142ba16
Veronika Irvine <veronika>
2002-11-09 20:43:57 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Button.java
@@ -19,6 +19,10 @@ import org.eclipse.swt.graphics.*;
 public class Button extends Control {
 	String text;
 	Image image;
+	int cIcon;
+	boolean displayImage;
+	int textExtentX;
+	int textExtentY;
 	
 public Button (Composite parent, int style) {
 	super (parent, checkStyle (style));
@@ -48,23 +52,56 @@ static int checkStyle (int style) {
 
 public Point computeSize (int wHint, int hHint, boolean changed) {
 	checkWidget();
-	// NEEDS WORK
-	// - empty string/icon case is broken
+	// NEEDS WORK - empty string
 	if ((style & SWT.ARROW) != 0) {
-		return new Point (10, 10);
+		int width = 10, height = 10;
+		if (wHint != SWT.DEFAULT) width = wHint;
+		if (hHint != SWT.DEFAULT) height = hHint;
+		return new Point (width, height);
 	}
 	Rect rect = new Rect();
 	short [] base = new short [1];
 	OS.GetBestControlRect (handle, rect, base);
 	int width = rect.right - rect.left;
 	int height = rect.bottom - rect.top;
+
 	if ((style & (SWT.CHECK | SWT.RADIO)) != 0) {
-		height -= 17;
+		if (displayImage) {
+			Rectangle bounds = image.getBounds();
+			width += bounds.width + 20;
+			height += bounds.height;
+		} else {
+			width += 8;
+		}
+	} else {
+		// If any theme was applied to a bevel button, the compute size 
+		// needs to be fixed
+		if ((style & SWT.FLAT) == 0 || (style & SWT.TOGGLE) != 0) {
+			if (displayImage) {
+				Rectangle bounds = image.getBounds();
+				width = bounds.width;
+				height = bounds.height;
+				if ((style & SWT.PUSH) != 0) {
+					width += 28;
+					height += 10;
+				}
+				if ((style & SWT.TOGGLE) != 0) {
+					width += 6;
+					height += 6;
+				}
+			} else {
+				if ((style & SWT.PUSH) != 0) {
+					width = textExtentX + 16;
+					height = textExtentY;
+				}
+				if ((style & SWT.TOGGLE) != 0) {
+					width = textExtentX - 10;
+					height = textExtentY - 2;
+				}
+			}
+		}
 	}
-	if ((style & SWT.PUSH) != 0 && (style & SWT.FLAT) == 0) {
-		width += 1;
-		height -= 11;
-	}
+	width = Math.max(20, width);
 	if (wHint != SWT.DEFAULT) width = wHint;
 	if (hHint != SWT.DEFAULT) height = hHint;
 	return new Point (width, height);
@@ -144,6 +181,14 @@ void createHandle () {
 		OS.SetControlData (handle, OS.kControlEntireControl, OS.kControlBevelButtonTextAlignTag, 2, new short [] {(short)textAlignment});
 		OS.SetControlData (handle, OS.kControlEntireControl, OS.kControlBevelButtonGraphicAlignTag, 2, new short [] {(short)graphicAlignment});
 	}
+}
+
+void destroyWidget () {
+	if (cIcon != 0) {
+		destroyCIcon(cIcon);
+		cIcon = 0;
+	}
+	super.destroyWidget();
 }
 
 public int getAlignment () {
@@ -241,6 +286,18 @@ public void setAlignment (int alignment) {
 	OS.HIViewSetNeedsDisplay (handle, true);
 }
 
+public void setBounds (int x, int y, int width, int height) {
+	checkWidget ();
+	/* Bug in MacOS X. When setting the height of a bevel button 
+	 * to a value less than 20, the button is drawn incorrectly.
+	 * The fix is to force the height to be greater than or equal to 20.
+	 */
+	if ((style & SWT.ARROW) == 0) {
+		height = Math.max (20, height);
+	}
+	super.setBounds (x, y, width, height);
+}
+
 public boolean setFocus () {
 	checkWidget ();
 	if ((style & SWT.ARROW) != 0) return false;
@@ -250,7 +307,18 @@ public boolean setFocus () {
 public void setImage (Image image) {
 	checkWidget();
 	if ((style & SWT.ARROW) != 0) return;
+	if (cIcon != 0) {
+		destroyCIcon(cIcon);
+		cIcon = 0;
+	}
 	this.image = image;
+	if (image == null) {
+		ControlButtonContentInfo inContent = new ControlButtonContentInfo();
+		inContent.contentType = (short)OS.kControlContentTextOnly;
+		OS.SetBevelButtonContentInfo(handle, inContent);
+		displayImage = false;
+		return;
+	}
 	if (text != null) {
 		char [] buffer = new char [1];
 		int ptr = OS.CFStringCreateWithCharacters (OS.kCFAllocatorDefault, buffer, 1);
@@ -258,17 +326,13 @@ public void setImage (Image image) {
 		OS.SetControlTitleWithCFString (handle, ptr);
 		OS.CFRelease (ptr);
 	}
-	//int kControlContentIconRef = 132;
-	//int kOnSystemDisk = -32768;
-	//int kSystemIconsCreator = ('m'<<24) + ('a'<<16) + ('c'<<8) + 's';
-	//int kColorSyncFolderIcon = ('p'<<24) + ('r'<<16) + ('o'<<8) + 'f';
-	//int [] iconRef = new int [1];
-	//OS.GetIconRef ((short)kOnSystemDisk, kSystemIconsCreator, kColorSyncFolderIcon, iconRef);
-	//ControlButtonContentInfo inContent = new ControlButtonContentInfo ();
-	//inContent.contentType = (short)kControlContentIconRef;
-	//inContent.iconRef = iconRef [0];
-	//OS.SetBevelButtonContentInfo (handle, inContent);
-	//OS.HIViewSetNeedsDisplay (handle, true);
+	cIcon = createCIcon(image);
+	ControlButtonContentInfo inContent = new ControlButtonContentInfo ();
+	inContent.contentType = (short)OS.kControlContentCIconHandle;
+	inContent.iconRef = cIcon;
+	OS.SetBevelButtonContentInfo (handle, inContent);
+	OS.HIViewSetNeedsDisplay (handle, true);
+	displayImage = true;
 }
 
 boolean setRadioSelection (boolean value){
@@ -291,11 +355,12 @@ public void setText (String string) {
 	if (string == null) error (SWT.ERROR_NULL_ARGUMENT);
 	if ((style & SWT.ARROW) != 0) return;
 	text = string;
-	if (image != null) {
+	if (displayImage) {
 		ControlButtonContentInfo inContent = new ControlButtonContentInfo();
 		inContent.contentType = (short)OS.kControlContentTextOnly;
 		OS.SetBevelButtonContentInfo(handle, inContent);
 	}
+	displayImage = false;
 	char [] buffer = new char [text.length ()];
 	text.getChars (0, buffer.length, buffer, 0);
 	int i=0, j=0;
@@ -310,6 +375,12 @@ public void setText (String string) {
 	if (ptr == 0) error (SWT.ERROR_CANNOT_SET_TEXT);
 	OS.SetControlTitleWithCFString (handle, ptr);
 	OS.CFRelease (ptr);
+	Rect rect = new Rect();
+	short [] base = new short [1];
+	OS.GetBestControlRect (handle, rect, base);
+	textExtentX = rect.right - rect.left;
+	textExtentY = rect.bottom - rect.top;
+	
 }
 
 }
