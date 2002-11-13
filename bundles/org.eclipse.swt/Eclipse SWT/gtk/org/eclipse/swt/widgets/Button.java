277fb26da45e23bba683764c26ea082e00719f7b
Christophe Cornu <ccornu>
2002-11-13 17:30:30 +0000

    PR26004 - force activated button to get focus, indirectly cause events to be properly flushed

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Button.java
@@ -355,10 +355,18 @@ int gtk_focus_out_event (int widget, int event) {
 	return result;
 }
 
+int gtk_mnemonic_activate (int widget, int arg1) {
+	setFocus();
+	return 0;
+}
+
 void hookEvents () {
 	super.hookEvents();
 	Display display = getDisplay ();
 	OS.g_signal_connect (handle, OS.clicked, display.windowProc2, CLICKED);
+	if (labelHandle != 0) {
+		OS.g_signal_connect (labelHandle, OS.mnemonic_activate, display.windowProc3, MNEMONIC_ACTIVATE);
+	}
 }
 
 void register () {
