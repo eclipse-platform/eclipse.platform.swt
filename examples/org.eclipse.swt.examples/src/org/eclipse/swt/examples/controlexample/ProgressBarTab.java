30113414ea1b407652f295085c0a7c8eea691cda
Grant Gayed <ggayed>
2002-11-15 21:21:06 +0000

    add INDETERMINATE style

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/ProgressBarTab.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/ProgressBarTab.java
@@ -18,7 +18,8 @@ class ProgressBarTab extends RangeTab {
 
 	/* Style widgets added to the "Style" group */
 	Button smoothButton;
-	
+	Button indeterminateButton;
+
 	/**
 	 * Creates the Tab within a given instance of ControlExample.
 	 */
@@ -31,66 +32,79 @@ class ProgressBarTab extends RangeTab {
 	 */
 	void createExampleGroup() {
 		super.createExampleGroup ();
-		
+
 		/* Create a group for the progress bar */
 		progressBarGroup = new Group (exampleGroup, SWT.NULL);
 		progressBarGroup.setLayout (new GridLayout ());
 		progressBarGroup.setLayoutData (new GridData (GridData.GRAB_HORIZONTAL | GridData.HORIZONTAL_ALIGN_FILL | GridData.VERTICAL_ALIGN_FILL));
 		progressBarGroup.setText ("ProgressBar");
 	}
-	
+
 	/**
 	 * Creates the "Example" widgets.
 	 */
 	void createExampleWidgets () {
-		
+
 		/* Compute the widget style */
 		int style = SWT.NONE;
 		if (horizontalButton.getSelection ()) style |= SWT.HORIZONTAL;
 		if (verticalButton.getSelection ()) style |= SWT.VERTICAL;
 		if (smoothButton.getSelection ()) style |= SWT.SMOOTH;
 		if (borderButton.getSelection ()) style |= SWT.BORDER;
-	
+		if (indeterminateButton.getSelection ()) style |= SWT.INDETERMINATE;
+
 		/* Create the example widgets */
 		progressBar1 = new ProgressBar (progressBarGroup, style);
 		progressBar1.setMaximum (100);
 		progressBar1.setSelection (50);
 	}
-	
+
 	/**
 	 * Creates the "Style" group.
 	 */
 	void createStyleGroup () {
 		super.createStyleGroup ();
-	
+
 		/* Create the extra widgets */
 		smoothButton = new Button (styleGroup, SWT.CHECK);
 		smoothButton.setText ("SWT.SMOOTH");
+		indeterminateButton = new Button (styleGroup, SWT.CHECK);
+		indeterminateButton.setText ("SWT.INDETERMINATE");
 	}
-	
+
 	/**
 	 * Gets the "Example" widget children.
 	 */
 	Control [] getExampleWidgets () {
 		return new Control [] {progressBar1};
 	}
-	
+
 	/**
 	 * Gets the text for the tab folder item.
 	 */
 	String getTabText () {
 		return "ProgressBar";
 	}
-	
+
 	/**
 	 * Sets the state of the "Example" widgets.
 	 */
 	void setExampleWidgetState () {
 		super.setExampleWidgetState ();
+		if (indeterminateButton.getSelection ()) {
+			selectionScale.setEnabled (false);
+			minimumScale.setEnabled (false);
+			maximumScale.setEnabled (false);
+		} else {
+			selectionScale.setEnabled (true);
+			minimumScale.setEnabled (true);
+			maximumScale.setEnabled (true);
+		}
 		maximumScale.setMaximum (progressBar1.getMaximum ());
 		smoothButton.setSelection ((progressBar1.getStyle () & SWT.SMOOTH) != 0);
+		indeterminateButton.setSelection ((progressBar1.getStyle () & SWT.INDETERMINATE) != 0);
 	}
-	
+
 	/**
 	 * Sets the maximum of the "Example" widgets.
 	 */
@@ -98,15 +112,15 @@ class ProgressBarTab extends RangeTab {
 		progressBar1.setMaximum (maximumScale.getSelection ());
 		updateScales ();
 	}
-	
+
 	/**
 	 * Sets the minimim of the "Example" widgets.
 	 */
-	void setWidgetMinimum () { 
+	void setWidgetMinimum () {
 		progressBar1.setMinimum (minimumScale.getSelection ());
 		updateScales ();
 	}
-	
+
 	/**
 	 * Sets the selection of the "Example" widgets.
 	 */
@@ -122,6 +136,6 @@ class ProgressBarTab extends RangeTab {
 	void updateScales () {
 		minimumScale.setSelection (progressBar1.getMinimum ());
 		selectionScale.setSelection (progressBar1.getSelection ());
-		maximumScale.setSelection (progressBar1.getMaximum ());				
- 	}
+		maximumScale.setSelection (progressBar1.getMaximum ());
+	}
 }
