a87d9548b307ff07763132487d2e29bd7bf42638
Silenio Quarti <silenio>
2002-11-20 20:09:30 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/Image.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/Image.java
@@ -809,7 +809,7 @@ public int internal_new_GC (GCData data) {
 		data.device = device;
 		data.background = device.COLOR_WHITE.handle;
 		data.foreground = device.COLOR_BLACK.handle;
-		data.font = device.getSystemFont ();
+		data.font = device.systemFont;
 		data.image = this;
 	}
 	return context;
