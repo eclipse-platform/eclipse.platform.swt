705b81e96deddfca50bbb26828053ba23c623b13
Silenio Quarti <silenio>
2002-11-27 21:03:46 +0000

    fonts

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/Device.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/Device.java
@@ -431,14 +431,14 @@ protected void init () {
 	COLOR_WHITE = new Color (this, 0xFF,0xFF,0xFF);
 	
 	/* Initialize the system font slot */
-	short id = OS.GetAppFont();
-	short style = (short)0; 
-	short size = OS.GetDefFontSize();
-	int[] font = new int[1];
-	if (OS.FMGetFontFromFontFamilyInstance(id, style, font, null) != 0) {
-		SWT.error(SWT.ERROR_NO_HANDLES);
-	}
-	systemFont = Font.carbon_new (this, font[0], id, style, size);
+	byte [] family = new byte [256];
+	short [] size = new short [1];
+	byte [] style = new byte [1];
+	OS.GetThemeFont ((short) OS.kThemeSystemFont, (short) OS.smSystemScript, family, size, style);
+	short id = OS.FMGetFontFamilyFromName (family);
+	int [] font = new int [1]; 
+	OS.FMGetFontFromFontFamilyInstance (id, style [0], font, null);
+	systemFont = Font.carbon_new (this, font [0], id, style [0], size [0]);
 }
 
 /**	 
