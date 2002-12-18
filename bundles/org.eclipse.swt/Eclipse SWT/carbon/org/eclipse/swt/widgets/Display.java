94f67776a5d348c4659c4c274f0984fdd9bd4639
Silenio Quarti <silenio>
2002-12-18 17:17:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
@@ -1497,15 +1497,15 @@ public void timerExec (int milliseconds, Runnable runnable) {
 int timerProc (int id, int index) {
 	if (timerList == null) return 0;
 	if (0 <= index && index < timerList.length) {
-		if (allowTimers) {
+//		if (allowTimers) {
 			Runnable runnable = timerList [index];
 			timerList [index] = null;
 			timerIds [index] = 0;
 			if (runnable != null) runnable.run ();
-		} else {
-			timerIds [index] = -1;
-			wakeUp ();
-		}
+//		} else {
+//			timerIds [index] = -1;
+//			wakeUp ();
+//		}
 	}
 	return 0;
 }
