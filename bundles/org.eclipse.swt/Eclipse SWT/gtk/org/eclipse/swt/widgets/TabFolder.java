7a5cd3ff803e520ca4e89d6ccebd291664b68d47
Felipe Heidrich <fheidric>
2003-01-06 16:58:43 +0000

    28991

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TabFolder.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TabFolder.java
@@ -458,11 +458,17 @@ Point minimumSize (int wHint, int hHint, boolean flushCache) {
 	for (int i=0; i<children.length; i++) {
 		Control child = children [i];
 		int index = 0;
-		while (index < items.length) {
+		int count = 0;
+		int list = OS.gtk_container_get_children (handle);
+		if (list != 0) {
+			count = OS.g_list_length (list);
+			OS.g_list_free (list);
+		}
+		while (index < count) {
 			if (items [index].control == child) break;
 			index++;
 		}
-		if (index == items.length) {
+		if (index == count) {
 			Rectangle rect = child.getBounds ();
 			width = Math.max (width, rect.x + rect.width);
 			height = Math.max (height, rect.y + rect.height);
