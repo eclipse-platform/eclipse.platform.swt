c75844aea0b2f2be9b3c4ce477cd6734494eb0de
Lynn Kues <lkues>
2003-01-13 17:14:28 +0000

    27099

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/ST.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/ST.java
@@ -17,44 +17,44 @@ public class ST {
 	 */
 	 
 	/* Navigation Key Actions */	
-	public static final int LINE_UP = (1 << 24) + 1; 				// binding = SWT.ARROW_UP
-	public static final int LINE_DOWN = (1 << 24) + 2; 				// binding = SWT.ARROW_DOWN
-	public static final int LINE_START = (1 << 24) + 7; 				// binding = SWT.HOME
-	public static final int LINE_END = (1 << 24) + 8; 				// binding = SWT.END
-	public static final int COLUMN_PREVIOUS = (1 << 24) + 3; 		// binding = SWT.ARROW_LEFT
-	public static final int COLUMN_NEXT = (1 << 24) + 4; 			// binding = SWT.ARROW_RIGHT
-	public static final int PAGE_UP = (1 << 24) + 5; 				// binding = SWT.PAGE_UP
-	public static final int PAGE_DOWN = (1 << 24) + 6; 				// binding = SWT.PAGE_DOWN
-	public static final int WORD_PREVIOUS = (1 << 24) + 3 | 1 << 18;	// binding = SWT.MOD1 + SWT.ARROW_LEFT
-	public static final int WORD_NEXT = (1 << 24) + 4 | 1 << 18; 	// binding = SWT.MOD1 + SWT.ARROW_RIGHT
-	public static final int TEXT_START = (1 << 24) + 7 | 1 << 18; 	// binding = SWT.MOD1 + SWT.HOME
-	public static final int TEXT_END = (1 << 24) + 8 | 1 << 18; 		// binding = SWT.MOD1 + SWT.END
-	public static final int WINDOW_START = (1 << 24) + 5 | 1 << 18; 	// binding = SWT.MOD1 + SWT.PAGE_UP
-	public static final int WINDOW_END = (1 << 24) + 6 | 1 << 18; 	// binding = SWT.MOD1 + SWT.PAGE_DOWN
+	public static final int LINE_UP = 16777217; 			// binding = SWT.ARROW_UP
+	public static final int LINE_DOWN = 16777218; 		// binding = SWT.ARROW_DOWN
+	public static final int LINE_START = 16777223; 		// binding = SWT.HOME
+	public static final int LINE_END = 16777224; 		// binding = SWT.END
+	public static final int COLUMN_PREVIOUS = 16777219; 	// binding = SWT.ARROW_LEFT
+	public static final int COLUMN_NEXT = 16777220; 		// binding = SWT.ARROW_RIGHT
+	public static final int PAGE_UP = 16777221; 			// binding = SWT.PAGE_UP
+	public static final int PAGE_DOWN = 16777222; 		// binding = SWT.PAGE_DOWN
+	public static final int WORD_PREVIOUS = 17039363;	// binding = SWT.MOD1 + SWT.ARROW_LEFT
+	public static final int WORD_NEXT = 17039364; 		// binding = SWT.MOD1 + SWT.ARROW_RIGHT
+	public static final int TEXT_START = 17039367; 		// binding = SWT.MOD1 + SWT.HOME
+	public static final int TEXT_END = 17039368; 		// binding = SWT.MOD1 + SWT.END
+	public static final int WINDOW_START = 17039365; 	// binding = SWT.MOD1 + SWT.PAGE_UP
+	public static final int WINDOW_END = 17039366; 		// binding = SWT.MOD1 + SWT.PAGE_DOWN
 
 	/* Selection Key Actions */
-	public static final int SELECT_LINE_UP = (1 << 24) + 1 | 1 << 17; 					// binding = SWT.MOD2 + SWT.ARROW_UP
-	public static final int SELECT_LINE_DOWN = (1 << 24) + 2 | 1 << 17; 					// binding = SWT.MOD2 + SWT.ARROW_DOWN
-	public static final int SELECT_LINE_START = (1 << 24) + 7 | 1 << 17; 				// binding = SWT.MOD2 + SWT.HOME
-	public static final int SELECT_LINE_END = (1 << 24) + 8 | 1 << 17; 					// binding = SWT.MOD2 + SWT.END
-	public static final int SELECT_COLUMN_PREVIOUS = (1 << 24) + 3 | 1 << 17; 			// binding = SWT.MOD2 + SWT.ARROW_LEFT
-	public static final int SELECT_COLUMN_NEXT = (1 << 24) + 4 | 1 << 17; 				// binding = SWT.MOD2 + SWT.ARROW_RIGHT
-	public static final int SELECT_PAGE_UP = (1 << 24) + 5 | 1 << 17; 					// binding = SWT.MOD2 + SWT.PAGE_UP
-	public static final int SELECT_PAGE_DOWN = (1 << 24) + 6 | 1 << 17; 					// binding = SWT.MOD2 + SWT.PAGE_DOWN
-	public static final int SELECT_WORD_PREVIOUS = (1 << 24) + 3 | 1 << 18 | 1 << 17;	// binding = SWT.MOD1 + SWT.MOD2 + SWT.ARROW_LEFT
-	public static final int SELECT_WORD_NEXT = (1 << 24) + 4 | 1 << 18 | 1 << 17; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.ARROW_RIGHT
-	public static final int SELECT_TEXT_START = (1 << 24) + 7 | 1 << 18 | 1 << 17; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.HOME
-	public static final int SELECT_TEXT_END = (1 << 24) + 8 | 1 << 18 | 1 << 17; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.END
-	public static final int SELECT_WINDOW_START = (1 << 24) + 5 | 1 << 18 | 1 << 17; 	// binding = SWT.MOD1 + SWT.MOD2 + SWT.PAGE_UP
-	public static final int SELECT_WINDOW_END = (1 << 24) + 6 | 1 << 18 | 1 << 17; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.PAGE_DOWN
+	public static final int SELECT_LINE_UP = 16908289; 			// binding = SWT.MOD2 + SWT.ARROW_UP
+	public static final int SELECT_LINE_DOWN = 16908290; 		// binding = SWT.MOD2 + SWT.ARROW_DOWN
+	public static final int SELECT_LINE_START = 16908295; 		// binding = SWT.MOD2 + SWT.HOME
+	public static final int SELECT_LINE_END = 16908296; 			// binding = SWT.MOD2 + SWT.END
+	public static final int SELECT_COLUMN_PREVIOUS = 16908291;	// binding = SWT.MOD2 + SWT.ARROW_LEFT
+	public static final int SELECT_COLUMN_NEXT = 16908292; 		// binding = SWT.MOD2 + SWT.ARROW_RIGHT
+	public static final int SELECT_PAGE_UP = 16908293; 			// binding = SWT.MOD2 + SWT.PAGE_UP
+	public static final int SELECT_PAGE_DOWN = 16908294; 		// binding = SWT.MOD2 + SWT.PAGE_DOWN
+	public static final int SELECT_WORD_PREVIOUS = 17170435;		// binding = SWT.MOD1 + SWT.MOD2 + SWT.ARROW_LEFT
+	public static final int SELECT_WORD_NEXT = 17170436; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.ARROW_RIGHT
+	public static final int SELECT_TEXT_START = 17170439; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.HOME
+	public static final int SELECT_TEXT_END = 17170440; 			// binding = SWT.MOD1 + SWT.MOD2 + SWT.END
+	public static final int SELECT_WINDOW_START = 17170437; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.PAGE_UP
+	public static final int SELECT_WINDOW_END = 17170438; 		// binding = SWT.MOD1 + SWT.MOD2 + SWT.PAGE_DOWN
 
 	/* Modification Key Actions */
-	public static final int CUT = 0x7F| 1 << 17; 			// binding = SWT.MOD2 + SWT.DEL
-	public static final int COPY = (1 << 24) + 9 | 1 << 18; 	// binding = SWT.MOD1 + SWT.INSERT;
-	public static final int PASTE = (1 << 24) + 9 | 1 << 17;	// binding = SWT.MOD2 + SWT.INSERT ;
-	public static final int DELETE_PREVIOUS = '\b'; 			// binding = SWT.BS;
-	public static final int DELETE_NEXT = 0x7F; 				// binding = SWT.DEL;
+	public static final int CUT = 131199; 			// binding = SWT.MOD2 + SWT.DEL
+	public static final int COPY = 17039369; 		// binding = SWT.MOD1 + SWT.INSERT;
+	public static final int PASTE = 16908297;		// binding = SWT.MOD2 + SWT.INSERT ;
+	public static final int DELETE_PREVIOUS = '\b'; 	// binding = SWT.BS;
+	public static final int DELETE_NEXT = 0x7F; 		// binding = SWT.DEL;
 
 	/* Miscellaneous Key Actions */
-	public static final int TOGGLE_OVERWRITE = (1 << 24) + 9; // binding = SWT.INSERT;
+	public static final int TOGGLE_OVERWRITE = 16777225; // binding = SWT.INSERT;
 }
