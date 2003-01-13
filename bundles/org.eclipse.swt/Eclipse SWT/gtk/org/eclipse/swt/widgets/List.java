bc699eef7b657a1ac7762291a7622b72995628db
Grant Gayed <ggayed>
2003-01-13 18:26:49 +0000

    29408

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/List.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/List.java
@@ -996,6 +996,9 @@ public void select (int index) {
 	OS.g_signal_handlers_block_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 	OS.gtk_tree_model_iter_nth_child (modelHandle, iter, 0, index); 
 	OS.gtk_tree_selection_select_iter (selection, iter);
+	int path = OS.gtk_tree_model_get_path (modelHandle, iter);
+	OS.gtk_tree_view_set_cursor (handle, path, 0, false);
+	OS.gtk_tree_path_free (path);
 	OS.g_signal_handlers_unblock_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 	OS.g_free (iter);
 }
@@ -1027,6 +1030,11 @@ public void select (int start, int end) {
 	for (int index=start; index<=end; index++) {
 		OS.gtk_tree_model_iter_nth_child (modelHandle, iter, 0, index); 
 		OS.gtk_tree_selection_select_iter (selection, iter);
+		if ((style & SWT.SINGLE) != 0) {
+			int path = OS.gtk_tree_model_get_path (modelHandle, iter);
+			OS.gtk_tree_view_set_cursor (handle, path, 0, false);
+			OS.gtk_tree_path_free (path);
+		}
 	}
 	OS.g_signal_handlers_unblock_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 	OS.g_free (iter);
@@ -1061,7 +1069,12 @@ public void select (int [] indices) {
 		if (index < 0 || index > count) continue;
 		OS.gtk_tree_model_iter_nth_child (modelHandle, iter, 0, index); 
 		OS.gtk_tree_selection_select_iter (selection, iter);
-		if ((style & SWT.SINGLE) != 0) break;
+		if ((style & SWT.SINGLE) != 0) {
+			int path = OS.gtk_tree_model_get_path (modelHandle, iter);
+			OS.gtk_tree_view_set_cursor (handle, path, 0, false);
+			OS.gtk_tree_path_free (path);
+			break;
+		}
 	}
 	OS.g_signal_handlers_unblock_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 	OS.g_free (iter);
