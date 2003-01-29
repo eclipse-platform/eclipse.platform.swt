d1f793a00ea40288fb0795c0b59412f53c89c439
Christophe Cornu <ccornu>
2003-01-29 22:59:50 +0000

    PR30537

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -1322,9 +1322,11 @@ public void select (int index) {
 	int selection = OS.gtk_tree_view_get_selection (handle);
 	OS.g_signal_handlers_block_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 	OS.gtk_tree_selection_select_iter (selection, items [index].handle);
-	int path = OS.gtk_tree_model_get_path (modelHandle, items[index].handle);
-	OS.gtk_tree_view_set_cursor (handle, path, 0, false);
-	OS.gtk_tree_path_free (path);
+	if ((style & SWT.SINGLE) != 0) {
+		int path = OS.gtk_tree_model_get_path (modelHandle, items[index].handle);
+		OS.gtk_tree_view_set_cursor (handle, path, 0, false);
+		OS.gtk_tree_path_free (path);
+	}
 	OS.g_signal_handlers_unblock_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 }
 
