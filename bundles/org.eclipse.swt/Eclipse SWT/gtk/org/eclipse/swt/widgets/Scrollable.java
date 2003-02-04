add63531e54321a29c008d33afde4204f0cb5aee
Steve Northover <steve>
2003-02-04 01:05:19 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Scrollable.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Scrollable.java
@@ -266,11 +266,11 @@ int topHandle () {
 }
 
 int vScrollBarWidth() {
-	if (verticalBar==null) return 0;
+	if (verticalBar == null) return 0;
 	int vBarHandle = OS.GTK_SCROLLED_WINDOW_VSCROLLBAR(scrolledHandle);
-	if (vBarHandle==0) return 0;
+	if (vBarHandle == 0) return 0;
 	GtkRequisition requisition = new GtkRequisition();
-	OS.gtk_widget_size_request(vBarHandle, requisition);
+	OS.gtk_widget_size_request (vBarHandle, requisition);
 	int spacing = OS.GTK_SCROLLED_WINDOW_SCROLLBAR_SPACING(scrolledHandle);
 	return requisition.width + spacing;
 }
