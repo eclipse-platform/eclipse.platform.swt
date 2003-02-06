996648919fbac4546f2b3bed5d75c37e50d32651
Steve Northover <steve>
2003-02-06 21:40:33 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWT.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWT.java
@@ -730,6 +730,14 @@ public class SWT {
 	 * <li><code>Menu</code></li>
 	 * </ul></p>
 	 * 
+	 * <p>
+	 * <b>NOTE:</b> This API element is part of an interim API that is still under
+	 * development and is expected to change significantly before reaching stability.
+	 * It is being made available at this early stage to solicit feedback from
+	 * pioneering adopters on the understanding that any code that uses this API
+	 * will almost certainly be broken (repeatedly) as the API evolves.
+	 * </p>
+	 * 
 	 * @since 2.1
 	 */
 	public static final int LEFT_TO_RIGHT = 1 << 25;
@@ -748,6 +756,14 @@ public class SWT {
 	 * <li><code>Menu</code></li>
 	 * </ul></p>
 	 * 
+	 * <p>
+	 * <b>NOTE:</b> This API element is part of an interim API that is still under
+	 * development and is expected to change significantly before reaching stability.
+	 * It is being made available at this early stage to solicit feedback from
+	 * pioneering adopters on the understanding that any code that uses this API
+	 * will almost certainly be broken (repeatedly) as the API evolves.
+	 * </p>
+	 * 
 	 * @since 2.1
 	 */
 	public static final int RIGHT_TO_LEFT = 1 << 26;
@@ -758,6 +774,14 @@ public class SWT {
 	 * <li><code>Control</code></li>
 	 * <li><code>Menu</code></li>
 	 * </ul></p>
+	 * 
+	 * <p>
+	 * <b>NOTE:</b> This API element is part of an interim API that is still under
+	 * development and is expected to change significantly before reaching stability.
+	 * It is being made available at this early stage to solicit feedback from
+	 * pioneering adopters on the understanding that any code that uses this API
+	 * will almost certainly be broken (repeatedly) as the API evolves.
+	 * </p>
 	 * 
 	 * @since 2.1
 	 */
