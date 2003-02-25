1e70471be971a910643066a06f3c59c9f25d9a30
Grant Gayed <ggayed>
2003-02-25 15:53:10 +0000

    initial

---
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/EventConsole.java
@@ -0,0 +1,84 @@
+package org.eclipse.swt.examples.controlexample;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
+
+public class EventConsole {
+	Shell shell;
+	Text text;
+	boolean isActive = true;
+	static boolean [] eventsFilter = new boolean [35];
+	static {
+		int [] defaultFilter = {
+			SWT.KeyDown, SWT.KeyUp, SWT.MouseDown,
+			SWT.MouseUp, SWT.MouseDoubleClick, SWT.Selection,
+			SWT.DefaultSelection, SWT.Expand, SWT.Collapse,
+			SWT.Iconify, SWT.Deiconify, SWT.Close,
+			SWT.Show, SWT.Hide, SWT.Modify,
+			SWT.Verify, SWT.Activate, SWT.Deactivate,
+			SWT.Help, SWT.DragDetect, SWT.Arm,
+			SWT.Traverse, SWT.HardKeyDown, SWT.HardKeyUp
+		};
+		for (int i = 0; i < defaultFilter.length; i++) {
+			eventsFilter [defaultFilter [i]] = true;
+		}
+	}
+	
+	public EventConsole (Shell parent) {
+		super ();
+		shell = new Shell (parent);
+		shell.setText ("ControlExample Events");
+		shell.setLayout (new FillLayout ());
+		Point parentSize = parent.getSize();
+		shell.setBounds(10, parentSize.y - 100, parentSize.x / 2, 200);
+		text = new Text (shell, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
+		
+		Menu bar = new Menu (shell, SWT.BAR);
+		MenuItem fileItem = new MenuItem (bar, SWT.CASCADE);
+		fileItem.setText ("File");
+		shell.setMenuBar (bar);
+		Menu dropDown = new Menu (bar);
+		fileItem.setMenu (dropDown);
+
+		MenuItem close = new MenuItem (dropDown, SWT.NONE);
+		close.setText ("Dismiss");
+		close.addListener (SWT.Selection, new Listener () {
+			public void handleEvent (Event e) {
+				shell.dispose ();
+				isActive = false;
+			}
+		});
+		shell.addListener(SWT.Dispose, new Listener() {
+			public void handleEvent(Event event) {
+				isActive = false;
+			}
+		});
+	}
+	
+	public void close () {
+		shell.dispose ();
+		isActive = false;
+	}
+	
+	public void log (Event event) {
+		if (isActive && eventsFilter [event.type]) {
+			StringBuffer output = new StringBuffer ();
+			output.append ("Control: ");
+			output.append (event.widget.toString ());
+			output.append (" Type: ");
+			output.append (event.type);
+			output.append (" X: ");
+			output.append (event.x);
+			output.append (" Y: ");
+			output.append (event.y);
+			output.append ("\n");
+			text.append (output.toString ());
+		}
+	}
+
+	public void open () {
+		shell.open ();
+	}
+}
