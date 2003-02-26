8a3246b84b77d2fe701231e916fd116bcc49a2df
Felipe Heidrich <fheidric>
2003-02-26 17:23:36 +0000

    API fixing

--- bundles/org.eclipse.swt/Eclipse SWT Printing/carbon/org/eclipse/swt/printing/Printer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Printing/carbon/org/eclipse/swt/printing/Printer.java
@@ -7,7 +7,6 @@ package org.eclipse.swt.printing;
  * http://www.eclipse.org/legal/cpl-v10.html
  */
 
-import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
 
 /**
@@ -32,12 +31,7 @@ import org.eclipse.swt.graphics.*;
  */
 public final class Printer extends Device {
 	PrinterData data;
-	int printContext, xScreen, xDrawable, xtContext;
-	Font defaultFont;
 
-	static String APP_NAME = "SWT_Printer";
-	
-//	public static String XDefaultPrintServer = ":1";
 /**
  * Returns an array of <code>PrinterData</code> objects
  * representing all available printers.
@@ -45,54 +39,8 @@ public final class Printer extends Device {
  * @return the list of available printers
  */
 public static PrinterData[] getPrinterList() {
-	// TEMPORARY CODE
-	if (true) return new PrinterData[0];
-	
-	/* Connect to the default X print server */
-	/* AW
-	byte [] buffer = Converter.wcsToMbcs(null, XDefaultPrintServer, true);
-	int xtContext = OS.XtCreateApplicationContext ();
-	int pdpy =  OS.XtOpenDisplay (xtContext, buffer, null, null, 0, 0, new int [] {0}, 0);
-	if (pdpy == 0) {
-		// no print server
-		return new PrinterData[0];
-	}
-	*/
-
-	/* Get the list of printers */
-	/* AW
-	int [] listCount = new int[1];
-	int plist = OS.XpGetPrinterList(pdpy, null, listCount);
-	int printerCount = listCount[0];
-	if (plist == 0 || printerCount == 0) {
-		// no printers
-		//OS.XCloseDisplay(pdpy);
-		return new PrinterData[0];
-	}
-	*/
-    
-	/* Copy the printer names into PrinterData objects */
-	/* AW
-	int [] stringPointers = new int [printerCount * 2];
-	OS.memmove(stringPointers, plist, printerCount * 2 * 4);
-	PrinterData printerList[] = new PrinterData[printerCount];
-	for (int i = 0; i < printerCount; i++) {
-		String name = "";
-		int address = stringPointers[i * 2];
-		if (address != 0) {
-			int length = OS.strlen(address);
-			buffer = new byte [length];
-			OS.memmove(buffer, address, length);
-			// Use the character encoding for the default locale
-			name = new String(Converter.mbcsToWcs(null, buffer));
-		}
-		printerList[i] = new PrinterData(XDefaultPrintServer, name);
-	}
-	OS.XpFreePrinterList(plist);
-	OS.XtDestroyApplicationContext (xtContext);
+	PrinterData printerList[] = new PrinterData[0];
 	return printerList;
-	*/
-	return null;
 }
 
 /**
@@ -144,84 +92,6 @@ public Printer(PrinterData data) {
 	super(data);
 }
 
-protected void create(DeviceData deviceData) {
-	data = (PrinterData)deviceData;
-
-	/* Open the display for the X print server */	
-	String display_name = null;
-	String application_name = APP_NAME;
-	String application_class = APP_NAME;
-	if (data != null) {
-//		if (data.display_name != null) display_name = data.display_name;
-		if (data.driver != null) display_name = data.driver;
-	}
-	/* Use the character encoding for the default locale */
-	byte [] displayName = null, appName = null, appClass = null;
-	/* AW
-	if (display_name != null) displayName = Converter.wcsToMbcs (null, display_name, true);
-	if (application_name != null) appName = Converter.wcsToMbcs (null, application_name, true);
-	if (application_class != null) appClass = Converter.wcsToMbcs (null, application_class, true);
-	
-	xtContext = OS.XtCreateApplicationContext ();
-	xDisplay =  OS.XtOpenDisplay (xtContext, displayName, appName, appClass, 0, 0, new int [] {0}, 0);
-	if (xDisplay == 0) {
-	*/
-		/* no print server */
-		/* AW
-		SWT.error(SWT.ERROR_NO_HANDLES);
-	}
-	*/
-}
-
-protected void init() {
-	super.init();
-	
-	/* Create the printContext for the printer */
-	/* Use the character encoding for the default locale */
-	/* AW
-	byte[] name = Converter.wcsToMbcs(null, data.name, true);
-	printContext = OS.XpCreateContext(xDisplay, name);
-	if (printContext == OS.None) {
-		// can't create print context
-		//OS.XCloseDisplay(xDisplay);
-		SWT.error(SWT.ERROR_NO_HANDLES);
-	}
-	*/
-
-	/* Set the printContext into the display */
-	/* AW
-	OS.XpSetContext(xDisplay, printContext); 
-	*/
-
-	/* Get the printer's screen */
-	/* AW
-	xScreen = OS.XpGetScreenOfContext(xDisplay, printContext);
-	*/
-	
-	/* Initialize Motif */
-	/* AW
-	int widgetClass = OS.TopLevelShellWidgetClass();
-	int shellHandle = OS.XtAppCreateShell(null, null, widgetClass, xDisplay, null, 0);
-	OS.XtDestroyWidget(shellHandle);
-	*/
-	
-	/* Initialize the default font */
-	/* Use the character encoding for the default locale */
-	/* AW
-	byte [] buffer = Converter.wcsToMbcs(null, "-*-courier-medium-r-*-*-*-120-*-*-*-*-*-*", true);
-	int fontListEntry = OS.XmFontListEntryLoad(xDisplay, buffer, OS.XmFONT_IS_FONTSET, OS.XmFONTLIST_DEFAULT_TAG);
-	if (fontListEntry == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-	int defaultFontList = OS.XmFontListAppendEntry(0, fontListEntry);
-	OS.XmFontListEntryFree(new int[]{fontListEntry});
-	defaultFont = Font.motif_new(this, defaultFontList);
-	*/
-}
-
-protected void destroy() {
-	/* AW
-	if (xtContext != 0) OS.XtDestroyApplicationContext (xtContext);
-	*/
-}
 
 /**	 
  * Invokes platform specific functionality to allocate a new GC handle.
@@ -239,20 +109,6 @@ protected void destroy() {
  * @private
  */
 public int internal_new_GC(GCData data) {
-	/* AW
-	if (xDrawable == 0) SWT.error(SWT.ERROR_INVALID_ARGUMENT);
-	int xGC = OS.XCreateGC(xDisplay, xDrawable, 0, null);
-	if (xGC == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-	if (data != null) {
-		data.device = this;
-		data.display = xDisplay;
-		data.drawable = xDrawable; // not valid until after startJob
-		data.fontList = defaultFont.handle;
-		data.codePage = defaultFont.codePage;
-		data.colormap = OS.XDefaultColormapOfScreen(xScreen);
-	}
-	return xGC;
-	*/
 	return 0;
 }
 
@@ -272,9 +128,6 @@ public int internal_new_GC(GCData data) {
  * @private
  */
 public void internal_dispose_GC(int xGC, GCData data) {
-	/* AW
-	OS.XFreeGC(xDisplay, xGC);
-	*/
 }
 
 /**
@@ -298,24 +151,6 @@ public void internal_dispose_GC(int xGC, GCData data) {
  * @see #endJob
  */
 public boolean startJob(String jobName) {
-	checkDevice();
-	/* Use the character encoding for the default locale */
-	/* AW
-	byte [] buffer = Converter.wcsToMbcs(null, "*job-name: " + jobName, true);
-	OS.XpSetAttributes(xDisplay, printContext, OS.XPJobAttr, buffer, OS.XPAttrMerge);
-	OS.XpStartJob(xDisplay, OS.XPSpool);
-	*/
-
-	/* Create the xDrawable */
-	/* AW
-	XRectangle rect = new XRectangle();
-	short [] width = new short [1];
-	short [] height = new short [1];
-	OS.XpGetPageDimensions(xDisplay, printContext, width, height, rect);
-	xDrawable = OS.XCreateWindow(xDisplay, OS.XRootWindowOfScreen(xScreen), 
-		0, 0, rect.width, rect.height, 0,
-		OS.CopyFromParent, OS.CopyFromParent, OS.CopyFromParent, 0, 0);
-	*/
 	return true;
 }
 
@@ -331,11 +166,6 @@ public boolean startJob(String jobName) {
  * @see #endPage
  */
 public void endJob() {
-	checkDevice();
-	/* AW
-	OS.XpEndJob(xDisplay);
-	OS.XFlush(xDisplay);
-	*/
 }
 
 /**
@@ -346,10 +176,6 @@ public void endJob() {
  * </ul>
  */
 public void cancelJob() {
-	checkDevice();
-	/* AW
-	OS.XpCancelJob(xDisplay, true);
-	*/
 }
 
 /**
@@ -371,10 +197,6 @@ public void cancelJob() {
  * @see #endJob
  */
 public boolean startPage() {
-	checkDevice();
-	/* AW
-	OS.XpStartPage(xDisplay, xDrawable);
-	*/
 	return true;
 }
 
@@ -390,10 +212,6 @@ public boolean startPage() {
  * @see #endJob
  */
 public void endPage() {
-	checkDevice();
-	/* AW
-	OS.XpEndPage(xDisplay);
-	*/
 }
 
 /**
@@ -408,40 +226,7 @@ public void endPage() {
  * </ul>
  */
 public Point getDPI() {
-	checkDevice();
-	/* Use the character encoding for the default locale */
-	/* AW
-	byte [] buffer = Converter.wcsToMbcs(null, "default-printer-resolution", true);
-	int pool = OS.XpGetOneAttribute(xDisplay, printContext, OS.XPDocAttr, buffer);
-    int length = OS.strlen(pool);
-	buffer = new byte[length];
-	OS.memmove(buffer, pool, length);
-	OS.XtFree(pool);
-	String resolution = new String(buffer, 0, buffer.length);
-	int res = 300; // default
-	if (resolution.length() == 0) {
-	*/
-		/* If we can't get the info from the DocAttrs, ask the printer. */
-		/* Use the character encoding for the default locale */
-		/* AW
-		buffer = Converter.wcsToMbcs(null, "printer-resolutions-supported", true);
-		pool = OS.XpGetOneAttribute(xDisplay, printContext, OS.XPPrinterAttr, buffer);
-    		length = OS.strlen(pool);
-		buffer = new byte[length];
-		OS.memmove(buffer, pool, length);
-		OS.XtFree(pool);
-		int n = 0;
-		while (!Compatibility.isWhitespace((char)buffer[n]) && n < buffer.length) n++;
-		resolution = new String(buffer, 0, n);
-	}
-	if (resolution.length() != 0) {
-		try {
-			res = Integer.parseInt(resolution);
-		} catch (NumberFormatException ex) {}
-	}
-	return new Point(res, res);
-	*/
-	return new Point(72, 72);
+	return new Point(0, 0);
 }
 
 /**
@@ -458,15 +243,7 @@ public Point getDPI() {
  * @see #computeTrim
  */
 public Rectangle getBounds() {
-	checkDevice();
-	/* AW
-	XRectangle rect = new XRectangle();
-	short [] width = new short [1];
-	short [] height = new short [1];
-	OS.XpGetPageDimensions(xDisplay, printContext, width, height, rect);
-	return new Rectangle(0, 0, width[0], height[0]);
-	*/
-	return new Rectangle(0, 0, 600, 800);
+	return null;
 }
 
 /**
@@ -485,13 +262,7 @@ public Rectangle getBounds() {
  * @see #computeTrim
  */
 public Rectangle getClientArea() {
-	checkDevice();
-	/* AW
-	XRectangle rect = new XRectangle();
-	OS.XpGetPageDimensions(xDisplay, printContext, new short [1], new short [1], rect);
-	return new Rectangle(0, 0, rect.width, rect.height);
-	*/
-	return new Rectangle(0, 0, 600, 800);
+	return null;
 }
 
 /**
@@ -521,17 +292,7 @@ public Rectangle getClientArea() {
  * @see #getClientArea
  */
 public Rectangle computeTrim(int x, int y, int width, int height) {
-	checkDevice();
-	/* AW
-	XRectangle rect = new XRectangle();
-	short [] paperWidth = new short [1];
-	short [] paperHeight = new short [1];
-	OS.XpGetPageDimensions(xDisplay, printContext, paperWidth, paperHeight, rect);
-	int hTrim = paperWidth[0] - rect.width;
-	int vTrim = paperHeight[0] - rect.height;
-	return new Rectangle(x - rect.x, y - rect.y, width + hTrim, height + vTrim);
-	*/
-	return new Rectangle(x, y, width, height);
+	return new Rectangle(0,0,0,0);
 }
 
 /**
@@ -542,55 +303,6 @@ public Rectangle computeTrim(int x, int y, int width, int height) {
  */
 public PrinterData getPrinterData() {
 	return data;
-}
-
-/**
- * Returns a reasonable font for applications to use.
- * On some platforms, this will match the "default font"
- * or "system font" if such can be found.  This font
- * should not be free'd because it was allocated by the
- * system, not the application.
- * <p>
- * Typically, applications which want the default look
- * should simply not set the font on the widgets they
- * create. Widgets are always created with the correct
- * default font for the class of user-interface component
- * they represent.
- * </p>
- *
- * @return a font
- *
- * @exception SWTException <ul>
- *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
- * </ul>
- */
-public Font getSystemFont () {
-	checkDevice ();
-	return defaultFont;
-}
-
-protected void release() {
-	super.release();
-	if (defaultFont != null) {
-		/* AW
-		OS.XmFontListFree(defaultFont.handle);
-		*/
-		defaultFont = null;
-	}
-	if (printContext != 0) {
-		/* AW
-		OS.XpDestroyContext(xDisplay, printContext);
-		*/
-		printContext = 0;
-	}
-	if (xDrawable != 0) {
-		/* AW
-		OS.XDestroyWindow(xDisplay, xDrawable);
-		*/
-		xDrawable = 0;
-	}
-	xScreen = 0;
-	data = null;
 }
 
 }
