0379926df0ca2e34cb09e797cca0032957ed66b5
Steve Northover <steve>
2003-03-04 02:07:29 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
@@ -729,8 +729,7 @@ void updateAccelerator (int accelGroup, boolean add) {
 	if ((accelerator & SWT.ALT) != 0) mask |= OS.GDK_MOD1_MASK;
 	if ((accelerator & SWT.SHIFT) != 0) mask |= OS.GDK_SHIFT_MASK;
 	if ((accelerator & SWT.CONTROL) != 0) mask |= OS.GDK_CONTROL_MASK;
-	int modifiers = SWT.ALT | SWT.SHIFT | SWT.CONTROL;
-	int keysym = accelerator & ~modifiers;
+	int keysym = accelerator & SWT.KEY_MASK;
 	int newKey = Display.untranslateKey (keysym);
 	if (newKey != 0) {
 		keysym = newKey;
