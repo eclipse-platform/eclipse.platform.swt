281dc95420976f0ffb0cbab0dc7e5e4a8f3ac547
Veronika Irvine <veronika>
2003-03-05 15:00:14 +0000

    Converting Binary files to ASCII

--- bundles/org.eclipse.swt/Eclipse SWT/emulated/bidi/org/eclipse/swt/internal/BidiUtil.java
+++ bundles/org.eclipse.swt/Eclipse SWT/emulated/bidi/org/eclipse/swt/internal/BidiUtil.java
@@ -1,7 +1,7 @@
 package org.eclipse.swt.internal;/* * Copyright (c) 2001, 2002 IBM Corp.  All rights reserved.
  * This file is made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html */
+ * http://www.eclipse.org/legal/cpl-v10.html */
 import org.eclipse.swt.graphics.GC;
 
 /*
