f402a70e67234139ca45b4138051ff63f1d11867
Grant Gayed <ggayed>
2003-03-07 22:58:32 +0000

    add custom controls example to view group and example launcher

--- examples/org.eclipse.swt.examples.controls/src/org/eclipse/swt/examples/controls/CustomControlView.java
+++ examples/org.eclipse.swt.examples.controls/src/org/eclipse/swt/examples/controls/CustomControlView.java
@@ -6,46 +6,26 @@ package org.eclipse.swt.examples.controls;
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/cpl-v10.html
  */
- 
+
 import org.eclipse.swt.examples.controlexample.*;
 import org.eclipse.swt.widgets.*;
-import org.eclipse.ui.part.*;
 
 /**
- * <code>Controls</code> is a simple demonstration
- * of the native controls defined by SWT.  It consists of a workbench
+ * <code>CustomControls</code> is a simple demonstration
+ * of the custom controls defined by SWT.  It consists of a workbench
  * view and tab folder where each tab in the folder allows the
  * user to interact with a control.
  * 
  * @see ViewPart
  */
-public class ControlView extends ViewPart {
-	ControlExample instance = null;
-	
+public class CustomControlView extends ControlView {
+
 	/**
 	 * Create the example
 	 * 
 	 * @see ViewPart#createPartControl
 	 */
 	public void createPartControl(Composite frame) {
-		instance = new ControlExample(frame);
-	}
-
-	/**
-	 * Called when we must grab focus.
-	 * 
-	 * @see org.eclipse.ui.part.ViewPart#setFocus
-	 */
-	public void setFocus() {
-		instance.setFocus();
-	}
-
-	/**
-	 * Called when the View is to be disposed
-	 */	
-	public void dispose() {
-		instance.dispose();
-		instance = null;
-		super.dispose();
+		instance = new CustomControlExample(frame);
 	}
 }
