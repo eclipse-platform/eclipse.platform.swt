10e89b007d4123860fa03d0b764caa57bd2d8056
Grant Gayed <ggayed>
2003-03-07 21:40:35 +0000

    ensure SINGLE or MULTI style

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -1970,6 +1970,8 @@ void calculateTopIndex() {
 static int checkStyle(int style) {
 	if ((style & SWT.SINGLE) != 0) {
 		style &= ~(SWT.H_SCROLL | SWT.V_SCROLL);
+	} else {
+		style |= SWT.MULTI;
 	}
 	return style;
 }
