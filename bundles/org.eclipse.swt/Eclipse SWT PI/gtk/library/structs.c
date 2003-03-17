f92aec5deb4a565ea89e04280ed631a305c98831
Grant Gayed <ggayed>
2003-03-17 18:36:40 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/structs.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/structs.c
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) 2000, 2002 IBM Corp.  All rights reserved.
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others. All rights reserved.
  * The contents of this file are made available under the terms
  * of the GNU Lesser General Public License (LGPL) Version 2.1 that
  * accompanies this distribution (lgpl-v21.txt).  The LGPL is also
@@ -8,7 +8,10 @@
  * the LGPL accompanying this distribution and there is any conflict
  * between the two license versions, the terms of the LGPL accompanying
  * this distribution shall govern.
- */
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 
 #include "swt.h"
 #include "structs.h"
