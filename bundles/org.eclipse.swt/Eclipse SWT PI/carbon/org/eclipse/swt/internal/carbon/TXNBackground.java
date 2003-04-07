528f5173820ff84255a72997051dcf5d15609899
cvs2svn <platform-releng-dev@eclipse.org>
2003-04-07 22:02:08 +0000

    This commit was manufactured by cvs2svn to create branch 'R2_1_maintenance'.
    Sprout from master 2003-03-27 23:23:39 UTC Dean Roberts <droberts> 'Javadoc correction'
    Cherrypick from master 2003-04-07 22:02:07 UTC Silenio Quarti <silenio> '*** empty log message ***':
        bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/TXNBackground.java

---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/TXNBackground.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.internal.carbon;
+
+ 
+public class TXNBackground {
+	public int bgType;
+	public short bg_red;
+	public short bg_green;
+	public short bg_blue;
+	public static final int sizof = 10;
+}
