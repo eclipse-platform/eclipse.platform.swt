c36b29867250cba7303051a4b2fe5242a18b46b8
Felipe Heidrich <fheidric>
2003-05-05 20:11:55 +0000

    35319

--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Composite.java
@@ -536,7 +536,14 @@ void setBackgroundPixel (int pixel) {
 }
 boolean setBounds (int x, int y, int width, int height, boolean move, boolean resize) {
 	boolean changed = super.setBounds (x, y, width, height, move, resize);
-	if (changed && resize && layout != null) layout.layout (this, false);
+	if (changed && resize) {
+		if (focusHandle != 0) {
+			int [] argList = {OS.XmNwidth, 0, OS.XmNheight, 0};
+			OS.XtGetValues (handle, argList, argList.length / 2);
+			OS.XtConfigureWidget (focusHandle, 0, 0, argList [1], argList [3], 0);
+		}
+		if (layout != null) layout.layout (this, false);
+	}
 	return changed;
 }
 public boolean setFocus () {
