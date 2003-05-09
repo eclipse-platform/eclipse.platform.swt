3d97e92d9e7b8828b1459decce65b80b54db7894
Grant Gayed <ggayed>
2003-05-09 20:27:23 +0000

    font set -> font list

--- bundles/org.eclipse.swt/Eclipse SWT/gtk1x/org/eclipse/swt/widgets/FontDialog.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk1x/org/eclipse/swt/widgets/FontDialog.java
@@ -99,7 +99,7 @@ int destroyFunc (int widget, int colorInfo) {
  * selected in the dialog, or null if none is available.
  * 
  * @return the FontData for the selected font, or null
- * @deprecated use #getFontSet ()
+ * @deprecated use #getFontList ()
  */
 public FontData getFontData () {
 	return fontData;
@@ -111,7 +111,7 @@ public FontData getFontData () {
  * @return the FontData for the selected font, or null
  * @since 2.1.1
  */
-public FontData [] getFontSet () {
+public FontData [] getFontList () {
 	if (fontData == null) return null;
 	FontData [] result = new FontData [1];
 	result [0] = fontData;
@@ -179,7 +179,7 @@ public FontData open () {
  * the platform choose one.
  * 
  * @param fontData the FontData to use initially, or null
- * @deprecated use #setFontSet (FontData [])
+ * @deprecated use #setFontList (FontData [])
  */
 public void setFontData (FontData fontData) {
 	this.fontData = fontData;
@@ -192,7 +192,7 @@ public void setFontData (FontData fontData) {
  * @param fontData the set of FontData objects to use initially, or null
  * @since 2.1.1
  */
-public void setFontSet (FontData [] fontData) {
+public void setFontList (FontData [] fontData) {
 	if (fontData != null && fontData.length > 0) {
 		this.fontData = fontData [0];
 	} else {
