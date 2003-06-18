2c96739276442dd5c9c1c7f119e7c6c4774d2c1b
Silenio Quarti <silenio>
2003-06-18 22:00:41 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/motif/library/swt_custom.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/motif/library/swt_custom.c
@@ -14,8 +14,9 @@
 
 #define OS_NATIVE(func) Java_org_eclipse_swt_internal_motif_OS_##func
 
-JNIEXPORT jint JNICALL Java_org_eclipse_swt_internal_motif_OS_Call
-  (JNIEnv *env, jclass that, jint proc, jint arg0, jint arg1)
+#ifndef NO_Call
+JNIEXPORT jint JNICALL OS_NATIVE(Call)
+	(JNIEnv *env, jclass that, jint proc, jint arg0, jint arg1)
 {
 	jint rc;
 	NATIVE_ENTER(env, that, "Call\n")
@@ -23,9 +24,11 @@ JNIEXPORT jint JNICALL Java_org_eclipse_swt_internal_motif_OS_Call
 	NATIVE_EXIT(env, that, "Call\n")
 	return rc;
 }
+#endif
 
-JNIEXPORT jint JNICALL Java_org_eclipse_swt_internal_motif_OS_fd_1set_1sizeof
-  (JNIEnv *env, jclass that)
+#ifndef NO_fd_1set_1sizeof
+JNIEXPORT jint JNICALL OS_NATIVE(fd_1set_1sizeof)
+	(JNIEnv *env, jclass that)
 {
 	jint rc;
 	NATIVE_ENTER(env, that, "fd_1set_1sizeof\n")
@@ -33,66 +36,72 @@ JNIEXPORT jint JNICALL Java_org_eclipse_swt_internal_motif_OS_fd_1set_1sizeof
 	NATIVE_EXIT(env, that, "fd_1set_1sizeof\n")
 	return rc;
 }
+#endif
 
 static int RESOURCES_START;
 static int RESOURCES_END;
 
-JNIEXPORT void JNICALL Java_org_eclipse_swt_internal_motif_OS_setResourceMem
+#ifndef NO_fd_1set_1sizeof
+JNIEXPORT void JNICALL OS_NATIVE(setResourceMem)
   (JNIEnv *env, jclass that, jint start, jint end)
 {
     RESOURCES_START = start;
     RESOURCES_END = end;
 }
+#endif
 
+#ifndef NO_XtGetValues
 #define MAX_ARGS 32
 JNIEXPORT void JNICALL OS_NATIVE(XtGetValues)
   (JNIEnv *env, jclass that, jint widget, jintArray argList, jint numArgs)
 {
-    jint *argList1=NULL;
+	jint *argList1=NULL;
 
-    int valueBuff[MAX_ARGS];
-    int zeroBuff[MAX_ARGS];
-    int *values = valueBuff;
-    int *zeros = zeroBuff;
-    int i;
+#ifndef SOLARIS
+	int valueBuff[MAX_ARGS];
+	int zeroBuff[MAX_ARGS];
+	int *values = valueBuff;
+	int *zeros = zeroBuff;
+	int i;
+#endif
 
 	NATIVE_ENTER(env, that, "XtGetValues\n")
-    if (argList) argList1 = (*env)->GetIntArrayElements(env, argList, NULL);    
+	if (argList) argList1 = (*env)->GetIntArrayElements(env, argList, NULL);
 
 #ifndef SOLARIS
 	if (numArgs > MAX_ARGS) {
 		values = (int *) XtMalloc (numArgs * sizeof(int));
 		zeros = (int *) XtMalloc (numArgs * sizeof(int));
 	}
-    for (i = 0; i < numArgs; i++) {   
-        zeros[i] = values[i] = 0;
-        if (argList1[i * 2 + 1] == 0) {
-        	if ((RESOURCES_START <= argList1[i*2]) && (argList1[i*2] <= RESOURCES_END)) {
-	            zeros[i] = 1;
-    	        argList1[i * 2 + 1] = (int)&values[i];
-            }
-        }
-    }
+	for (i = 0; i < numArgs; i++) {   
+		zeros[i] = values[i] = 0;
+		if (argList1[i * 2 + 1] == 0) {
+			if ((RESOURCES_START <= argList1[i*2]) && (argList1[i*2] <= RESOURCES_END)) {
+				zeros[i] = 1;
+				argList1[i * 2 + 1] = (int)&values[i];
+			}
+		}
+	}
 #endif
-    XtGetValues((Widget)widget, (ArgList)argList1, numArgs);
+	XtGetValues((Widget)widget, (ArgList)argList1, numArgs);
 #ifndef SOLARIS
-    for (i = 0; i < numArgs; i++) {   
-        if (zeros[i]) {
-           char* charPtr = (char *)(argList1[i*2] - 1);
-           switch ((int)*charPtr) {
-              case 1: argList1[i * 2 + 1] = *(char *)(&values[i]); break;
-              case 2: argList1[i * 2 + 1] = *(short *)(&values[i]); break;
-              default:
-                 argList1[i * 2 + 1] = values[i];
-           }
-        }
-    }
+	for (i = 0; i < numArgs; i++) {   
+		if (zeros[i]) {
+			char* charPtr = (char *)(argList1[i*2] - 1);
+			switch ((int)*charPtr) {
+				case 1: argList1[i * 2 + 1] = *(char *)(&values[i]); break;
+				case 2: argList1[i * 2 + 1] = *(short *)(&values[i]); break;
+				default:
+					argList1[i * 2 + 1] = values[i];
+			}
+		}
+	}
 	if (numArgs > MAX_ARGS) {
 		XtFree((char *)values);
 		XtFree((char *)zeros);
 	}
 #endif
-
-    if (argList)(*env)->ReleaseIntArrayElements(env, argList, argList1, 0);
+	if (argList)(*env)->ReleaseIntArrayElements(env, argList, argList1, 0);
 	NATIVE_EXIT(env, that, "XtGetValues\n")
 }
+#endif
