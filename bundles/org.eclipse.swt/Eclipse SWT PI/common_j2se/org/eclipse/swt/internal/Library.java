21c5f4c920e53e781479fd6dc41b8c91057beeda
Felipe Heidrich <fheidric>
2003-06-23 21:49:04 +0000

    v3010

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -23,7 +23,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 9;
+	static int MINOR_VERSION = 010;
 	
 	/**
 	 * SWT revision number (must be >= 0)
