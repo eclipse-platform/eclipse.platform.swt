16213b00a222cdf4cbe02d0b56253ce946283de0
Veronika Irvine <veronika>
2003-07-15 21:01:40 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
@@ -284,10 +284,6 @@ public Rectangle getImageBounds (int index) {
 	int path = OS.gtk_tree_model_get_path (parent.modelHandle, handle);
 	OS.gtk_widget_realize (parentHandle);
 	OS.gtk_tree_view_get_cell_area (parentHandle, path, column, rect);
-	int iter = OS.g_malloc (OS.GtkTreeIter_sizeof ());
-	OS.gtk_tree_model_get_iter (parent.modelHandle, iter, path);
-	OS.gtk_tree_view_column_cell_set_cell_data (column, parent.modelHandle, iter, false, false);
-	OS.g_free (iter);
 	OS.gtk_tree_path_free (path);
 	/*
 	* The OS call gtk_cell_renderer_get_size provides the width of image to be drawn
@@ -299,6 +295,7 @@ public Rectangle getImageBounds (int index) {
 	* width.
 	*/
 	int [] w = new int[1];
+	OS.gtk_tree_view_column_cell_set_cell_data (column, parent.modelHandle, handle, false, false);
 	if (OS.gtk_major_version () * 100 + OS.gtk_minor_version ()  * 10 + OS.gtk_micro_version () < 213) {
 		OS.gtk_cell_renderer_get_size (pixbufRenderer, parentHandle, null, null, null, w, null);
 	} else {
