b793a454a5222757c1ab07d7ee6fab3591e468b1
Felipe Heidrich <fheidric>
2003-07-17 19:16:37 +0000

    39965 & 40329 (menu problem)

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
@@ -2073,10 +2073,13 @@ void releaseWidget () {
 		OS.gtk_im_context_reset (imHandle);
 		OS.gtk_im_context_set_client_window (imHandle, 0);
 	}
+	if (menu != null && !menu.isDisposed ()) {
+		menu.dispose ();
+	}
+	menu = null;
 	cursor = null;
 	toolTipText = null;
 	parent = null;
-	menu = null;
 	layoutData = null;
 }
 
