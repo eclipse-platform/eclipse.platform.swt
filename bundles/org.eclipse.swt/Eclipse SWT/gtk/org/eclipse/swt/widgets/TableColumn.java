28ab2bbfe176f7754a98cdfdd53774ea918c9cd6
Felipe Heidrich <fheidric>
2003-08-18 20:36:55 +0000

    41455

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableColumn.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableColumn.java
@@ -499,7 +499,7 @@ public void setText (String string) {
 public void setWidth (int width) {
 	checkWidget();
 	OS.gtk_tree_view_column_set_sizing (handle, OS.GTK_TREE_VIEW_COLUMN_FIXED);
-	OS.gtk_tree_view_column_set_fixed_width (handle, width);
+	OS.gtk_tree_view_column_set_fixed_width (handle, Math.max (1, width));
 }
 
 }
