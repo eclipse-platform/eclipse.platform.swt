e9a1477165ad64d557ee73753a6f703c8ce2dfc8
Silenio Quarti <silenio>
2003-09-03 16:21:47 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Device.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Device.java
@@ -230,18 +230,19 @@ void dispose_Object (Object object) {
 
 int EnumFontFamProc (int lpelfe, int lpntme, int FontType, int lParam) {
 	boolean isScalable = (FontType & OS.RASTER_FONTTYPE) == 0;
-	if ((lParam == 1) != isScalable) return 1;
-	
-	/* Add the log font to the list of log fonts */
-	if (nFonts == logFonts.length) {
-		LOGFONT [] newLogFonts = new LOGFONT [logFonts.length + 128];
-		System.arraycopy (logFonts, 0, newLogFonts, 0, nFonts);
-		logFonts = newLogFonts;
+	boolean scalable = lParam == 1;
+	if (isScalable == scalable) {
+		/* Add the log font to the list of log fonts */
+		if (nFonts == logFonts.length) {
+			LOGFONT [] newLogFonts = new LOGFONT [logFonts.length + 128];
+			System.arraycopy (logFonts, 0, newLogFonts, 0, nFonts);
+			logFonts = newLogFonts;
+		}
+		LOGFONT logFont = logFonts [nFonts];
+		if (logFont == null) logFont = OS.IsUnicode ? (LOGFONT)new LOGFONTW () : new LOGFONTA ();
+		OS.MoveMemory (logFont, lpelfe, LOGFONT.sizeof);
+		logFonts [nFonts++] = logFont;
 	}
-	LOGFONT logFont = logFonts [nFonts];
-	if (logFont == null) logFont = OS.IsUnicode ? (LOGFONT)new LOGFONTW () : new LOGFONTA ();
-	OS.MoveMemory (logFont, lpelfe, LOGFONT.sizeof);
-	logFonts [nFonts++] = logFont;
 	return 1;
 }
 
