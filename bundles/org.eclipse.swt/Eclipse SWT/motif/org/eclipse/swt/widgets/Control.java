416a08179927121b7062d0bd809db8ded7abe984
Steve Northover <steve>
2003-09-15 22:52:49 +0000

    41962

--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/widgets/Control.java
@@ -2591,7 +2591,7 @@ int traversalCode (int key, XKeyEvent xEvent) {
 	int [] argList = new int [] {OS.XmNtraversalOn, 0};
 	OS.XtGetValues (handle, argList, argList.length / 2);
 	if (argList [1] == 0) return 0;
-	int code = SWT.TRAVERSE_RETURN | SWT.TRAVERSE_TAB_NEXT | SWT.TRAVERSE_TAB_PREVIOUS;
+	int code = SWT.TRAVERSE_RETURN | SWT.TRAVERSE_TAB_NEXT | SWT.TRAVERSE_TAB_PREVIOUS | SWT.TRAVERSE_PAGE_NEXT | SWT.TRAVERSE_PAGE_PREVIOUS;
 	Shell shell = getShell ();
 	if (shell.parent != null) code |= SWT.TRAVERSE_ESCAPE;
 	if (getNavigationType () == OS.XmNONE) {
