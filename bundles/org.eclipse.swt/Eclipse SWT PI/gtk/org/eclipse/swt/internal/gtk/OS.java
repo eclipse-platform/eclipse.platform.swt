aecc38a38bb9dd630cb775493cd7de4bbca7a3c3
Grant Gayed <ggayed>
2003-10-01 19:19:29 +0000

    for text interface

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/OS.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/OS.java
@@ -287,7 +287,7 @@ public class OS {
 	public static final byte[] mode = signal("mode");
 	public static final byte[] xalign = signal("xalign");
 	
-static byte [] signal (String name) {
+protected static byte [] signal (String name) {
 	int length = name.length ();
 	char [] chars = new char [length];
 	name.getChars (0, length, chars, 0);
@@ -365,7 +365,9 @@ public static final synchronized native void g_object_unref(int object);
 public static final synchronized native int g_quark_from_string(byte[] string);
 public static final synchronized native int g_signal_connect(int instance, byte[] detailed_signal, int proc, int data);
 public static final synchronized native int g_signal_connect_after(int instance, byte[] detailed_signal, int proc, int data);
+public static final synchronized native void g_signal_emit_by_name(int instance, byte[] detailed_signal);
 public static final synchronized native void g_signal_emit_by_name(int instance, byte[] detailed_signal, int data);
+public static final synchronized native void g_signal_emit_by_name(int instance, byte[] detailed_signal, int data1, int data2);
 public static final synchronized native void g_signal_emit_by_name(int instance, byte[] detailed_signal, byte [] data);
 public static final synchronized native void g_signal_handler_disconnect(int instance, int handler_id);
 public static final synchronized native int g_signal_handlers_block_matched(int instance, int mask, int signal_id, int detail, int closure, int func, int data);
