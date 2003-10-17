eb6ee159904f9241e05b025f0bc502b5451c0558
Christophe Cornu <ccornu>
2003-10-17 16:27:41 +0000

    Browser test suite

---
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllBrowserTests.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.tests.junit;
+
+
+import junit.framework.*;
+import junit.textui.*;
+
+/**
+ * Suite for testing all of the graphics test cases.
+ */
+public class AllBrowserTests {
+public static void main(String[] args) {
+	TestRunner.run (suite());
+}
+public static Test suite() {
+	TestSuite suite = new TestSuite();
+
+	suite.addTest(Test_org_eclipse_swt_browser_Browser.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_LocationListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_ProgressAdapter.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_ProgressListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_StatusTextListener.suite());
+
+	return suite;
+}
+}
