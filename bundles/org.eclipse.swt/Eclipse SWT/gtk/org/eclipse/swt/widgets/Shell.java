6e424e9e28bf71e917559ecb03b86644fa3aa7e6
Silenio Quarti <silenio>
2003-10-31 22:49:36 +0000

    24319

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -508,9 +508,10 @@ void createHandle (int index) {
 		OS.gtk_style_get_black (OS.gtk_widget_get_style (shellHandle), color);
 		OS.gtk_widget_modify_bg (shellHandle, 0, color);
 	}
-	//TEMPORARY CODE
 	int bits = SWT.PRIMARY_MODAL | SWT.APPLICATION_MODAL | SWT.SYSTEM_MODAL;
-	boolean modal = (style & bits) != 0 || (parent != null && (parent.style & bits) != 0);
+	boolean modal = (style & bits) != 0;
+	//TEMPORARY CODE
+	if ((style & SWT.ON_TOP) == 0) modal |= (parent != null && (parent.style & bits) != 0);
 	OS.gtk_window_set_modal (shellHandle, modal);
 }
 
