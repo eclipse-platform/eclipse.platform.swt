00552d95fb7af76ce43e0364cfd2da48ec008236
Steve Northover <steve>
2003-11-06 21:29:32 +0000

    26095

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
@@ -826,6 +826,31 @@ LRESULT WM_PAINT (int wParam, int lParam) {
 	return LRESULT.ZERO;
 }
 
+LRESULT WM_PRINTCLIENT (int wParam, int lParam) {
+	LRESULT result = super.WM_PRINTCLIENT (wParam, lParam);
+	if (result != null) return result;
+	if ((state & CANVAS) != 0) {
+		RECT rect = new RECT ();
+		OS.GetClientRect (handle, rect);
+		if ((style & SWT.NO_BACKGROUND) == 0) {
+			drawBackground (wParam, rect);
+		}
+		GCData data = new GCData ();
+		data.device = display;
+		GC gc = GC.win32_new (wParam, data);
+		Event event = new Event ();
+		event.gc = gc;
+		event.x = rect.left;
+		event.y = rect.top;
+		event.width = rect.right - rect.left;
+		event.height = rect.bottom - rect.top;
+		sendEvent (SWT.Paint, event);
+		event.gc = null;
+		gc.dispose ();
+	}
+	return result;
+}
+
 LRESULT WM_SETFONT (int wParam, int lParam) {
 	return super.WM_SETFONT (font = wParam, lParam);
 }
