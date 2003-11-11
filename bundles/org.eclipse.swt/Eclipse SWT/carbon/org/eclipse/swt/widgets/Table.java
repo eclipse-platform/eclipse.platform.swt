1d777d46a95b487e88a7364257da6ecfbe3e45c0
Silenio Quarti <silenio>
2003-11-11 16:50:04 +0000

    46368 remove(int[])

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Table.java
@@ -1427,15 +1427,23 @@ public void remove (int start, int end) {
  * </ul>
  */
 public void remove (int [] indices) {
+	checkWidget ();
 	if (indices == null) error (SWT.ERROR_NULL_ARGUMENT);
+	if (indices.length == 0) return;
 	int [] newIndices = new int [indices.length];
 	System.arraycopy (indices, 0, newIndices, 0, indices.length);
 	sort (newIndices);
+	int start = newIndices [newIndices.length - 1], end = newIndices [0];
+	if (!(0 <= start && start <= end && start < itemCount)) {
+		error (SWT.ERROR_INVALID_RANGE);
+	}
 	int last = -1;
 	for (int i=0; i<newIndices.length; i++) {
 		int index = newIndices [i];
-		if (index != last || i == 0) remove (index);
-		last = index;
+		if (index != last) {
+			remove (index);
+			last = index;
+		}
 	}
 }
 
