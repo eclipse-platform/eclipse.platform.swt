d29ce6ba4b401d4d2aa1f89d2e8b28c039f4a5f8
Silenio Quarti <silenio>
2003-11-11 17:32:38 +0000

    46368 remove(int[])

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -1418,17 +1418,20 @@ public void remove (int start, int end) {
 public void remove (int [] indices) {
 	checkWidget();
 	if (indices == null) error (SWT.ERROR_NULL_ARGUMENT);
+	if (indices.length == 0) return;
 	int [] newIndices = new int [indices.length];
 	System.arraycopy (indices, 0, newIndices, 0, indices.length);
 	sort (newIndices);
+	int start = newIndices [newIndices.length - 1], end = newIndices [0];
+	if (!(0 <= start && start <= end && end < itemCount)) {
+		error (SWT.ERROR_INVALID_RANGE);
+	}
 	int last = -1;
 	for (int i=0; i<newIndices.length; i++) {
 		int index = newIndices [i];
-		if (index != last || i == 0) {
-			if (index < 0 || index >= itemCount) error (SWT.ERROR_INVALID_RANGE);
+		if (index != last) {
 			TableItem item = items [index];
 			OS.gtk_list_store_remove (modelHandle, item.handle);
-			// BUG - disposed callback could remove an item
 			item.releaseResources ();
 			System.arraycopy (items, index + 1, items, index, --itemCount - index);
 			items [itemCount] = null;
