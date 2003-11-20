669a07bb7713f15ebc278c3044d58de132af6a43
Felipe Heidrich <fheidric>
2003-11-20 18:42:46 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
@@ -851,6 +851,7 @@ public int indexOf (String string) {
 public int indexOf (String string, int start) {
 	checkWidget();
 	if (string == null) error (SWT.ERROR_NULL_ARGUMENT);
+	if (!(0 <= start && start < items.length)) return -1;
 	for (int i=start; i<items.length; i++) {
 		if (string.equals(items [i])) return i;
 	}
