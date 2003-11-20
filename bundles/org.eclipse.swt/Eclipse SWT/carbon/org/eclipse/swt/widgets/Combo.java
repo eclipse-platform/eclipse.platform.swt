7c34a0023a964b335446915b0ffd9cabef424007
Veronika Irvine <veronika>
2003-11-20 16:20:34 +0000

    make indexOf consistent

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Combo.java
@@ -842,6 +842,7 @@ public int indexOf (String string, int start) {
 	checkWidget();
 	if (string == null) error (SWT.ERROR_NULL_ARGUMENT);
 	int count = getItemCount ();
+	if (!(0 <= start && start < count)) return -1;
 	for (int i=start; i<count; i++) {
 		if (string.equals (getItem (i))) {
 			return i;
