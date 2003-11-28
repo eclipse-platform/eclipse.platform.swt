252085764269c552bf6c216a18cc48273f5e99bb
Grant Gayed <ggayed>
2003-11-28 21:04:44 +0000

    47556

--- bundles/org.eclipse.swt/Eclipse SWT Program/win32/org/eclipse/swt/program/Program.java
+++ bundles/org.eclipse.swt/Eclipse SWT Program/win32/org/eclipse/swt/program/Program.java
@@ -36,7 +36,9 @@ Program () {
 
 /**
  * Finds the program that is associated with an extension.
- * The extension may or may not begin with a '.'.
+ * The extension may or may not begin with a '.'.  Note that
+ * a <code>Display</code> must already exist to guarantee that
+ * this method returns an appropriate result.
  *
  * @param extension the program extension
  * @return the program or <code>null</code>
@@ -64,7 +66,9 @@ public static Program findProgram (String extension) {
 }
 
 /**
- * Answer all program extensions in the operating system.
+ * Answer all program extensions in the operating system.  Note
+ * that a <code>Display</code> must already exist to guarantee
+ * that this method returns an appropriate result.
  *
  * @return an array of extensions
  */
@@ -153,7 +157,9 @@ static Program getProgram (String key) {
 }
 
 /**
- * Answers all available programs in the operating system.
+ * Answers all available programs in the operating system.  Note
+ * that a <code>Display</code> must already exist to guarantee
+ * that this method returns an appropriate result.
  *
  * @return an array of programs
  */
@@ -189,7 +195,9 @@ public static Program [] getPrograms () {
 /**
  * Launches the executable associated with the file in
  * the operating system.  If the file is an executable,
- * then the executable is launched.
+ * then the executable is launched.  Note that a <code>Display</code>
+ * must already exist to guarantee that this method returns
+ * an appropriate result.
  *
  * @param fileName the file or program name
  * @return <code>true</code> if the file is launched, otherwise <code>false</code>
