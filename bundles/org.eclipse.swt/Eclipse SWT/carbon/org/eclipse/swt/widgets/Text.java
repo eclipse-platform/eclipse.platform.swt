9822fb145677732451cc3dfb92bc570fca1d2bdb
Christophe Cornu <ccornu>
2003-12-08 22:29:53 +0000

    47928

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Text.java
@@ -744,7 +744,9 @@ public String getText () {
 public String getText (int start, int end) {
 	checkWidget ();
 	int length = OS.TXNDataSize (txnObject) / 2;
-	if (!(0 <= start && start <= end && end < length)) error (SWT.ERROR_INVALID_RANGE);
+	start = Math.max (0, start);
+	end = Math.min (end, length - 1);
+	if (start > end) return "";
 	return getTXNText (start, end + 1);
 }
 
