1fca8f6a51a3a04433e10488eeac39d35191089f
Carolyn MacLeod <carolyn>
2003-12-18 23:40:40 +0000

    Upgrading tests to 3.0 API.

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_TableTree.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_TableTree.java
@@ -101,32 +101,8 @@ public void test_selectAll() {
 	warnUnimpl("Test test_selectAll not written");
 }
 
-public void test_setBackgroundLorg_eclipse_swt_graphics_Color() {
-	warnUnimpl("Test test_setBackgroundLorg_eclipse_swt_graphics_Color not written");
-}
-
-public void test_setEnabledZ() {
-	warnUnimpl("Test test_setEnabledZ not written");
-}
-
-public void test_setFontLorg_eclipse_swt_graphics_Font() {
-	warnUnimpl("Test test_setFontLorg_eclipse_swt_graphics_Font not written");
-}
-
-public void test_setForegroundLorg_eclipse_swt_graphics_Color() {
-	warnUnimpl("Test test_setForegroundLorg_eclipse_swt_graphics_Color not written");
-}
-
-public void test_setMenuLorg_eclipse_swt_widgets_Menu() {
-	warnUnimpl("Test test_setMenuLorg_eclipse_swt_widgets_Menu not written");
-}
-
 public void test_setSelection$Lorg_eclipse_swt_custom_TableTreeItem() {
 	warnUnimpl("Test test_setSelection$Lorg_eclipse_swt_custom_TableTreeItem not written");
-}
-
-public void test_setToolTipTextLjava_lang_String() {
-	warnUnimpl("Test test_setToolTipTextLjava_lang_String not written");
 }
 
 public void test_showItemLorg_eclipse_swt_custom_TableTreeItem() {
@@ -154,23 +130,17 @@ public static java.util.Vector methodNames() {
 	methodNames.addElement("test_deselectAll");
 	methodNames.addElement("test_getItemCount");
 	methodNames.addElement("test_getItemHeight");
+	methodNames.addElement("test_getItemLorg_eclipse_swt_graphics_Point");
 	methodNames.addElement("test_getItems");
 	methodNames.addElement("test_getSelection");
 	methodNames.addElement("test_getSelectionCount");
 	methodNames.addElement("test_getTable");
-	methodNames.addElement("test_getItemLorg_eclipse_swt_graphics_Point");
 	methodNames.addElement("test_indexOfLorg_eclipse_swt_custom_TableTreeItem");
 	methodNames.addElement("test_removeAll");
 	methodNames.addElement("test_removeSelectionListenerLorg_eclipse_swt_events_SelectionListener");
 	methodNames.addElement("test_removeTreeListenerLorg_eclipse_swt_events_TreeListener");
 	methodNames.addElement("test_selectAll");
-	methodNames.addElement("test_setBackgroundLorg_eclipse_swt_graphics_Color");
-	methodNames.addElement("test_setEnabledZ");
-	methodNames.addElement("test_setFontLorg_eclipse_swt_graphics_Font");
-	methodNames.addElement("test_setForegroundLorg_eclipse_swt_graphics_Color");
-	methodNames.addElement("test_setMenuLorg_eclipse_swt_widgets_Menu");
 	methodNames.addElement("test_setSelection$Lorg_eclipse_swt_custom_TableTreeItem");
-	methodNames.addElement("test_setToolTipTextLjava_lang_String");
 	methodNames.addElement("test_showItemLorg_eclipse_swt_custom_TableTreeItem");
 	methodNames.addElement("test_showSelection");
 	methodNames.addAll(Test_org_eclipse_swt_widgets_Composite.methodNames()); // add superclass method names
@@ -183,23 +153,17 @@ protected void runTest() throws Throwable {
 	else if (getName().equals("test_deselectAll")) test_deselectAll();
 	else if (getName().equals("test_getItemCount")) test_getItemCount();
 	else if (getName().equals("test_getItemHeight")) test_getItemHeight();
+	else if (getName().equals("test_getItemLorg_eclipse_swt_graphics_Point")) test_getItemLorg_eclipse_swt_graphics_Point();
 	else if (getName().equals("test_getItems")) test_getItems();
 	else if (getName().equals("test_getSelection")) test_getSelection();
 	else if (getName().equals("test_getSelectionCount")) test_getSelectionCount();
 	else if (getName().equals("test_getTable")) test_getTable();
-	else if (getName().equals("test_getItemLorg_eclipse_swt_graphics_Point")) test_getItemLorg_eclipse_swt_graphics_Point();
 	else if (getName().equals("test_indexOfLorg_eclipse_swt_custom_TableTreeItem")) test_indexOfLorg_eclipse_swt_custom_TableTreeItem();
 	else if (getName().equals("test_removeAll")) test_removeAll();
 	else if (getName().equals("test_removeSelectionListenerLorg_eclipse_swt_events_SelectionListener")) test_removeSelectionListenerLorg_eclipse_swt_events_SelectionListener();
 	else if (getName().equals("test_removeTreeListenerLorg_eclipse_swt_events_TreeListener")) test_removeTreeListenerLorg_eclipse_swt_events_TreeListener();
 	else if (getName().equals("test_selectAll")) test_selectAll();
-	else if (getName().equals("test_setBackgroundLorg_eclipse_swt_graphics_Color")) test_setBackgroundLorg_eclipse_swt_graphics_Color();
-	else if (getName().equals("test_setEnabledZ")) test_setEnabledZ();
-	else if (getName().equals("test_setFontLorg_eclipse_swt_graphics_Font")) test_setFontLorg_eclipse_swt_graphics_Font();
-	else if (getName().equals("test_setForegroundLorg_eclipse_swt_graphics_Color")) test_setForegroundLorg_eclipse_swt_graphics_Color();
-	else if (getName().equals("test_setMenuLorg_eclipse_swt_widgets_Menu")) test_setMenuLorg_eclipse_swt_widgets_Menu();
 	else if (getName().equals("test_setSelection$Lorg_eclipse_swt_custom_TableTreeItem")) test_setSelection$Lorg_eclipse_swt_custom_TableTreeItem();
-	else if (getName().equals("test_setToolTipTextLjava_lang_String")) test_setToolTipTextLjava_lang_String();
 	else if (getName().equals("test_showItemLorg_eclipse_swt_custom_TableTreeItem")) test_showItemLorg_eclipse_swt_custom_TableTreeItem();
 	else if (getName().equals("test_showSelection")) test_showSelection();
 	else super.runTest();
