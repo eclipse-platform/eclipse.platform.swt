b21726b0701ceef32387eb9a9164e3ff9feeadb5
Felipe Heidrich <fheidric>
2004-01-06 16:51:26 +0000

    pr 49381

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
@@ -1713,6 +1713,7 @@ int widgetStyle () {
 	int bits = super.widgetStyle ();
 	if ((style & SWT.PASSWORD) != 0) bits |= OS.ES_PASSWORD;
 	if ((style & SWT.CENTER) != 0) bits |= OS.ES_CENTER;
+	if ((style & SWT.RIGHT) != 0) bits |= OS.ES_RIGHT;
 	if ((style & SWT.READ_ONLY) != 0) bits |= OS.ES_READONLY;
 	if ((style & SWT.SINGLE) != 0) return bits | OS.ES_AUTOHSCROLL;
 	bits |= OS.ES_MULTILINE | OS.ES_AUTOHSCROLL | OS.ES_NOHIDESEL;	
