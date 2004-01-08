344f8af8dc9aaa139797cf87c464fbafe5aa067b
Steve Northover <steve>
2004-01-08 00:17:41 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
@@ -1718,13 +1718,13 @@ int wcsToMbcsPos (int wcsPos) {
 }
 
 int widgetStyle () {
-	int bits = super.widgetStyle ();
+	int bits = super.widgetStyle () | OS.ES_AUTOHSCROLL;
 	if ((style & SWT.PASSWORD) != 0) bits |= OS.ES_PASSWORD;
 	if ((style & SWT.CENTER) != 0) bits |= OS.ES_CENTER;
 	if ((style & SWT.RIGHT) != 0) bits |= OS.ES_RIGHT;
 	if ((style & SWT.READ_ONLY) != 0) bits |= OS.ES_READONLY;
-	if ((style & SWT.SINGLE) != 0) return bits | OS.ES_AUTOHSCROLL;
-	bits |= OS.ES_MULTILINE | OS.ES_NOHIDESEL | OS.ES_AUTOHSCROLL | OS.ES_AUTOVSCROLL;	
+	if ((style & SWT.SINGLE) != 0) return bits;
+	bits |= OS.ES_MULTILINE | OS.ES_NOHIDESEL | OS.ES_AUTOVSCROLL;	
 	if ((style & SWT.WRAP) != 0) bits &= ~(OS.WS_HSCROLL | OS.ES_AUTOHSCROLL);
 	return bits;
 }
