c0922b04510c980c4fdbfc07d5f670a2e0c2b69e
Veronika Irvine <veronika>
2004-01-14 21:16:38 +0000

    Adding anti-aliasing

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CBanner.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CBanner.java
@@ -60,8 +60,7 @@ public class CBanner extends Composite {
 	static final int BORDER_STRIPE = 2;
 	static final int INDENT = 5;
 	
-	static RGB BORDER1 = null;
-	static RGB BORDER2 = null;
+	static RGB BORDER1 = CTabFolder2.borderInsideRGB;
 	
 		
 /**
@@ -74,13 +73,6 @@ public class CBanner extends Composite {
  */
 public CBanner(Composite parent, int style) {
 	super(parent, checkStyle(style));
-	
-	if (BORDER1 == null) {
-		BORDER1 = getDisplay().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW).getRGB();
-	}
-	if (BORDER2 == null) {
-		BORDER2 = getDisplay().getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW).getRGB();
-	}
 	
 	addPaintListener(new PaintListener() {
 		public void paintControl(PaintEvent event) {
@@ -269,40 +261,47 @@ void onPaint(GC gc) {
 	if (curve == null) updateCurve();
 	Point size = getSize();
 	int[] line1 = new int[curve.length+6];
-	int[] line2 = new int[curve.length+6];
 	int index = 0;
 	int x = curveStart;
 	int y = 0;
-	line1[index] = x + 1;
-	line2[index++] = x + 1;
-	line1[index] = size.y - BORDER_STRIPE;
-	line2[index++] = size.y - BORDER_STRIPE + 1;
+	line1[index++] = x +1;
+	line1[index++] = size.y - BORDER_STRIPE;
 	for (int i = 0; i < curve.length/2; i++) {
-		line1[index]=x+curve[2*i];
-		line2[index++]=x+curve[2*i];
-		line1[index]=y+curve[2*i+1];
-		line2[index++]=y+curve[2*i+1]+1;
+		line1[index++]=x+curve[2*i];
+		line1[index++]=y+curve[2*i+1];
 	}
-	line1[index] = x + CURVE_WIDTH;
-	line2[index++] = x + CURVE_WIDTH;
-	line1[index] = 0;
-	line2[index++] = 1;
-	line1[index] = size.x;
-	line2[index++] = size.x;
-	line1[index] = 0;
-	line2[index++] = 1;
+	line1[index++] = x + CURVE_WIDTH;
+	line1[index++] = 0;
+	line1[index++] = size.x;
+	line1[index++] = 0;
 	
 	Color background = getBackground();
+		
+	// Anti- aliasing
+	int[] line2 = new int[line1.length];
+	index = 0;
+	for (int i = 0; i < line1.length/2; i++) { 
+		line2[index] = line1[index++]  - 1;
+		line2[index] = line1[index++];
+	}
+	int[] line3 = new int[line1.length];
+	index = 0;
+	for (int i = 0; i < line1.length/2; i++) {
+		line3[index] = line1[index++] + 1;
+		line3[index] = line1[index++];
+	}
+	RGB from = BORDER1;
+	RGB to = background.getRGB();
+	int red = from.red + 3*(to.red - from.red)/4;
+	int green = from.green + 3*(to.green - from.green)/4;
+	int blue = from.blue + 3*(to.blue - from.blue)/4;
+	Color color = new Color(getDisplay(), red, green, blue);
+	gc.setForeground(color);
+	gc.drawPolyline(line2);
+	gc.drawPolyline(line3);
+	color.dispose();
 	
 	int x1 = Math.max(0, curveStart - CURVE_TAIL);
-	Color border2 = new Color(getDisplay(), BORDER2);
-	gc.setForeground(background);
-	gc.setBackground(border2);
-	gc.fillGradientRectangle(x1, size.y - BORDER_STRIPE + 1, curveStart-x1+1, 1, false);
-	gc.setForeground(border2);
-	gc.drawPolyline(line2);
-	border2.dispose();
-	
 	Color border1 = new Color(getDisplay(), BORDER1);
 	gc.setForeground(background);
 	gc.setBackground(border1);
