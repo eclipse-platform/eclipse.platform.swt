857a214c04de4c83cec313adcbe9f7564b9c62bb
Veronika Irvine <veronika>
2004-01-16 14:46:59 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder2.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder2.java
@@ -1100,7 +1100,7 @@ void drawChevron(GC gc) {
 	int y = chevronRect.y + indent;
 	switch (chevronImageState) {
 		case NORMAL: {
-			int[] shape = new int[] {x,y, x+9,y, x+9,y+2, x+5,y+6, x+4,y+6, x,y+2, x,y};
+			int[] shape = onBottom ? new int[]{x,y+9, x+9,y+9, x+9,y+7, x+5,y+3, x+4,y+3, x,y+7, x,y+9} : new int[]{x,y, x+9,y, x+9,y+2, x+5,y+6, x+4,y+6, x,y+2, x,y};
 			gc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
 			gc.fillPolygon(shape);
 			gc.setForeground(borderColor1);
@@ -1108,7 +1108,7 @@ void drawChevron(GC gc) {
 			break;
 		}
 		case HOT: {
-			int[] shape = new int[] {x,y, x+9,y, x+9,y+2, x+5,y+6, x+4,y+6, x,y+2, x,y};
+			int[] shape = onBottom ? new int[]{x,y+9, x+9,y+9, x+9,y+7, x+5,y+3, x+4,y+3, x,y+7, x,y+9} : new int[] {x,y, x+9,y, x+9,y+2, x+5,y+6, x+4,y+6, x,y+2, x,y};
 			gc.setBackground(display.getSystemColor(SWT.COLOR_WHITE));
 			gc.fillPolygon(shape);
 			Color border = new Color(display, CHEVRON_BORDER);
@@ -1118,7 +1118,7 @@ void drawChevron(GC gc) {
 			break;
 		}
 		case SELECTED: {
-			int[] shape = new int[] {x+1,y+1, x+10,y+1, x+10,y+3, x+6,y+7, x+5,y+7, x+1,y+3, x+1,y+1};
+			int[] shape = onBottom ? new int[]{x+1,y+10, x+10,y+10, x+10,y+8, x+6,y+4, x+5,y+4, x+1,y+8, x+1,y+10} : new int[] {x+1,y+1, x+10,y+1, x+10,y+3, x+6,y+7, x+5,y+7, x+1,y+3, x+1,y+1};
 			Color fill = new Color(display, CHEVRON_FILL);
 			gc.setBackground(fill);
 			gc.fillPolygon(shape);
