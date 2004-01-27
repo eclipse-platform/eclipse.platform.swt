d3be55c0397f0f302145b55a1f35770295196855
Felipe Heidrich <fheidric>
2004-01-27 20:46:46 +0000

    backport 39443

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -752,12 +752,12 @@ void releaseWidget () {
  */
 public void removeAll () {
 	checkWidget ();
-	OS.gtk_tree_store_clear (modelHandle);
 	for (int i=0; i<items.length; i++) {
 		TreeItem item = items [i];
 		if (item != null && !item.isDisposed ()) item.releaseResources ();
 	}
 	items = new TreeItem[4];
+	OS.gtk_tree_store_clear (modelHandle);
 }
 
 /**
