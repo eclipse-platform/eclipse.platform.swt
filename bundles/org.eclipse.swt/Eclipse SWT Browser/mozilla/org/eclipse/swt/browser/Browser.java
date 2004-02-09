9bd86ae482da2807366664b7d01b4a80edb46ead
Christophe Cornu <ccornu>
2004-02-09 18:04:50 +0000

    51372

--- bundles/org.eclipse.swt/Eclipse SWT Browser/mozilla/org/eclipse/swt/browser/Browser.java
+++ bundles/org.eclipse.swt/Eclipse SWT Browser/mozilla/org/eclipse/swt/browser/Browser.java
@@ -679,7 +679,7 @@ public String getUrl() {
 		if (rc != XPCOM.NS_OK) error(rc);
 		int length = XPCOM.nsCString_Length(aSpec);
 		int buffer = XPCOM.nsCString_get(aSpec);
-		dest = new byte[length + 1];
+		dest = new byte[length];
 		XPCOM.memmove(dest, buffer, length);
 		XPCOM.nsCString_delete(aSpec);
 		uri.Release();
@@ -1547,7 +1547,7 @@ int OnLocationChange(int aWebProgress, int aRequest, int aLocation) {
 	location.GetSpec(aSpec);
 	int length = XPCOM.nsCString_Length(aSpec);
 	int buffer = XPCOM.nsCString_get(aSpec);
-	byte[] dest = new byte[length + 1];
+	byte[] dest = new byte[length];
 	XPCOM.memmove(dest, buffer, length);
 	XPCOM.nsCString_delete(aSpec);
 
@@ -1757,7 +1757,7 @@ int OnStartURIOpen(int aURI, int retval) {
 	int length = XPCOM.nsCString_Length(aSpec);
 	int buffer = XPCOM.nsCString_get(aSpec);
 	buffer = XPCOM.nsCString_get(aSpec);
-	byte[] dest = new byte[length + 1];
+	byte[] dest = new byte[length];
 	XPCOM.memmove(dest, buffer, length);
 	XPCOM.nsCString_delete(aSpec);
 	
