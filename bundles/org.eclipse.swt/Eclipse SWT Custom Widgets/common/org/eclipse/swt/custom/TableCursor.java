14872c5e80adfe81dc2adbebd244eb3a58cf3607
Felipe Heidrich <fheidric>
2004-02-11 19:38:42 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/TableCursor.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/TableCursor.java
@@ -12,7 +12,6 @@ package org.eclipse.swt.custom;
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.internal.Platform;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.events.*;
 
