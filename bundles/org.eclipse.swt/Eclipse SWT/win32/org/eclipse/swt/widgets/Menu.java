fbadd50f32bfc64c1bf0bdb0a432d96a6057edd6
Steve Northover <steve>
2004-02-20 17:40:42 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Menu.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Menu.java
@@ -862,13 +862,14 @@ public Point getSize () {
 		}
 	} else {
 		int count = GetMenuItemCount (handle);
-		if (count == 0) return new Point (0, 0);
-		RECT rect = new RECT ();
-		int hwndParent = parent.handle;
-		if (OS.GetMenuItemRect (hwndParent, handle, count - 1, rect)) {
-			int width = rect.right + 4;
-			int height = rect.bottom + 4;
-			return new Point (width, height);
+		if (count != 0) {
+			RECT rect = new RECT ();
+			int hwndParent = parent.handle;
+			if (OS.GetMenuItemRect (hwndParent, handle, count - 1, rect)) {
+				int width = rect.right + 4;
+				int height = rect.bottom + 4;
+				return new Point (width, height);
+			}
 		}
 	}
 	return new Point (0, 0);
