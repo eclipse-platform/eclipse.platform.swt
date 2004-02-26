a225ecce9d1fce035d68b0f395d8d3d7a4ef9eec
Christophe Cornu <ccornu>
2004-02-26 19:06:36 +0000

    atk compiled without warnings

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/atk_custom.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/atk_custom.c
@@ -18,6 +18,8 @@
 
 #define OS_NATIVE(func) Java_org_eclipse_swt_internal_accessibility_gtk_ATK_##func
 
+#ifndef SWT_PTR_SIZE_64
+
 #ifndef NO_call__II
 typedef jint (*call_P1) (jint);
 JNIEXPORT jint JNICALL OS_NATIVE(call__II)
@@ -94,3 +96,84 @@ JNIEXPORT jint JNICALL OS_NATIVE(call__IIIIIIII)
 	NATIVE_EXIT(env, that, "call__IIIIIIII\n")
 }
 #endif
+
+#else
+
+#ifndef NO_call__JJ
+typedef jlong (*call_P1) (jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1)
+{
+	NATIVE_ENTER(env, that, "call__JJ\n")
+	return ((call_P1)arg0)(arg1);
+	NATIVE_EXIT(env, that, "call__JJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJ
+typedef jlong (*call_P2) (jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2)
+{
+	NATIVE_ENTER(env, that, "call__JJJ\n")
+	return ((call_P2)arg0)(arg1, arg2);
+	NATIVE_EXIT(env, that, "call__JJJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJJ
+typedef jlong (*call_P3) (jlong, jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2, jlong arg3)
+{
+	NATIVE_ENTER(env, that, "call__JJJJ\n")
+	return ((call_P3)arg0)(arg1, arg2, arg3);
+	NATIVE_EXIT(env, that, "call__JJJJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJJJ
+typedef jlong (*call_P4) (jlong, jlong, jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2, jlong arg3, jlong arg4)
+{
+	NATIVE_ENTER(env, that, "call__JJJJJ\n")
+	return ((call_P4)arg0)(arg1, arg2, arg3, arg4);
+	NATIVE_EXIT(env, that, "call__JJJJJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJJJJ
+typedef jlong (*call_P5) (jlong, jlong, jlong, jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJJJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2, jlong arg3, jlong arg4, jlong arg5)
+{
+	NATIVE_ENTER(env, that, "call__JJJJJJ\n")
+	return ((call_P5)arg0)(arg1, arg2, arg3, arg4, arg5);
+	NATIVE_EXIT(env, that, "call__JJJJJJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJJJJJ
+typedef jlong (*call_P6) (jlong, jlong, jlong, jlong, jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJJJJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2, jlong arg3, jlong arg4, jlong arg5, jlong arg6)
+{
+	NATIVE_ENTER(env, that, "call__JJJJJJJ\n")
+	return ((call_P6)arg0)(arg1, arg2, arg3, arg4, arg5, arg6);
+	NATIVE_EXIT(env, that, "call__JJJJJJJ\n")
+}
+#endif
+
+#ifndef NO_call__JJJJJJJJ
+typedef jlong (*call_P7) (jlong, jlong, jlong, jlong, jlong, jlong, jlong);
+JNIEXPORT jlong JNICALL OS_NATIVE(call__JJJJJJJJ)
+	(JNIEnv *env, jclass that, jlong arg0, jlong arg1, jlong arg2, jlong arg3, jlong arg4, jlong arg5, jlong arg6, jlong arg7)
+{
+	NATIVE_ENTER(env, that, "call__JJJJJJJJ\n")
+	return ((call_P7)arg0)(arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+	NATIVE_EXIT(env, that, "call__JJJJJJJJ\n")
+}
+#endif
+
+#endif /* SWT_PTR_SIZE_64 */
