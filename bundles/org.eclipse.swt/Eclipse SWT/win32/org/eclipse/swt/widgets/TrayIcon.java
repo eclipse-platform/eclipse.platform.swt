ae0f9596a7a10fcf1b8a2d135edeca5b286860b8
Steve Northover <steve>
2004-03-11 00:01:40 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/TrayIcon.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/TrayIcon.java
@@ -1,4 +1,15 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.swt.widgets;
+
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.events.*;
@@ -7,7 +18,7 @@ import org.eclipse.swt.internal.win32.*;
 
 /**
  * Instances of this class represent icons that can be placed on the
- * platform's system tray or task bar status area.
+ * system tray or task bar status area.
  * 
  * <dt><b>Events:</b></dt>
  * <dd>Selection, DefaultSelection</dd>
