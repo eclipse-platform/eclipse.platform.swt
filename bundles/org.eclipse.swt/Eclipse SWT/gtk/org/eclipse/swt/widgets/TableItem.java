213c02fd8994857dc973f79e42d0907fd4e3f624
Silenio Quarti <silenio>
2004-03-22 15:42:56 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableItem.java
@@ -108,6 +108,8 @@ public TableItem (Table parent, int style) {
 
 void clear () {
 	if (cached || (parent.style & SWT.VIRTUAL) == 0) {
+		text = "";
+		image = null;
 		int columnCount = OS.gtk_tree_model_get_n_columns (parent.modelHandle);
 		for (int i=0; i<columnCount; i++) {
 			OS.gtk_list_store_set (parent.modelHandle, handle, i, 0, -1);
