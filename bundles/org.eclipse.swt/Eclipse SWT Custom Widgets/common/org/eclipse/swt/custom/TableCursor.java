d811039967253e2256c9d31100f032d9b4bb3113
Steve Northover <steve>
2004-04-22 20:18:44 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/TableCursor.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/TableCursor.java
@@ -83,26 +83,26 @@ import org.eclipse.swt.events.*;
  *				text.setFocus();
  *			}
  *		});
- *		// Hide the TableCursor when the user hits the "CTRL" or "SHIFT" key.
+ *		// Hide the TableCursor when the user hits the "MOD1" or "MOD2" key.
  *		// This alows the user to select multiple items in the table.
  *		cursor.addKeyListener(new KeyAdapter() {
  *			public void keyPressed(KeyEvent e) {
- *				if (e.keyCode == SWT.CTRL || 
- *				    e.keyCode == SWT.SHIFT || 
- *				    (e.stateMask & SWT.CONTROL) != 0 || 
- *				    (e.stateMask & SWT.SHIFT) != 0) {
+ *				if (e.keyCode == SWT.MOD1 || 
+ *				    e.keyCode == SWT.MOD2 || 
+ *				    (e.stateMask & SWT.MOD1) != 0 || 
+ *				    (e.stateMask & SWT.MOD2) != 0) {
  *					cursor.setVisible(false);
  *				}
  *			}
  *		});
- *		// Show the TableCursor when the user releases the "SHIFT" or "CTRL" key.
+ *		// Show the TableCursor when the user releases the "MOD2" or "MOD1" key.
  *		// This signals the end of the multiple selection task.
  *		table.addKeyListener(new KeyAdapter() {
  *			public void keyReleased(KeyEvent e) {
- *				if (e.keyCode == SWT.CONTROL && (e.stateMask & SWT.SHIFT) != 0) return;
- *				if (e.keyCode == SWT.SHIFT && (e.stateMask & SWT.CONTROL) != 0) return;
- *				if (e.keyCode != SWT.CONTROL && (e.stateMask & SWT.CONTROL) != 0) return;
- *				if (e.keyCode != SWT.SHIFT && (e.stateMask & SWT.SHIFT) != 0) return;
+ *				if (e.keyCode == SWT.MOD1 && (e.stateMask & SWT.MOD2) != 0) return;
+ *				if (e.keyCode == SWT.MOD2 && (e.stateMask & SWT.MOD1) != 0) return;
+ *				if (e.keyCode != SWT.MOD1 && (e.stateMask & SWT.MOD1) != 0) return;
+ *				if (e.keyCode != SWT.MOD2 && (e.stateMask & SWT.MOD2) != 0) return;
  *			
  *				TableItem[] selection = table.getSelection();
  *				TableItem row = (selection.length == 0) ? table.getItem(table.getTopIndex()) : selection[0];
