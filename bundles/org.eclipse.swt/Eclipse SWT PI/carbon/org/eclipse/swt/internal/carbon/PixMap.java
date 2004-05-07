cb6ae703c1e93b68e3c26b1c7f8cbc020d29ec26
Christophe Cornu <ccornu>
2004-05-07 19:51:48 +0000

    copyright update

--- bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/PixMap.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/PixMap.java
@@ -1,5 +1,6 @@
 /*******************************************************************************
  * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Portions Copyright (c) 1983-2002, Apple Computer, Inc.
  * All rights reserved. This program and the accompanying materials 
  * are made available under the terms of the Common Public License v1.0
  * which accompanies this distribution, and is available at
