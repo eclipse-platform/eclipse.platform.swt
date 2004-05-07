bc11e53f5b1355e29da0c7c29f02b052bc2d59d7
Christophe Cornu <ccornu>
2004-05-07 20:09:27 +0000

    copyrights update

--- bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/CGPoint.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/CGPoint.java
@@ -1,14 +1,15 @@
-/*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+/**********************************************************************
+ * Copyright (c) 2003-2004 IBM Corp.
  * Portions Copyright (c) 1983-2002, Apple Computer, Inc.
- * All rights reserved. This program and the accompanying materials 
- * are made available under the terms of the Common Public License v1.0
+ *
+ * All rights reserved.   This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/cpl-v10.html
- * 
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
  * Contributors:
  *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ **********************************************************************/
 package org.eclipse.swt.internal.carbon;
 
 
