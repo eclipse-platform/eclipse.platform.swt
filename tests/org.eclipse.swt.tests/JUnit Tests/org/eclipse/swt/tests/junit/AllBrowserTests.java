626276d95726686c8c8e2d0fea5016c9ce5fb590
Christophe Cornu <ccornu>
2004-05-31 14:42:09 +0000

    reenable tests - note they require being run from within the eclipse launcher or with a script that set MOZILLA_FIVE_HOME and LD_LIBRARY_PATH to the mozilla location

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllBrowserTests.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllBrowserTests.java
@@ -24,22 +24,18 @@ public static void main(String[] args) {
 }
 public static Test suite() {
 	TestSuite suite = new TestSuite();
-//@todo regression caused by Display/Control change on 19/04/2004
-//	suite.addTest(Test_org_eclipse_swt_browser_Browser.suite());
-	
-//	@todo bug 62760 releng junit tests don't start from eclipse
-//  launcher. Disable junit tests until resolved.
-//	suite.addTest(Test_org_eclipse_swt_browser_CloseWindowListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_LocationAdapter.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_LocationListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_OpenWindowListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_ProgressAdapter.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_ProgressListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_StatusTextListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_TitleListener.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_VisibilityWindowAdapter.suite());
-//	suite.addTest(Test_org_eclipse_swt_browser_VisibilityWindowListener.suite());
-//	suite.addTest(org.eclipse.swt.tests.junit.browser.Test_BrowserSuite.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_Browser.suite());	
+	suite.addTest(Test_org_eclipse_swt_browser_CloseWindowListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_LocationAdapter.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_LocationListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_OpenWindowListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_ProgressAdapter.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_ProgressListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_StatusTextListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_TitleListener.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_VisibilityWindowAdapter.suite());
+	suite.addTest(Test_org_eclipse_swt_browser_VisibilityWindowListener.suite());
+	suite.addTest(org.eclipse.swt.tests.junit.browser.Test_BrowserSuite.suite());
 
 	return suite;
 }
