9024a2395a6ace1e4868e89d2664af9d1fc17e6b
Steve Northover <steve>
2004-06-02 15:51:29 +0000

    65359

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Label.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Label.java
@@ -515,8 +515,8 @@ public void setImage (Image image) {
 public void setText (String string) {
 	checkWidget();
 	if (string == null) error (SWT.ERROR_NULL_ARGUMENT);
-	text = string;
 	if ((style & SWT.SEPARATOR) != 0) return;
+	text = string;
 	char [] chars = fixMnemonic (string);
 	byte [] buffer = Converter.wcsToMbcs (null, chars, true);
 	OS.gtk_label_set_text_with_mnemonic (labelHandle, buffer);
