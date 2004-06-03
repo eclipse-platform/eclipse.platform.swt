0a5b94ac0966e0e53ac2bfdc3bf266632ccfed32
Carolyn MacLeod <carolyn>
2004-06-03 22:16:00 +0000

    v3055

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -23,7 +23,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 54;
+    static int MINOR_VERSION = 55;
 	
 	/**
 	 * SWT revision number (must be >= 0)
