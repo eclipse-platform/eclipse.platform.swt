3b118360fba6dd5890cba9d5d52543cb13d89b96
Veronika Irvine <veronika>
2004-06-17 19:02:48 +0000

    bug 65975

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Clipboard.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Clipboard.java
@@ -266,7 +266,27 @@ public void setContents(Object[] data, Transfer[] dataTypes) {
 	
 	this.data = data;
 	this.transferAgents = dataTypes;
-	if (COM.OleSetClipboard(this.iDataObject.getAddress()) != COM.S_OK ) {
+
+	int result = COM.OleSetClipboard(this.iDataObject.getAddress());
+	
+	/*
+	* Bug in Windows. When a new application takes control
+	* of the clipboard, other applications may open the 
+	* clipboard to determine if they want to record the 
+	* clipoard updates.  When this happens, the clipboard 
+	* can not be flushed until the other aplication is
+	* finished.  To allow other applications to get the
+	* data, use PeekMessage() to enable cross thread
+	* message sends.
+	*/
+	int retryCount = 0;
+	while (result != COM.S_OK && retryCount++ < 10) {
+		try { Thread.sleep(50);} catch (Throwable t) {}
+		MSG msg = new MSG();
+		COM.PeekMessage (msg, 0, 0, 0, OS.PM_NOREMOVE | OS.PM_NOYIELD);
+		result = COM.OleSetClipboard(this.iDataObject.getAddress());
+	}
+	if (result != COM.S_OK) {
 		DND.error(DND.ERROR_CANNOT_SET_CLIPBOARD);
 	}
 }
