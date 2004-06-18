894e8bb5e179a5104bc368c93a7f76e653db3d0a
Silenio Quarti <silenio>
2004-06-18 20:41:21 +0000

    66962

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Button.java
@@ -719,8 +719,8 @@ public void setText (String string) {
 
 int traversalCode (int key, GdkEventKey event) {
 	int code = super.traversalCode (key, event);
-	if ((style & SWT.PUSH) != 0) return code;
-	return code | SWT.TRAVERSE_ARROW_NEXT | SWT.TRAVERSE_ARROW_PREVIOUS;
+	if ((style & SWT.RADIO) != 0) code |= SWT.TRAVERSE_ARROW_NEXT | SWT.TRAVERSE_ARROW_PREVIOUS;
+	return code;
 }
 
 }
