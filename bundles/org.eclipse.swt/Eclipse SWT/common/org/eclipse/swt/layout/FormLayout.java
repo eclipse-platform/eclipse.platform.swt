026ec216fd746b5e9f0ce3f8740b6030120c3888
Steve Northover <steve>
2004-07-19 17:21:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/layout/FormLayout.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/layout/FormLayout.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.swt.layout;
 
-import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.widgets.*;
 
