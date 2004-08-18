6fe91aaf7d8b35e691ebd0e7dfaa6bdce89682a1
Felipe Heidrich <fheidric>
2004-08-18 21:50:47 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/photon/library/build.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/photon/library/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #*******************************************************************************
 # Copyright (c) 2000, 2003 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials 
@@ -8,8 +9,6 @@
 # Contributors:
 #     IBM Corporation - initial API and implementation
 #*******************************************************************************
-
-#!/bin/sh
 
 export IVE_HOME=~/ive/bin
 
