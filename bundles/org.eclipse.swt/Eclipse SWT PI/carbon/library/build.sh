e1c77b3688b5f649077ca9f56cc26b32fc6a589a
Felipe Heidrich <fheidric>
2004-08-18 21:57:24 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/carbon/library/build.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/library/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 #*******************************************************************************
 # Copyright (c) 2000, 2003 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials 
@@ -8,7 +9,5 @@
 # Contributors:
 #     IBM Corporation - initial API and implementation
 #*******************************************************************************
-
-#!/bin/sh
 
 make -f make_carbon.mak $1 $2 $3 $4 $5 $6 $7 $8 $9
