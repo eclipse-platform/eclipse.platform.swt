1f2d6d7e03d28796f88f1cd0a8579f8dd597bf43
Felipe Heidrich <fheidric>
2004-08-30 22:16:47 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Caret.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Caret.java
@@ -86,7 +86,7 @@ int defaultFont () {
 	int hFont = 0;
 	if (hwndIME != 0) {
 		hFont = OS.SendMessage (hwndIME, OS.WM_GETFONT, 0, 0);
-	} 
+	}
 	if (hFont == 0) {
 		hFont = OS.SendMessage (hwnd, OS.WM_GETFONT, 0, 0);
 	}
