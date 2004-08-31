3ce860f13770876814ebe9a67578aa18d5d82b4c
Christophe Cornu <ccornu>
2004-08-31 19:18:50 +0000

    71019

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ToolItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ToolItem.java
@@ -181,6 +181,7 @@ void createHandle (int index) {
 			separatorHandle = isVertical ? OS.gtk_hseparator_new() : OS.gtk_vseparator_new();
 			if (separatorHandle == 0) error (SWT.ERROR_NO_HANDLES);
 			OS.gtk_widget_set_size_request (separatorHandle, isVertical ? 15 : 6, isVertical ? 6 : 15);
+			OS.gtk_widget_set_size_request (handle, isVertical ? 15 : 6, isVertical ? 6 : 15);
 			OS.gtk_container_add (handle, separatorHandle);
 			OS.gtk_widget_show (separatorHandle);
 			break;
