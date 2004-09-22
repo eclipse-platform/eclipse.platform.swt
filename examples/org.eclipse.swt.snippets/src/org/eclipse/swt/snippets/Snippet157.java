7aa05fbdeec6e000158c8d9891fcfab00374d951
Veronika Irvine <veronika>
2004-09-22 15:34:50 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet157.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet157.java
@@ -26,48 +26,48 @@ public class Snippet157 extends Applet {
 	java.awt.Canvas awtParent;
 
 public void init () {
-	Thread thread = new Thread (new Runnable() {
-		public void run() {
+	Thread thread = new Thread (new Runnable () {
+		public void run () {
 			// This line is a hack required to support multiple restarts
-			org.eclipse.swt.internal.win32.OS.OleInitialize(0);
+			org.eclipse.swt.internal.win32.OS.OleInitialize (0);
 			
-			setLayout(new java.awt.GridLayout(1, 1));
-			awtParent = new java.awt.Canvas();
-			add(awtParent);
-			display = new org.eclipse.swt.widgets.Display();
-			swtParent = org.eclipse.swt.awt.SWT_AWT.new_Shell(display, awtParent);
-			swtParent.setLayout(new org.eclipse.swt.layout.FillLayout());
-			org.eclipse.swt.ole.win32.OleFrame frame = new org.eclipse.swt.ole.win32.OleFrame(swtParent, org.eclipse.swt.SWT.NONE);
+			setLayout(new java.awt.GridLayout (1, 1));
+			awtParent = new java.awt.Canvas ();
+			add (awtParent);
+			display = new org.eclipse.swt.widgets.Display ();
+			swtParent = org.eclipse.swt.awt.SWT_AWT.new_Shell (display, awtParent);
+			swtParent.setLayout (new org.eclipse.swt.layout.FillLayout ());
+			org.eclipse.swt.ole.win32.OleFrame frame = new org.eclipse.swt.ole.win32.OleFrame (swtParent, org.eclipse.swt.SWT.NONE);
 			org.eclipse.swt.ole.win32.OleClientSite site;
 			try {
-				site = new org.eclipse.swt.ole.win32.OleClientSite(frame, org.eclipse.swt.SWT.NONE, "Word.Document");
+				site = new org.eclipse.swt.ole.win32.OleClientSite (frame, org.eclipse.swt.SWT.NONE, "Word.Document");
 			} catch (org.eclipse.swt.SWTException e) {
-				String str = "Create OleClientSite Error" + "\n" + e.toString();
-				System.out.println(str);
+				String str = "Create OleClientSite Error" + e.toString ();
+				System.out.println (str);
 				return;
 			}
-			setSize(500, 500);
-			validate();
-			site.doVerb(org.eclipse.swt.ole.win32.OLE.OLEIVERB_SHOW);
+			setSize (500, 500);
+			validate ();
+			site.doVerb (org.eclipse.swt.ole.win32.OLE.OLEIVERB_SHOW);
 			
-			while (swtParent != null && !swtParent.isDisposed()) {
-				if (!display.readAndDispatch()) display.sleep ();		
+			while (swtParent != null && !swtParent.isDisposed ()) {
+				if (!display.readAndDispatch ()) display.sleep ();		
 			}	
 		}
 	});
 	thread.start ();
 }
- public void stop(){
- 	if (display != null && !display.isDisposed()){
- 		display.syncExec(new Runnable() {
- 			public void run() {
- 				if (swtParent != null && !swtParent.isDisposed()) swtParent.dispose();
+ public void stop (){
+ 	if (display != null && !display.isDisposed ()){
+ 		display.syncExec(new Runnable () {
+ 			public void run () {
+ 				if (swtParent != null && !swtParent.isDisposed ()) swtParent.dispose ();
  				swtParent = null;
- 				display.dispose();
+ 				display.dispose ();
  				display = null;
  			}
  		});
- 		remove(awtParent);
+ 		remove (awtParent);
  		awtParent = null;
  	}
  }
