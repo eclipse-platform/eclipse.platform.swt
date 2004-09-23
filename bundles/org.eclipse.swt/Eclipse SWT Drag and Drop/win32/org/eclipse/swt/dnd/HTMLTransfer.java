926ea099bbdd4954a212a84650d08bfb3f8da2cd
Veronika Irvine <veronika>
2004-09-23 17:48:06 +0000

    bug 68267

---
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/HTMLTransfer.java
@@ -0,0 +1,129 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2004 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.dnd;
+
+import org.eclipse.swt.internal.ole.win32.*;
+
+/**
+ * The class <code>HTMLTransfer</code> provides a platform specific mechanism 
+ * for converting text in HTML format represented as a java <code>String</code> 
+ * to a platform specific representation of the data and vice versa.  See 
+ * <code>Transfer</code> for additional information.
+ * 
+ * <p>An example of a java <code>String</code> containing HTML text is shown 
+ * below:</p>
+ * 
+ * <code><pre>
+ *     String htmlData = "<p>This is a paragraph of text.</p>";
+ * </code></pre>
+ */
+public class HTMLTransfer extends ByteArrayTransfer {
+
+	static HTMLTransfer _instance = new HTMLTransfer();
+	static final String HTML_FORMAT = "HTML Format"; //$NON-NLS-1$
+	static final int HTML_FORMATID = registerType(HTML_FORMAT);
+	static int CodePage = COM.GetACP();
+
+private HTMLTransfer() {}
+
+/**
+ * Returns the singleton instance of the HTMLTransfer class.
+ *
+ * @return the singleton instance of `the HTMLTransfer class
+ */
+public static HTMLTransfer getInstance () {
+	return _instance;
+}
+
+/**
+ * This implementation of <code>javaToNative</code> converts HTML-formatted text
+ * represented by a java <code>String</code> to a platform specific representation.
+ * For additional information see <code>Transfer#javaToNative</code>.
+ * 
+ * @param object a java <code>String</code> containing HTML text
+ * @param transferData an empty <code>TransferData</code> object; this
+ *  object will be filled in on return with the platform specific format of the data
+ */
+public void javaToNative (Object object, TransferData transferData){
+	transferData.result = COM.E_FAIL;
+	if (object == null || !(object instanceof String)) return;
+	if (!isSupportedType(transferData)) {
+		// did not match the TYMED
+		transferData.stgmedium = new STGMEDIUM();
+		transferData.result = COM.DV_E_TYMED;
+		return;
+	}
+	// HTML Format is stored as a null terminated byte array
+	String string = (String)object;
+	int count = string.length();
+	if (count == 0) return;
+	char[] chars = new char[count + 1];
+	string.getChars(0, count, chars, 0);
+	int cchMultiByte = COM.WideCharToMultiByte(CodePage, 0, chars, -1, null, 0, null, null);
+	if (cchMultiByte == 0) {
+		transferData.stgmedium = new STGMEDIUM();
+		transferData.result = COM.DV_E_STGMEDIUM;
+		return;
+	}
+	int lpMultiByteStr = COM.GlobalAlloc(COM.GMEM_FIXED | COM.GMEM_ZEROINIT, cchMultiByte);
+	COM.WideCharToMultiByte(CodePage, 0, chars, -1, lpMultiByteStr, cchMultiByte, null, null);
+	transferData.stgmedium = new STGMEDIUM();
+	transferData.stgmedium.tymed = COM.TYMED_HGLOBAL;
+	transferData.stgmedium.unionField = lpMultiByteStr;
+	transferData.stgmedium.pUnkForRelease = 0;
+	transferData.result = COM.S_OK;
+	return;
+}
+
+/**
+ * This implementation of <code>nativeToJava</code> converts a platform specific 
+ * representation of HTML text to a java <code>String</code>.
+ * For additional information see <code>Transfer#nativeToJava</code>.
+ * 
+ * @param transferData the platform specific representation of the data to be 
+ * been converted
+ * @return a java <code>String</code> containing HTML text if the 
+ * conversion was successful; otherwise null
+ */
+public Object nativeToJava(TransferData transferData){
+	if (!isSupportedType(transferData) || transferData.pIDataObject == 0) return null;
+	IDataObject data = new IDataObject(transferData.pIDataObject);
+	data.AddRef();
+	STGMEDIUM stgmedium = new STGMEDIUM();
+	FORMATETC formatetc = transferData.formatetc;
+	stgmedium.tymed = COM.TYMED_HGLOBAL;	
+	transferData.result = data.GetData(formatetc, stgmedium);
+	data.Release();	
+	if (transferData.result != COM.S_OK) return null;
+	int hMem = stgmedium.unionField;
+	try {
+		int lpMultiByteStr = COM.GlobalLock(hMem);
+		if (lpMultiByteStr == 0) return null;
+		try {
+			int cchWideChar  = COM.MultiByteToWideChar (CodePage, COM.MB_PRECOMPOSED, lpMultiByteStr, -1, null, 0);
+			if (cchWideChar == 0) return null;
+			char[] lpWideCharStr = new char [cchWideChar - 1];
+			COM.MultiByteToWideChar (CodePage, COM.MB_PRECOMPOSED, lpMultiByteStr, -1, lpWideCharStr, lpWideCharStr.length);
+			return new String(lpWideCharStr);
+		} finally {
+			COM.GlobalUnlock(hMem);
+		}
+	} finally {
+		COM.GlobalFree(hMem);
+	}
+}
+protected int[] getTypeIds(){
+	return new int[] {HTML_FORMATID};
+}
+protected String[] getTypeNames(){
+	return new String[] {HTML_FORMAT}; 
+}
+}
