9d0addebc12002cd6b98c86ba33faf9b788dcecd
Grant Gayed <ggayed>
2004-09-29 21:01:16 +0000

    75268

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet2.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet2.java
@@ -56,6 +56,7 @@ public static void main (String [] args) {
                         items[i].dispose();
                         TableItem item = new TableItem(table, SWT.NONE, j);
                         item.setText(values);
+                        items = table.getItems();
                         break;
                     }
                 }
