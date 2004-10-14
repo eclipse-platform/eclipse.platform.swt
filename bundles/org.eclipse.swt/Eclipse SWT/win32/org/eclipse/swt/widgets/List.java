c89e85e19e24329b8bcf25ca2209aef535233ce6
Felipe Heidrich <fheidric>
2004-10-14 19:34:49 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/List.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/List.java
@@ -1391,7 +1391,7 @@ public void setSelection (String [] items) {
  */
 public void setSelection (int index) {
 	checkWidget ();
-	if ((style & SWT.MULTI) != 0) deselectAll ();
+	deselectAll ();
 	select (index, true);
 	if ((style & SWT.MULTI) != 0) {
 		if (index >= 0) setFocusIndex (index);
