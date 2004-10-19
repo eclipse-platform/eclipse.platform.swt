8fd543c924826dfc972ef6a8022268f1984aa82b
Silenio Quarti <silenio>
2004-10-19 18:31:08 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Composite.java
@@ -853,18 +853,20 @@ LRESULT WM_PRINTCLIENT (int wParam, int lParam) {
 		if ((style & SWT.NO_BACKGROUND) == 0) {
 			drawBackground (wParam, rect);
 		}
-		GCData data = new GCData ();
-		data.device = display;
-		GC gc = GC.win32_new (wParam, data);
-		Event event = new Event ();
-		event.gc = gc;
-		event.x = rect.left;
-		event.y = rect.top;
-		event.width = rect.right - rect.left;
-		event.height = rect.bottom - rect.top;
-		sendEvent (SWT.Paint, event);
-		event.gc = null;
-		gc.dispose ();
+		if (hooks (SWT.Paint) || filters (SWT.Paint)) {
+			GCData data = new GCData ();
+			data.device = display;
+			GC gc = GC.win32_new (wParam, data);
+			Event event = new Event ();
+			event.gc = gc;
+			event.x = rect.left;
+			event.y = rect.top;
+			event.width = rect.right - rect.left;
+			event.height = rect.bottom - rect.top;
+			sendEvent (SWT.Paint, event);
+			event.gc = null;
+			gc.dispose ();
+		}
 		OS.RestoreDC (wParam, nSavedDC);
 	}
 	return result;
