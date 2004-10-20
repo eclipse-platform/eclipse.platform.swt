30bd40de9d2ae2a77507c63aba7a3394965f95c1
Christophe Cornu <ccornu>
2004-10-20 14:30:00 +0000

    could not draw on display

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Display.java
@@ -1950,6 +1950,7 @@ public int /*long*/ internal_new_GC (GCData data) {
 	int /*long*/ root = OS.GDK_ROOT_PARENT ();
 	int /*long*/ gdkGC = OS.gdk_gc_new (root);
 	if (gdkGC == 0) SWT.error (SWT.ERROR_NO_HANDLES);
+	OS.gdk_gc_set_subwindow (gdkGC, OS.GDK_INCLUDE_INFERIORS);
 	if (data != null) {
 		int mask = SWT.LEFT_TO_RIGHT | SWT.RIGHT_TO_LEFT;
 		if ((data.style & mask) == 0) {
