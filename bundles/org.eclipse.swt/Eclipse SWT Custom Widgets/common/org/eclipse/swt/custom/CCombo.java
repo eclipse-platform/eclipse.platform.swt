d3e6e1240786b37729a4402b475774900289164f
Veronika Irvine <veronika>
2004-10-26 13:58:34 +0000

    correct computeSize

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CCombo.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CCombo.java
@@ -311,10 +311,11 @@ public Point computeSize(int wHint, int hHint, boolean changed) {
 	gc.dispose();
 	Point textSize = text.computeSize(SWT.DEFAULT, SWT.DEFAULT, changed);
 	Point arrowSize = arrow.computeSize(SWT.DEFAULT, SWT.DEFAULT, changed);
+	Point listSize = list.computeSize (wHint, SWT.DEFAULT, changed);
 	int borderWidth = getBorderWidth();
 	
 	height = Math.max(hHint, Math.max(textSize.y, arrowSize.y) + 2*borderWidth);
-	width = Math.max(wHint, textWidth + 2*spacer + arrowSize.x + 2*borderWidth);
+	width = Math.max(wHint, Math.max(textWidth + 2*spacer + arrowSize.x + 2*borderWidth, listSize.x));
 	return new Point(width, height);
 }
 void createPopup(String[] items, int selectionIndex) {		
