bb23dd383855ac549a8b8c2d4868e173e6df611b
Christophe Cornu <ccornu>
2004-11-01 20:43:35 +0000

    v3111

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -23,7 +23,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 110;
+    static int MINOR_VERSION = 111;
 	
 	/**
 	 * SWT revision number (must be >= 0)
