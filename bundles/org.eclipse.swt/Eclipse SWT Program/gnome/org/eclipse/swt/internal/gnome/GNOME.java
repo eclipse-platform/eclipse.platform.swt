336995515e1c32c21ee12adc0409c7a2a83f8e0b
Silenio Quarti <silenio>
2004-11-09 21:41:14 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Program/gnome/org/eclipse/swt/internal/gnome/GNOME.java
+++ bundles/org.eclipse.swt/Eclipse SWT Program/gnome/org/eclipse/swt/internal/gnome/GNOME.java
@@ -23,7 +23,10 @@ public static final int GNOME_ICON_LOOKUP_FLAGS_NONE = 0;
 public static final int GNOME_PARAM_NONE = 0;
 public static final int GNOME_VFS_MIME_APPLICATION_ARGUMENT_TYPE_URIS = 0;
 
-public static final synchronized native int GnomeVFSMimeApplication_sizeof();
+/** 64 bit */
+public static final native int GnomeVFSMimeApplication_sizeof();
+
+/** Natives */
 public static final synchronized native void g_free(int /*long*/ mem);
 public static final synchronized native int /*long*/ g_list_next(int /*long*/ list);
 public static final synchronized native void g_object_unref(int /*long*/ object);
@@ -37,5 +40,5 @@ public static final synchronized native void gnome_vfs_mime_extensions_list_free
 public static final synchronized native int /*long*/ gnome_vfs_mime_get_default_application(byte[] mimeType);
 public static final synchronized native int /*long*/ gnome_vfs_mime_get_extensions_list(int /*long*/ mime_type);
 public static final synchronized native void gnome_vfs_mime_registered_mime_type_list_free(int /*long*/ list);
-public static final synchronized native void memmove (GnomeVFSMimeApplication dest, int /*long*/ src, int /*long*/ count);
+public static final native void memmove (GnomeVFSMimeApplication dest, int /*long*/ src, int /*long*/ count);
 }
