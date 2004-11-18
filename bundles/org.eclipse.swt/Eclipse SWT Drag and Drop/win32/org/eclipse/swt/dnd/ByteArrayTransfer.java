39e6ce6322e9cfb9f775060422069d6b957e191a
Veronika Irvine <veronika>
2004-11-18 23:02:49 +0000

    validate method

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/ByteArrayTransfer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/ByteArrayTransfer.java
@@ -210,8 +210,4 @@ protected Object nativeToJava(TransferData transferData) {
 boolean _validate(Object object) {
 	return (object != null && object instanceof byte[] && ((byte[])object).length > 0);
 }
-
-protected boolean validate(Object object) {
-	return true;
-}
 }
--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Transfer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/win32/org/eclipse/swt/dnd/Transfer.java
@@ -110,17 +110,6 @@ abstract protected void javaToNative (Object object, TransferData transferData);
 abstract protected Object nativeToJava(TransferData transferData);
 
 /**
- * Test that the object is of the correct format for this Transfer class.
- * 
- * @param object a java representation of the data to be converted
- * 
- * @return true if object is of the correct form for this transfer type
- * 
- * @since 3.1
- */
-abstract protected boolean validate(Object object);
-
-/**
  * Registers a name for a data type and returns the associated unique identifier.
  *
  * <p>You may register the same type more than once, the same unique identifier 
@@ -140,5 +129,18 @@ public static int registerType(String formatName) {
 	// If name already exists in registry, return its assigned value
 	TCHAR chFormatName = new TCHAR(0, formatName, true);
 	return OS.RegisterClipboardFormat(chFormatName);
+}
+
+/**
+ * Test that the object is of the correct format for this Transfer class.
+ * 
+ * @param object a java representation of the data to be converted
+ * 
+ * @return true if object is of the correct form for this transfer type
+ * 
+ * @since 3.1
+ */
+protected boolean validate(Object object) {
+	return true;
 }
 }
