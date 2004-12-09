f92e363f9303f1f51ffeab390ea048bf1c567f9d
Grant Gayed <ggayed>
2004-12-09 20:01:50 +0000

    *** empty log message ***

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/performance/SwtJunit.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/performance/SwtJunit.java
@@ -8,7 +8,7 @@
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
-package org.eclipse.swt.tests.junit;
+package org.eclipse.swt.tests.junit.performance;
 
 
 /**
