928185015339562000c4075c7f4024db762346ae
Christophe Cornu <ccornu>
2005-01-11 15:10:47 +0000

    disable debug info

--- bundles/org.eclipse.swt.tools/Icon Exe/org/eclipse/swt/tools/internal/IconExe.java
+++ bundles/org.eclipse.swt.tools/Icon Exe/org/eclipse/swt/tools/internal/IconExe.java
@@ -101,7 +101,7 @@ public class IconExe {
 	
 	public static final String VERSION = "20050111";
 	
-	static final boolean DEBUG = true;
+	static final boolean DEBUG = false;
 	public static class IconResInfo {
 		ImageData data;
 		int offset;
