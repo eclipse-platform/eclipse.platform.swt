1b41a9845a8f4f6b85aa2f3ace4f445454666061
Steve Northover <steve>
2005-01-28 20:13:53 +0000

    83791

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Combo.java
@@ -1278,33 +1278,8 @@ void setBounds (int x, int y, int width, int height, int flags) {
 			}
 		}
 		SetWindowPos (handle, 0, x, y, width, height, flags);
-		return;
-	}
-
-	/*
-	* Bug in Windows.  If the combo box has the CBS_SIMPLE style,
-	* the list portion of the combo box is not redrawn when the
-	* combo box is resized.  The fix is to force a redraw when
-	* the size has changed.
-	*/
-	if (parent.lpwp != null || (flags & OS.SWP_NOSIZE) != 0 || !OS.IsWindowVisible (handle)) {
+	} else {
 		super.setBounds (x, y, width, height, flags);
-		return;
-	}
-	RECT rect = new RECT ();
-	OS.GetWindowRect (handle, rect);
-	super.setBounds (x, y, width, height, flags);
-	int oldWidth = rect.right - rect.left, oldHeight = rect.bottom - rect.top;
-	if (oldWidth != width || oldHeight != height) {
-		if (OS.IsWinCE) {	
-			int hwndText = OS.GetDlgItem (handle, CBID_EDIT);
-			if (hwndText != 0) OS.InvalidateRect (hwndText, null, true);
-			int hwndList = OS.GetDlgItem (handle, CBID_LIST);
-			if (hwndList != 0) OS.InvalidateRect (hwndList, null, true);
-		} else {
-			int uFlags = OS.RDW_ERASE | OS.RDW_INVALIDATE | OS.RDW_ALLCHILDREN;
-			OS.RedrawWindow (handle, null, 0, uFlags);
-		}
 	}
 }
 
@@ -1795,6 +1770,28 @@ LRESULT WM_SETFOCUS (int wParam, int lParam) {
 }
 
 LRESULT WM_SIZE (int wParam, int lParam) {	
+	/*
+	* Bug in Windows.  If the combo box has the CBS_SIMPLE style,
+	* the list portion of the combo box is not redrawn when the
+	* combo box is resized.  The fix is to force a redraw when
+	* the size has changed.
+	*/
+	if ((style & SWT.SIMPLE) != 0) {
+		LRESULT result = super.WM_SIZE (wParam, lParam);
+		if (OS.IsWindowVisible (handle)) {
+			if (OS.IsWinCE) {	
+				int hwndText = OS.GetDlgItem (handle, CBID_EDIT);
+				if (hwndText != 0) OS.InvalidateRect (hwndText, null, true);
+				int hwndList = OS.GetDlgItem (handle, CBID_LIST);
+				if (hwndList != 0) OS.InvalidateRect (hwndList, null, true);
+			} else {
+				int uFlags = OS.RDW_ERASE | OS.RDW_INVALIDATE | OS.RDW_ALLCHILDREN;
+				OS.RedrawWindow (handle, null, 0, uFlags);
+			}
+		}
+		return result;
+	}
+	
 	/*
 	* Feature in Windows.  When an editable drop down combo box
 	* contains text that does not correspond to an item in the
