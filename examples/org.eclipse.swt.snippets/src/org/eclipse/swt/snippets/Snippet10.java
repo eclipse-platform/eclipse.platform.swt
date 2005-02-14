a105c19dfe537e5d89966b89a1f6928630b047d6
Silenio Quarti <silenio>
2005-02-14 20:28:24 +0000

    advanced graphics

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet10.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet10.java
@@ -10,6 +10,50 @@
  *******************************************************************************/
 package org.eclipse.swt.snippets;
 
-public class Snippet10 {
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
 
+public class Snippet10 {
+	public static void main(String[] args) {
+		final Display display = new Display();
+		final Shell shell = new Shell(display);
+		shell.setText("Advanced Graphics");
+		FontData fd = shell.getFont().getFontData()[0];
+		final Font font = new Font(display, fd.getName(), 60, SWT.BOLD | SWT.ITALIC);
+		final Image image = new Image(display, 640, 480);
+		final Rectangle rect = image.getBounds();
+		GC gc = new GC(image);
+		gc.setBackground(display.getSystemColor(SWT.COLOR_RED));
+		gc.fillOval(rect.x, rect.y, rect.width, rect.height);
+		gc.dispose();
+		shell.addListener(SWT.Paint, new Listener() {
+			public void handleEvent(Event event) {
+				GC gc = event.gc;				
+				Transform tr = new Transform(display);
+				tr.translate(50, 120);
+				tr.rotate(-30);
+				gc.setAlpha(100);
+				gc.drawImage(image, 0, 0, rect.width, rect.height, 0, 0, rect.width / 2, rect.height / 2);
+				gc.setTransform(tr);
+				Path path = new Path(display);
+				path.addString("SWT", 0, 0, font);
+				gc.setBackground(display.getSystemColor(SWT.COLOR_GREEN));
+				gc.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
+				gc.fillPath(path);
+				gc.drawPath(path);
+				tr.dispose();
+				path.dispose();
+			}			
+		});
+		shell.setSize(shell.computeSize(rect.width / 2, rect.height / 2));
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		image.dispose();
+		font.dispose();
+		display.dispose();
+	}
 }
