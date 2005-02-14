4a7d5bd6f0bb8e167883756463c56f072568937c
Felipe Heidrich <fheidric>
2005-02-14 23:25:30 +0000

    link widget snippets

---
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet182.java
@@ -0,0 +1,38 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Common Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/cpl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.snippets;
+
+/*
+ * Link example snippet: create a link widget
+ * 
+ * For a list of all SWT example snippets see
+ * http://dev.eclipse.org/viewcvs/index.cgi/%7Echeckout%7E/platform-swt-home/dev.html#snippets
+ */
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+
+public class Snippet182 {
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		Link link = new Link(shell, SWT.BORDER);
+		link.setText("This a very simple <A>link</A> widget.");
+		link.setSize(140, 40);
+		shell.pack ();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+	}
+}
