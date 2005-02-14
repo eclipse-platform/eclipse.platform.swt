8fc6d3c5f4eeed9db853d3bce47a83679e47ab51
Christophe Cornu <ccornu>
2005-02-14 21:57:47 +0000

    v3123

--- bundles/org.eclipse.swt/Eclipse SWT PI/cairo/library/cairo_structs.h
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cairo/library/cairo_structs.h
@@ -1,13 +1,23 @@
-/*******************************************************************************
-* Copyright (c) 2000, 2004 IBM Corporation and others.
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Common Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/cpl-v10.html
-* 
-* Contributors:
-*     IBM Corporation - initial API and implementation
-*******************************************************************************/
+/* ***** BEGIN LICENSE BLOCK *****
+* Version: MPL 1.1
+*
+* The contents of this file are subject to the Mozilla Public License Version
+* 1.1 (the "License"); you may not use this file except in compliance with
+* the License. You may obtain a copy of the License at
+* http://www.mozilla.org/MPL/
+*
+* Software distributed under the License is distributed on an "AS IS" basis,
+* WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+* for the specific language governing rights and limitations under the
+* License.
+*
+* Contributor(s):
+*
+* IBM
+* -  Binding to permit interfacing between Cairo and SWT
+* -  Copyright (C) 2005 IBM Corp.  All Rights Reserved.
+*
+* ***** END LICENSE BLOCK ***** */
 
 #include "cairo.h"
 
