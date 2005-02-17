aae468902359c222878246ca5690fe9c8baf6c55
Veronika Irvine <veronika>
2005-02-17 19:27:19 +0000

    Bug 84557 setWidth does not make column visible if parent is not realized

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableColumn.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TableColumn.java
@@ -558,6 +558,12 @@ public void setWidth (int width) {
 	checkWidget();
 	if (width > 0) {
 		useFixedWidth = true;
+		/*
+		* Bug in GTK.  For some reason, calling gtk_tree_view_column_set_visible()
+		* when the parent is not realized fails to show the column. The fix is to
+		* ensure that the table has been realized.
+		*/
+		OS.gtk_widget_realize (parent.handle);
 		OS.gtk_tree_view_column_set_fixed_width (handle, width);
 		OS.gtk_tree_view_column_set_visible (handle, true);
 	} else {
