ff4f7abf188cf71a2994120a01a05f1aeace595a
Billy Biggs <bbiggs>
2005-02-18 19:20:06 +0000

    85849.  Stop asking for GraphicsExpose events on images.

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/graphics/GC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/graphics/GC.java
@@ -189,39 +189,40 @@ public void copyArea(int srcX, int srcY, int width, int height, int destX, int d
 	int deltaX = destX - srcX, deltaY = destY - srcY;
 	if (deltaX == 0 && deltaY == 0) return;
 	int /*long*/ drawable = data.drawable;
-	OS.gdk_gc_set_exposures(handle, true);
+	if (data.image == null) OS.gdk_gc_set_exposures(handle, true);
 	OS.gdk_draw_drawable(drawable, handle, drawable, srcX, srcY, destX, destY, width, height);
-	OS.gdk_gc_set_exposures(handle, false);
-	if (data.image != null) return;
-	boolean disjoint = (destX + width < srcX) || (srcX + width < destX) || (destY + height < srcY) || (srcY + height < destY);
-	GdkRectangle rect = new GdkRectangle ();
-	if (disjoint) {
-		rect.x = srcX;
-		rect.y = srcY;
-		rect.width = width;
-		rect.height = height;
-		OS.gdk_window_invalidate_rect (drawable, rect, false);
-//		OS.gdk_window_clear_area_e(drawable, srcX, srcY, width, height);
-	} else {
-		if (deltaX != 0) {
-			int newX = destX - deltaX;
-			if (deltaX < 0) newX = destX + width;
-			rect.x = newX;
+	if (data.image == null) {
+		OS.gdk_gc_set_exposures(handle, false);
+		boolean disjoint = (destX + width < srcX) || (srcX + width < destX) || (destY + height < srcY) || (srcY + height < destY);
+		GdkRectangle rect = new GdkRectangle ();
+		if (disjoint) {
+			rect.x = srcX;
 			rect.y = srcY;
-			rect.width = Math.abs(deltaX);
+			rect.width = width;
 			rect.height = height;
 			OS.gdk_window_invalidate_rect (drawable, rect, false);
-//			OS.gdk_window_clear_area_e(drawable, newX, srcY, Math.abs(deltaX), height);
-		}
-		if (deltaY != 0) {
-			int newY = destY - deltaY;
-			if (deltaY < 0) newY = destY + height;
-			rect.x = srcX;
-			rect.y = newY;
-			rect.width = width;
-			rect.height = Math.abs(deltaY);
-			OS.gdk_window_invalidate_rect (drawable, rect, false);
-//			OS.gdk_window_clear_area_e(drawable, srcX, newY, width, Math.abs(deltaY));
+//			OS.gdk_window_clear_area_e(drawable, srcX, srcY, width, height);
+		} else {
+			if (deltaX != 0) {
+				int newX = destX - deltaX;
+				if (deltaX < 0) newX = destX + width;
+				rect.x = newX;
+				rect.y = srcY;
+				rect.width = Math.abs(deltaX);
+				rect.height = height;
+				OS.gdk_window_invalidate_rect (drawable, rect, false);
+//				OS.gdk_window_clear_area_e(drawable, newX, srcY, Math.abs(deltaX), height);
+			}
+			if (deltaY != 0) {
+				int newY = destY - deltaY;
+				if (deltaY < 0) newY = destY + height;
+				rect.x = srcX;
+				rect.y = newY;
+				rect.width = width;
+				rect.height = Math.abs(deltaY);
+				OS.gdk_window_invalidate_rect (drawable, rect, false);
+//				OS.gdk_window_clear_area_e(drawable, srcX, newY, width, Math.abs(deltaY));
+			}
 		}
 	}
 }
--- bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/graphics/GC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/motif/org/eclipse/swt/graphics/GC.java
@@ -151,23 +151,24 @@ public void copyArea(int x, int y, int width, int height, int destX, int destY) 
 	if (deltaX == 0 && deltaY == 0) return;
 	int xDisplay = data.display;
 	int xDrawable = data.drawable;
-	OS.XSetGraphicsExposures (xDisplay, handle, true);
+	if (data.image == null) OS.XSetGraphicsExposures (xDisplay, handle, true);
 	OS.XCopyArea(xDisplay, xDrawable, xDrawable, handle, x, y, width, height, destX, destY);
-	OS.XSetGraphicsExposures (xDisplay, handle, false);
-	if (data.image != null) return;
-	boolean disjoint = (destX + width < x) || (x + width < destX) || (destY + height < y) || (y + height < destY);
-	if (disjoint) {
-		OS.XClearArea (xDisplay, xDrawable, x, y, width, height, true);
-	} else {
-		if (deltaX != 0) {
-			int newX = destX - deltaX;
-			if (deltaX < 0) newX = destX + width;
-			OS.XClearArea (xDisplay, xDrawable, newX, y, Math.abs (deltaX), height, true);
-		}
-		if (deltaY != 0) {
-			int newY = destY - deltaY;
-			if (deltaY < 0) newY = destY + height;
-			OS.XClearArea (xDisplay, xDrawable, x, newY, width, Math.abs (deltaY), true);
+	if (data.image == null) {
+		OS.XSetGraphicsExposures (xDisplay, handle, false);
+		boolean disjoint = (destX + width < x) || (x + width < destX) || (destY + height < y) || (y + height < destY);
+		if (disjoint) {
+			OS.XClearArea (xDisplay, xDrawable, x, y, width, height, true);
+		} else {
+			if (deltaX != 0) {
+				int newX = destX - deltaX;
+				if (deltaX < 0) newX = destX + width;
+				OS.XClearArea (xDisplay, xDrawable, newX, y, Math.abs (deltaX), height, true);
+			}
+			if (deltaY != 0) {
+				int newY = destY - deltaY;
+				if (deltaY < 0) newY = destY + height;
+				OS.XClearArea (xDisplay, xDrawable, x, newY, width, Math.abs (deltaY), true);
+			}
 		}
 	}
 }
