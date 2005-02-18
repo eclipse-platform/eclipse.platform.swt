5db2047dab39b15ff2936cf51daab170f9747a2e
Carolyn MacLeod <carolyn>
2005-02-18 16:34:20 +0000

    Now calls Standalone Paint Example - initial release

--- examples/org.eclipse.swt.examples.paint/src/org/eclipse/swt/examples/paint/PaintPlugin.java
+++ examples/org.eclipse.swt.examples.paint/src/org/eclipse/swt/examples/paint/PaintPlugin.java
@@ -15,14 +15,12 @@ import org.eclipse.core.runtime.*;
 import org.eclipse.ui.plugin.*;
 import org.osgi.framework.BundleContext;
 
-import java.text.*;
 import java.util.*;
 
 /**
  * The main plugin class to be used in the desktop.
  */
 public class PaintPlugin extends AbstractUIPlugin {
-	//The shared instance.
 	private static PaintPlugin plugin;
 	private static ResourceBundle resourceBundle;
 
@@ -35,13 +33,6 @@ public class PaintPlugin extends AbstractUIPlugin {
 	}
 	
 	/**
-	 * Returns the shared instance.
-	 */
-	public static PaintPlugin getDefault() {
-		return plugin;
-	}
-
-	/**
 	 * Log an error to the ILog for this plugin
 	 * 
 	 * @param message the localized error message text
@@ -52,36 +43,6 @@ public class PaintPlugin extends AbstractUIPlugin {
 			new Status(IStatus.ERROR, plugin.getBundle().getSymbolicName(), 0, message, exception));
 	}
 
-	/**
-	 * Returns a string from the resource bundle.
-	 * We don't want to crash because of a missing String.
-	 * Returns the key if not found.
-	 */
-	public static String getResourceString(String key) {
-		try {
-			return resourceBundle.getString(key);
-		} catch (MissingResourceException e) {
-			return key;
-		} catch (NullPointerException e) {
-			return "!" + key + "!";
-		}			
-	}
-
-	/**
-	 * Returns a string from the resource bundle and binds it
-	 * with the given arguments. If the key is not found,
-	 * return the key.
-	 */
-	public static String getResourceString(String key, Object[] args) {
-		try {
-			return MessageFormat.format(getResourceString(key), args);
-		} catch (MissingResourceException e) {
-			return key;
-		} catch (NullPointerException e) {
-			return "!" + key + "!";
-		}
-	}
-	
     public void start(BundleContext context) throws Exception {
         super.start(context);
         resourceBundle = Platform.getResourceBundle(getBundle());
