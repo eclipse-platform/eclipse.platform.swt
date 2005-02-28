e2b04c5a78e8200f4dd7331962de1396873fdde6
Veronika Irvine <veronika>
2005-02-28 23:09:44 +0000

    Updated legal documentation for Eclipse 3.1 stream and CPL to EPL transition

--- bundles/org.eclipse.swt.opengl/win32/library/build.bat
+++ bundles/org.eclipse.swt.opengl/win32/library/build.bat
@@ -1,9 +1,9 @@
 rem *******************************************************************************
-rem  Copyright (c) 2000, 2003 IBM Corporation and others.
-rem  All rights reserved. This program and the accompanying materials 
-rem  are made available under the terms of the Common Public License v1.0
-rem  which accompanies this distribution, and is available at
-rem  http://www.eclipse.org/legal/cpl-v10.html
+rem  Copyright (c) 2000, 2005 IBM Corporation and others.
+rem  All rights reserved. This program and the accompanying materials
+rem  are made available under the terms of the Eclipse Public License v1.0
+rem  which accompanies this distribution, and is available at 
+rem  http://www.eclipse.org/legal/epl-v10.html
 rem  
 rem  Contributors:
 rem      IBM Corporation - initial API and implementation
