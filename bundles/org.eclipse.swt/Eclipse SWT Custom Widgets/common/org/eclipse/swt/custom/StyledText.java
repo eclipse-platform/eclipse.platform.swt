eb011d3aeafde7f5376c18ebde6161e907df66cf
Veronika Irvine <veronika>
2005-04-06 18:06:22 +0000

    Bug 90232 If StyledText wraps, it will never require a horizontal scrollbar so ignore H_SCROLL in this case.  This is consistent with Text.

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -2007,6 +2007,9 @@ static int checkStyle(int style) {
 		style &= ~(SWT.H_SCROLL | SWT.V_SCROLL | SWT.WRAP | SWT.MULTI);
 	} else {
 		style |= SWT.MULTI;
+		if ((style & SWT.WRAP) != 0) {
+			style &= ~SWT.H_SCROLL;
+		}
 	}
 	return style;
 }
