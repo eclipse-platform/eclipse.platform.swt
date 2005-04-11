09d3d27112c3718ac3fdb353aa909421cf271858
Steve Northover <steve>
2005-04-11 19:30:27 +0000

    Put back original code because the bug was Menu.getItems() returning array with null's

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Menu.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Menu.java
@@ -817,13 +817,9 @@ String getNameText () {
 	int length = items.length;
 	if (length > 0) {
 		for (int i=0; i<length-1; i++) {
-			if (items [i] != null) {
-				result = result + items [i].getNameText() + ", ";
-			}
+			result = result + items [i].getNameText() + ", ";
 		}
-		if (items [length-1] != null) {
-			result = result + items [length-1].getNameText ();
-		}
+		result = result + items [length-1].getNameText ();
 	}
 	return result;
 }
