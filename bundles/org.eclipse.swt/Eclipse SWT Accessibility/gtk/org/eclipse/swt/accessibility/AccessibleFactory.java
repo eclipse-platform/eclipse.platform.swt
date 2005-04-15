6cd1ca6632ac80e9ec1f495926ca79670db9ab98
Veronika Irvine <veronika>
2005-04-15 12:31:50 +0000

    bug 81242 - check for no more callbacks and clean up compiler warnings

--- bundles/org.eclipse.swt/Eclipse SWT Accessibility/gtk/org/eclipse/swt/accessibility/AccessibleFactory.java
+++ bundles/org.eclipse.swt/Eclipse SWT Accessibility/gtk/org/eclipse/swt/accessibility/AccessibleFactory.java
@@ -15,6 +15,7 @@ import java.util.*;
 import org.eclipse.swt.internal.*;
 import org.eclipse.swt.internal.accessibility.gtk.*;
 import org.eclipse.swt.internal.gtk.*;
+import org.eclipse.swt.*;
 
 class AccessibleFactory {
 	int /*long*/ handle;
@@ -26,11 +27,11 @@ class AccessibleFactory {
 	
 	static final Hashtable Types = new Hashtable (9);
 	static final Hashtable Factories = new Hashtable (9);	
-	static final int /*long*/ DefaultParentType = ATK.g_type_from_name (Converter.wcsToMbcs (null, "GtkAccessible", true));
-	static final byte[] FACTORY_PARENTTYPENAME = Converter.wcsToMbcs (null, "AtkObjectFactory", true);
-	static final byte[] SWT_TYPE_PREFIX = Converter.wcsToMbcs (null, "SWT", false);
-	static final byte[] CHILD_TYPENAME = Converter.wcsToMbcs (null, "Child", false);
-	static final byte[] FACTORY_TYPENAME = Converter.wcsToMbcs (null, "SWTFactory", true);
+	static final int /*long*/ DefaultParentType = OS.g_type_from_name (Converter.wcsToMbcs (null, "GtkAccessible", true)); //$NON-NLS-1$
+	static final byte[] FACTORY_PARENTTYPENAME = Converter.wcsToMbcs (null, "AtkObjectFactory", true); //$NON-NLS-1$
+	static final byte[] SWT_TYPE_PREFIX = Converter.wcsToMbcs (null, "SWT", false); //$NON-NLS-1$
+	static final byte[] CHILD_TYPENAME = Converter.wcsToMbcs (null, "Child", false); //$NON-NLS-1$
+	static final byte[] FACTORY_TYPENAME = Converter.wcsToMbcs (null, "SWTFactory", true); //$NON-NLS-1$
 	static final int[] actionRoles = {
 		ACC.ROLE_CHECKBUTTON, ACC.ROLE_COMBOBOX, ACC.ROLE_LINK,
 		ACC.ROLE_MENUITEM, ACC.ROLE_PUSHBUTTON, ACC.ROLE_RADIOBUTTON,
@@ -88,94 +89,102 @@ class AccessibleFactory {
 	static final int /*long*/ SelectionIfaceDefinition;
 	static final int /*long*/ TextIfaceDefinition;
 	static {
-		AtkActionCB_get_keybinding = new Callback (AccessibleObject.class, "atkAction_get_keybinding", 2);
-		AtkActionCB_get_name = new Callback (AccessibleObject.class, "atkAction_get_name", 2);
-		AtkComponentCB_get_extents = new Callback (AccessibleObject.class, "atkComponent_get_extents", 6);
-		AtkComponentCB_get_position = new Callback (AccessibleObject.class, "atkComponent_get_position", 4);
-		AtkComponentCB_get_size = new Callback (AccessibleObject.class, "atkComponent_get_size", 4);
-		AtkComponentCB_ref_accessible_at_point = new Callback (AccessibleObject.class, "atkComponent_ref_accessible_at_point", 4);
-		AtkHypertextCB_get_link = new Callback (AccessibleObject.class, "atkHypertext_get_link", 2);
-		AtkHypertextCB_get_n_links = new Callback (AccessibleObject.class, "atkHypertext_get_n_links", 1);
-		AtkHypertextCB_get_link_index = new Callback (AccessibleObject.class, "atkHypertext_get_link_index", 2);
-		AtkObjectCB_get_name = new Callback (AccessibleObject.class, "atkObject_get_name", 1);
-		AtkObjectCB_get_description = new Callback (AccessibleObject.class, "atkObject_get_description", 1);
-		AtkObjectCB_get_n_children = new Callback (AccessibleObject.class, "atkObject_get_n_children", 1);
-		AtkObjectCB_get_role = new Callback (AccessibleObject.class, "atkObject_get_role", 1);
-		AtkObjectCB_get_parent = new Callback (AccessibleObject.class, "atkObject_get_parent", 1);
-		AtkObjectCB_ref_state_set = new Callback (AccessibleObject.class, "atkObject_ref_state_set", 1);
-		AtkObjectCB_get_index_in_parent = new Callback (AccessibleObject.class, "atkObject_get_index_in_parent", 1);
-		AtkObjectCB_ref_child = new Callback (AccessibleObject.class, "atkObject_ref_child", 2);
-		AtkSelectionCB_is_child_selected = new Callback (AccessibleObject.class, "atkSelection_is_child_selected", 2);
-		AtkSelectionCB_ref_selection = new Callback (AccessibleObject.class, "atkSelection_ref_selection", 2);
-		AtkTextCB_get_caret_offset = new Callback (AccessibleObject.class, "atkText_get_caret_offset", 1);
-		AtkTextCB_get_n_selections = new Callback (AccessibleObject.class, "atkText_get_n_selections", 1);
-		AtkTextCB_get_selection = new Callback (AccessibleObject.class, "atkText_get_selection", 4);
-		AtkTextCB_get_text = new Callback (AccessibleObject.class, "atkText_get_text", 3);
-		AtkTextCB_get_text_after_offset = new Callback (AccessibleObject.class, "atkText_get_text_after_offset", 5);
-		AtkTextCB_get_text_at_offset = new Callback (AccessibleObject.class, "atkText_get_text_at_offset", 5);
-		AtkTextCB_get_text_before_offset = new Callback (AccessibleObject.class, "atkText_get_text_before_offset", 5);
-		AtkTextCB_get_character_at_offset = new Callback (AccessibleObject.class, "atkText_get_character_at_offset", 2);
-		AtkTextCB_get_character_count = new Callback (AccessibleObject.class, "atkText_get_character_count", 1);
-		GObjectClass_finalize = new Callback (AccessibleObject.class, "gObjectClass_finalize", 1);
-		GTypeInfo_base_init_type = new Callback (AccessibleFactory.class, "gTypeInfo_base_init_type", 1);
+		AtkActionCB_get_keybinding = newCallback ("atkAction_get_keybinding", 2); //$NON-NLS-1$
+		AtkActionCB_get_name = newCallback ("atkAction_get_name", 2); //$NON-NLS-1$
+		AtkComponentCB_get_extents = newCallback ("atkComponent_get_extents", 6); //$NON-NLS-1$
+		AtkComponentCB_get_position = newCallback ("atkComponent_get_position", 4); //$NON-NLS-1$
+		AtkComponentCB_get_size = newCallback ("atkComponent_get_size", 4); //$NON-NLS-1$
+		AtkComponentCB_ref_accessible_at_point = newCallback ("atkComponent_ref_accessible_at_point", 4); //$NON-NLS-1$
+		AtkHypertextCB_get_link = newCallback ("atkHypertext_get_link", 2); //$NON-NLS-1$
+		AtkHypertextCB_get_n_links = newCallback ("atkHypertext_get_n_links", 1); //$NON-NLS-1$
+		AtkHypertextCB_get_link_index = newCallback ("atkHypertext_get_link_index", 2); //$NON-NLS-1$
+		AtkObjectCB_get_name = newCallback ("atkObject_get_name", 1); //$NON-NLS-1$
+		AtkObjectCB_get_description = newCallback ("atkObject_get_description", 1); //$NON-NLS-1$
+		AtkObjectCB_get_n_children = newCallback ("atkObject_get_n_children", 1); //$NON-NLS-1$
+		AtkObjectCB_get_role = newCallback ("atkObject_get_role", 1); //$NON-NLS-1$
+		AtkObjectCB_get_parent = newCallback ("atkObject_get_parent", 1); //$NON-NLS-1$
+		AtkObjectCB_ref_state_set = newCallback ("atkObject_ref_state_set", 1); //$NON-NLS-1$
+		AtkObjectCB_get_index_in_parent = newCallback ("atkObject_get_index_in_parent", 1); //$NON-NLS-1$
+		AtkObjectCB_ref_child = newCallback ("atkObject_ref_child", 2); //$NON-NLS-1$
+		AtkSelectionCB_is_child_selected = newCallback ("atkSelection_is_child_selected", 2); //$NON-NLS-1$
+		AtkSelectionCB_ref_selection = newCallback ("atkSelection_ref_selection", 2); //$NON-NLS-1$
+		AtkTextCB_get_caret_offset = newCallback ("atkText_get_caret_offset", 1); //$NON-NLS-1$
+		AtkTextCB_get_n_selections = newCallback ("atkText_get_n_selections", 1); //$NON-NLS-1$
+		AtkTextCB_get_selection = newCallback ("atkText_get_selection", 4); //$NON-NLS-1$
+		AtkTextCB_get_text = newCallback ("atkText_get_text", 3); //$NON-NLS-1$
+		AtkTextCB_get_text_after_offset = newCallback ("atkText_get_text_after_offset", 5); //$NON-NLS-1$
+		AtkTextCB_get_text_at_offset = newCallback ( "atkText_get_text_at_offset", 5); //$NON-NLS-1$
+		AtkTextCB_get_text_before_offset = newCallback ("atkText_get_text_before_offset", 5); //$NON-NLS-1$
+		AtkTextCB_get_character_at_offset = newCallback ("atkText_get_character_at_offset", 2); //$NON-NLS-1$
+		AtkTextCB_get_character_count = newCallback ("atkText_get_character_count", 1); //$NON-NLS-1$
+		GObjectClass_finalize = newCallback ("gObjectClass_finalize", 1); //$NON-NLS-1$
+		GTypeInfo_base_init_type = newCallback ("gTypeInfo_base_init_type", 1); //$NON-NLS-1$
 		/* Action interface */
-		InitActionIfaceCB = new Callback (AccessibleFactory.class, "initActionIfaceCB", 1);
+		InitActionIfaceCB = newCallback ("initActionIfaceCB", 1); //$NON-NLS-1$
 		GInterfaceInfo interfaceInfo = new GInterfaceInfo ();
 		interfaceInfo.interface_init = InitActionIfaceCB.getAddress ();
 		ActionIfaceDefinition = OS.g_malloc (GInterfaceInfo.sizeof);  
-		ATK.memmove (ActionIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
+		OS.memmove (ActionIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
 		/* Component interface */
-		InitComponentIfaceCB = new Callback (AccessibleFactory.class, "initComponentIfaceCB", 1);
+		InitComponentIfaceCB = newCallback ("initComponentIfaceCB", 1); //$NON-NLS-1$
 		interfaceInfo = new GInterfaceInfo ();
 		interfaceInfo.interface_init = InitComponentIfaceCB.getAddress ();
 		ComponentIfaceDefinition = OS.g_malloc (GInterfaceInfo.sizeof);
-		ATK.memmove (ComponentIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
+		OS.memmove (ComponentIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
 		/* Hypertext interface */
-		InitHypertextIfaceCB = new Callback (AccessibleFactory.class, "initHypertextIfaceCB", 1);
+		InitHypertextIfaceCB = newCallback ("initHypertextIfaceCB", 1); //$NON-NLS-1$
 		interfaceInfo = new GInterfaceInfo ();
 		interfaceInfo.interface_init = InitHypertextIfaceCB.getAddress ();
 		HypertextIfaceDefinition = OS.g_malloc (GInterfaceInfo.sizeof);  
-		ATK.memmove (HypertextIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
+		OS.memmove (HypertextIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
 		/* Selection interface */
-		InitSelectionIfaceCB = new Callback (AccessibleFactory.class, "initSelectionIfaceCB", 1);
+		InitSelectionIfaceCB = newCallback ("initSelectionIfaceCB", 1); //$NON-NLS-1$
 		interfaceInfo = new GInterfaceInfo ();
 		interfaceInfo.interface_init = InitSelectionIfaceCB.getAddress ();
 		SelectionIfaceDefinition = OS.g_malloc (GInterfaceInfo.sizeof);  
-		ATK.memmove (SelectionIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
+		OS.memmove (SelectionIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
 		/* Text interface */
-		InitTextIfaceCB = new Callback (AccessibleFactory.class, "initTextIfaceCB", 1);
+		InitTextIfaceCB = newCallback ("initTextIfaceCB", 1); //$NON-NLS-1$
 		interfaceInfo = new GInterfaceInfo ();
 		interfaceInfo.interface_init = InitTextIfaceCB.getAddress ();
 		TextIfaceDefinition = OS.g_malloc (GInterfaceInfo.sizeof);  
-		ATK.memmove (TextIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
+		OS.memmove (TextIfaceDefinition, interfaceInfo, GInterfaceInfo.sizeof);
 	}
 
+	static private Callback newCallback (String method, int argCount) {
+		Callback callback = new Callback (AccessibleFactory.class, method, argCount);
+		if (callback.getAddress () == 0) SWT.error (SWT.ERROR_NO_MORE_CALLBACKS);
+		return callback;
+	}
+	
 	private AccessibleFactory (int /*long*/ widgetType) {
 		super ();
 		/* If DefaultParentType is 0 then OS accessibility is not active */
 		if (DefaultParentType == 0) return;
-		widgetTypeName = ATK.g_type_name (widgetType);
+		widgetTypeName = OS.g_type_name (widgetType);
 		int widgetTypeNameLength = OS.strlen (widgetTypeName) + 1;
 		byte[] buffer = new byte [widgetTypeNameLength];
 		OS.memmove (buffer, widgetTypeName, widgetTypeNameLength);
 		byte[] factoryName = new byte [FACTORY_TYPENAME.length + widgetTypeNameLength];
 		System.arraycopy (FACTORY_TYPENAME, 0, factoryName, 0, FACTORY_TYPENAME.length);
 		System.arraycopy (buffer, 0, factoryName, FACTORY_TYPENAME.length, widgetTypeNameLength);
-		if (ATK.g_type_from_name (factoryName) == 0) {
+		if (OS.g_type_from_name (factoryName) == 0) {
 			/* register the factory */
 			int /*long*/ registry = ATK.atk_get_default_registry ();
 			int /*long*/ previousFactory = ATK.atk_registry_get_factory (registry, widgetType);
 			objectParentType = ATK.atk_object_factory_get_accessible_type (previousFactory);
 			if (objectParentType == 0) objectParentType = DefaultParentType;
-			int /*long*/ factoryParentType = ATK.g_type_from_name (FACTORY_PARENTTYPENAME);
-			gTypeInfo_base_init_factory  = new Callback (this, "gTypeInfo_base_init_factory", 1);
+			int /*long*/ factoryParentType = OS.g_type_from_name (FACTORY_PARENTTYPENAME);
+			gTypeInfo_base_init_factory  = new Callback (this, "gTypeInfo_base_init_factory", 1); //$NON-NLS-1$
+			int address = gTypeInfo_base_init_factory.getAddress ();
+			if (address == 0) SWT.error (SWT.ERROR_NO_MORE_CALLBACKS);
 			GTypeInfo typeInfo = new GTypeInfo ();
-			typeInfo.base_init = gTypeInfo_base_init_factory.getAddress ();
+			typeInfo.base_init = address;
 			typeInfo.class_size = (short)ATK.AtkObjectFactoryClass_sizeof ();
 			typeInfo.instance_size = (short)ATK.AtkObjectFactory_sizeof ();
 			handle = OS.g_malloc (GTypeInfo.sizeof); 
-			ATK.memmove (handle, typeInfo, GTypeInfo.sizeof); 
-			int /*long*/ swtFactory = ATK.g_type_register_static (factoryParentType, factoryName, handle, 0);
+			OS.memmove (handle, typeInfo, GTypeInfo.sizeof); 
+			int /*long*/ swtFactory = OS.g_type_register_static (factoryParentType, factoryName, handle, 0);
 			ATK.atk_registry_set_factory_type (registry, widgetType, swtFactory);
 		}
 	}
@@ -192,7 +201,7 @@ class AccessibleFactory {
 			* we don't care about this control, so create it with the parent's
 			* type so that its accessibility callbacks will not pass though here 
 			*/  
-			int /*long*/ result = ATK.g_object_new (objectParentType, 0);
+			int /*long*/ result = OS.g_object_new (objectParentType, 0);
 			ATK.atk_object_initialize (result, widget);
 			return result;
 		}
@@ -251,10 +260,10 @@ class AccessibleFactory {
 		}
 		String swtTypeName = new String (SWT_TYPE_PREFIX);
 		swtTypeName += new String (widgetTypeName);
-		if (action) swtTypeName += "Action";
-		if (hypertext) swtTypeName += "Hypertext";
-		if (selection) swtTypeName += "Selection";
-		if (text) swtTypeName += "Text";
+		if (action) swtTypeName += "Action"; //$NON-NLS-1$
+		if (hypertext) swtTypeName += "Hypertext"; //$NON-NLS-1$
+		if (selection) swtTypeName += "Selection"; //$NON-NLS-1$
+		if (text) swtTypeName += "Text"; //$NON-NLS-1$
 
 		int /*long*/ type = 0;
 		LONG typeInt = (LONG)Types.get (swtTypeName);
@@ -263,24 +272,24 @@ class AccessibleFactory {
 		} else {
 			/* define the type */
 			int /*long*/ queryPtr = OS.g_malloc (GTypeQuery.sizeof);
-			ATK.g_type_query (parentType, queryPtr);
+			OS.g_type_query (parentType, queryPtr);
 			GTypeQuery query = new GTypeQuery ();
-			ATK.memmove (query, queryPtr, GTypeQuery.sizeof);
+			OS.memmove (query, queryPtr, GTypeQuery.sizeof);
 			OS.g_free (queryPtr);
 			GTypeInfo typeInfo = new GTypeInfo ();
 			typeInfo.base_init = GTypeInfo_base_init_type.getAddress ();
 			typeInfo.class_size = (short) query.class_size;
 			typeInfo.instance_size = (short) query.instance_size;
 			ObjectIfaceDefinition = OS.g_malloc (GTypeInfo.sizeof); 
-			ATK.memmove (ObjectIfaceDefinition, typeInfo, GTypeInfo.sizeof);
+			OS.memmove (ObjectIfaceDefinition, typeInfo, GTypeInfo.sizeof);
 			byte[] nameBytes = new byte [swtTypeName.length () + 1];
 			System.arraycopy(swtTypeName.getBytes (), 0, nameBytes, 0, swtTypeName.length ()); 
-			type = ATK.g_type_register_static (parentType, nameBytes, ObjectIfaceDefinition, 0);
-			ATK.g_type_add_interface_static (type, AccessibleObject.ATK_COMPONENT_TYPE, ComponentIfaceDefinition);
-			if (action) ATK.g_type_add_interface_static (type, AccessibleObject.ATK_ACTION_TYPE, ActionIfaceDefinition);
-			if (hypertext) ATK.g_type_add_interface_static (type, AccessibleObject.ATK_HYPERTEXT_TYPE, HypertextIfaceDefinition);
-			if (selection) ATK.g_type_add_interface_static (type, AccessibleObject.ATK_SELECTION_TYPE, SelectionIfaceDefinition);
-			if (text) ATK.g_type_add_interface_static (type, AccessibleObject.ATK_TEXT_TYPE, TextIfaceDefinition);
+			type = OS.g_type_register_static (parentType, nameBytes, ObjectIfaceDefinition, 0);
+			OS.g_type_add_interface_static (type, AccessibleObject.ATK_COMPONENT_TYPE, ComponentIfaceDefinition);
+			if (action) OS.g_type_add_interface_static (type, AccessibleObject.ATK_ACTION_TYPE, ActionIfaceDefinition);
+			if (hypertext) OS.g_type_add_interface_static (type, AccessibleObject.ATK_HYPERTEXT_TYPE, HypertextIfaceDefinition);
+			if (selection) OS.g_type_add_interface_static (type, AccessibleObject.ATK_SELECTION_TYPE, SelectionIfaceDefinition);
+			if (text) OS.g_type_add_interface_static (type, AccessibleObject.ATK_TEXT_TYPE, TextIfaceDefinition);
 			Types.put (swtTypeName, new LONG (type));
 		}
 		return type;
@@ -290,8 +299,10 @@ class AccessibleFactory {
 		int /*long*/ atkObjectFactoryClass = ATK.ATK_OBJECT_FACTORY_CLASS (klass);
 		AtkObjectFactoryClass objectFactoryClassStruct = new AtkObjectFactoryClass ();
 		ATK.memmove (objectFactoryClassStruct, atkObjectFactoryClass);
-		atkObjectFactoryCB_create_accessible = new Callback (this, "atkObjectFactory_create_accessible", 1);
-		objectFactoryClassStruct.create_accessible = atkObjectFactoryCB_create_accessible.getAddress ();
+		atkObjectFactoryCB_create_accessible = new Callback (this, "atkObjectFactory_create_accessible", 1); //$NON-NLS-1$
+		int address = atkObjectFactoryCB_create_accessible.getAddress ();
+		if (address == 0) SWT.error (SWT.ERROR_NO_MORE_CALLBACKS);
+		objectFactoryClassStruct.create_accessible = address;
 		ATK.memmove (atkObjectFactoryClass, objectFactoryClassStruct); 
 		return 0;
 	}
@@ -307,11 +318,11 @@ class AccessibleFactory {
 		objectClass.ref_state_set = AtkObjectCB_ref_state_set.getAddress ();
 		objectClass.get_index_in_parent = AtkObjectCB_get_index_in_parent.getAddress ();
 		objectClass.ref_child = AtkObjectCB_ref_child.getAddress ();
-		int /*long*/ gObjectClass = ATK.G_OBJECT_CLASS (klass);
+		int /*long*/ gObjectClass = OS.G_OBJECT_CLASS (klass);
 		GObjectClass objectClassStruct = new GObjectClass ();
-		ATK.memmove (objectClassStruct, gObjectClass);
+		OS.memmove (objectClassStruct, gObjectClass);
 		objectClassStruct.finalize = GObjectClass_finalize.getAddress ();
-		ATK.memmove (gObjectClass, objectClassStruct); 
+		OS.memmove (gObjectClass, objectClassStruct); 
 		ATK.memmove (klass, objectClass);
 		return 0;
 	}
@@ -373,7 +384,7 @@ class AccessibleFactory {
 
 	static void registerAccessible (Accessible accessible) {
 		int /*long*/ controlHandle = accessible.getControlHandle ();
-		int /*long*/ widgetType = ATK.G_OBJECT_TYPE (controlHandle);
+		int /*long*/ widgetType = OS.G_OBJECT_TYPE (controlHandle);
 		AccessibleFactory factory = (AccessibleFactory) Factories.get (new LONG (widgetType));
 		if (factory == null) {
 			factory = new AccessibleFactory (widgetType);
@@ -388,7 +399,7 @@ class AccessibleFactory {
 	
 	static void unregisterAccessible (Accessible accessible) {
 		int /*long*/ controlHandle = accessible.getControlHandle ();
-		int /*long*/ widgetType = ATK.G_OBJECT_TYPE (controlHandle);
+		int /*long*/ widgetType = OS.G_OBJECT_TYPE (controlHandle);
 		AccessibleFactory factory = (AccessibleFactory) Factories.get (new LONG (widgetType));
 		if (factory != null) {
 			factory.removeAccessible (accessible);
