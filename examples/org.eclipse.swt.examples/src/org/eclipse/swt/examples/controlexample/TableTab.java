dc92378f396b7e8cdf4772d7501e0edaa92342ab
Grant Gayed <ggayed>
2005-05-16 15:43:44 +0000

    94437

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/TableTab.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/TableTab.java
@@ -26,7 +26,7 @@ class TableTab extends ScrollableTab {
 	Button checkButton, fullSelectionButton, hideSelectionButton;
 
 	/* Other widgets added to the "Other" group */
-	Button multipleColumns, headerVisibleButton, linesVisibleButton;
+	Button multipleColumns, moveableColumns, headerVisibleButton, linesVisibleButton;
 	
 	/* Controls and resources added to the "Colors" group */
 	Button itemForegroundButton, itemBackgroundButton, itemFontButton;
@@ -154,23 +154,31 @@ class TableTab extends ScrollableTab {
 		super.createOtherGroup ();
 	
 		/* Create display controls specific to this example */
+		headerVisibleButton = new Button (otherGroup, SWT.CHECK);
+		headerVisibleButton.setText (ControlExample.getResourceString("Header_Visible"));
 		multipleColumns = new Button (otherGroup, SWT.CHECK);
 		multipleColumns.setText (ControlExample.getResourceString("Multiple_Columns"));
 		multipleColumns.setSelection(true);
-		headerVisibleButton = new Button (otherGroup, SWT.CHECK);
-		headerVisibleButton.setText (ControlExample.getResourceString("Header_Visible"));
+		moveableColumns = new Button (otherGroup, SWT.CHECK);
+		moveableColumns.setText (ControlExample.getResourceString("Moveable_Columns"));
+		moveableColumns.setSelection(false);
 		linesVisibleButton = new Button (otherGroup, SWT.CHECK);
 		linesVisibleButton.setText (ControlExample.getResourceString("Lines_Visible"));
 	
 		/* Add the listeners */
+		headerVisibleButton.addSelectionListener (new SelectionAdapter () {
+			public void widgetSelected (SelectionEvent event) {
+				setWidgetHeaderVisible ();
+			}
+		});
 		multipleColumns.addSelectionListener (new SelectionAdapter () {
 			public void widgetSelected (SelectionEvent event) {
 				recreateExampleWidgets ();
 			}
 		});
-		headerVisibleButton.addSelectionListener (new SelectionAdapter () {
+		moveableColumns.addSelectionListener (new SelectionAdapter () {
 			public void widgetSelected (SelectionEvent event) {
-				setWidgetHeaderVisible ();
+				setColumnsMoveable ();
 			}
 		});
 		linesVisibleButton.addSelectionListener (new SelectionAdapter () {
@@ -220,6 +228,7 @@ class TableTab extends ScrollableTab {
 		} else {
 			new TableColumn(table1, SWT.NONE);
 		}
+		setColumnsMoveable();
 		for (int i=0; i<16; i++) {
 			TableItem item = new TableItem (table1, SWT.NONE);
 			item.setImage (instance.images [i % 3]);
@@ -235,7 +244,7 @@ class TableTab extends ScrollableTab {
 			item.setText (tableData [index]);
 		} else {
 			item.setText (node);
-		}		
+		}
 	}
 	
 	/**
@@ -388,6 +397,14 @@ class TableTab extends ScrollableTab {
 		for (int i = 0; i < columnCount; i++) {
 			TableColumn tableColumn = table1.getColumn(i);
 			tableColumn.pack();
+		}
+	}
+
+	void setColumnsMoveable () {
+		boolean selection = moveableColumns.getSelection();
+		TableColumn[] columns = table1.getColumns();
+		for (int i = 0; i < columns.length; i++) {
+			columns[i].setMoveable(selection);
 		}
 	}
 
