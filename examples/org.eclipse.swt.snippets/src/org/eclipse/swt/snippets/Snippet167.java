e18cfcf45bc5a4f1c8ebfbe166501385af73ffc7
Veronika Irvine <veronika>
2005-06-01 17:06:10 +0000

    Modify example to show scrolling widgets that are not the same size

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet167.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet167.java
@@ -33,13 +33,13 @@ public static void main (String [] args) {
 	final ScrolledComposite sc1 = new ScrolledComposite (shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 	Button button1 = new Button (sc1, SWT.PUSH);
 	button1.setText ("Button 1");
-	button1.setSize (400, 400);
+	button1.setSize (400, 300);
 	sc1.setContent (button1);
 
 	final ScrolledComposite sc2 = new ScrolledComposite (shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 	Button button2 = new Button(sc2, SWT.PUSH);
 	button2.setText ("Button 2");
-	button2.setSize (400, 400);
+	button2.setSize (300, 400);
 	sc2.setContent (button2);
 
 	final ScrollBar vBar1 = sc1.getVerticalBar ();
@@ -48,16 +48,16 @@ public static void main (String [] args) {
 	final ScrollBar hBar2 = sc2.getHorizontalBar ();
 	SelectionListener listener1 = new SelectionAdapter () {
 		public void widgetSelected (SelectionEvent e) {
-			sc2.setOrigin (
-				hBar1.getSelection (),
-				vBar1.getSelection ());
+			int x =  hBar1.getSelection() * (hBar2.getMaximum() - hBar2.getThumb()) / Math.max(1, hBar1.getMaximum() - hBar1.getThumb());
+			int y =  vBar1.getSelection() * (vBar2.getMaximum() - vBar2.getThumb()) / Math.max(1, vBar1.getMaximum() - vBar1.getThumb());
+			sc2.setOrigin (x, y);
 		}
 	};
 	SelectionListener listener2 = new SelectionAdapter () {
 		public void widgetSelected (SelectionEvent e) {
-			sc1.setOrigin (
-				hBar2.getSelection (),
-				vBar2.getSelection ());
+			int x =  hBar2.getSelection() * (hBar1.getMaximum() - hBar1.getThumb()) / Math.max(1, hBar2.getMaximum() - hBar2.getThumb());
+			int y =  vBar2.getSelection() * (vBar1.getMaximum() - vBar1.getThumb()) / Math.max(1, vBar2.getMaximum() - vBar2.getThumb());
+			sc1.setOrigin (x, y);
 		}
 	};
 	vBar1.addSelectionListener (listener1);
@@ -65,7 +65,7 @@ public static void main (String [] args) {
 	vBar2.addSelectionListener (listener2);
 	hBar2.addSelectionListener (listener2);
 
-	shell.setSize (600, 300);
+	shell.setSize (400, 300);
 	shell.open ();
 	while (!shell.isDisposed ()) {
 		if (!display.readAndDispatch ()) display.sleep ();
