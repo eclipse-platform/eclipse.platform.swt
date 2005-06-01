eb5f47a40049d5bc2a7de317051a8f635536c429
Veronika Irvine <veronika>
2005-06-01 15:47:33 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet84.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet84.java
@@ -27,8 +27,40 @@ public static void main(String[] args) {
 	Display display = new Display();
 	Shell shell = new Shell(display);
 	shell.setLayout(new FillLayout());
+	
+	final Label label = new Label(shell, SWT.BORDER);
+	label.setText("Drag Source");
+	DragSource source = new DragSource(label, DND.DROP_COPY | DND.DROP_MOVE | DND.DROP_LINK);
+	source.setTransfer(new Transfer[] {TextTransfer.getInstance()});
+	source.addDragListener(new DragSourceAdapter() {
+		public void dragSetData(DragSourceEvent event) {
+			event.data = "Text Transferred";
+		}
+		public void dragFinished(DragSourceEvent event) {
+			if (event.doit) {
+				String operation = null;
+				switch (event.detail) {
+				case DND.DROP_MOVE:
+					operation = "moved"; break;
+				case DND.DROP_COPY:
+					operation = "copied"; break;
+				case DND.DROP_LINK:
+					operation = "linked"; break;
+				case DND.DROP_NONE:
+					operation = "disallowed"; break;
+				default:
+					operation = "unknown"; break;
+				}
+				label.setText("Drag Source (data "+operation+")");
+			} else {
+				label.setText("Drag Source (drag cancelled)");
+			}
+		}
+	});
+
 	final Text text = new Text(shell, SWT.BORDER | SWT.MULTI);
-	DropTarget target = new DropTarget(text, DND.DROP_DEFAULT | DND.DROP_COPY);
+	text.setText("Drop Target");
+	DropTarget target = new DropTarget(text, DND.DROP_DEFAULT | DND.DROP_COPY | DND.DROP_MOVE | DND.DROP_LINK);
 	target.setTransfer(new Transfer[] {TextTransfer.getInstance()});
 	target.addDropListener(new DropTargetAdapter() {
 		public void dragEnter(DropTargetEvent event) {
@@ -38,7 +70,20 @@ public static void main(String[] args) {
 			if (event.detail == DND.DROP_DEFAULT) event.detail = DND.DROP_COPY;
 		}
 		public void drop(DropTargetEvent event) {
-			text.setText((String)event.data);
+			String operation = null;
+			switch (event.detail) {
+			case DND.DROP_MOVE:
+				operation = "moved"; break;
+			case DND.DROP_COPY:
+				operation = "copied"; break;
+			case DND.DROP_LINK:
+				operation = "linked"; break;
+			case DND.DROP_NONE:
+				operation = "disallowed"; break;
+			default:
+				operation = "unknown"; break;
+			}
+			text.append("\n"+operation+(String)event.data);
 		}
 	});
 	
