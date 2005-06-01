46b0d391c037bee7f49eca48c32f9ca46fc1a686
Veronika Irvine <veronika>
2005-06-01 17:38:45 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet149.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet149.java
@@ -28,19 +28,22 @@ public class Snippet149 {
 		shell.setLayout (new FillLayout ());
 		Table table = new Table (shell, SWT.BORDER);
 		table.setHeaderVisible (true);
+		table.setLinesVisible(true);
 		for (int i=0; i<2; i++) {
 			TableColumn column = new TableColumn (table, SWT.NONE);
 		}
 		table.getColumn (0).setText ("Task");
 		table.getColumn (1).setText ("Progress");
-		for (int i=0; i<4; i++) {
+		for (int i=0; i<40; i++) {
 			TableItem item = new TableItem (table, SWT.NONE);
 			item.setText ("Task " + i);
-			ProgressBar bar = new ProgressBar (table, SWT.NONE);
-			bar.setSelection (i * 25);
-			TableEditor editor = new TableEditor (table);
-			editor.grabHorizontal = editor.grabVertical = true;
-			editor.setEditor (bar, item, 1);
+			if ( i % 5 == 0) {
+				ProgressBar bar = new ProgressBar (table, SWT.NONE);
+				bar.setSelection (i);
+				TableEditor editor = new TableEditor (table);
+				editor.grabHorizontal = editor.grabVertical = true;
+				editor.setEditor (bar, item, 1);
+			}
 		}
 		table.getColumn (0).pack ();
 		table.getColumn (1).setWidth (128);
