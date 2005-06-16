bad49bb857864010e0be570d38f6094d749c657b
Veronika Irvine <veronika>
2005-06-16 13:00:21 +0000

    javadoc

--- bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/Browser.java
+++ bundles/org.eclipse.swt/Eclipse SWT Browser/win32/org/eclipse/swt/browser/Browser.java
@@ -572,7 +572,7 @@ public void addCloseWindowListener(CloseWindowListener listener) {
 
 /**	 
  * Adds the listener to the collection of listeners who will be
- * notified when the current location is changed or about to be changed.
+ * notified when the current location has changed or is about to change.
  * <p>
  * This notification typically occurs when the application navigates
  * to a new location with {@link #setUrl(String)} or when the user
