94c3b6440b49d6bba6ace54503f0a15dbfe61b4b
Steve Northover <steve>
2005-07-07 22:30:35 +0000

    fix formatting

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet3.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet3.java
@@ -22,47 +22,51 @@ import org.eclipse.swt.graphics.*;
 
 public class Snippet3 {
 
-public static void main (String [] args) {
-        Display display = new Display ();
-        Shell shell = new Shell (display);
-        final Table table = new Table (shell, SWT.BORDER | SWT.V_SCROLL | SWT.FULL_SELECTION);
-        table.setHeaderVisible (true);
-        table.setLinesVisible (true);
-        final int rowCount = 64, columnCount = 4;
-        for (int i=0; i < columnCount; i++) {
-                TableColumn column = new TableColumn (table, SWT.NONE);
-                column.setText ("Column " + i);
-        }
-        for (int i=0; i < rowCount; i++) {
-                TableItem item = new TableItem (table, SWT.NONE);
-                for (int j=0; j < columnCount; j++) {
-                        item.setText (j, "Item " + i + "-" + j);
-                }
-        }
-        for (int i=0; i < columnCount; i++) {
-                table.getColumn (i).pack ();
-        }
-        Point size = table.computeSize (SWT.DEFAULT, 200);
-        table.setSize (size);
-        shell.pack ();
-        table.addListener (SWT.MouseDown, new Listener () {
-                public void handleEvent (Event event) {
-                        Point pt = new Point (event.x, event.y);
-                        TableItem item = table.getItem (pt);
-                        if (item == null) return;
-                        for (int i=0; i < columnCount; i++) {
-                                Rectangle rect = item.getBounds (i);
-                                if (rect.contains (pt)) {
-                                        int index = table.indexOf (item);
-                                        System.out.println ("Item " + index + "-" + i);
-                                }
-                        } 
-                }
-        });
-        shell.open ();
-        while (!shell.isDisposed()) {
-                if (!display.readAndDispatch ()) display.sleep ();
-        }
-        display.dispose ();
+public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	final Table table = new Table(shell, SWT.BORDER | SWT.V_SCROLL
+			| SWT.FULL_SELECTION);
+	table.setHeaderVisible(true);
+	table.setLinesVisible(true);
+	final int rowCount = 64, columnCount = 4;
+	for (int i = 0; i < columnCount; i++) {
+		TableColumn column = new TableColumn(table, SWT.NONE);
+		column.setText("Column " + i);
+	}
+	for (int i = 0; i < rowCount; i++) {
+		TableItem item = new TableItem(table, SWT.NONE);
+		for (int j = 0; j < columnCount; j++) {
+			item.setText(j, "Item " + i + "-" + j);
+		}
+	}
+	for (int i = 0; i < columnCount; i++) {
+		table.getColumn(i).pack();
+	}
+	Point size = table.computeSize(SWT.DEFAULT, 200);
+	table.setSize(size);
+	shell.pack();
+	table.addListener(SWT.MouseDown, new Listener() {
+		public void handleEvent(Event event) {
+			Point pt = new Point(event.x, event.y);
+			TableItem item = table.getItem(pt);
+			if (item == null)
+				return;
+			for (int i = 0; i < columnCount; i++) {
+				Rectangle rect = item.getBounds(i);
+				if (rect.contains(pt)) {
+					int index = table.indexOf(item);
+					System.out.println("Item " + index + "-" + i);
+				}
+			}
+		}
+	});
+	shell.open();
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch())
+			display.sleep();
+	}
+	display.dispose();
 }
+
 }
