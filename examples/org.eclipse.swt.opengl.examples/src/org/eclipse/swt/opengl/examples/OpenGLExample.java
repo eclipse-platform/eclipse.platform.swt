14d66b7df7582c81a12aa0563720ef8e6bea121e
Billy Biggs <bbiggs>
2005-09-23 03:29:15 +0000

    First attempt at turning the OpenGL example into a plugin.

--- examples/org.eclipse.swt.opengl.examples/src/org/eclipse/swt/opengl/examples/OpenGLExample.java
+++ examples/org.eclipse.swt.opengl.examples/src/org/eclipse/swt/opengl/examples/OpenGLExample.java
@@ -15,40 +15,23 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
+import java.util.*;
+
 public class OpenGLExample {
+	private static ResourceBundle resourceBundle =
+		ResourceBundle.getBundle("examples_opengl");
 	private TabFolder tabFolder;
 	private OpenGLTab[] tabs;
 	private int sleep;
-
-	/**
-	 * Renders the scene of the current tab.
-	 */
-	void display() {
-		int index = tabFolder.getSelectionIndex();
-		tabs[index].render();
-		tabs[index].swap();
-	}
 	
 	/**
-	 * Disposes of all contained tabs.
+	 * Creates an instance of an OpenGLExample embedded inside
+	 * the supplied parent Composite.
+	 * 
+	 * @param parent the container of the example
 	 */
-	void dispose() {
-		tabFolder = null;
-		for (int i = 0; i < tabs.length; i++) {
-			tabs[i].dispose();
-		}
-	}
-
-	/**
-	 * Runs the OpenGL example
-	 */
-	void run() {
-		final Display display = new Display();
-		final Shell shell =
-			new Shell(display, SWT.SHELL_TRIM | SWT.NO_BACKGROUND);
-		shell.setLayout(new FillLayout());
-
-		tabFolder = new TabFolder(shell, SWT.NONE);
+	OpenGLExample(Composite parent) {		
+		tabFolder = new TabFolder(parent, SWT.NONE);
 		tabs =
 			new OpenGLTab[] {
 				new AntialiasingTab(),
@@ -82,32 +65,68 @@ public class OpenGLExample {
 			sleep = tabs[0].getSleepLength();
 		}
 		
-		shell.setText("OpenGL Example");
 		Runnable timer = new Runnable() {
 			public void run() {
-				if (shell.isDisposed()) return;
+				if (tabFolder.isDisposed()) return;
 				display();
-				display.timerExec(sleep, this);
+				tabFolder.getDisplay().timerExec(sleep, this);
 			}
 		};
 		timer.run();
-		shell.addListener(SWT.Dispose, new Listener() {
+		tabFolder.addListener(SWT.Dispose, new Listener() {
 			public void handleEvent(Event e) {
 				dispose();
 			}
 		});
-		shell.open();
-		while (!shell.isDisposed()) {
-			if (!display.readAndDispatch()) {
-				display.sleep();
-			}
-		}
+	}
+
+	/**
+	 * Renders the scene of the current tab.
+	 */
+	void display() {
+		int index = tabFolder.getSelectionIndex();
+		tabs[index].render();
+		tabs[index].swap();
 	}
 	
+	/**
+	 * Disposes of all contained tabs.
+	 */
+	void dispose() {
+		tabFolder = null;
+		for (int i = 0; i < tabs.length; i++) {
+			tabs[i].dispose();
+		}
+	}
+
+	/**
+	 * Gets a string from the resource bundle.
+	 * We don't want to crash because of a missing String.
+	 * Returns the key if not found.
+	 */
+	static String getResourceString(String key) {
+		try {
+			return resourceBundle.getString(key);
+		} catch (MissingResourceException e) {
+			return key;
+		} catch (NullPointerException e) {
+			return "!" + key + "!";
+		}			
+	}
+
 	/**
 	 * Invokes as a standalone program.
 	 */
 	public static void main(String[] args) {
-		new OpenGLExample().run();
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+		OpenGLExample instance = new OpenGLExample(shell);
+		shell.setText(getResourceString("window.title"));
+		shell.open();
+		while (! shell.isDisposed()) {
+			if (! display.readAndDispatch()) display.sleep();
+		}
+		instance.dispose();
 	}
 }
