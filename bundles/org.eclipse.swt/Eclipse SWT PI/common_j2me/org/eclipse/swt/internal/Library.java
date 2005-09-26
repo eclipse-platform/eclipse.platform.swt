f110f3e90d6fd539b61655ecd33ca4276a81b7f8
Billy Biggs <bbiggs>
2005-09-26 22:22:39 +0000

    v3207

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 206;
+    static int MINOR_VERSION = 207;
 	
 	/**
 	 * SWT revision number (must be >= 0)
