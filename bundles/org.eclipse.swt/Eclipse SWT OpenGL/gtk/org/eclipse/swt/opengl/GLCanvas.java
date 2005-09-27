8c5ab29737a8c6f927651bdd71b84f989570d2f6
Billy Biggs <bbiggs>
2005-09-27 14:10:29 +0000

    Copyright fix.

--- bundles/org.eclipse.swt/Eclipse SWT OpenGL/gtk/org/eclipse/swt/opengl/GLCanvas.java
+++ bundles/org.eclipse.swt/Eclipse SWT OpenGL/gtk/org/eclipse/swt/opengl/GLCanvas.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,8 +29,7 @@ public class GLCanvas extends Canvas {
 	int /*long*/ xWindow;
 	int /*long*/ glWindow;
 	XVisualInfo vinfo;
-
-	private static final int MAX_ATTRIBUTES = 32;
+	static final int MAX_ATTRIBUTES = 32;
 
 /**
  * Create a GLCanvas widget using the attributes described in the GLData
