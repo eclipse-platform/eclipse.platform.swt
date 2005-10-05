4785a8104373bad4519f6d94aa534bcd95768f53
Billy Biggs <bbiggs>
2005-10-05 22:19:47 +0000

    Actually set the size of the vbox.

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -1072,6 +1072,7 @@ void resizeBounds (int width, int height, boolean notify) {
 	if (enableWindow != 0) {
 		OS.gdk_window_resize (enableWindow, width, height);
 	}
+	OS.gtk_widget_set_size_request (vboxHandle, width, height);
 	forceResize (width, height);
 	if (notify) {
 		resized = true;
