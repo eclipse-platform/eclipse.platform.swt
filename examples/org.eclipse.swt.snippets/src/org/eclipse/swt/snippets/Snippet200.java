4819d09b804c31c99bcffbe1f28922cbd2496655
Veronika Irvine <veronika>
2005-10-06 14:40:34 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet200.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet200.java
@@ -11,40 +11,53 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Create a path from some text
+ * Fill a shape with a predefined pattern
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
  */
 import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
-public class Snippet198 {
+public class Snippet200 {
 public static void main(String[] args) {
 	Display display = new Display();
-	FontData data = display.getSystemFont().getFontData()[0];
-	Font font = new Font(display, data.getName(), 96, SWT.BOLD | SWT.ITALIC);
-	final Color green = display.getSystemColor(SWT.COLOR_GREEN);
-	final Color blue = display.getSystemColor(SWT.COLOR_BLUE);
-	final Path path;
+	//define a pattern on an image
+	final Image image = new Image(display, 1000, 1000);
+	Color blue = display.getSystemColor(SWT.COLOR_BLUE);
+	Color yellow = display.getSystemColor(SWT.COLOR_YELLOW);
+	Color white = display.getSystemColor(SWT.COLOR_WHITE);
+	GC gc = new GC(image);
+	gc.setBackground(white);
+	gc.setForeground(yellow);
+	gc.fillGradientRectangle(0, 0, 1000, 1000, true);
+	for (int i=-500; i<1000; i+=10) {
+		gc.setForeground(blue);
+		gc.drawLine(i, 0, 500 + i, 1000);
+		gc.drawLine(500 + i, 0, i, 1000);
+	}	
+	gc.dispose();
+	final Pattern pattern;
 	try {
-		path = new Path(display);
-		path.addString("SWT", 0, 0, font);
+		pattern = new Pattern(display, image);
 	} catch (SWTException e) {
 		//Advanced Graphics not supported.  
 		//This new API requires the Cairo Vector engine on GTK and Motif and GDI+ on Windows.
 		System.out.println(e.getMessage());
 		return;
 	}
+	
 	Shell shell = new Shell(display);
-	shell.addListener(SWT.Paint, new Listener() {
-		public void handleEvent(Event e) {			
-			GC gc = e.gc;
-			gc.setBackground(green);
-			gc.setForeground(blue);
-			gc.fillPath(path);
-			gc.drawPath(path);
+	shell.setLayout(new FillLayout());
+	Composite c = new Composite(shell, SWT.DOUBLE_BUFFERED);
+	c.addListener(SWT.Paint, new Listener() {
+		public void handleEvent(Event event) {
+			Rectangle r = ((Composite)event.widget).getClientArea();
+			GC gc = event.gc;
+			gc.setBackgroundPattern(pattern);
+			gc.fillOval(5, 5, r.width - 10, r.height - 10);	
 		}
 	});
 	shell.open();
@@ -52,8 +65,7 @@ public static void main(String[] args) {
 		if (!display.readAndDispatch())
 			display.sleep();
 	}
-	path.dispose();
-	font.dispose();
+	pattern.dispose();
 	display.dispose();
 }
 }
