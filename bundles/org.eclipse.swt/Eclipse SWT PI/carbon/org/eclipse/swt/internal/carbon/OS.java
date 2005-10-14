257a168d7d24eff096db14008d248b67c174d134
Silenio Quarti <silenio>
2005-10-14 12:10:10 +0000

    still support Jaguar

--- bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/OS.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/carbon/org/eclipse/swt/internal/carbon/OS.java
@@ -20,12 +20,13 @@ public class OS extends Platform {
 		Library.loadLibrary ("swt-pi");
 	}
 	
-	public static final boolean HIVIEW = true;
+	public static final boolean HIVIEW;
 	public static final int VERSION;
 	static {
 		int [] response = new int [1];
 		OS.Gestalt (OS.gestaltSystemVersion, response);
 		VERSION  = response [0] & 0xffff;
+		HIVIEW = VERSION >= 0x1030;
 	}
 
 	/** Constants */
