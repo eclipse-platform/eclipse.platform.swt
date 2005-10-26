0e83e69e34c1016b74ad6756e1511fc7078c76c8
Silenio Quarti <silenio>
2005-10-26 18:04:01 +0000

    wrong test for is printer

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/GC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/graphics/GC.java
@@ -1163,7 +1163,7 @@ public void drawText (String string, int x, int y, int flags) {
 	boolean mode = true;
 	switch (data.textAntialias) {
 		case SWT.DEFAULT:
-			if (data.window != 0 && data.control == 0) mode = false;
+			if (data.window == 0 && data.control == 0) mode = false;
 			break;
 		case SWT.OFF: mode = false; break;
 	}
@@ -2270,7 +2270,7 @@ public void setAntialias(int antialias) {
 	switch (antialias) {
 		case SWT.DEFAULT:
 			/* Printer is off by default */
-			if (data.window != 0 && data.control == 0) mode = false;
+			if (data.window == 0 && data.control == 0) mode = false;
 			break;
 		case SWT.OFF: mode = false; break;
 		case SWT.ON: mode = true; break;
