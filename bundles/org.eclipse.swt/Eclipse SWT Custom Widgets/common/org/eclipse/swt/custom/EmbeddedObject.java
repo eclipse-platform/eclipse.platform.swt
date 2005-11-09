7348a99d384d379d493c1cc0ac205bef3f9cb8b8
Veronika Irvine <veronika>
2005-11-09 16:01:53 +0000

    fix compile warnings

---
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/EmbeddedObject.java
@@ -0,0 +1,166 @@
+
+/* UNDER CONSTRUCTION - DO NOT USE THIS CLASS */
+
+/*******************************************************************************
+ * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.custom;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+
+/**
+ * The Interface that needs to be implemented by object to be embedded in
+ * a StyledText.  
+ * <p>
+ *
+ * @see StyledText
+ */
+public abstract class EmbeddedObject {
+	public abstract int getAscent();
+	public abstract int getDescent();
+	public abstract int getAdvance();
+	public abstract void draw(GC gc, int x, int y, int ascent, int descent);
+
+	/**
+	 * Helper class to embedded a Control in a StyledText
+	 * 
+	 */
+	public static class EmbeddedControl extends EmbeddedObject {
+		Control control;
+		int alignment;
+		
+		/**
+		 * 
+		 * 
+		 * @param control the control
+		 * @param alignment the vertical alignment for the control
+		 * SWT.TOP == top of the line
+		 * SWT.CENTER == baseline
+		 * SWT.BOTTOM == bottom of the line
+		 */		
+		public EmbeddedControl(Control control, int alignment) {
+			if (control == null) SWT.error(SWT.ERROR_NULL_ARGUMENT);
+			if (control.isDisposed()) SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+			this.control = control;
+			this.alignment = alignment & (SWT.TOP | SWT.CENTER | SWT.BOTTOM);
+			if (this.alignment == 0) this.alignment = SWT.CENTER;
+		}
+		
+		public int getAscent() {
+			return control.getSize().y;
+		}
+		public int getDescent() {
+			return 0;
+		}
+		public int getAdvance() {
+			return control.getSize().x;
+		}
+		public void draw(GC gc, int x, int y, int ascent, int descent) {
+			switch (alignment) {
+				case SWT.TOP:
+					control.setLocation(x, y);
+					break;
+				case SWT.CENTER:
+					control.setLocation(x, y + ascent - getAscent());
+					break;
+				case SWT.BOTTOM:
+					control.setLocation(x, y + ascent + descent - getAscent());
+					break;
+			}
+		}
+	}
+	
+	/**
+	 * Helper class to embedded a Image in a StyledText
+	 * 
+	 */
+	public static class EmbeddedImage extends EmbeddedObject {
+		Image image;
+		int alignment;
+		
+		/**
+		 * 
+		 * 
+		 * @param image the image
+		 * @param alignment the vertical alignment for the control
+		 * SWT.TOP == top of the line
+		 * SWT.CENTER == baseline
+		 * SWT.BOTTOM == bottom of the line
+		 */		
+		public EmbeddedImage(Image image, int alignment) {
+			if (image == null) SWT.error(SWT.ERROR_NULL_ARGUMENT);
+			if (image.isDisposed()) SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+			this.image = image;
+			this.alignment = alignment & (SWT.TOP | SWT.CENTER | SWT.BOTTOM);
+			if (this.alignment == 0) this.alignment = SWT.CENTER;
+		}
+		
+		public int getAscent() {
+			return image.getBounds().height;
+		}
+		public int getDescent() {
+			return 0;
+		}
+		public int getAdvance() {
+			return image.getBounds().width;
+		}
+		public void draw(GC gc, int x, int y, int ascent, int descent) {
+			switch (alignment) {
+				case SWT.TOP:
+					gc.drawImage(image, x, y);
+					break;
+				case SWT.CENTER:
+					gc.drawImage(image, x, y + ascent - getAscent());				
+					break;
+				case SWT.BOTTOM:
+					gc.drawImage(image, x, y + ascent + descent - getAscent());
+					break;
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 *
+	 */	
+	public static class Bullet extends EmbeddedObject {
+		
+		/**
+		 * @param style bullet image, check mark image, number, roman, 
+		 */		
+		public Bullet(int style, int level, int index) {
+			
+		}
+		public int getAscent() {			
+			return 8;
+		}
+		public int getDescent() {
+			return 0;
+		}
+		public int getAdvance() {
+			return 50;
+		}
+		public void draw(GC gc, int x, int y, int ascent, int descent) {
+			gc.setBackground(Display.getCurrent().getSystemColor(SWT.COLOR_BLACK));
+			int height = Math.max(6, Math.min(10, ascent / 2));
+			gc.setAdvanced(true);
+			if (gc.getAdvanced()) {
+				gc.setAntialias(SWT.ON);
+			}
+			if ((height & 1) == 0) height++;
+			gc.fillOval(x + 50 - height - 4, y + ascent  - height, height, height);
+			gc.setAdvanced(false);  
+		}
+	}
+}
+
