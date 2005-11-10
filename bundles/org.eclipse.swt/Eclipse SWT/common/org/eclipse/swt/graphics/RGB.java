2503053f30d7051ae67b1f8960c73f8d3decfc0a
Veronika Irvine <veronika>
2005-11-10 14:19:30 +0000

    Bug 112358 add support for hue, saturation and brightness

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/RGB.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/RGB.java
@@ -10,7 +10,6 @@
  *******************************************************************************/
 package org.eclipse.swt.graphics;
 
-
 import org.eclipse.swt.internal.SerializableCompatibility;
 import org.eclipse.swt.*;
 
@@ -79,6 +78,109 @@ public RGB(int red, int green, int blue) {
 }
 
 /**
+* Constructs an instance of this class with the given
+* hue, saturation, and brightness.
+*
+* @param hue the hue value for the HSB color (from 0 to 360)
+* @param saturation the saturation value for the HSB color (from 0 to 1)
+* @param brightness the brightness value for the HSB color (from 0 to 1)
+*
+* @exception IllegalArgumentException <ul>
+*    <li>ERROR_INVALID_ARGUMENT - if the hue is not between 0 and 360 or
+*    the saturation or brightness is not between 0 and 1</li>
+* </ul>
+* 
+* @since 3.2
+*/
+public RGB(float hue, float saturation, float brightness) {
+	if (hue < 0 || hue > 360 || saturation < 0 || saturation > 1 || 
+		brightness < 0 || brightness > 1) {
+		SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+	}
+	float r, g, b;
+	if (saturation == 0) {
+		r = g = b = brightness; 
+	} else {
+		if (hue == 360) hue = 0;
+		hue /= 60;	
+		int i = (int)hue;
+		float f = hue - i;
+		float p = brightness * (1 - saturation);
+		float q = brightness * (1 - saturation * f);
+		float t = brightness * (1 - saturation * (1 - f));
+		switch(i) {
+			case 0:
+				r = brightness;
+				g = t;
+				b = p;
+				break;
+			case 1:
+				r = q;
+				g = brightness;
+				b = p;
+				break;
+			case 2:
+				r = p;
+				g = brightness;
+				b = t;
+				break;
+			case 3:
+				r = p;
+				g = q;
+				b = brightness;
+				break;
+			case 4:
+				r = t;
+				g = p;
+				b = brightness;
+				break;
+			case 5:
+			default:
+				r = brightness;
+				g = p;
+				b = q;
+				break;
+		}
+	}
+	red = (int)(r * 255 + 0.5);
+	green = (int)(g * 255 + 0.5);
+	blue = (int)(b * 255 + 0.5);	
+}
+
+/**
+ * Returns the hue, saturation, and brightness of the color.
+ * 
+ * @return color space values in float format (hue, saturation, brightness)
+ *
+ * @since 3.2
+ */
+public float[] getHSB() {	
+	float r = red / 255f;
+	float g = green / 255f;
+	float b = blue / 255f;
+	float max = Math.max(Math.max(r, g), b);
+	float min = Math.min(Math.min(r, g), b);
+	float delta = max - min;
+	float hue = 0;
+	float brightness = max;
+	float saturation = max == 0 ? 0 : (max - min) / max;
+	if (delta != 0) {
+		if (r == max) {
+			hue = (g  - b) / delta;
+		} else {
+			if (g == max) {
+				hue = 2 + (b - r) / delta;	
+			} else {
+				hue = 4 + (r - g) / delta;
+			}
+		}
+		hue *= 60;
+		if (hue < 0) hue += 360;
+	}
+	return new float[] {hue, saturation, brightness};
+}	
+
+/**
  * Compares the argument to the receiver, and returns true
  * if they represent the <em>same</em> object using a class
  * specific comparison.
@@ -88,7 +190,7 @@ public RGB(int red, int green, int blue) {
  *
  * @see #hashCode()
  */
-public boolean equals (Object object) {
+public boolean equals(Object object) {
 	if (object == this) return true;
 	if (!(object instanceof RGB)) return false;
 	RGB rgb = (RGB)object;
@@ -105,7 +207,7 @@ public boolean equals (Object object) {
  *
  * @see #equals(Object)
  */
-public int hashCode () {
+public int hashCode() {
 	return (blue << 16) | (green << 8) | red;
 }
 
@@ -115,7 +217,7 @@ public int hashCode () {
  *
  * @return a string representation of the <code>RGB</code>
  */
-public String toString () {
+public String toString() {
 	return "RGB {" + red + ", " + green + ", " + blue + "}"; //$NON-NLS-1$//$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 }
 
