7a850966fcebc2510dcdae26a2185eb84e5caa97
Carolyn MacLeod <carolyn>
2005-11-28 22:16:37 +0000

    remove unused imports

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet190.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet190.java
@@ -11,9 +11,7 @@
 package org.eclipse.swt.snippets;
 
 import org.eclipse.swt.*;
-import org.eclipse.swt.custom.*;
 import org.eclipse.swt.events.*;
-import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
