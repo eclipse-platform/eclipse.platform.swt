97166dcd8b41734d1899cdfbd7aa425feb9c8f7d
Billy Biggs <bbiggs>
2005-12-09 20:41:30 +0000

    118670 - Make StyledText.getLinePixel(int)/getLineIndex(int) public API

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -3591,8 +3591,13 @@ StyledTextEvent getLineStyleData(int lineOffset, String line) {
  * lineIndex == lineCount it returns the bottomPixel of the last line.
  * It means this function can be used to retrive the bottom pixel of any line. 
  *  
+ * WARNING API STILL UNDER CONSTRUCTION AND SUBJECT TO CHANGE
+ * 
+ * @since 3.2
  */
-int getLinePixel(int lineIndex) {
+public int getLinePixel(int lineIndex) {
+	checkWidget();
+	//FIXME: range checking?
 	int lineCount = content.getLineCount();
 	if (lineIndex > lineCount) {
 		lineIndex = lineCount;
@@ -3620,8 +3625,14 @@ int getLinePixel(int lineIndex) {
 /**
  * Returns the line index for a y, relative to the client area.
  * The line index returned is always in the range 0..lineCount - 1.
+ *
+ * WARNING API STILL UNDER CONSTRUCTION AND SUBJECT TO CHANGE
+ *
+ * @since 3.2
  */
-int getLineIndex(int y) {
+public int getLineIndex(int y) {
+	checkWidget();
+	//FIXME: range checking?
 	y -= topMargin;
 	if (isFixedLineHeight()) {
 		int lineHeight = renderer.getLineHeight();
