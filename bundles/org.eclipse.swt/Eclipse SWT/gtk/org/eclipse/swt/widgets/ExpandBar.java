cf6d924932aa6fed339ec8b1638742047d9b02cf
Felipe Heidrich <fheidric>
2006-02-03 20:38:59 +0000

    expand bar & item - initial version

---
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ExpandBar.java
@@ -0,0 +1,137 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.widgets;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.internal.gtk.*;
+import org.eclipse.swt.events.*;
+
+public class ExpandBar extends Composite {
+	
+public ExpandBar (Composite parent, int style) {
+	super (parent, style);
+}
+
+public void addExpandListener (ExpandListener listener) {
+	checkWidget ();
+	if (listener == null) error (SWT.ERROR_NULL_ARGUMENT);
+	TypedListener typedListener = new TypedListener (listener);
+	addListener (SWT.Expand, typedListener);
+	addListener (SWT.Collapse, typedListener);
+}
+
+void createHandle (int index) {
+	if (OS.GTK_VERSION < OS.VERSION (2, 4, 0)) {
+		// TODO
+		error (SWT.ERROR_NO_HANDLES);
+	}
+	state |= HANDLE;
+	fixedHandle = OS.g_object_new (display.gtk_fixed_get_type (), 0);
+	if (fixedHandle == 0) error (SWT.ERROR_NO_HANDLES);
+	OS.gtk_fixed_set_has_window (fixedHandle, true);
+	handle = OS.gtk_vbox_new (false, 0);
+	if (handle == 0) error (SWT.ERROR_NO_HANDLES);
+	if ((style & SWT.V_SCROLL) != 0) {
+		scrolledHandle = OS.gtk_scrolled_window_new (0, 0);
+		if (scrolledHandle == 0) error (SWT.ERROR_NO_HANDLES);
+		int vsp = (style & SWT.V_SCROLL) != 0 ? OS.GTK_POLICY_AUTOMATIC : OS.GTK_POLICY_NEVER;
+		OS.gtk_scrolled_window_set_policy (scrolledHandle, OS.GTK_POLICY_NEVER, vsp);
+		OS.gtk_container_add (fixedHandle, scrolledHandle);
+		OS.gtk_scrolled_window_add_with_viewport (scrolledHandle, handle);
+	} else {
+		OS.gtk_container_add (fixedHandle, handle);
+	}
+	OS.gtk_container_set_border_width (handle, 0);
+}
+
+int /*long*/ eventHandle () {
+	return fixedHandle;
+}
+
+public int indexOf (ExpandItem item) {
+	checkWidget();
+	if (item == null) error (SWT.ERROR_NULL_ARGUMENT);
+	ExpandItem [] items = getItems ();
+	for (int i = 0; i < items.length; i++) {
+		if (item == items [i]) return i;
+	}
+	return -1;
+}
+
+public ExpandItem [] getItems () {
+	checkWidget();
+	int /*long*/ list = OS.gtk_container_get_children (handle);
+	if (list == 0) return new ExpandItem [0];
+	int count = OS.g_list_length (list);
+	ExpandItem [] result = new ExpandItem [count];
+	for (int i=0; i<count; i++) {
+		int /*long*/ data = OS.g_list_nth_data (list, i);
+		Widget widget = display.getWidget (data);
+		result [i] = (ExpandItem) widget;
+	}
+	OS.g_list_free (list);
+	return result;
+}
+
+public ExpandItem getItem (int index) {
+	checkWidget();
+	if (!(0 <= index && index < getItemCount ())) error (SWT.ERROR_INVALID_RANGE);
+	return getItems () [index];
+}
+
+public int getItemCount () {
+	checkWidget();
+	int /*long*/ list = OS.gtk_container_get_children (handle);
+	if (list == 0) return 0;
+	int itemCount = OS.g_list_length (list);
+	OS.g_list_free (list);
+	return itemCount;
+}
+
+public int getSpacing () {
+	checkWidget ();
+	return OS.gtk_container_get_border_width (handle);
+}
+
+void relayout () {
+	ExpandItem [] items = getItems ();
+	int yScroll = 0;
+	if (scrolledHandle != 0) {
+		int adjustmentHandle = OS.gtk_scrolled_window_get_vadjustment (scrolledHandle);
+		GtkAdjustment adjustment = new GtkAdjustment ();
+		OS.memmove (adjustment, adjustmentHandle);
+		yScroll = (int)adjustment.value;
+	}
+	for (int i=0; i<items.length; i++) {
+		ExpandItem item = items [i];
+		if (item != null) item.resizeControl (yScroll);
+	}
+}
+
+public void removeExpandListener(ExpandListener listener) {
+	checkWidget ();
+	if (listener == null) error (SWT.ERROR_NULL_ARGUMENT);
+	if (eventTable == null) return;
+	eventTable.unhook (SWT.Expand, listener);
+	eventTable.unhook (SWT.Collapse, listener);
+}
+
+void setScrollBarValue (ScrollBar bar) {
+	relayout ();
+}
+
+public void setSpacing (int spacing) {
+	checkWidget ();
+	if (spacing < 0) return;
+	OS.gtk_box_set_spacing (handle, spacing);
+	OS.gtk_container_set_border_width (handle, spacing);
+}
+}
---
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ExpandItem.java
@@ -0,0 +1,233 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.widgets;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.internal.*;
+import org.eclipse.swt.internal.gtk.*;
+import org.eclipse.swt.graphics.*;
+
+public class ExpandItem extends Item {
+	ExpandBar parent;
+	Control control;
+	ImageList imageList;
+	int /*long*/ clientHandle, boxHandle, labelHandle, imageHandle;
+	
+public ExpandItem (ExpandBar parent, int style) {
+	super (parent, style);
+	this.parent = parent;
+	createWidget (parent.getItemCount ());
+}
+
+public ExpandItem (ExpandBar parent, int style, int index) {
+	super (parent, style);
+	this.parent = parent;
+	int count = parent.getItemCount ();
+	if (!(0 <= index && index <= count)) {
+		error (SWT.ERROR_ITEM_NOT_ADDED);
+	}
+	createWidget (index);
+}
+
+protected void checkSubclass () {
+	if (!isValidSubclass ()) error (SWT.ERROR_INVALID_SUBCLASS);
+}
+
+void createHandle (int index) {
+	state |= HANDLE;
+	handle = OS.gtk_expander_new (null);
+	if (handle == 0) error (SWT.ERROR_NO_HANDLES);
+	clientHandle = OS.g_object_new (display.gtk_fixed_get_type (), 0);
+	if (clientHandle == 0) error (SWT.ERROR_NO_HANDLES);
+	OS.gtk_container_add (handle, clientHandle);	
+	boxHandle = OS.gtk_hbox_new (false, 4);
+	if (boxHandle == 0) error (SWT.ERROR_NO_HANDLES);
+	labelHandle = OS.gtk_label_new (null);
+	if (labelHandle == 0) error (SWT.ERROR_NO_HANDLES);
+	imageHandle = OS.gtk_image_new ();
+	if (imageHandle == 0) error (SWT.ERROR_NO_HANDLES);
+	OS.gtk_container_add (boxHandle, imageHandle);
+	OS.gtk_container_add (boxHandle, labelHandle);
+	OS.gtk_expander_set_label_widget (handle, boxHandle);
+}
+
+void createWidget (int index) {
+	super.createWidget (index);
+	showWidget (index);
+	parent.relayout ();
+}
+
+void deregister() {
+	super.deregister();
+	display.removeWidget (clientHandle);
+	display.removeWidget (boxHandle);
+	display.removeWidget (labelHandle);
+	display.removeWidget (imageHandle);
+}
+
+public void dispose () {
+	if (isDisposed ()) return;
+	ExpandBar parent = this.parent;
+	super.dispose ();
+	parent.relayout ();
+}
+
+public Control getControl () {
+	checkWidget ();
+	return control;
+}
+
+public boolean getExpanded () {
+	checkWidget ();
+	return OS.gtk_expander_get_expanded (handle);
+}
+
+public int getHeight () {
+	checkWidget ();
+	GtkRequisition requisition = new GtkRequisition ();
+	OS.gtk_widget_size_request (clientHandle, requisition);
+	return requisition.height;
+}
+
+public ExpandBar getParent () {
+	checkWidget();
+	if (parent == null) error (SWT.ERROR_WIDGET_DISPOSED);
+	return parent;
+}
+
+int /*long*/ gtk_activate (int /*long*/ widget) {
+	Event event = new Event ();
+	event.item = this;
+	int type = OS.gtk_expander_get_expanded (handle) ? SWT.Expand :SWT.Collapse;
+	parent.sendEvent (type, event);
+	return 0;
+}
+
+int gtk_size_allocate (int widget, int allocation) {
+	parent.relayout ();
+	return 0;
+}
+
+void hookEvents () {
+	super.hookEvents();
+	OS.g_signal_connect_closure (handle, OS.activate, display.closures [ACTIVATE], false);
+	OS.g_signal_connect_closure (handle, OS.activate, display.closures [ACTIVATE_INVERSE], true);
+	OS.g_signal_connect_closure (clientHandle, OS.size_allocate, display.closures [SIZE_ALLOCATE], true);
+}
+
+void register() {
+	super.register();
+	display.addWidget (clientHandle, this);
+	display.addWidget (boxHandle, this);
+	display.addWidget (labelHandle, this);
+	display.addWidget (imageHandle, this);
+}
+
+void releaseHandle () {
+	super.releaseHandle ();
+	clientHandle = boxHandle = labelHandle = imageHandle = 0;
+}
+
+void releaseWidget () {
+	super.releaseWidget ();
+	if (imageList != null) imageList.dispose ();
+	imageList = null;
+	parent = null;
+	control = null;
+}
+
+void resizeControl (int yScroll) {
+	if (control != null && !control.isDisposed ()) {
+		boolean visible = OS.gtk_expander_get_expanded (handle);
+		if (visible) {
+			int x = OS.GTK_WIDGET_X (clientHandle);
+			int y = OS.GTK_WIDGET_Y (clientHandle);
+			if (x != -1 && y != -1) {
+				int width = OS.GTK_WIDGET_WIDTH (clientHandle);
+				int height = OS.GTK_WIDGET_HEIGHT (clientHandle);
+				control.setBounds (x, y - yScroll, width, height, true, true);
+			}
+		}
+		control.setVisible (visible);
+	}
+}
+
+public void setControl (Control control) {
+	checkWidget ();
+	if (control != null) {
+		if (control.isDisposed ()) error (SWT.ERROR_INVALID_ARGUMENT);
+		if (control.parent != parent) error (SWT.ERROR_INVALID_PARENT);
+	}
+	if (this.control == control) return;
+	this.control = control;
+	if (control != null) {
+		control.setVisible (OS.gtk_expander_get_expanded (handle));
+	}
+	parent.relayout ();
+}
+
+public void setExpanded (boolean expanded) {
+	checkWidget ();
+	OS.gtk_expander_set_expanded (handle, expanded);
+	parent.relayout ();
+}
+
+public void setImage (Image image) {
+	super.setImage (image);
+	if (imageList != null) imageList.dispose ();
+	imageList = null;
+	if (image != null) {
+		if (image.isDisposed ()) error (SWT.ERROR_INVALID_ARGUMENT);
+		imageList = new ImageList ();
+		int imageIndex = imageList.add (image);
+		int /*long*/ pixbuf = imageList.getPixbuf (imageIndex);
+		OS.gtk_image_set_from_pixbuf (imageHandle, pixbuf);
+		if (text.length () == 0) OS.gtk_widget_hide (labelHandle);
+		OS.gtk_widget_show (imageHandle);
+	} else {
+		OS.gtk_image_set_from_pixbuf (imageHandle, 0);
+		OS.gtk_widget_show (labelHandle);
+		OS.gtk_widget_hide (imageHandle);
+	}
+}
+
+public void setHeight (int height) {
+	checkWidget ();
+	if (height < 0) return;
+	OS.gtk_widget_set_size_request (clientHandle, -1, height);
+	parent.relayout ();
+}
+
+public void setText (String string) {
+	super.setText (string);
+	byte [] buffer = Converter.wcsToMbcs (null, string, true);
+	OS.gtk_label_set_text (labelHandle, buffer);
+}
+
+void showWidget (int index) {
+	OS.gtk_widget_show (handle);
+	OS.gtk_widget_show (clientHandle);
+	OS.gtk_container_add (parent.handle, handle);
+	OS.gtk_box_set_child_packing (parent.handle, handle, false, false, 0, OS.GTK_PACK_START);	
+	if (boxHandle != 0) OS.gtk_widget_show (boxHandle);
+	if (labelHandle != 0) OS.gtk_widget_show (labelHandle);
+}
+
+int /*long*/ windowProc (int /*long*/ handle, int /*long*/ user_data) {
+	switch ((int)/*64*/user_data) {
+		case ACTIVATE_INVERSE: {
+			parent.relayout ();
+			return 0;
+		}
+	}
+	return super.windowProc (handle, user_data);
+}
+}
