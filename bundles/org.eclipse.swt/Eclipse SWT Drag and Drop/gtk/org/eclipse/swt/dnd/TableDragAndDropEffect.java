4a795913b60ed25d527adf08c6a21bdcb7cafbdd
Veronika Irvine <veronika>
2006-02-24 20:58:35 +0000

    add support for drag source image

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/TableDragAndDropEffect.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/TableDragAndDropEffect.java
@@ -10,16 +10,18 @@
  *******************************************************************************/
 package org.eclipse.swt.dnd;
 
-
+import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.internal.gtk.*;
 import org.eclipse.swt.widgets.*;
 
 class TableDragAndDropEffect extends DragAndDropEffect {
-	private Table table;
-	private int scrollIndex = -1;
-	private long scrollBeginTime;
-	private static final int SCROLL_HYSTERESIS = 150; // milli seconds
+	Table table;
+	
+	int scrollIndex = -1;
+	long scrollBeginTime;
+	
+	static final int SCROLL_HYSTERESIS = 150; // milli seconds
 
 TableDragAndDropEffect(Table table) {
 	this.table = table;
@@ -49,6 +51,67 @@ Widget getItem(int x, int y) {
 		}
 	}
 	return item;
+}
+
+ImageData getDragSourceImage(int x, int y) {
+	if (OS.GTK_VERSION < OS.VERSION (2, 2, 0)) return null;
+	/*
+	* Bug in GTK.  gtk_tree_selection_get_selected_rows() segmentation faults
+	* in versions smaller than 2.2.4 if the model is NULL.  The fix is
+	* to give a valid pointer instead.
+	*/
+	int /*long*/ handle = table.handle;
+	int /*long*/ selection = OS.gtk_tree_view_get_selection (handle);
+	int /*long*/ [] model = OS.GTK_VERSION < OS.VERSION (2, 2, 4) ? new int /*long*/ [1] : null;
+	int /*long*/ list = OS.gtk_tree_selection_get_selected_rows (selection, model);
+	if (list == 0) return null;
+	int count = Math.min(10, OS.g_list_length (list));
+	Image image = null;
+	Display display = table.getDisplay();
+	if (count == 1) {
+			int /*long*/ path = OS.g_list_nth_data (list, 0);
+			int /*long*/ pixmap = OS.gtk_tree_view_create_row_drag_icon(handle, path);
+			image =  Image.gtk_new(display, SWT.ICON, pixmap, 0); 
+	} else {
+		int width = 0, height = 0;
+		int[] w = new int[1], h = new int[1];
+		int[] yy = new int[count], hh = new int[count];
+		int/*long*/[] pixmaps = new int/*long*/[count];
+		GdkRectangle rect = new GdkRectangle ();
+		for (int i=0; i<count; i++) {
+			int /*long*/ path = OS.g_list_nth_data (list, i);
+			OS.gtk_tree_view_get_cell_area (handle, path, 0, rect);
+			pixmaps[i] = OS.gtk_tree_view_create_row_drag_icon(handle, path);
+			OS.gdk_drawable_get_size(pixmaps[i], w, h);
+			width = Math.max(width, w[0]);
+			height = rect.y + h[0] - yy[0];
+			yy[i] = rect.y;
+			hh[i] = h[0];
+		}
+		int /*long*/ source = OS.gdk_pixmap_new(OS.GDK_ROOT_PARENT(), width, height, -1);
+		int /*long*/ gcSource = OS.gdk_gc_new(source);
+		int /*long*/ mask = OS.gdk_pixmap_new(OS.GDK_ROOT_PARENT(), width, height, 1);
+		int /*long*/ gcMask = OS.gdk_gc_new(mask);
+		GdkColor color = new GdkColor();
+		color.pixel = 0;
+		OS.gdk_gc_set_foreground(gcMask, color);
+		OS.gdk_draw_rectangle(mask, gcMask, 1, 0, 0, width, height);
+		color.pixel = 1;
+		OS.gdk_gc_set_foreground(gcMask, color);
+		for (int i=0; i<count; i++) {
+			OS.gdk_draw_drawable(source, gcSource, pixmaps[i], 0, 0, 0, yy[i] - yy[0], -1, -1);
+			OS.gdk_draw_rectangle(mask, gcMask, 1, 0, yy[i] - yy[0], width, hh[i]);
+			OS.g_object_unref(pixmaps[i]);
+		}
+		OS.g_object_unref(gcSource);
+		OS.g_object_unref(gcMask);
+		 image  = Image.gtk_new(display, SWT.ICON, source, mask);
+	}
+	OS.g_list_free (list);
+	
+	ImageData imageData = image.getImageData();
+	image.dispose();
+	return imageData;
 }
 
 void showDropTargetEffect(int effect, int x, int y) {
