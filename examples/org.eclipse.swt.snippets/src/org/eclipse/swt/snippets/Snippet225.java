04a838dbf606266cd909960f2ecc433b1aeea7f7
Felipe Heidrich <fheidric>
2006-03-06 18:46:55 +0000

    *** empty log message ***

---
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet225.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.snippets;
+  
+/*
+ * Tooltip example snippet: how to use a balloon tooltip with a tray item
+ *
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ * 
+ * @since 3.0
+ */
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.widgets.*;
+
+public class Snippet225 {
+
+public static void main(String[] args) {
+	Display display = new Display();
+	Shell shell = new Shell(display);
+	Image image = null;
+	final ToolTip tip = new ToolTip(shell, SWT.BALLOON | SWT.ICON_INFORMATION);
+	tip.setMessage("Here goes some message for the user, when the message is too long it wraps. I should say something cool but nothing comes to my mind");
+	Tray tray = display.getSystemTray();
+	if (tray != null) {
+		TrayItem item = new TrayItem(tray, SWT.NONE);
+		image = new Image(display, Snippet225.class.getResourceAsStream("eclipse.png"));
+		item.setImage(image);
+		tip.setText("Notification at the tray item");
+		item.setToolTip(tip);
+	} else {
+		tip.setText("Notification anywhere in the screen");
+		tip.setLocation(400, 400);
+	}
+	Button button = new Button (shell, SWT.PUSH);
+	button.setText("Press for balloon tip");
+	button.addListener(SWT.Selection, new Listener() {
+		public void handleEvent(Event event) {
+			tip.setVisible(true);
+		}
+	});
+	button.pack();
+	shell.setBounds(50, 50, 300, 200);
+	shell.open();
+	while (!shell.isDisposed()) {
+		if (!display.readAndDispatch()) display.sleep();
+	}
+	if (image != null) image.dispose();
+	display.dispose();
+}
+}
