57c7ab755b958a9a87ff41ff9f485913d917fbfa
Silenio Quarti <silenio>
2006-03-07 17:05:35 +0000

    130363 - packed TableColumns are much too wide

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/TreeItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/TreeItem.java
@@ -212,29 +212,28 @@ static int checkIndex (int index) {
 }
 
 int calculateWidth (int index, GC gc) {
-	int width = this.width;
-	if (index != 0 || (index == 0 && width == -1)) {
-		Image image = getImage (index);
-		String text = getText (index);
-		if (image != null) width += image.getBounds ().width + parent.getGap ();
-		if (text != null && text.length () > 0) width += gc.stringExtent (text).x;
-		if (parent.hooks (SWT.MeasureItem)) {
-			Event event = new Event ();
-			event.item = this;
-			event.index = index;
-			event.gc = gc;
-			Rectangle bounds = getBounds ();
-			event.width = width;
-			event.height = bounds.height;
-			parent.sendEvent (SWT.MeasureItem, event);
-			if (parent.itemHeight == -1) {
-				parent.itemHeight = event.height;
-				OS.SetDataBrowserTableViewRowHeight (parent.handle, (short) event.height);
-			}
-			width = event.width;
+	if (index == 0 && width != -1) return width;
+	int width = 0;
+	Image image = getImage (index);
+	String text = getText (index);
+	if (image != null) width += image.getBounds ().width + parent.getGap ();
+	if (text != null && text.length () > 0) width += gc.stringExtent (text).x;
+	if (parent.hooks (SWT.MeasureItem)) {
+		Event event = new Event ();
+		event.item = this;
+		event.index = index;
+		event.gc = gc;
+		Rectangle bounds = getBounds ();
+		event.width = width;
+		event.height = bounds.height;
+		parent.sendEvent (SWT.MeasureItem, event);
+		if (parent.itemHeight == -1) {
+			parent.itemHeight = event.height;
+			OS.SetDataBrowserTableViewRowHeight (parent.handle, (short) event.height);
 		}
-		if (index == 0) this.width = width;
+		width = event.width;
 	}
+	if (index == 0) this.width = width;
 	return width;
 }
 
