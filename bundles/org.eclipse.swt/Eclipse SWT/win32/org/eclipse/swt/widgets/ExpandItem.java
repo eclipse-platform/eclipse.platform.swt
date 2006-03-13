16238098f61f651ce5f68d8befa84829aecf0a2d
Felipe Heidrich <fheidric>
2006-03-13 17:21:19 +0000

    Bug 131159 - ExpandItem needs getHeaderHeight()

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/ExpandItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/ExpandItem.java
@@ -280,7 +280,18 @@ public boolean getExpanded () {
 	return expanded;
 }
 
-int getHeaderHeight () {
+/**
+ * Returns the height of the receiver's header 
+ *
+ * @return the height of the header 
+ *
+ * @exception SWTException <ul>
+ *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
+ *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
+ * </ul>
+ */
+public int getHeaderHeight () {
+	checkWidget ();
 	return Math.max (ExpandBar.HEADER_HEIGHT, imageHeight);
 }
 
