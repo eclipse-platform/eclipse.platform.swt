a1a9bae89c53a83cd4c0ac19a58fd251490400da
Felipe Heidrich <fheidric>
2006-03-14 19:22:12 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/emulated/expand/org/eclipse/swt/widgets/ExpandItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/emulated/expand/org/eclipse/swt/widgets/ExpandItem.java
@@ -214,6 +214,7 @@ public void setImage (Image image) {
 
 public void setHeight (int height) {
 	checkWidget ();
+	if (height < 0) return;
 	setBounds (0, 0, width, height, false, true);
 	if (expanded) parent.layoutItems (parent.indexOf (this) + 1, true);
 }
