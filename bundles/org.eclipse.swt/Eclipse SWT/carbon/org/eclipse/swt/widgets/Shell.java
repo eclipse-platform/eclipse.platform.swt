ee04fe1310b19fc43abe1e40713363808dedb8c1
Silenio Quarti <silenio>
2006-04-06 20:33:19 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Shell.java
@@ -507,8 +507,6 @@ void createHandle () {
 void createWidget () {
 	super.createWidget ();
 	resizeBounds ();
-//	setBackgroundImage(new Image(display, "/Users/ssq/Desktop/workspace/Silenio/irmaos.jpg"));
-//	setBackgroundMode(SWT.INHERIT_FORCE);
 }
 
 void deregister () {
