90b00ebfdec0daa7bdd7c39c16236fd3c23cf6c8
Steve Northover <steve>
2006-04-10 19:54:12 +0000

    135649 - Mouse wheel scrolls invisible scrollbars

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Scrollable.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Scrollable.java
@@ -368,7 +368,7 @@ void resizeClientArea () {
 boolean sendMouseWheel (short wheelAxis, int wheelDelta) {
 	if ((state & CANVAS) != 0) {
 		ScrollBar bar = wheelAxis == OS.kEventMouseWheelAxisX ? horizontalBar : verticalBar;
-		if (bar != null && bar.getVisible ()) {
+		if (bar != null && bar.getEnabled ()) {
 			bar.setSelection (Math.max (0, bar.getSelection () - bar.getIncrement () * wheelDelta));
 			Event event = new Event ();
 		    event.detail = wheelDelta > 0 ? SWT.PAGE_UP : SWT.PAGE_DOWN;	
