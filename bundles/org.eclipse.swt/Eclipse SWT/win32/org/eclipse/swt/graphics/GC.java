701d021960531008d992ff963238073b1f450b63
Silenio Quarti <silenio>
2006-04-19 13:31:06 +0000

    127716 - GC.drawPoint() calls aren't affected by a Transform

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/GC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/GC.java
@@ -1468,6 +1468,11 @@ public void drawPath (Path path) {
  */
 public void drawPoint (int x, int y) {
 	if (handle == 0) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
+	if (data.gdipGraphics != 0) {
+		initGdip(false, true);
+		Gdip.Graphics_FillRectangle(data.gdipGraphics, data.gdipBrush, x, y, 1, 1);
+		return;
+	}
 	OS.SetPixel (handle, x, y, OS.GetTextColor (handle));
 }
 
