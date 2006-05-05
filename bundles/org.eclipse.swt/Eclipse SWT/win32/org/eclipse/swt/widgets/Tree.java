e75c82f6284332014cea017beaa3a89e03173b57
Steve Northover <steve>
2006-05-05 14:49:07 +0000

    138319  - SWTException: Failed to execute runnable (IllegalArgumentException: Index out of bounds)

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
@@ -3284,6 +3284,8 @@ void releaseWidget () {
  */
 public void removeAll () {
 	checkWidget ();
+	hFirstIndexOf = hLastIndexOf = 0;
+	itemCount = -1;
 	for (int i=0; i<items.length; i++) {
 		TreeItem item = items [i];
 		if (item != null && !item.isDisposed ()) {
