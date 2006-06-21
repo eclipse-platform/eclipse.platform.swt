d48407beb920d0e3450775aac915b9e3cf85b96b
Steve Northover <steve>
2006-06-21 16:18:15 +0000

    fix Javadoc to include 3.3

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/MouseEvent.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/MouseEvent.java
@@ -60,6 +60,8 @@ public final class MouseEvent extends TypedEvent {
 	 * the number times the mouse has been clicked, as defined
 	 * by the operating system; 1 for the first click, 2 for the
 	 * second click and so on.
+	 * 
+	 * @since 3.3
 	 */
 	public int count;
 
