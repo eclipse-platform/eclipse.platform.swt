6dce95c7bac82718c59672a2a21a82d9007c1492
Steve Northover <steve>
2006-06-27 17:18:56 +0000

    fix comment

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -1646,12 +1646,11 @@ void setToolTipText (int /*long*/ widget, String newString, String oldString) {
 	* Feature in GTK.  There is no API to position a tooltip.
 	* The fix is to connect to the size_allocate signal for
 	* the tooltip window and position it before it is mapped.
+	*
+	* Bug in Solaris-GTK.  Invoking gtk_tooltips_force_window()
+	* can cause a crash in older versions of GTK.  The fix is
+	* to avoid this call if the GTK version is older than 2.2.x.
 	*/
-	/*
-	 * Bug in Solaris-GTK.  Invoking gtk_tooltips_force_window
-	 * can cause a crash in older versions of GTK.  The workaround is
-	 * to not make this call if the GTK version is older than 2.2.x.
-	 */
 	if (OS.GTK_VERSION >= OS.VERSION (2, 2, 1)) { 
 		OS.gtk_tooltips_force_window (tooltipsHandle);
 	}
--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ToolTip.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/ToolTip.java
@@ -224,10 +224,10 @@ void createHandle (int index) {
 		handle = OS.gtk_tooltips_new ();
 		if (handle == 0) SWT.error (SWT.ERROR_NO_HANDLES);
 		/*
-		 * Bug in Solaris-GTK.  Invoking gtk_tooltips_force_window
-		 * can cause a crash in older versions of GTK.  The workaround is
-		 * to not make this call if the GTK version is older than 2.2.x.
-		 */
+		* Bug in Solaris-GTK.  Invoking gtk_tooltips_force_window()
+		* can cause a crash in older versions of GTK.  The fix is
+		* to avoid this call if the GTK version is older than 2.2.x.
+		*/
 		if (OS.GTK_VERSION >= OS.VERSION (2, 2, 1)) { 
 			OS.gtk_tooltips_force_window (handle);
 		}
