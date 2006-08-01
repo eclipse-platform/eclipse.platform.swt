ec11b1e99b3dd5a45b6257429fe326259f31bfea
Steve Northover <steve>
2006-08-01 19:59:33 +0000

    124836 - CLabel.shortenText(...) takes almost forever for long text

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CLabel.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CLabel.java
@@ -753,23 +753,29 @@ public void setToolTipText (String string) {
 protected String shortenText(GC gc, String t, int width) {
 	if (t == null) return null;
 	int w = gc.textExtent(ELLIPSIS, DRAW_FLAGS).x;
+	if (width<=w) return t;
 	int l = t.length();
-	int pivot = l/2;
-	int s = pivot;
-	int e = pivot+1;
-	while (s >= 0 && e < l) {
-		String s1 = t.substring(0, s);
-		String s2 = t.substring(e, l);
+	int max = l/2;
+	int min = 0;
+	int mid = (max+min)/2 - 1;
+	if (mid <= 0) return t;
+	while (min < mid && mid < max) {
+		String s1 = t.substring(0, mid);
+		String s2 = t.substring(l-mid, l);
 		int l1 = gc.textExtent(s1, DRAW_FLAGS).x;
 		int l2 = gc.textExtent(s2, DRAW_FLAGS).x;
-		if (l1+w+l2 < width) {
-			t = s1 + ELLIPSIS + s2;
-			break;
+		if (l1+w+l2 > width) {
+			max = mid;			
+			mid = (max+min)/2;
+		} else if (l1+w+l2 < width) {
+			min = mid;
+			mid = (max+min)/2;
+		} else {
+			min = max;
 		}
-		s--;
-		e++;
 	}
-	return t;
+	if (mid == 0) return t;
+ 	return t.substring(0, mid)+ELLIPSIS+t.substring(l-mid, l);
 }
 
 private String[] splitString(String text) {
