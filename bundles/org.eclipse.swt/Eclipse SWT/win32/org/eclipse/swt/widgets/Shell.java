b93ccd7b259b3cc145deee17bed288eb190069e6
Steve Northover <steve>
2006-08-25 21:32:55 +0000

    call MDI proc when there is an MDI handle

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Shell.java
@@ -425,6 +425,9 @@ int callWindowProc (int hwnd, int msg, int wParam, int lParam) {
 	if (hwnd == toolTipHandle || hwnd == balloonTipHandle) {
 		return OS.CallWindowProc (ToolTipProc, hwnd, msg, wParam, lParam);
 	}
+	if (hwndMDIClient != 0) {
+		return OS.DefFrameProc (hwnd, hwndMDIClient, msg, wParam, lParam);
+	}
 	if ((style & SWT.TOOL) != 0) {
 		int trim = SWT.TITLE | SWT.CLOSE | SWT.MIN | SWT.MAX | SWT.BORDER | SWT.RESIZE;
 		if ((style & trim) == 0) return OS.DefWindowProc (hwnd, msg, wParam, lParam);
@@ -436,9 +439,6 @@ int callWindowProc (int hwnd, int msg, int wParam, int lParam) {
 				return OS.DefWindowProc (hwnd, msg, wParam, lParam);
 		}
 		return OS.CallWindowProc (DialogProc, hwnd, msg, wParam, lParam);
-	}
-	if (hwndMDIClient != 0) {
-		return OS.DefFrameProc (hwnd, hwndMDIClient, msg, wParam, lParam);
 	}
 	return OS.DefWindowProc (hwnd, msg, wParam, lParam);
 }
