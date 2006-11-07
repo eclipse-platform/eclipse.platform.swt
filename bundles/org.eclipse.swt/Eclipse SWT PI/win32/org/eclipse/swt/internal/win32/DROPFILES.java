ba7a110ee9c9901e3bbaef17828d649f87cd9004
Grant Gayed <ggayed>
2006-11-07 22:29:13 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/DROPFILES.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/DROPFILES.java
@@ -15,10 +15,10 @@ public class DROPFILES {
 //	POINT pt; // Drop point. The coordinates depend on fNC.
 	public int pt_x;
 	public int pt_y;
-	public int fNC;   // Nonclient area flag. If this member is TRUE, pt specifies the screen 
+	public boolean fNC; // Nonclient area flag. If this member is TRUE, pt specifies the screen 
 				      // coordinates of a point in a window's nonclient area. If it is FALSE, 
 				      // pt specifies the client coordinates of a point in the client area. 
-	public int fWide; // Value that indicates whether the file contains ANSI or Unicode 
+	public boolean fWide; // Value that indicates whether the file contains ANSI or Unicode 
 					  // characters. If it is zero, it contains ANSI characters. Otherwise, it 
 				      // contains Unicode characters.
 	public static final int sizeof = OS.DROPFILES_sizeof ();
