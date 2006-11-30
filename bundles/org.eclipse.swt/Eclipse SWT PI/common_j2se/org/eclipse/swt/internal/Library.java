3f4f11935cadbb78e8d83414ccd7f9dd63c75e21
Silenio Quarti <silenio>
2006-11-30 21:31:40 +0000

    158903  - org.eclipse.swt.internal.Library needs a way to override java.library.path (+loading from jar)

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -10,6 +10,8 @@
  *******************************************************************************/
 package org.eclipse.swt.internal;
 
+import java.io.*;
+
 public class Library {
 
 	/* SWT Version - Mmmm (M=major, mmm=minor) */
@@ -34,7 +36,10 @@ public class Library {
 	 */
 	public static final int JAVA_VERSION, SWT_VERSION;
 
+	static final String SEPARATOR;
+
 static {
+	SEPARATOR = System.getProperty("file.separator");
 	JAVA_VERSION = parseVersion(System.getProperty("java.version"));
 	SWT_VERSION = SWT_VERSION(MAJOR_VERSION, MINOR_VERSION);
 }
@@ -83,6 +88,54 @@ public static int SWT_VERSION (int major, int minor) {
 	return major * 1000 + minor;
 }
 
+static boolean extract (String fileName, String mappedName) {
+	FileOutputStream os = null;
+	InputStream is = null;
+	File file = new File(fileName);
+	try {
+		if (!file.exists ()) {
+			is = Library.class.getResourceAsStream ("/" + mappedName); //$NON-NLS-1$
+			if (is != null) {
+				int read;
+				byte [] buffer = new byte [4096];
+				os = new FileOutputStream (fileName);
+				while ((read = is.read (buffer)) != -1) {
+					os.write(buffer, 0, read);
+				}
+				os.close ();
+				is.close ();
+				if (!Platform.PLATFORM.equals ("win32")) { //$NON-NLS-1$
+					try {
+						Runtime.getRuntime ().exec (new String []{"chmod", "755", fileName}).waitFor(); //$NON-NLS-1$ //$NON-NLS-2$
+					} catch (Throwable e4) {}
+				}
+				if (load (fileName)) return true;
+			}
+		}
+	} catch (Throwable e4) {
+		try {
+			if (os != null) os.close ();
+		} catch (IOException e) {}
+		try {
+			if (is != null) is.close ();
+		} catch (IOException e) {}
+	}
+	if (file.exists ()) file.delete ();
+	return false;
+}
+
+static boolean load (String libName) {
+	try {
+		if (libName.indexOf (SEPARATOR) != -1) {
+			System.load (libName);
+		} else {
+			System.loadLibrary (libName);
+		}		
+		return true;
+	} catch (UnsatisfiedLinkError e3) {}
+	return false;
+}
+
 /**
  * Loads the shared library that matches the version of the
  * Java code which is currently running.  SWT shared libraries
@@ -118,19 +171,39 @@ public static void loadLibrary (String name) {
 		if (REVISION > 0) version += "r" + REVISION; //$NON-NLS-1$
 	}
 
-	try {
-		String newName = name + "-" + platform + "-" + version; //$NON-NLS-1$ //$NON-NLS-2$
-		System.loadLibrary (newName);
-		return;
-	} catch (UnsatisfiedLinkError e1) {		
-		try {
-			String newName = name + "-" + platform; //$NON-NLS-1$
-			System.loadLibrary (newName);
-			return;
-		} catch (UnsatisfiedLinkError e2) {
-			throw e1;
-		}
+	String libName1 = name + "-" + platform + "-" + version;  //$NON-NLS-1$ //$NON-NLS-2$
+	String libName2 = name + "-" + platform;  //$NON-NLS-1$
+	String mappedName1 = System.mapLibraryName (libName1);
+	String mappedName2 = System.mapLibraryName (libName2);
+
+	/* Try loading library from swt library path */
+	String path = System.getProperty ("swt.library.path"); //$NON-NLS-1$
+	if (path != null) {
+		path = new File(path).getAbsolutePath ();
+		if (load (path + SEPARATOR + mappedName1)) return;
+		if (load (path + SEPARATOR + mappedName2)) return;
 	}
+
+	/* Try loading library from java library path */
+	if (load (libName1)) return;
+	if (load (libName2)) return;
+	
+	/* Try loading library from the tmp directory if swt library path is not specified */
+	if (path == null) {
+		path = System.getProperty ("java.io.tmpdir"); //$NON-NLS-1$
+		path = new File(path).getAbsolutePath ();
+		if (load (path + SEPARATOR + mappedName1)) return;
+		if (load (path + SEPARATOR + mappedName2)) return;
+	}
+		
+	/* Try extracting and loading library from jar */
+	if (path != null) {
+		if (extract (path + SEPARATOR + mappedName1, mappedName1)) return;
+		if (extract (path + SEPARATOR + mappedName2, mappedName2)) return;
+	}
+	
+	/* Failed to find the library */
+	throw new UnsatisfiedLinkError ("no " + libName1 + " or " + libName2 + " in swt.library.path, java.libary.path or the jar file");
 }
 
 }
