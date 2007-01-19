a5125824cc8c231f80f973c016c3182936c19f64
Steve Northover <steve>
2007-01-19 14:49:35 +0000

    fix copyright and PTR_SIZEOF

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/C.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/C.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -15,8 +15,7 @@ public class C extends Platform {
 		Library.loadLibrary ("swt"); //$NON-NLS-1$
 	}
 
-	public static final int PTR_SIZEOF = 4;//PTR_sizeof ();
-
+public static final int PTR_SIZEOF = PTR_sizeof ();
 public static final native void free (int /*long*/ ptr);
 public static final native int /*long*/ malloc (int size);
 public static final native int PTR_sizeof ();
