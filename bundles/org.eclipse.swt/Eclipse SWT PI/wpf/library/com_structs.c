be7a6b56ec36f2937282f99ce8f6d3a09d752bb5
Felipe Heidrich <fheidric>
2007-01-29 17:18:00 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/wpf/library/com_structs.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/wpf/library/com_structs.c
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,38 +11,4 @@
 
 #include "swt.h"
 #include "com_structs.h"
-
-#ifndef NO_POINT
-typedef struct POINT_FID_CACHE {
-	int cached;
-	jclass clazz;
-	jfieldID x, y;
-} POINT_FID_CACHE;
-
-POINT_FID_CACHE POINTFc;
-
-void cachePOINTFields(JNIEnv *env, jobject lpObject)
-{
-	if (POINTFc.cached) return;
-	POINTFc.clazz = (*env)->GetObjectClass(env, lpObject);
-	POINTFc.x = (*env)->GetFieldID(env, POINTFc.clazz, "x", "I");
-	POINTFc.y = (*env)->GetFieldID(env, POINTFc.clazz, "y", "I");
-	POINTFc.cached = 1;
-}
-
-POINT *getPOINTFields(JNIEnv *env, jobject lpObject, POINT *lpStruct)
-{
-	if (!POINTFc.cached) cachePOINTFields(env, lpObject);
-	lpStruct->x = (*env)->GetIntField(env, lpObject, POINTFc.x);
-	lpStruct->y = (*env)->GetIntField(env, lpObject, POINTFc.y);
-	return lpStruct;
-}
-
-void setPOINTFields(JNIEnv *env, jobject lpObject, POINT *lpStruct)
-{
-	if (!POINTFc.cached) cachePOINTFields(env, lpObject);
-	(*env)->SetIntField(env, lpObject, POINTFc.x, (jint)lpStruct->x);
-	(*env)->SetIntField(env, lpObject, POINTFc.y, (jint)lpStruct->y);
-}
-#endif
 
--- bundles/org.eclipse.swt/Eclipse SWT PI/wpf/library/com_structs.h
+++ bundles/org.eclipse.swt/Eclipse SWT PI/wpf/library/com_structs.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2000, 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,16 +10,4 @@
  *******************************************************************************/
 
 #include "com.h"
-
-#ifndef NO_POINT
-void cachePOINTFields(JNIEnv *env, jobject lpObject);
-POINT *getPOINTFields(JNIEnv *env, jobject lpObject, POINT *lpStruct);
-void setPOINTFields(JNIEnv *env, jobject lpObject, POINT *lpStruct);
-#define POINT_sizeof() sizeof(POINT)
-#else
-#define cachePOINTFields(a,b)
-#define getPOINTFields(a,b,c) NULL
-#define setPOINTFields(a,b,c)
-#define POINT_sizeof() 0
-#endif
 
