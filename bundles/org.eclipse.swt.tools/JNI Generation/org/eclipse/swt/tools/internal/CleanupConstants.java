6d6d628a2115119a60f6458783e918940a8d3866
Silenio Quarti <silenio>
2007-01-29 21:37:41 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt.tools/JNI Generation/org/eclipse/swt/tools/internal/CleanupConstants.java
+++ bundles/org.eclipse.swt.tools/JNI Generation/org/eclipse/swt/tools/internal/CleanupConstants.java
@@ -69,19 +69,20 @@ public void generate(Field field) {
 		}
 	}
 	unusedCount++;
-//	output("NOT USED=" + field.toString() + " \n");
+	output("NOT USED=" + field.toString() + " \n");
 }
 
 public static void main(String[] args) {
-	if (args.length < 2) {
-		System.out.println("Usage: java CleanupConstants <OS className> <src path> <class source>");
+	if (args.length < 3) {
+		System.out.println("Usage: java CleanupConstants <OS className> <class source> <src path1> <src path2");
 		return;
 	}
 	try {
 		CleanupConstants gen = new CleanupConstants();
 		String clazzName = args[0];
-		String[] sourcePath = new String[]{args[1]};
-		String classSource = args[2];
+		String classSource = args[1];
+		String[] sourcePath = new String[args.length - 2];
+		System.arraycopy(args, 2, sourcePath, 0, sourcePath.length);
 		Class clazz = Class.forName(clazzName);
 		gen.setSourcePath(sourcePath);
 		gen.setClassSourcePath(classSource);
