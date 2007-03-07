030bcb5f6efddc473fd624ba5bb2e6e54cf74a0f
Felipe Heidrich <fheidric>
2007-03-07 20:47:42 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/wpf/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/wpf/org/eclipse/swt/widgets/Shell.java
@@ -1062,6 +1062,7 @@ public void setEnabled (boolean enabled) {
 	checkWidget ();
 	if (enabled == getEnabled ()) return;
 	super.setEnabled (enabled);
+	if ((style & SWT.ON_TOP) != 0) return;
 	if (enabled && OS.Window_IsActive (shellHandle)) {
 		if (!restoreFocus ()) traverseGroup (true);
 	}
