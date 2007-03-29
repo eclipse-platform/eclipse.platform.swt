47e5aecda9915fca2d85e1d06251c6561a47a0a7
Felipe Heidrich <fheidric>
2007-03-29 23:05:49 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -155,13 +155,12 @@ public class StyledText extends Canvas {
 	int indent;
 	int lineSpacing;
 
-	final static boolean IS_CARBON, IS_GTK, IS_MOTIF, IS_WPF;
+	final static boolean IS_CARBON, IS_GTK, IS_MOTIF;
 	static {
 		String platform = SWT.getPlatform();
 		IS_CARBON = "carbon".equals(platform);
 		IS_GTK = "gtk".equals(platform);
 		IS_MOTIF = "motif".equals(platform);
-		IS_WPF = "wpf".equals(platform);
 	}
 
 	/**
