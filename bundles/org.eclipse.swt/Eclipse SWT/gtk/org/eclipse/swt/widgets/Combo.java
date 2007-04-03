90f805dbcc98ea687ba6fdc454a49c9c2699aa6f
Silenio Quarti <silenio>
2007-04-03 17:45:59 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
@@ -566,7 +566,7 @@ int /*long*/ fontHandle () {
 
 int /*long*/ focusHandle () {
 	if (OS.GTK_VERSION >= OS.VERSION (2, 4, 0)) {
-		if ((style & SWT.READ_ONLY) != 0) return buttonHandle;
+		if ((style & SWT.READ_ONLY) != 0 && buttonHandle != 0) return buttonHandle;
 	}
 	if (entryHandle != 0) return entryHandle;
 	return super.focusHandle ();
