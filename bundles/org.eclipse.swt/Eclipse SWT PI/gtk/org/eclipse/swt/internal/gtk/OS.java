120f8a8a579e1dc38ca2f9e9ac17edc3ffcce856
Silenio Quarti <silenio>
2007-04-27 18:54:17 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/OS.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/OS.java
@@ -301,6 +301,7 @@ public class OS extends C {
 	public static final int GTK_RC_BG = 1 << 1;
 	public static final int GTK_RC_FG = 1 << 0;
 	public static final int GTK_RC_TEXT = 1 << 2;
+	public static final int GTK_RC_BASE = 1 << 3;
 	public static final int GTK_RESPONSE_APPLY = 0xfffffff6;
 	public static final int GTK_RESPONSE_CANCEL = 0xfffffffa;
 	public static final int GTK_RESPONSE_OK = 0xfffffffb;
