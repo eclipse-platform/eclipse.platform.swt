a3631b719138ad2b6d28b6d8b526057f5d8135fd
Kevin Barnes <krbarnes>
2007-05-01 20:59:27 +0000

    Bug 172757 - popups close on resize - removing fix due to bug 184836

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -668,8 +668,9 @@ void hookEvents () {
 	OS.g_signal_connect_closure_by_id (shellHandle, display.signalIds [MAP_EVENT], 0, display.shellMapProcClosure, false);
 	OS.g_signal_connect_closure_by_id (shellHandle, display.signalIds [ENTER_NOTIFY_EVENT], 0, display.closures [ENTER_NOTIFY_EVENT], false);
 	OS.g_signal_connect_closure (shellHandle, OS.move_focus, display.closures [MOVE_FOCUS], false);
-	int /*long*/ window = OS.GTK_WIDGET_WINDOW (shellHandle);
-	OS.gdk_window_add_filter  (window, display.filterProc, shellHandle);
+	/* This code is intentionally commented. */
+//	int /*long*/ window = OS.GTK_WIDGET_WINDOW (shellHandle);
+//	OS.gdk_window_add_filter  (window, display.filterProc, shellHandle);
 }
 
 public boolean isEnabled () {
@@ -928,7 +929,8 @@ int /*long*/ gtk_focus_in_event (int /*long*/ widget, int /*long*/ event) {
 	if (widget != shellHandle) {
 		return super.gtk_focus_in_event (widget, event);
 	}
-	if (display.lastFocusChangeEvent.mode == OS.NotifyUngrab) return 0;
+	/* This code is intentionally commented.*/
+//	if (display.lastFocusChangeEvent.mode == OS.NotifyUngrab) return 0;
 	if (tooltipsHandle != 0) OS.gtk_tooltips_enable (tooltipsHandle);
 	/*
 	* Feature in GTK. The GTK combo box popup under some window managers
@@ -952,7 +954,8 @@ int /*long*/ gtk_focus_out_event (int /*long*/ widget, int /*long*/ event) {
 	if (widget != shellHandle) {
 		return super.gtk_focus_out_event (widget, event);
 	}
-	if (display.lastFocusChangeEvent.mode == OS.NotifyGrab || display.lastFocusChangeEvent.mode == OS.NotifyWhileGrabbed) return 0;
+	/* This code is intentionally commented. */
+//	if (display.lastFocusChangeEvent.mode == OS.NotifyGrab || display.lastFocusChangeEvent.mode == OS.NotifyWhileGrabbed) return 0;
 	if (tooltipsHandle != 0) OS.gtk_tooltips_disable (tooltipsHandle);
 	/*
 	* Feature in GTK. The GTK combo box popup under some window managers
@@ -1725,8 +1728,9 @@ void releaseWidget () {
 	if (display.activeShell == this) display.activeShell = null;
 	if (tooltipsHandle != 0) OS.g_object_unref (tooltipsHandle);
 	tooltipsHandle = 0;
-	int /*long*/ window = OS.GTK_WIDGET_WINDOW (shellHandle);
-	OS.gdk_window_remove_filter(window, display.filterProc, shellHandle);
+	/* This code is intentionally commented. */
+//	int /*long*/ window = OS.GTK_WIDGET_WINDOW (shellHandle);
+//	OS.gdk_window_remove_filter(window, display.filterProc, shellHandle);
 	region = null;
 	lastActive = null;
 }
