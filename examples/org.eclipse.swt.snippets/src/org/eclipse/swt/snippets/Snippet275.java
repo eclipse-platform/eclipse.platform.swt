509dee0fa7caf40814964791632d2396ebef2a87
Grant Gayed <ggayed>
2007-07-12 14:09:06 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet275.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet275.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Canvas snippet: update a Canvas frequently
+ * Canvas snippet: update part of a Canvas frequently
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
