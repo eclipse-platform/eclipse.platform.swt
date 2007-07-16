3d425b802bd95345d9ce8e9110947cfe00a94b18
Silenio Quarti <silenio>
2007-07-16 15:15:31 +0000

    building 64 bit from network drive

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -12,39 +12,41 @@ rem ****************************************************************************
 
 @echo off
 
-IF "%1"=="x86_64" GOTO X86_64
-IF "%1"=="IA64" GOTO IA64
+IF x.%SWT_BUILDDIR%==x. set SWT_BUILDDIR=j:\teamswt\swt-builddir
+
+IF x.%1==x.x86 GOTO X86
+IF x.%1==x.x86_64 GOTO X86_64
+IF x.%1==x.ia64 GOTO IA64
 
 :X86
 
-IF "%DEV_TOOLS%"=="" set DEV_TOOLS=K:\dev\PRODUCTS
+IF x.%DEV_TOOLS%==x. set DEV_TOOLS=K:\dev\PRODUCTS
 call %DEV_TOOLS%\msvc60\vc98\bin\vcvars32.bat
-IF "%MSSDK%"=="" set MSSDK=%DEV_TOOLS%\platsdk\feb2003
+IF x.%MSSDK%==x. set MSSDK=%DEV_TOOLS%\platsdk\feb2003
 call %MSSDK%\setenv /XP32 /RETAIL
-IF "%OUTPUT_DIR%"=="" set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86
-IF "%JAVA_HOME%"=="" set JAVA_HOME=j:\teamswt\swt-builddir\ibm-jdk1.4.1
-IF "%XULRUNNER_SDK%"=="" set XULRUNNER_SDK=j:\teamswt\swt-builddir\gecko-sdk
+IF x.%OUTPUT_DIR%==x. set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86
+IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-jdk1.4.1
+IF x.%XULRUNNER_SDK%==x. set XULRUNNER_SDK=%SWT_BUILDDIR%\gecko-sdk
 set XULRUNNER_MAKE=make_xulrunner make_xpcominit
-IF "%1"=="X86" shift
+IF x.%1==x.x86 shift
 GOTO MAKE
 
 :X86_64
 
-IF "%MSSDK%"=="" set MSSDK=c:\sdk
+IF x.%MSSDK%==x. set MSSDK="%SWT_BUILDDIR%\MSSDKs\Microsoft Platform SDK for Windows Server 2003 R2"
 call %MSSDK%\setenv /X64 /RETAIL
-IF "%OUTPUT_DIR%"=="" set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
-IF "%JAVA_HOME%"=="" set JAVA_HOME=C:\jdk150-64
+IF x.%OUTPUT_DIR%==x. set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
+IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-sdk50-x86_64
 set CFLAGS=-DSWT_PTR_SIZE_64
 shift
 GOTO MAKE
 
-
 :IA64
 
-IF "%MSSDK%"=="" set MSSDK=c:\sdk
+IF x.%MSSDK%==x. set MSSDK="%SWT_BUILDDIR%\MSSDKs\Microsoft Platform SDK for Windows Server 2003 R2"
 call %MSSDK%\setenv /SRV64 /RETAIL
-IF "%OUTPUT_DIR%"=="" set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.ia64
-IF "%JAVA_HOME%"=="" set JAVA_HOME=C:\jdk142_ia64
+IF x.%OUTPUT_DIR%==x. set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.ia64
+IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-sdk142-ia64
 set CFLAGS=-DSWT_PTR_SIZE_64
 shift
 GOTO MAKE
