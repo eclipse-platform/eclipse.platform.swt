ccdc7255454df20ae1092b88d4aa90b0900e4fe5
Felipe Heidrich <fheidric>
2007-09-11 18:59:00 +0000

    Bug 120616 [KeyBindings] interactions: Ctrl+Shift+I does not arrive at display KeyDown filter

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
@@ -1369,7 +1369,10 @@ void hookEvents () {
 }
 
 int /*long*/ imContext () {
-	return (style & SWT.SINGLE) != 0 ? OS.GTK_ENTRY_IM_CONTEXT (handle) : OS.GTK_TEXTVIEW_IM_CONTEXT (handle);
+	if ((style & SWT.SINGLE) != 0) {
+		return OS.gtk_editable_get_editable (handle) ? OS.GTK_ENTRY_IM_CONTEXT (handle) : 0;
+	} 
+	return OS.GTK_TEXTVIEW_IM_CONTEXT (handle);
 }
 
 /**
