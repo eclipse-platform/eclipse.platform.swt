369c599db7632a37011e90f7f5fd408519cc0290
Felipe Heidrich <fheidric>
2007-09-12 20:17:03 +0000

    fix Bug 87774 problem 1a (StyledText: Caret at end of line is clipped)

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -4648,7 +4648,7 @@ int getCaretLine() {
 }
 int getWrapWidth () {
 	if (wordWrap && !isSingleLine()) {
-		int width = clientAreaWidth - leftMargin - rightMargin;
+		int width = clientAreaWidth - leftMargin - rightMargin - getCaretWidth();
 		return width > 0 ? width : 1;
 	}
 	return -1;
