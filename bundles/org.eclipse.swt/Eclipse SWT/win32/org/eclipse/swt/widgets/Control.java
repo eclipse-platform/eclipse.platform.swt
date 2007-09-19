fe1179fec4e5c55336830f29e326f19e3a9554b8
Steve Northover <steve>
2007-09-19 19:49:01 +0000

    get rid of flashing when control is moved and background image is inherited

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
@@ -2512,44 +2512,40 @@ void setBounds (int x, int y, int width, int height, int flags) {
 }
 
 void setBounds (int x, int y, int width, int height, int flags, boolean defer) {
-	int /*long*/ topHandle = topHandle ();
-	if (defer && parent != null) {
-		forceResize ();
-		if (OS.GetWindow (handle, OS.GW_CHILD) == 0) {
-			if (findImageControl () != null) {
-				flags |= OS.SWP_NOCOPYBITS;
-			} else {
-				if (OS.COMCTL32_MAJOR >= 6 && OS.IsAppThemed ()) {
-					if (findThemeControl () != null) flags |= OS.SWP_NOCOPYBITS;
-				}
-			}
-		}
-		WINDOWPOS [] lpwp = parent.lpwp;
-		if (lpwp == null) {
-			SetWindowPos (topHandle, 0, x, y, width, height, flags);
-		} else {
-			int index = 0;
-			while (index < lpwp.length) {
-				if (lpwp [index] == null) break;
-				index ++;
-			}
-			if (index == lpwp.length) {
-				WINDOWPOS [] newLpwp = new WINDOWPOS [lpwp.length + 4];
-				System.arraycopy (lpwp, 0, newLpwp, 0, lpwp.length);
-				parent.lpwp = lpwp = newLpwp;
-			}
-			WINDOWPOS wp = new WINDOWPOS ();
-			wp.hwnd = topHandle;
-			wp.x = x;
-			wp.y = y;
-			wp.cx = width;
-			wp.cy = height;
-			wp.flags = flags;
-			lpwp [index] = wp;
-		}
+	if (findImageControl () != null) {
+		if (backgroundImage == null) flags |= OS.SWP_NOCOPYBITS;
 	} else {
-		SetWindowPos (topHandle, 0, x, y, width, height, flags);
+		if (OS.GetWindow (handle, OS.GW_CHILD) == 0) {
+			if (OS.COMCTL32_MAJOR >= 6 && OS.IsAppThemed ()) {
+				if (findThemeControl () != null) flags |= OS.SWP_NOCOPYBITS;
+			}
+		}
 	}
+	int /*long*/ topHandle = topHandle ();
+	if (defer && parent != null && parent.lpwp != null) {
+		forceResize ();
+		int index = 0;
+		WINDOWPOS [] lpwp = parent.lpwp;
+		while (index < lpwp.length) {
+			if (lpwp [index] == null) break;
+			index ++;
+		}
+		if (index == lpwp.length) {
+			WINDOWPOS [] newLpwp = new WINDOWPOS [lpwp.length + 4];
+			System.arraycopy (lpwp, 0, newLpwp, 0, lpwp.length);
+			parent.lpwp = lpwp = newLpwp;
+		}
+		WINDOWPOS wp = new WINDOWPOS ();
+		wp.hwnd = topHandle;
+		wp.x = x;
+		wp.y = y;
+		wp.cx = width;
+		wp.cy = height;
+		wp.flags = flags;
+		lpwp [index] = wp;
+		return;
+	}
+	SetWindowPos (topHandle, 0, x, y, width, height, flags);
 }
 
 /**
