6f2fb087f0013102d5d85bdddd96450f297dbe0c
Steve Northover <steve>
2007-10-09 21:26:48 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet281.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet281.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * draw a multi-gradient (without advanced graphics)
+ * draw a multi-gradient
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
@@ -21,7 +21,7 @@ import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
-public class Snippet280 {
+public class Snippet281 {
 
 public static void main (String [] args) {
 	final Display display = new Display ();
@@ -33,19 +33,20 @@ public static void main (String [] args) {
 			Rectangle rect = shell.getClientArea ();
 			Color color1 = new Color (display, 234, 246, 253);
 			Color color2 = new Color (display, 217, 240, 252);
-			gc.setForeground(color1);
-			gc.setBackground(color2);
-			gc.fillGradientRectangle (rect.x, rect.y, rect.width, rect.height / 2, true);
-			color1.dispose ();
-			color2.dispose ();
 			Color color3 = new Color (display, 190, 230, 253);
 			Color color4 = new Color (display, 167, 217, 245);
-			gc.setForeground(color3);
-			gc.setBackground(color4);
-			gc.fillGradientRectangle (rect.x, rect.y + (rect.height / 2), rect.width, rect.height / 2 + 1, true);
+			Pattern p1 = new Pattern (display, 0, 0, 0, rect.height / 2, color1, color2);
+			gc.setBackgroundPattern (p1);
+			gc.fillRectangle (rect.x, rect.y, rect.width, rect.height / 2);
+			Pattern p2 = new Pattern (display, 0, rect.height / 2, 0, rect.height, color3, color4);
+			gc.setBackgroundPattern (p2);
+			gc.fillRectangle (rect.x, rect.y + (rect.height / 2), rect.width, rect.height / 2 + 1);
+			p1.dispose ();
+			p2.dispose ();
+			color1.dispose ();
+			color2.dispose ();
 			color3.dispose ();
 			color4.dispose ();
-			
 		}
 	});
 	shell.setSize (200, 64);
