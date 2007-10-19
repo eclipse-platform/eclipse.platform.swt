07e2a59a9f95423dd94e93f17c1d85884434abf2
Bogdan Gheorghe <gheorghe>
2007-10-19 14:55:46 +0000

    NullPointer in recursive resize

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Control.java
@@ -2525,28 +2525,30 @@ void setBounds (int x, int y, int width, int height, int flags, boolean defer) {
 		}
 	}
 	int /*long*/ topHandle = topHandle ();
-	if (defer && parent != null && parent.lpwp != null) {
+	if (defer && parent != null) {
 		forceResize ();
-		int index = 0;
-		WINDOWPOS [] lpwp = parent.lpwp;
-		while (index < lpwp.length) {
-			if (lpwp [index] == null) break;
-			index ++;
+		if (parent.lpwp != null) {
+			int index = 0;
+			WINDOWPOS [] lpwp = parent.lpwp;
+			while (index < lpwp.length) {
+				if (lpwp [index] == null) break;
+				index ++;
+			}
+			if (index == lpwp.length) {
+				WINDOWPOS [] newLpwp = new WINDOWPOS [lpwp.length + 4];
+				System.arraycopy (lpwp, 0, newLpwp, 0, lpwp.length);
+				parent.lpwp = lpwp = newLpwp;
+			}
+			WINDOWPOS wp = new WINDOWPOS ();
+			wp.hwnd = topHandle;
+			wp.x = x;
+			wp.y = y;
+			wp.cx = width;
+			wp.cy = height;
+			wp.flags = flags;
+			lpwp [index] = wp;
+			return;
 		}
-		if (index == lpwp.length) {
-			WINDOWPOS [] newLpwp = new WINDOWPOS [lpwp.length + 4];
-			System.arraycopy (lpwp, 0, newLpwp, 0, lpwp.length);
-			parent.lpwp = lpwp = newLpwp;
-		}
-		WINDOWPOS wp = new WINDOWPOS ();
-		wp.hwnd = topHandle;
-		wp.x = x;
-		wp.y = y;
-		wp.cx = width;
-		wp.cy = height;
-		wp.flags = flags;
-		lpwp [index] = wp;
-		return;
 	}
 	SetWindowPos (topHandle, 0, x, y, width, height, flags);
 }
