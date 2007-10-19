5224072813ce3fd5641be636a650fa66956ef036
Steve Northover <steve>
2007-10-19 23:10:12 +0000

    fix missing external gtk_paper_size_free()

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/os.c
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/os.c
@@ -10079,7 +10079,23 @@ JNIEXPORT void JNICALL OS_NATIVE(_1gtk_1paper_1size_1free)
 	(JNIEnv *env, jclass that, jint arg0)
 {
 	OS_NATIVE_ENTER(env, that, _1gtk_1paper_1size_1free_FUNC);
+/*
 	gtk_paper_size_free(arg0);
+*/
+	{
+		static int initialized = 0;
+		static void *handle = NULL;
+		typedef void (*FPTR)(jint);
+		static FPTR fptr;
+		if (!initialized) {
+			if (!handle) handle = dlopen(gtk_paper_size_free_LIB, RTLD_LAZY);
+			if (handle) fptr = (FPTR)dlsym(handle, "gtk_paper_size_free");
+			initialized = 1;
+		}
+		if (fptr) {
+			(*fptr)(arg0);
+		}
+	}
 	OS_NATIVE_EXIT(env, that, _1gtk_1paper_1size_1free_FUNC);
 }
 #endif
