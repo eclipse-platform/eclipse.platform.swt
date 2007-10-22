28e10d8f31e3a9648d4df39711198f4cb9feb5c0
Bogdan Gheorghe <gheorghe>
2007-10-22 21:33:49 +0000

    v3347 for M20071022

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 346;
+    static int MINOR_VERSION = 347;
 	
 	/**
 	 * SWT revision number (must be >= 0)
