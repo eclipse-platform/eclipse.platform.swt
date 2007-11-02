bb26b871fab9c3a547e19d73de0dadeae6e67e41
Steve Northover <steve>
2007-11-02 15:36:49 +0000

    208566 - [64] cheese can appear if SetPoint is running

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Display.java
@@ -253,7 +253,7 @@ public class Display extends Device {
 	
 	/* Resize and move recursion */
 	int resizeCount;
-	static final int RESIZE_LIMIT = 6;
+	static final int RESIZE_LIMIT = 4;
 	
 	/* Display Data */
 	Object data;
