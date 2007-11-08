650ef5b61a9d31b44646d8949446c0c4184954e3
Silenio Quarti <silenio>
2007-11-08 20:48:57 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/SWTBox.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/SWTBox.java
@@ -1,6 +1,6 @@
 package org.eclipse.swt.internal.cocoa;
 
-public class SWTBox extends NSView {
+public class SWTBox extends NSBox {
 
 public void setTag(int tag) {
 	OS.objc_msgSend(id, OS.sel_setTag_1, tag);
