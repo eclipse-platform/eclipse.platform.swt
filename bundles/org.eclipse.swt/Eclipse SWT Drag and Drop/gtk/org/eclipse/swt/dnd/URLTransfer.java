62f84b2abd7402bfae84adccab6985a7d20de9ae
Duong Nguyen <dnguyen>
2007-11-15 22:49:19 +0000

    Bug 100095 - [DND] Consider making URLTransfer public

---
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/URLTransfer.java
@@ -0,0 +1,120 @@
+/*******************************************************************************
+ * Copyright (c) 2007 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.dnd;
+
+import org.eclipse.swt.internal.gtk.*;
+
+import java.net.*;
+
+/**
+ * The class <code>URLTransfer</code> provides a platform specific mechanism 
+ * for converting text in URL format represented as a java <code>String</code> 
+ * to a platform specific representation of the data and vice versa.  See 
+ * <code>Transfer</code> for additional information. The string  
+ * must be a fully specified url.
+ * 
+ * <p>An example of a java <code>String[]</code> containing a URL is shown 
+ * below:</p>
+ * 
+ * <code><pre>
+ *     String urlData = "http://www.eclipse.org";
+ * </code></pre>
+ */
+public class URLTransfer extends ByteArrayTransfer {
+
+	static URLTransfer _instance = new URLTransfer();
+	private static final String TEXT_UNICODE = "text/unicode"; //$NON-NLS-1$
+	private static final int TEXT_UNICODE_ID = registerType(TEXT_UNICODE);	
+	
+private URLTransfer() {}
+
+/**
+ * Returns the singleton instance of the URLTransfer class.
+ *
+ * @return the singleton instance of the URLTransfer class
+ */
+public static URLTransfer getInstance () {
+	return _instance;
+}
+
+/**
+ * This implementation of <code>javaToNative</code> converts a URL 
+ * represented by a java <code>String</code> to a platform specific representation.
+ * For additional information see <code>Transfer#javaToNative</code>.
+ * 
+ * @param object a java <code>String</code> containing a URL
+ * @param transferData an empty <code>TransferData</code> object; this
+ *  object will be filled in on return with the platform specific format of the data
+ */
+public void javaToNative (Object object, TransferData transferData){
+	transferData.result = 0;
+	if (!checkURL(object) || !isSupportedType(transferData)) {
+		DND.error(DND.ERROR_INVALID_DATA);
+	}
+	String string = (String)object;
+	int charCount = string.length();
+	char [] chars = new char[charCount +1];
+	string.getChars(0, charCount , chars, 0);
+	int byteCount = chars.length*2;
+	int /*long*/ pValue = OS.g_malloc(byteCount);
+	if (pValue == 0) return;
+	OS.memmove(pValue, chars, byteCount);
+	transferData.length = byteCount;
+	transferData.format = 8;
+	transferData.pValue = pValue;
+	transferData.result = 1;		
+}
+
+/**
+ * This implementation of <code>nativeToJava</code> converts a platform specific 
+ * representation of a URL <code>String</code>.
+ * For additional information see <code>Transfer#nativeToJava</code>.
+ * 
+ * @param transferData the platform specific representation of the data to be 
+ * been converted
+ * @return a java <code>String</code> containing a URL if the 
+ * conversion was successful; otherwise null
+ */
+public Object nativeToJava(TransferData transferData){
+	if (!isSupportedType(transferData) ||  transferData.pValue == 0) return null;
+	/* Ensure byteCount is a multiple of 2 bytes */
+	int size = (transferData.format * transferData.length / 8) / 2 * 2;
+	if (size <= 0) return null;			
+	byte[] chars = new byte [size];
+	OS.memmove (chars, transferData.pValue, size);
+	String string = new String (chars);
+	int end = string.indexOf('\0');
+	return (end == -1) ? string : string.substring(0, end);
+}
+
+protected int[] getTypeIds(){
+	return new int[] {TEXT_UNICODE_ID};
+}
+
+protected String[] getTypeNames(){
+	return new String[] {TEXT_UNICODE}; 
+}
+
+boolean checkURL(Object object) {
+	if (object == null  || !(object instanceof String) || ((String)object).length() == 0) return false;
+	String url = (String)object;
+	try {
+		new URL(url);
+	} catch (java.net.MalformedURLException e) {
+		return false;
+	}
+	return true;
+}
+
+protected boolean validate(Object object) {
+	return checkURL(object);
+}
+}
