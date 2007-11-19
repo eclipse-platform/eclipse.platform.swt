536fea109939f314c8db78904a303b501123d0c7
Silenio Quarti <silenio>
2007-11-19 18:59:26 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/graphics/Image.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/graphics/Image.java
@@ -211,97 +211,86 @@ public Image(Device device, Image srcImage, int flag) {
 	this.type = srcImage.type;
 
 	/* Get source image size */
-// 	int width = OS.CGImageGetWidth(srcImage.handle);
-// 	int height = OS.CGImageGetHeight(srcImage.handle);
-// 	int bpr = OS.CGImageGetBytesPerRow(srcImage.handle);
-// 	int bpc = OS.CGImageGetBitsPerComponent(srcImage.handle);
-// 	int bpp = OS.CGImageGetBitsPerPixel(srcImage.handle);
-//	int colorspace = OS.CGImageGetColorSpace(srcImage.handle);
-//	int alphaInfo = OS.kCGImageAlphaNoneSkipFirst;
-// 	
-//	/* Copy transparent pixel and alpha data when necessary */
-//	alphaInfo = OS.CGImageGetAlphaInfo(srcImage.handle);
-//	transparentPixel = srcImage.transparentPixel;
-//	alpha = srcImage.alpha;
-//	if (srcImage.alphaData != null) {
-//		alphaData = new byte[srcImage.alphaData.length];
-//		System.arraycopy(srcImage.alphaData, 0, alphaData, 0, alphaData.length);
-//	}
-//
-//	/* Create the image */
-//	int dataSize = height * bpr;
-//	data = OS.NewPtr(dataSize);
-//	if (data == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-//	int provider = OS.CGDataProviderCreateWithData(0, data, dataSize, device.releaseProc);
-//	if (provider == 0) {
-//		OS.DisposePtr(data);
-//		SWT.error(SWT.ERROR_NO_HANDLES);
-//	}
-//	handle = OS.CGImageCreate(width, height, bpc, bpp, bpr, colorspace, alphaInfo, provider, null, true, 0);
-//	OS.CGDataProviderRelease(provider);
-//	if (handle == 0) {
-//		OS.DisposePtr(data);
-//		SWT.error(SWT.ERROR_NO_HANDLES);
-//	}
-//	
-//	OS.memmove(data, srcImage.data, dataSize);
-//	if (flag == SWT.IMAGE_COPY) return;
-//	
-//	/* Apply transformation */
-//	switch (flag) {
-//		case SWT.IMAGE_DISABLE: {
-//			Color zeroColor = device.getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW);
-//			RGB zeroRGB = zeroColor.getRGB();
-//			byte zeroRed = (byte)zeroRGB.red;
-//			byte zeroGreen = (byte)zeroRGB.green;
-//			byte zeroBlue = (byte)zeroRGB.blue;
-//			Color oneColor = device.getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
-//			RGB oneRGB = oneColor.getRGB();
-//			byte oneRed = (byte)oneRGB.red;
-//			byte oneGreen = (byte)oneRGB.green;
-//			byte oneBlue = (byte)oneRGB.blue;
-//			byte[] line = new byte[bpr];
-//			for (int y=0; y<height; y++) {
-//				OS.memmove(line, data + (y * bpr), bpr);
-//				int offset = 0;
-//				for (int x=0; x<width; x++) {
-//					int red = line[offset+1] & 0xFF;
-//					int green = line[offset+2] & 0xFF;
-//					int blue = line[offset+3] & 0xFF;
-//					int intensity = red * red + green * green + blue * blue;
-//					if (intensity < 98304) {
-//						line[offset+1] = zeroRed;
-//						line[offset+2] = zeroGreen;
-//						line[offset+3] = zeroBlue;
-//					} else {
-//						line[offset+1] = oneRed;
-//						line[offset+2] = oneGreen;
-//						line[offset+3] = oneBlue;
-//					}
-//					offset += 4;
-//				}
-//				OS.memmove(data + (y * bpr), line, bpr);
-//			}
-//			break;
-//		}
-//		case SWT.IMAGE_GRAY: {			
-//			byte[] line = new byte[bpr];
-//			for (int y=0; y<height; y++) {
-//				OS.memmove(line, data + (y * bpr), bpr);
-//				int offset = 0;
-//				for (int x=0; x<width; x++) {
-//					int red = line[offset+1] & 0xFF;
-//					int green = line[offset+2] & 0xFF;
-//					int blue = line[offset+3] & 0xFF;
-//					byte intensity = (byte)((red+red+green+green+green+green+green+blue) >> 3);
-//					line[offset+1] = line[offset+2] = line[offset+3] = intensity;
-//					offset += 4;
-//				}
-//				OS.memmove(data + (y * bpr), line, bpr);
-//			}
-//			break;
-//		}
-//	}
+	NSSize size = srcImage.handle.size();
+ 	int width = (int)size.width;
+ 	int height = (int)size.height;
+ 	NSBitmapImageRep srcRep = srcImage.imageRep;
+ 	int bpr = srcRep.bytesPerRow();
+
+	/* Copy transparent pixel and alpha data when necessary */
+	transparentPixel = srcImage.transparentPixel;
+	alpha = srcImage.alpha;
+	if (srcImage.alphaData != null) {
+		alphaData = new byte[srcImage.alphaData.length];
+		System.arraycopy(srcImage.alphaData, 0, alphaData, 0, alphaData.length);
+	}
+
+	/* Create the image */
+	handle = (NSImage)new NSImage().alloc();
+	handle = handle.initWithSize(size);
+	NSBitmapImageRep rep = imageRep = (NSBitmapImageRep)new NSBitmapImageRep().alloc();
+	rep = rep.initWithBitmapDataPlanes_pixelsWide_pixelsHigh_bitsPerSample_samplesPerPixel_hasAlpha_isPlanar_colorSpaceName_bitmapFormat_bytesPerRow_bitsPerPixel_(0, width, height, srcRep.bitsPerSample(), srcRep.samplesPerPixel(), srcRep.samplesPerPixel() == 4, srcRep.isPlanar(), new NSString(OS.NSDeviceRGBColorSpace()), OS.NSAlphaFirstBitmapFormat | OS.NSAlphaNonpremultipliedBitmapFormat, srcRep.bytesPerRow(), srcRep.bitsPerPixel());
+	handle.addRepresentation(rep);
+	
+	int data = rep.bitmapData();
+	OS.memmove(data, srcImage.imageRep.bitmapData(), width * height * 4);
+	if (flag == SWT.IMAGE_COPY) return;
+	
+	/* Apply transformation */
+	switch (flag) {
+		case SWT.IMAGE_DISABLE: {
+			Color zeroColor = device.getSystemColor(SWT.COLOR_WIDGET_NORMAL_SHADOW);
+			RGB zeroRGB = zeroColor.getRGB();
+			byte zeroRed = (byte)zeroRGB.red;
+			byte zeroGreen = (byte)zeroRGB.green;
+			byte zeroBlue = (byte)zeroRGB.blue;
+			Color oneColor = device.getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+			RGB oneRGB = oneColor.getRGB();
+			byte oneRed = (byte)oneRGB.red;
+			byte oneGreen = (byte)oneRGB.green;
+			byte oneBlue = (byte)oneRGB.blue;
+			byte[] line = new byte[bpr];
+			for (int y=0; y<height; y++) {
+				OS.memmove(line, data + (y * bpr), bpr);
+				int offset = 0;
+				for (int x=0; x<width; x++) {
+					int red = line[offset+1] & 0xFF;
+					int green = line[offset+2] & 0xFF;
+					int blue = line[offset+3] & 0xFF;
+					int intensity = red * red + green * green + blue * blue;
+					if (intensity < 98304) {
+						line[offset+1] = zeroRed;
+						line[offset+2] = zeroGreen;
+						line[offset+3] = zeroBlue;
+					} else {
+						line[offset+1] = oneRed;
+						line[offset+2] = oneGreen;
+						line[offset+3] = oneBlue;
+					}
+					offset += 4;
+				}
+				OS.memmove(data + (y * bpr), line, bpr);
+			}
+			break;
+		}
+		case SWT.IMAGE_GRAY: {			
+			byte[] line = new byte[bpr];
+			for (int y=0; y<height; y++) {
+				OS.memmove(line, data + (y * bpr), bpr);
+				int offset = 0;
+				for (int x=0; x<width; x++) {
+					int red = line[offset+1] & 0xFF;
+					int green = line[offset+2] & 0xFF;
+					int blue = line[offset+3] & 0xFF;
+					byte intensity = (byte)((red+red+green+green+green+green+green+blue) >> 3);
+					line[offset+1] = line[offset+2] = line[offset+3] = intensity;
+					offset += 4;
+				}
+				OS.memmove(data + (y * bpr), line, bpr);
+			}
+			break;
+		}
+	}
 }
 
 /**
