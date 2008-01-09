a1a6b6995d1fcb7aee374fcf42e1955214f86461
Carolyn MacLeod <carolyn>
2008-01-09 16:44:17 +0000

    Snippet39: create a CCombo

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet39.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet39.java
@@ -10,6 +10,41 @@
  *******************************************************************************/
 package org.eclipse.swt.snippets;
 
-public class Snippet39 {
+/*
+ * CCombo example snippet: create a CCombo
+ *
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ */
+import org.eclipse.swt.*;
+import org.eclipse.swt.custom.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.widgets.*;
 
+public class Snippet39 {
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new GridLayout());
+		
+		CCombo combo = new CCombo(shell, SWT.FLAT | SWT.BORDER);
+		combo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
+		for (int i = 0; i < 5; i++) {
+			combo.add("item" + i);
+		}
+		combo.setText("item0");
+
+		combo.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("Item selected");
+			};
+		});
+
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+	}
 }
