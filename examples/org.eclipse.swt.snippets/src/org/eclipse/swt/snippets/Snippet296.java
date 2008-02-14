4a45faf0133d6906cc4a1ac0a29f09b4e7440666
Grant Gayed <ggayed>
2008-02-14 16:13:19 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet296.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet296.java
@@ -24,57 +24,57 @@ import org.eclipse.swt.widgets.*;
 public class Snippet296 {
 	
 public static void main (String[] args) {
-    final Display display = new Display ();
-    Shell shell = new Shell (display);
-    shell.setBounds (10, 10, 200, 300);
-    final ScrolledComposite sc = new ScrolledComposite (shell, SWT.VERTICAL);
-    sc.setBounds (10, 10, 180, 200);
-    final int clientWidth = sc.getClientArea ().width;
+	final Display display = new Display ();
+	Shell shell = new Shell (display);
+	shell.setBounds (10, 10, 200, 300);
+	final ScrolledComposite sc = new ScrolledComposite (shell, SWT.VERTICAL);
+	sc.setBounds (10, 10, 180, 200);
+	final int clientWidth = sc.getClientArea ().width;
 
-    final Tree tree = new Tree (sc, SWT.NONE);
-    for (int i = 0; i < 99; i++) {
-        TreeItem item = new TreeItem (tree, SWT.NONE);
-        item.setText ("item " + i);
-        new TreeItem (item, SWT.NONE).setText ("child");
-    }
-    sc.setContent (tree);
-    int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
-    tree.setSize (clientWidth, prefHeight);
-    /*
-     * The following listener ensures that the Tree is always large
-     * enough to not need to show its own vertical scrollbar.
-     */
-    tree.addTreeListener (new TreeListener () {
-        public void treeExpanded (TreeEvent e) {
-            int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
-            tree.setSize (clientWidth, prefHeight);
-        }
-        public void treeCollapsed (TreeEvent e) {
-            int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
-            tree.setSize (clientWidth, prefHeight);
-        }
-    });
+	final Tree tree = new Tree (sc, SWT.NONE);
+	for (int i = 0; i < 99; i++) {
+		TreeItem item = new TreeItem (tree, SWT.NONE);
+		item.setText ("item " + i);
+		new TreeItem (item, SWT.NONE).setText ("child");
+	}
+	sc.setContent (tree);
+	int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
+	tree.setSize (clientWidth, prefHeight);
+	/*
+	 * The following listener ensures that the Tree is always large
+	 * enough to not need to show its own vertical scrollbar.
+	 */
+	tree.addTreeListener (new TreeListener () {
+		public void treeExpanded (TreeEvent e) {
+			int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
+			tree.setSize (clientWidth, prefHeight);
+		}
+		public void treeCollapsed (TreeEvent e) {
+			int prefHeight = tree.computeSize (SWT.DEFAULT, SWT.DEFAULT).y;
+			tree.setSize (clientWidth, prefHeight);
+		}
+	});
 
-    Button downButton = new Button (shell, SWT.PUSH);
-    downButton.setBounds (10, 220, 80, 30);
-    downButton.setText ("Down 10px");
-    downButton.addListener (SWT.Selection, new Listener () {
-        public void handleEvent (Event event) {
-            sc.setOrigin (0, sc.getOrigin ().y + 10);
-        }
-    });
-    Button upButton = new Button (shell, SWT.PUSH);
-    upButton.setBounds (100, 220, 80, 30);
-    upButton.setText ("Up 10px");
-    upButton.addListener (SWT.Selection, new Listener () {
-        public void handleEvent (Event event) {
-            sc.setOrigin (0, sc.getOrigin ().y - 10);
-        }
-    });
-    shell.open ();
-    while (!shell.isDisposed ()) {
-        if (!display.readAndDispatch ()) display.sleep ();
-    }
-    display.dispose ();
+	Button downButton = new Button (shell, SWT.PUSH);
+	downButton.setBounds (10, 220, 80, 30);
+	downButton.setText ("Down 10px");
+	downButton.addListener (SWT.Selection, new Listener () {
+		public void handleEvent (Event event) {
+			sc.setOrigin (0, sc.getOrigin ().y + 10);
+		}
+	});
+	Button upButton = new Button (shell, SWT.PUSH);
+	upButton.setBounds (100, 220, 80, 30);
+	upButton.setText ("Up 10px");
+	upButton.addListener (SWT.Selection, new Listener () {
+		public void handleEvent (Event event) {
+			sc.setOrigin (0, sc.getOrigin ().y - 10);
+		}
+	});
+	shell.open ();
+	while (!shell.isDisposed ()) {
+		if (!display.readAndDispatch ()) display.sleep ();
+	}
+	display.dispose ();
 }
 }
