6322ab217edb895e445f77f69acdb2a39dd947a8
Carolyn MacLeod <carolyn>
2008-02-21 21:19:03 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet297.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet297.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Table example snippet: create a table with columns and header images
+ * Table example snippet: create a table with columns header images
  *
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
