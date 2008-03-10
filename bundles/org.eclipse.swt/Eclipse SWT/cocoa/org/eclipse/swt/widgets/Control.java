7319ea76449839519ae1976d3a4dd6a44ee2a2c5
Kevin Barnes <krbarnes>
2008-03-10 18:54:13 +0000

    reversing changes

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -1292,14 +1292,12 @@ boolean hasFocus () {
  */
 public int internal_new_GC (GCData data) {
 	checkWidget();
-	NSGraphicsContext current = NSGraphicsContext.currentContext();
-	NSGraphicsContext context = NSGraphicsContext.graphicsContextWithWindow(view.window());
-	NSGraphicsContext.setCurrentContext(context);
-	NSAffineTransform transform = NSAffineTransform.transform();
-	NSRect rect = topView().frame(); //FIME: Needs to be updated when view size changes
-	transform.translateXBy(0, rect.height);
-	transform.scaleXBy(1, -1);
-	transform.set();
+	int context = 0;
+	if (data != null && data.paintRect != null) {
+		context = NSGraphicsContext.currentContext().id;
+	} else {
+		context = NSGraphicsContext.graphicsContextWithWindow(view.window()).id;
+	}
 	if (data != null) {
 		int mask = SWT.LEFT_TO_RIGHT | SWT.RIGHT_TO_LEFT;
 		if ((data.style & mask) == 0) {
@@ -1313,8 +1311,7 @@ public int internal_new_GC (GCData data) {
 		data.background = control.getBackgroundColor ().handle;
 		data.font = font != null ? font : defaultFont ();		
 	}
-	NSGraphicsContext.setCurrentContext(current);
-	return context.id;
+	return context;
 }
 
 /**	 
@@ -1331,9 +1328,7 @@ public int internal_new_GC (GCData data) {
  * @param data the platform specific GC data 
  */
 public void internal_dispose_GC (int context, GCData data) {
-	checkWidget ();
-	//performance? Carbon used CGContextSynchronize instead of CGContextFlush.
-	OS.objc_msgSend(context, OS.sel_flushGraphics);
+	checkWidget ();	
 }
 
 /**
