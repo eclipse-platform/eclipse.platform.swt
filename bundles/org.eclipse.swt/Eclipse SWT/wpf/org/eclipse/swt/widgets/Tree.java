67ff1f4cd46152a0f514cc916a071eff1d8f9f01
Kevin Barnes <krbarnes>
2008-03-17 14:57:55 +0000

    null check

--- bundles/org.eclipse.swt/Eclipse SWT/wpf/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/wpf/org/eclipse/swt/widgets/Tree.java
@@ -804,6 +804,7 @@ public int getHeaderHeight () {
  */
 public boolean getHeaderVisible () {
 	checkWidget ();
+	if (gvColumns == 0) return false;
 	int column = OS.GridViewColumnCollection_default (gvColumns, 0);
 	int header = OS.GridViewColumn_Header (column);
 	boolean visible = OS.UIElement_Visibility (header) == OS.Visibility_Visible;
