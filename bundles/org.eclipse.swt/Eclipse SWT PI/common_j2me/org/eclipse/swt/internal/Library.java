357660456ecb371d96816a94583c432813cd272d
Duong Nguyen <dnguyen>
2008-03-17 21:05:08 +0000

    v3434 for I20080318

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 433;
+    static int MINOR_VERSION = 434;
 	
 	/**
 	 * SWT revision number (must be >= 0)
