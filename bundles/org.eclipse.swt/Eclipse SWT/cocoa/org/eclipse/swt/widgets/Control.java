ea56eb1d77331a78b6a356377f8f6d91d536206f
Grant Gayed <ggayed>
2008-03-19 16:15:30 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -1550,19 +1550,14 @@ boolean sendKeyEvent (NSEvent nsEvent, int type) {
 		int keyCode = Display.translateKey (keys.characterAtIndex (i) & 0xFFFF);
 		if (keyCode != 0) {
 			event.keyCode = keyCode;
+			switch (event.keyCode) {
+				case SWT.BS: event.character = '\b'; break;
+				case SWT.DEL: event.character = 0x7F; break;
+			}
 		} else {
 			event.character = (char) keys.characterAtIndex (i);
 			//TODO - get unshifted vaules for Shift+1
 			event.keyCode = keyCodes.characterAtIndex (i);
-			/* 
-			* Feature on OSX.  The backspace key (labeled Delete on Mac keyboards) is mapped to SWT's
-			* Delete key code.  Fix the Backspace and Delete cases to be consistent with other platforms.
-			*/
-			if (event.keyCode == 127) {
-				event.keyCode = event.character = '\b';
-			} else if ((event.keyCode & 0xFFFF) == OS.NSDeleteFunctionKey) {
-				event.keyCode = event.character = 127;
-			}
 		}
 		setInputState (event, nsEvent, type);
 		if (sendKeyEvent (type, event)) {
