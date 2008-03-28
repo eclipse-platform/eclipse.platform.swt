867309de6a2e74382934d9b48eadafbb2b40e750
Duong Nguyen <dnguyen>
2008-03-28 18:50:27 +0000

    Bug 126545 - [CTabFolder] API to enable / disable Close-Button on TabItems    - undoing (removing) new API CTabFolder.getShowClose()    - use SWT.CLOSE getStyle() to get the showClose state.

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
@@ -1457,25 +1457,6 @@ public int getSelectionIndex() {
 	return selectedIndex;
 }
 /**
- * Returns <code>true</code> to indicate that the close button can be shown on tabs.
- * Otherwise return <code>false</code>. When this value is <code>false</code>, it will
- * override the showClose state of the individual tabs. The initial value is defined by 
- * the style (SWT.CLOSE) that was used to create the receiver.
- * 
- * @return <code>true</code> if the tabs can display the close button
- *
- * @exception SWTException <ul>
- *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
- *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
- * </ul>
- * 
- * @since 3.4
- */
-public boolean getShowClose() {
-	checkWidget();
-	return showClose;
-}
-/**
  * Returns <code>true</code> if the CTabFolder is rendered
  * with a simple, traditional shape.
  * 
@@ -1507,6 +1488,8 @@ public int getStyle() {
 	style &= ~(SWT.SINGLE | SWT.MULTI);
 	style |= single ? SWT.SINGLE : SWT.MULTI;
 	if (borderLeft != 0) style |= SWT.BORDER;
+	style &= ~SWT.CLOSE;
+	if (showClose) style |= SWT.CLOSE;
 	return style;
 }
 /**
