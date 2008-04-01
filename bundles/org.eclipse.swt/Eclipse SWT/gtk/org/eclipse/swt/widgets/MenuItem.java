6d969a62108c0eb45ec8fdbc21373ec0a1da5533
Felipe Heidrich <fheidric>
2008-04-01 18:33:11 +0000

    Bug 225151 GTK MenuItem Accelerator Consuming Characters (SWT.COMMAND)

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
@@ -850,6 +850,7 @@ public void setText (String string) {
 
 void updateAccelerator (int /*long*/ accelGroup, boolean add) {
 	if (accelerator == 0 || !getEnabled ()) return;
+	if ((accelerator & SWT.COMMAND) != 0) return;
 	int mask = 0;
 	if ((accelerator & SWT.ALT) != 0) mask |= OS.GDK_MOD1_MASK;
 	if ((accelerator & SWT.SHIFT) != 0) mask |= OS.GDK_SHIFT_MASK;
