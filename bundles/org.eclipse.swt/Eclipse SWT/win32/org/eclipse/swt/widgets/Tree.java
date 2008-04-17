2111ececac5e16563dcf9291794d1e640911aae6
Steve Northover <steve>
2008-04-17 21:11:21 +0000

    fill background rect using the measure event

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
@@ -1057,6 +1057,9 @@ LRESULT CDDS_ITEMPREPAINT (NMTVCUSTOMDRAW nmcd, int /*long*/ wParam, int /*long*
 							fillBackground (hDC, clrTextBk, rect);
 						} else {
 							RECT textRect = item.getBounds (index, true, false, false, false, true, hDC);
+							if (measureEvent != null) {
+								textRect.right = Math.min (cellRect.right, measureEvent.x + measureEvent.width);
+							}
 							fillBackground (hDC, clrTextBk, textRect);
 						}
 					} else {
