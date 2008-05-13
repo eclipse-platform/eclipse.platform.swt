db31883b9f41fd00bb5b0a446bfa1b3f53fba5d7
Grant Gayed <ggayed>
2008-05-13 14:20:24 +0000

    fix tabs

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet219.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet219.java
@@ -24,43 +24,43 @@ import org.eclipse.swt.widgets.*;
  */
 public class Snippet219 {
 	public static void main(String[] args) {
-        final Display display = new Display ();
-        final Image image = display.getSystemImage(SWT.ICON_INFORMATION);
-        final Shell shell = new Shell (display, SWT.NO_TRIM);
-        Region region = new Region();
-        final ImageData imageData = image.getImageData();
-        if (imageData.alphaData != null) {
-        	Rectangle pixel = new Rectangle(0, 0, 1, 1);
-        	for (int y = 0; y < imageData.height; y++) {
-	            for (int x = 0; x < imageData.width; x++) {
-	                if (imageData.getAlpha(x, y) == 255) {
-	                	pixel.x = imageData.x + x;
-    					pixel.y = imageData.y + y;
-	                    region.add(pixel);
-	                } 
-	            }
-	        }
-        } else {
-        	ImageData mask = imageData.getTransparencyMask();
-        	Rectangle pixel = new Rectangle(0, 0, 1, 1);
-    		for (int y = 0; y < mask.height; y++) {
-    			for (int x = 0; x < mask.width; x++) {
-    				if (mask.getPixel(x, y) != 0) {
-    					pixel.x = imageData.x + x;
-    					pixel.y = imageData.y + y;
-    					region.add(pixel);
-    				}
-    			}
-    		}
-        }
-        shell.setRegion(region);
+		final Display display = new Display ();
+		final Image image = display.getSystemImage(SWT.ICON_INFORMATION);
+		final Shell shell = new Shell (display, SWT.NO_TRIM);
+		Region region = new Region();
+		final ImageData imageData = image.getImageData();
+		if (imageData.alphaData != null) {
+			Rectangle pixel = new Rectangle(0, 0, 1, 1);
+			for (int y = 0; y < imageData.height; y++) {
+				for (int x = 0; x < imageData.width; x++) {
+					if (imageData.getAlpha(x, y) == 255) {
+						pixel.x = imageData.x + x;
+						pixel.y = imageData.y + y;
+						region.add(pixel);
+					} 
+				}
+			}
+		} else {
+			ImageData mask = imageData.getTransparencyMask();
+			Rectangle pixel = new Rectangle(0, 0, 1, 1);
+			for (int y = 0; y < mask.height; y++) {
+				for (int x = 0; x < mask.width; x++) {
+					if (mask.getPixel(x, y) != 0) {
+						pixel.x = imageData.x + x;
+						pixel.y = imageData.y + y;
+						region.add(pixel);
+					}
+				}
+			}
+		}
+		shell.setRegion(region);
 
-        Listener l = new Listener() {
-        	int startX, startY;
-            public void handleEvent(Event e)  {
-            	if (e.type == SWT.KeyDown && e.character == SWT.ESC) {
-            		shell.dispose();
-            	}
+		Listener l = new Listener() {
+			int startX, startY;
+			public void handleEvent(Event e)  {
+				if (e.type == SWT.KeyDown && e.character == SWT.ESC) {
+					shell.dispose();
+				}
 				if (e.type == SWT.MouseDown && e.button == 1) {
 					startX = e.x;
 					startY = e.y; 
@@ -74,21 +74,20 @@ public class Snippet219 {
 				if (e.type == SWT.Paint) {
 					e.gc.drawImage(image, imageData.x, imageData.y);
 				}
-            }
-        };
-        shell.addListener(SWT.KeyDown, l);
-        shell.addListener(SWT.MouseDown, l);
-        shell.addListener(SWT.MouseMove, l);
-        shell.addListener(SWT.Paint, l);
+			}
+		};
+		shell.addListener(SWT.KeyDown, l);
+		shell.addListener(SWT.MouseDown, l);
+		shell.addListener(SWT.MouseMove, l);
+		shell.addListener(SWT.Paint, l);
 
-        shell.setSize(imageData.x + imageData.width, imageData.y + imageData.height);
-        shell.open ();
-        while (!shell.isDisposed ()) {
-            if (!display.readAndDispatch ())
-                display.sleep ();
-        }
-        region.dispose();
-        image.dispose ();
-        display.dispose ();
-    }
+		shell.setSize(imageData.x + imageData.width, imageData.y + imageData.height);
+		shell.open ();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+		region.dispose();
+		image.dispose ();
+		display.dispose ();
+	}
 }
