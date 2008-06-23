760edfbbe3cc2559b5b64837460d851a3258d311
Steve Northover <steve>
2008-06-23 16:22:57 +0000

    move quick failure test to the top

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Text.java
@@ -1129,10 +1129,10 @@ public String getText () {
  */
 public String getText (int start, int end) {
 	checkWidget ();
+	if (!(start <= end && 0 <= end)) return "";
 	if (txnObject == 0) {
 		return new String (getEditText (start, end));
 	} else {
-		if (!(start <= end && 0 <= end)) return "";
 		int length = OS.TXNDataSize (txnObject) / 2;
 		end = Math.min (end, length - 1);
 		if (start > end) return "";
