7816becef036b9069e7ac1fdaa415b3c1691031a
Carolyn MacLeod <carolyn>
2008-10-02 17:52:28 +0000

    class comment javadoc

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/AccessibleShapesExample.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/AccessibleShapesExample.java
@@ -10,6 +10,13 @@
  *******************************************************************************/
 package org.eclipse.swt.examples.accessibility;
 
+/**
+ * This example creates some Shapes in a Shell and uses a FillLayout to position them.
+ * The Shape class is a simple example of the use of the SWT Accessibility API.
+ * A Shape is accessible, but it does not have any accessible children.
+ * 
+ * @see TrafficLight - for an example of a class that does have accessible children
+ */
 import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
