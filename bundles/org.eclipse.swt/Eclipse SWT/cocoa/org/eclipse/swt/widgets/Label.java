bd9da146b656668d415cecc0437afa60502dda1d
Grant Gayed <ggayed>
2008-10-10 14:36:42 +0000

    248881 - Dialog text doesn't wrap

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Label.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Label.java
@@ -110,17 +110,30 @@ public Point computeSize (int wHint, int hHint, boolean changed) {
 	NSRect oldRect = view.frame ();
 	int width = DEFAULT_WIDTH;
 	int height = DEFAULT_HEIGHT;
-	if (isImage && image != null) {
-		NSImage nsimage = image.handle;
-		NSSize size = nsimage.size ();
-		width = (int) size.width;
-		height = (int) size.height;
-	} else {
+	if ((style & SWT.SEPARATOR) != 0) {
 		((NSBox) view).sizeToFit ();
 		NSRect newRect = view.frame ();
 		width = (int) newRect.width;
 		height = (int) newRect.height;
 		view.setFrame (oldRect);
+	} else {
+		if (isImage) {
+			if (image != null) {
+				NSImage nsimage = image.handle;
+				NSSize size = nsimage.size ();
+				width = (int)size.width;
+				height = (int)size.height;
+			} else {
+				width = height = 0;
+			}
+		} else {
+			NSRect rect = new NSRect ();
+			rect.width = wHint != SWT.DEFAULT ? wHint : Float.MAX_VALUE;
+			rect.height = hHint != SWT.DEFAULT ? hHint : Float.MAX_VALUE;
+			NSSize size = textView.cell ().cellSizeForBounds (rect);
+			width = (int)size.width;
+			height = (int)size.height;
+		}
 	}
 	if (wHint != SWT.DEFAULT) width = wHint;
 	if (hHint != SWT.DEFAULT) height = hHint;
@@ -149,10 +162,8 @@ void createHandle () {
 		textWidget.setBordered(false);
 		textWidget.setEditable(false);
 		textWidget.setDrawsBackground(false);
-		if ((style & SWT.WRAP) != 0) {
-			NSTextFieldCell cell = new NSTextFieldCell(textWidget.cell());
-			cell.setWraps(true);
-		}
+		NSTextFieldCell cell = new NSTextFieldCell(textWidget.cell());
+		cell.setWraps ((style & SWT.WRAP) != 0);
 		
 		widget.addSubview(imageWidget);
 		widget.addSubview(textWidget);
