835e16ee699d5e7703467af3a6a75dff9c8f5499
Carolyn MacLeod <carolyn>
2008-10-16 19:00:52 +0000

    fix copyrights, add a bit of javadoc

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/AccessibleShapesExample.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/AccessibleShapesExample.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2006 IBM Corporation and others.
+ * Copyright (c) 2008 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -14,8 +14,6 @@ package org.eclipse.swt.examples.accessibility;
  * This example creates some Shapes in a Shell and uses a FillLayout to position them.
  * The Shape class is a simple example of the use of the SWT Accessibility API.
  * A Shape is accessible, but it does not have any accessible children.
- * 
- * @see TrafficLight - for an example of a class that does have accessible children
  */
 import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
