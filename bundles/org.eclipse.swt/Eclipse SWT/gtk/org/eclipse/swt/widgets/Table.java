5b1a45ed4c3760b7125580dc6accb138ea6353fd
Bogdan Gheorghe <gheorghe>
2008-10-22 15:49:44 +0000

    249880 - [Dialogs] Table.remove(int, int) very slow with large lists

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -2270,17 +2270,18 @@ public void remove (int start, int end) {
 	}
 	int /*long*/ selection = OS.gtk_tree_view_get_selection (handle);
 	int /*long*/ iter = OS.g_malloc (OS.GtkTreeIter_sizeof ());
-	OS.gtk_tree_model_iter_nth_child (modelHandle, iter, 0, start);
-	int index = start;
-	while (index <= end) {
+	int index = end;
+	while (index >= start) {
+		OS.gtk_tree_model_iter_nth_child (modelHandle, iter, 0, index);
 		TableItem item = items [index];
 		if (item != null && !item.isDisposed ()) item.release (false);
 		OS.g_signal_handlers_block_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
 		OS.gtk_list_store_remove (modelHandle, iter);
 		OS.g_signal_handlers_unblock_matched (selection, OS.G_SIGNAL_MATCH_DATA, 0, 0, 0, 0, CHANGED);
-		index++;
+		index--;
 	}
 	OS.g_free (iter);
+	index = end + 1;
 	System.arraycopy (items, index, items, start, itemCount - index);
 	for (int i=itemCount-(index-start); i<itemCount; i++) items [i] = null;
 	itemCount = itemCount - (index - start);
