711e54bb9fbcd95deeea36bec49c2d34adf9e10f
Grant Gayed <ggayed>
2008-10-27 16:21:54 +0000

    make header text clip

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TreeColumn.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TreeColumn.java
@@ -227,8 +227,12 @@ void drawInteriorWithFrame_inView (int /*long*/ id, int /*long*/ sel, int /*long
 	if (displayText != null) {
 		NSMutableDictionary dict = NSMutableDictionary.dictionaryWithCapacity(4);
 		dict.setObject (headerCell.font (), OS.NSFontAttributeName);
+		NSMutableParagraphStyle paragraphStyle = (NSMutableParagraphStyle)new NSMutableParagraphStyle ().alloc ().init ();
+		paragraphStyle.autorelease ();
+		paragraphStyle.setLineBreakMode (OS.NSLineBreakByClipping);
+		dict.setObject (paragraphStyle, OS.NSParagraphStyleAttributeName);
 		NSString string = NSString.stringWith (displayText);
-		attrString = ((NSAttributedString)new NSAttributedString ().alloc ()).initWithString (string, null);
+		attrString = ((NSAttributedString)new NSAttributedString ().alloc ()).initWithString (string, dict);
 		stringSize = attrString.size ();
 		contentWidth += Math.ceil (stringSize.width);
 		if (image != null) contentWidth += MARGIN; /* space between image and text */
