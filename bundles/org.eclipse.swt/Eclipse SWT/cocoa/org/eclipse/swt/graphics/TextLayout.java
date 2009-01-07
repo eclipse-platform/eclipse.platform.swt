eb8aab6b49c5876a109a67b08b0b8c4a0b12dc5d
Kevin Barnes <krbarnes>
2009-01-07 20:03:57 +0000

    260273 - Weird line duplication in text editor

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/graphics/TextLayout.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/graphics/TextLayout.java
@@ -644,7 +644,7 @@ public Rectangle getBounds() {
 		if (text.length() == 0) {
 			Font font = this.font != null ? this.font : device.systemFont;
 			NSFont nsFont = font.handle;
-			rect.height = Math.max(rect.height, layoutManager.defaultLineHeightForFont(nsFont));
+			rect.height = layoutManager.defaultLineHeightForFont(nsFont);
 		}
 		rect.height = Math.max(rect.height, ascent + descent) + spacing;
 		return new Rectangle(0, 0, (int)rect.width, (int)rect.height);
