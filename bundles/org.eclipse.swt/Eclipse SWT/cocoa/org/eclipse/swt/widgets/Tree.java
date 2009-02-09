4d57a207c4c5b5b183eda12d27fb28f92029c289
Silenio Quarti <silenio>
2009-02-09 18:22:04 +0000

    double checking for null

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Tree.java
@@ -2260,7 +2260,7 @@ boolean setScrollWidth () {
 boolean setScrollWidth (boolean set, TreeItem[] items, boolean recurse) {
 	if (items == null) return false;
 	if (ignoreRedraw || drawCount != 0) return false;
-	if (columnCount != 0 || items == null) return false;
+	if (columnCount != 0) return false;
 	GC gc = new GC (this);
 	int newWidth = calculateWidth (items, 0, gc, recurse);
 	gc.dispose ();
