5a140b463e4f8cefb3f4935039a580a51cf61954
Silenio Quarti <silenio>
2009-02-12 18:07:03 +0000

    262978 - jumpness in quick outline

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Scrollable.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Scrollable.java
@@ -190,7 +190,9 @@ public Rectangle getClientArea () {
 	checkWidget();
 	if (scrollView != null) {
 		NSSize size = scrollView.contentSize();
-		return new Rectangle(0, 0, (int)size.width, (int)size.height);
+		NSClipView contentView = scrollView.contentView();
+		NSRect bounds = contentView.bounds();
+		return new Rectangle((int)bounds.x, (int)bounds.y, (int)size.width, (int)size.height);
 	} else {
 		NSRect rect = view.bounds();
 		return new Rectangle(0, 0, (int)rect.width, (int)rect.height);
