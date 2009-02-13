bfcc5e828d0dd96b499e859b3fc644a21d3cf37c
Silenio Quarti <silenio>
2009-02-13 17:09:44 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/NSClipView.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/NSClipView.java
@@ -24,6 +24,10 @@ public NSClipView(id id) {
 	super(id);
 }
 
+public boolean copiesOnScroll() {
+	return OS.objc_msgSend_bool(this.id, OS.sel_copiesOnScroll);
+}
+
 public NSCursor documentCursor() {
 	int /*long*/ result = OS.objc_msgSend(this.id, OS.sel_documentCursor);
 	return result != 0 ? new NSCursor(result) : null;
@@ -31,6 +35,10 @@ public NSCursor documentCursor() {
 
 public void scrollToPoint(NSPoint newOrigin) {
 	OS.objc_msgSend(this.id, OS.sel_scrollToPoint_, newOrigin);
+}
+
+public void setCopiesOnScroll(boolean flag) {
+	OS.objc_msgSend(this.id, OS.sel_setCopiesOnScroll_, flag);
 }
 
 public void setDocumentCursor(NSCursor anObj) {
