bd39debe79a6c1308b3b5a2c7d1fbfbe8ffc4f03
Kevin Barnes <krbarnes>
2009-02-18 17:54:14 +0000

    formatting

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Text.java
@@ -1399,11 +1399,11 @@ public void setEchoChar (char echo) {
 	checkWidget ();
 	if ((style & SWT.MULTI) != 0) return;
 	if ((style & SWT.PASSWORD) == 0) {
-			Point selection = getSelection ();
-			String text = getText ();
-			echoCharacter = echo;
-			setEditText (text);
-			setSelection (selection);
+		Point selection = getSelection ();
+		String text = getText ();
+		echoCharacter = echo;
+		setEditText (text);
+		setSelection (selection);
 	}
 }
 
