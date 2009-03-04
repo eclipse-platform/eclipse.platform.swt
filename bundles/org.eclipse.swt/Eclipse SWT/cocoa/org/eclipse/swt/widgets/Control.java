75820f5fbd4df571315ddd6a20d8e92fac5c23d5
Kevin Barnes <krbarnes>
2009-03-04 21:21:18 +0000

    266385 - Mylyn notifications don't close properly

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -1660,8 +1660,11 @@ int /*long*/ hitTest (int /*long*/ id, int /*long*/ sel, NSPoint point) {
 	if ((state & DISABLED) != 0) return 0;
 	if (!isActive ()) return 0;
 	if (regionPath != null) {
-		NSPoint pt = view.superview().convertPoint_toView_(point, view);
-		return regionPath.containsPoint(pt) ? view.id : 0;
+		NSView superview = new NSView(id).superview();
+		if (superview != null) {
+			NSPoint pt = superview.convertPoint_toView_(point, view);
+			if (!regionPath.containsPoint(pt)) return 0;
+		}
 	}
 	return super.hitTest(id, sel, point);
 }
