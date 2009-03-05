97fd0ac369cf01c79a83269dd215bc66fb80f094
Steve Northover <steve>
2009-03-05 18:17:39 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet315.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet315.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Button example snippet: create a tri-state button (toggle three state)
+ * Button example snippet: create a tri-state button (toggle three states)
  *
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
