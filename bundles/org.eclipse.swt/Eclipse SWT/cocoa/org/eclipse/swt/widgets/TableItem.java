6e4532f962b5b3d7509bc044b21319f82022e2bd
Silenio Quarti <silenio>
2009-03-17 21:15:42 +0000

    junit test - setImage with bad index must not fail

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TableItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TableItem.java
@@ -595,8 +595,11 @@ void redraw (int columnIndex) {
 		if (parent.columnCount == 0) {
 			index = (parent.style & SWT.CHECK) != 0 ? 1 : 0;
 		} else {
-			TableColumn column = parent.getColumn (columnIndex);
-			index = (int)/*64*/tableView.columnWithIdentifier (column.nsColumn);
+			if (0 <= columnIndex && columnIndex < parent.columnCount) {
+				index = (int)/*64*/tableView.columnWithIdentifier (parent.columns[columnIndex].nsColumn);
+			} else {
+				return;
+			}
 		}
 		rect = tableView.frameOfCellAtColumn (index, parent.indexOf (this));
 	}
--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TreeItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TreeItem.java
@@ -906,8 +906,11 @@ void redraw (int columnIndex) {
 		if (parent.columnCount == 0) {
 			index = (parent.style & SWT.CHECK) != 0 ? 1 : 0;
 		} else {
-			TreeColumn column = parent.getColumn (columnIndex);
-			index = (int)/*64*/outlineView.columnWithIdentifier (column.nsColumn);
+			if (0 <= columnIndex && columnIndex < parent.columnCount) {
+				index = (int)/*64*/outlineView.columnWithIdentifier (parent.columns[columnIndex].nsColumn);
+			} else {
+				return;
+			}
 		}
 		rect = outlineView.frameOfCellAtColumn (index, outlineView.rowForItem (handle));
 	}
