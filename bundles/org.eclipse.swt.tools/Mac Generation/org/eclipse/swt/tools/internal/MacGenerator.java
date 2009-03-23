c5a4f175f14b3b967abb14aa67050eb14ed80f82
Silenio Quarti <silenio>
2009-03-23 19:26:40 +0000

    *** empty log message ***

--- bundles/org.eclipse.swt.tools/Mac Generation/org/eclipse/swt/tools/internal/MacGenerator.java
+++ bundles/org.eclipse.swt.tools/Mac Generation/org/eclipse/swt/tools/internal/MacGenerator.java
@@ -624,13 +624,14 @@ void saveExtraAttributes(String xmlPath, Document document) {
 		String packageName = getPackageName(mainClassName);
 		String fileName = outputDir + packageName.replace('.', '/') + "/" + getFileName(xmlPath) + ".extras";
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		DOMWriter writer = new DOMWriter(new PrintStream(out), false);
+		DOMWriter writer = new DOMWriter(new PrintStream(out));
 		String[] names = getIDAttributeNames();
 		String[] filter = new String[names.length + 2];
 		filter[0] = "class_method";
 		filter[1] = "swt_.*";
 		System.arraycopy(names, 0, filter, 2, names.length);
 		writer.setAttributeFilter(filter);
+		writer.setNodeFilter("swt_");
 		writer.print(document);
 		if (out.size() > 0) output(out.toByteArray(), fileName);
 	} catch (Exception e) {
