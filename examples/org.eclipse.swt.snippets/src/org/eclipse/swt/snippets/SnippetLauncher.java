2a43aaa776776f81917e114e6a9f760cbed562ab
Carolyn MacLeod <carolyn>
2009-04-29 07:41:44 +0000

    don't run printer snippets, do run fixed snippets

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/SnippetLauncher.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/SnippetLauncher.java
@@ -24,7 +24,7 @@ public class SnippetLauncher {
 			if (files.length > 0) count = files.length;
 		}
 		for (int i = 1; i < count; i++) {
-			if (i == 136 || i == 151 || i == 180 || i == 219) continue;
+			if (i == 132 || i == 133 || i == 292 || i == 318) continue; // avoid printing to printer
 			String className = "Snippet" + i;
 			Class clazz = null;
 			try {
