3a0834dbaa36ec3da5b4ce1acdea598df0d796b3
Kevin Barnes <krbarnes>
2009-05-05 20:40:49 +0000

    v3547 for I20090505

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 546;
+    static int MINOR_VERSION = 547;
 	
 	/**
 	 * SWT revision number (must be >= 0)
