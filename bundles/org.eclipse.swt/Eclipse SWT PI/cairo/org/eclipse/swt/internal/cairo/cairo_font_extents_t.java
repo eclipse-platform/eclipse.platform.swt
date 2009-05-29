f93927ac01168752ebf09b43f9b889be49486ca6
Carolyn MacLeod <carolyn>
2009-05-29 21:30:31 +0000

    After copyright bash for 3.5RC4

--- bundles/org.eclipse.swt/Eclipse SWT PI/cairo/org/eclipse/swt/internal/cairo/cairo_font_extents_t.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cairo/org/eclipse/swt/internal/cairo/cairo_font_extents_t.java
@@ -15,7 +15,7 @@
  *
  * IBM
  * -  Binding to permit interfacing between Cairo and SWT
- * -  Copyright (C) 2005 IBM Corp.  All Rights Reserved.
+ * -  Copyright (C) 2005, 2006 IBM Corp.  All Rights Reserved.
  *
  * ***** END LICENSE BLOCK ***** */
 package org.eclipse.swt.internal.cairo;
--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build-ce.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build-ce.bat
@@ -1,6 +1,6 @@
 @echo off
 rem *******************************************************************************
-rem  Copyright (c) 2000, 2005 IBM Corporation and others.
+rem  Copyright (c) 2000, 2009 IBM Corporation and others.
 rem  All rights reserved. This program and the accompanying materials
 rem  are made available under the terms of the Eclipse Public License v1.0
 rem  which accompanies this distribution, and is available at
