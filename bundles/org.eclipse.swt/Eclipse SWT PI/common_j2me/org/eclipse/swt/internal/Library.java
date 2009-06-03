f668a8b5c4a8de99d906dfcfcb506d44da204539
Bogdan Gheorghe <gheorghe>
2009-06-03 19:40:27 +0000

    v3550 for I20090603

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 549;
+    static int MINOR_VERSION = 550;
 	
 	/**
 	 * SWT revision number (must be >= 0)
