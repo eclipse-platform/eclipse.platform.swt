fabff6846e01e41060d3fc1a6f6b55bf77e19003
Grant Gayed <ggayed>
2009-07-06 20:20:56 +0000

    revert comment

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet320.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet320.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Text example snippet: implement a simple content assist
+ * Text example snippet: implement content assist
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
