40318b61a336290fe8b55a4cac94b443add35b99
Felipe Heidrich <fheidric>
2009-07-09 18:10:18 +0000

    Update current version from v3553 to v3554

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 553;
+    static int MINOR_VERSION = 554;
 	
 	/**
 	 * SWT revision number (must be >= 0)
