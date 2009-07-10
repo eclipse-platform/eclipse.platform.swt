68343c68e581f7864ace6c73291389e9b5e15156
Felipe Heidrich <fheidric>
2009-07-10 18:23:01 +0000

    Update current version from v3602 to v3603

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 602;
+    static int MINOR_VERSION = 603;
 	
 	/**
 	 * SWT revision number (must be >= 0)
