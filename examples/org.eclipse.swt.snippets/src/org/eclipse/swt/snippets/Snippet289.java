3bcad4c2e7409f7f077a3537465aa9a9afbcf245
Bogdan Gheorghe <gheorghe>
2009-08-12 22:48:30 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet289.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet289.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Combo example snippet: add a new number item to a combo
+ * Combo example snippet: add a new number item to a combo box
  *
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
