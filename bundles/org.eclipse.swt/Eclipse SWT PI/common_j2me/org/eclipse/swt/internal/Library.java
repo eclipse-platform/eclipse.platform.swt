dd886d8cb8e95b436013bd0b4a7e204a1ac4f6f7
Felipe Heidrich <fheidric>
2009-08-31 19:07:55 +0000

    Update current version from v3609 to v3610

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 609;
+    static int MINOR_VERSION = 610;
 	
 	/**
 	 * SWT revision number (must be >= 0)
