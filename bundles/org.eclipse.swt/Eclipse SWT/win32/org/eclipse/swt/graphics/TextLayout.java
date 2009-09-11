a57bc570b3244345cd34af7093548fdeb5f2d2fd
Felipe Heidrich <fheidric>
2009-09-11 16:08:23 +0000

    Bug 133133: [Bidi] Numeral contextual behavior is not respected in eclipse editor

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/TextLayout.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/TextLayout.java
@@ -2457,10 +2457,8 @@ StyleItem[] itemize () {
 	if ((orientation & SWT.RIGHT_TO_LEFT) != 0) {
 		scriptState.uBidiLevel = 1;
 		scriptState.fArabicNumContext = true;
-		SCRIPT_DIGITSUBSTITUTE psds = new SCRIPT_DIGITSUBSTITUTE();
-		OS.ScriptRecordDigitSubstitution(OS.LOCALE_USER_DEFAULT, psds);
-		OS.ScriptApplyDigitSubstitution(psds, scriptControl, scriptState);
 	}
+	OS.ScriptApplyDigitSubstitution(null, scriptControl, scriptState);
 	
 	int /*long*/ hHeap = OS.GetProcessHeap();
 	int /*long*/ pItems = OS.HeapAlloc(hHeap, OS.HEAP_ZERO_MEMORY, MAX_ITEM * SCRIPT_ITEM.sizeof);
