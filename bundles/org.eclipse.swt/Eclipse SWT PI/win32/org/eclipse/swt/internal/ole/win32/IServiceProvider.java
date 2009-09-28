5ecd8e70469ae7820e2381931325258ca7d93719
Carolyn MacLeod <carolyn>
2009-09-28 18:35:07 +0000

    Accessibility update (IServiceProvider is used by IA2 and UIA)

---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/ole/win32/IServiceProvider.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * Copyright (c) 2009 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.internal.ole.win32;
+
+public class IServiceProvider extends IUnknown
+{
+public IServiceProvider(int /*long*/ address) {
+	super(address);
+}
+public int QueryService(GUID iid1, GUID iid2, int /*long*/ ppvObject[]) {
+	return COM.VtblCall(3, address, iid1, iid2, ppvObject);
+}
+}
