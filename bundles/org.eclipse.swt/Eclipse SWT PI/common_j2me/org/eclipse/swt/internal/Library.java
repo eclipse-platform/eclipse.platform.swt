4c90cf2195ed05fd919b9ef4c9ca510d1f1cf82f
Felipe Heidrich <fheidric>
2009-11-10 00:35:15 +0000

    Update current version from v3618 to v3619

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 618;
+    static int MINOR_VERSION = 619;
 	
 	/**
 	 * SWT revision number (must be >= 0)
