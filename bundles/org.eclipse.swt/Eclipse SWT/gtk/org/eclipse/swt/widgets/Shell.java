4c0e6ba4b69579a10f5aa2002e774ab773eb3f0c
Felipe Heidrich <fheidric>
2009-11-12 18:12:30 +0000

    Bug 23980 -  platform specific shell resize behaviour (comment 37.a)

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -907,10 +907,10 @@ int getResizeMode (double x, double y) {
 	int height = OS.GTK_WIDGET_HEIGHT (shellHandle);
 	int border = OS.gtk_container_get_border_width (shellHandle);
 	int mode = 0;
-	if (x <= border) {
-		mode = OS.GDK_LEFT_SIDE;
-		if (y <= border + 16) mode = OS.GDK_TOP_LEFT_CORNER;
-		else if (y >= height - border - 16) mode = OS.GDK_BOTTOM_LEFT_CORNER;
+	if (y >= height - border) {
+		mode = OS.GDK_BOTTOM_SIDE ;
+		if (x >= width - border - 16) mode = OS.GDK_BOTTOM_RIGHT_CORNER;
+		else if (x <= border + 16) mode = OS.GDK_BOTTOM_LEFT_CORNER;
 	} else if (x >= width - border) {
 		mode = OS.GDK_RIGHT_SIDE;
 		if (y <= border + 16) mode = OS.GDK_TOP_RIGHT_CORNER;
@@ -919,10 +919,10 @@ int getResizeMode (double x, double y) {
 		mode = OS.GDK_TOP_SIDE;
 		if (x <= border + 16) mode = OS.GDK_TOP_LEFT_CORNER;
 		else if (x >= width - border - 16) mode = OS.GDK_TOP_RIGHT_CORNER;
-	} else if (y >= height - border) {
-		mode = OS.GDK_BOTTOM_SIDE ;
-		if (x <= border + 16) mode = OS.GDK_BOTTOM_LEFT_CORNER;
-		else if (x >= width - border - 16) mode = OS.GDK_BOTTOM_RIGHT_CORNER;
+	} else if (x <= border) {
+		mode = OS.GDK_LEFT_SIDE;
+		if (y <= border + 16) mode = OS.GDK_TOP_LEFT_CORNER;
+		else if (y >= height - border - 16) mode = OS.GDK_BOTTOM_LEFT_CORNER;
 	}
 	return mode;
 }
