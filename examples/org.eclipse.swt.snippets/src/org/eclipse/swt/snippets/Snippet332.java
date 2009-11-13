51a851b43d5a5b67f153def5a601fa54671236bd
Grant Gayed <ggayed>
2009-11-13 15:04:27 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet332.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet332.java
@@ -20,7 +20,7 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
-public class Snippet51 {
+public class Snippet332 {
 
 public static void main(String [] args) {
 	Display display = new Display();
