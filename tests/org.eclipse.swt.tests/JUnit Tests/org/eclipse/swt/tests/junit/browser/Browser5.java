53bde6dda6297af52f56639222d5c24ba5bd4df0
Grant Gayed <ggayed>
2009-11-16 14:53:42 +0000

    *** empty log message ***

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/browser/Browser5.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/browser/Browser5.java
@@ -28,6 +28,7 @@ public class Browser5 {
 	static int index = 0;
 	static int cntPassed = 0;
 	static int cntClosed = 0;
+	public static boolean isMozilla = SwtJunit.isGTK || SwtJunit.isMotif;
 	
 	public static boolean test1(String url) {
 		if (verbose) System.out.println("javascript window.open with location and size parameters - args: "+url+"\n  Expected Event Sequence: Visibility.show");
@@ -158,9 +159,11 @@ public class Browser5 {
 		String[] urls = {url};
 		for (int i = 0; i < urls.length; i++) {
 			// TEST1 TEMPORARILY NOT RUN FOR MOZILLA
-			boolean result = test1(urls[i]); 
-			if (verbose) System.out.print(result ? "." : "E");
-			if (!result) fail++;
+			if (!isMozilla) {
+				boolean result = test1(urls[i]); 
+				if (verbose) System.out.print(result ? "." : "E");
+				if (!result) fail++;
+			}
 		}
 		return fail == 0;
 	}
