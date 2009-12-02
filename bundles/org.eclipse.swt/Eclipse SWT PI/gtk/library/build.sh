4599effb821d021833ad2e29cd1b066b6646833f
Bogdan Gheorghe <gheorghe>
2009-12-02 21:08:43 +0000

    Added PPC64

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
@@ -85,8 +85,7 @@ if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} 
 		export XLIB64
 	fi
 	if [ ${MODEL} = 'ppc64' ]; then
-		echo "HI in ppc64"
-		SWT_PTR_CFLAGS+=" -m64"	
+		SWT_PTR_CFLAGS=${SWT_PTR_CFLAGS} -m64	
 		SWT_LFLAGS=-m64
 		export SWT_LFLAGS
 	fi
