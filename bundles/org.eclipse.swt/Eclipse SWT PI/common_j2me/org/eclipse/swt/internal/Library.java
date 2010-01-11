f5f65d64888a4880268711bc01d6db76fac9db98
Felipe Heidrich <fheidric>
2010-01-11 21:15:25 +0000

    Update current version from v3625 to v3626

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 625;
+    static int MINOR_VERSION = 626;
 	
 	/**
 	 * SWT revision number (must be >= 0)
