13265fcb28422ca575933175c16195a10285f517
Carolyn MacLeod <carolyn>
2010-01-14 17:35:12 +0000

    Bug 278572 -  [layout] StackLayout very slow.. one line patch

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StackLayout.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StackLayout.java
@@ -117,7 +117,7 @@ protected void layout(Composite composite, boolean flushCache) {
 	rect.width -= 2 * marginWidth;
 	rect.height -= 2 * marginHeight;
 	for (int i = 0; i < children.length; i++) {
-		children[i].setBounds(rect);
+		if (children[i] == topControl) children[i].setBounds(rect);
 		children[i].setVisible(children[i] == topControl);
 			
 	}
