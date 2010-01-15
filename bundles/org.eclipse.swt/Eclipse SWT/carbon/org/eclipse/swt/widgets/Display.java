081619b131f8e4ef034a2e51a68513d77d8c4832
Silenio Quarti <silenio>
2010-01-15 15:55:48 +0000

    BugÊ295959 - [Widgets] Embedded native view can not receive Enter key event on Mac.  (back port to 3.5.x)

--- bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/carbon/org/eclipse/swt/widgets/Display.java
@@ -3960,12 +3960,13 @@ public void syncExec (Runnable runnable) {
 int textInputProc (int nextHandler, int theEvent, int userData) {
 	int theWindow = OS.GetUserFocusWindow ();
 	if (theWindow != 0) {
-		Widget widget = getFocusControl (theWindow, false);
+		int [] theControl = new int [1];
+		OS.GetKeyboardFocus (theWindow, theControl);
+		Widget widget = getWidget (theControl [0]);
 		if (widget != null) {
 			MenuTrackingData outData = new MenuTrackingData ();
 			if (OS.GetMenuTrackingData (0, outData) != OS.noErr) {
 				/* Stop the default event handler from activating the default button */
-				int [] theControl = new int [1];
 				OS.GetWindowDefaultButton (theWindow, theControl);
 				OS.SetWindowDefaultButton (theWindow, 0);
 				int result = widget.textInputProc (nextHandler, theEvent, userData);
