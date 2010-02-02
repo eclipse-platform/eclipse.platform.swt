3f089cd88d12856b1a32a2c2eaac8ab30e2a044c
Grant Gayed <ggayed>
2010-02-02 17:37:59 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet335.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet335.java
@@ -12,6 +12,7 @@ package org.eclipse.swt.snippets;
 
 /*
  * GridLayout snippet: show a wrappable Label that fills available width
+ * without expanding it
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
@@ -28,14 +29,25 @@ public static void main(String[] args) {
 	shell.setLayout(new GridLayout());
 
 	Label label = new Label(shell, SWT.WRAP | SWT.BORDER);
-	GridData data = new GridData();
-	data.widthHint = 10;					// <-- default width
-	data.horizontalAlignment = SWT.FILL;	// <-- grow to fill available width
-	label.setLayoutData(data);
+	GridData labelData = new GridData();
+	labelData.widthHint = 10;					// <-- default width
+	labelData.horizontalAlignment = SWT.FILL;	// <-- grow to fill available width
+	label.setLayoutData(labelData);
 	label.setText("Snippets are minimal stand-alone programs that demonstrate specific techniques or functionality.");
-
 	new Button(shell, SWT.PUSH).setText("This button determines the Shell's width");
+
+	/* do an initial pack() so that the Shell determines its required width */
 	shell.pack();
+
+	/* update the Label's width hint to match what the layout allocated for it */
+	labelData.widthHint = label.getBounds().width;
+	
+	/*
+	 * do a second pack() so that the Label will compute its required height
+	 * based on its correct width instead of its previously-set default width
+	 */
+	shell.pack();
+
 	shell.open();
 	while (!shell.isDisposed()) {
 		if (!display.readAndDispatch()) display.sleep();
