4074f32fe6048472af40b1ee4fe32eef17ac8db4
Felipe Heidrich <fheidric>
2010-02-10 19:22:19 +0000

    Bug 293228 - show progress in the Eclipse icon on the Windows 7 Task Bar

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/OS.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/org/eclipse/swt/internal/win32/OS.java
@@ -2018,6 +2018,7 @@ public class OS extends C {
 	public static final int WHEEL_DELTA = 120;
 	public static final int WHEEL_PAGESCROLL = 0xFFFFFFFF;
 	public static final int WHITE_BRUSH = 0;
+	public static final int WHITENESS = 0x00FF0062;
 	public static final int WM_ACTIVATE = 0x6;
 	public static final int WM_ACTIVATEAPP = 0x1c;
 	public static final int WM_APP = 0x8000;
