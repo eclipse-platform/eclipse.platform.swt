86966a73db67de79833a8050a98054210c5b57c9
Grant Gayed <ggayed>
2010-03-10 16:54:53 +0000

    initial

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet337.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet337.java
@@ -1,6 +1,11 @@
 package org.eclipse.swt.snippets;
 
-// IN PROGRESS
+/*
+ * SWT_AWT example snippet: launch SWT from AWT and keep both active
+ * 
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ */
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
@@ -8,7 +13,6 @@ import java.awt.EventQueue;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -18,65 +22,51 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.awt.SWT_AWT;
 import org.eclipse.swt.browser.Browser;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.*;
 
 public class Snippet337 {
 
-	static Display display;
+public static void main(String args[]) {
+	final Display display = new Display();
 
-	static boolean exit;
+	EventQueue.invokeLater(new Runnable() {
+		public void run() {
+			JFrame mainFrame = new JFrame("Main Window");
+			mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			JPanel mainPanel = new JPanel();
+			mainPanel.setLayout(new FlowLayout());
+			JButton launchBrowserButton = new JButton("Launch Browser");
+			launchBrowserButton.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					JFrame childFrame = new JFrame();
+					final Canvas canvas = new Canvas();
+					childFrame.setSize(850, 650);
+					childFrame.getContentPane().add(canvas);
+					childFrame.setVisible(true);
+					display.asyncExec(new Runnable() {
+						public void run() {
+							Shell shell = SWT_AWT.new_Shell(display, canvas);
+							shell.setSize(800, 600);
+							Browser browser = new Browser(shell, SWT.NONE);
+							browser.setLayoutData(new GridData(GridData.FILL_BOTH));
+							browser.setSize(800, 600);
+							browser.setUrl("http://www.eclipse.org");
+							shell.open();
+						}
+					});
+				}
+			});
 
-	public Snippet337() {
-		JFrame frame = new JFrame("Main Window");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new FlowLayout());
-
-		JButton launchBrowserButton = new JButton("Launch Browser");
-		launchBrowserButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				JFrame f = new JFrame();
-				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				final Canvas canvas = new Canvas();
-				f.setSize(850, 650);
-				f.getContentPane().add(canvas);
-				f.setVisible(true);
-				display.asyncExec(new Runnable() {
-					public void run() {
-						Shell shell = SWT_AWT.new_Shell(display, canvas);
-						shell.setSize(800, 600);
-						Browser browser = new Browser(shell, SWT.NONE);
-						browser.setLayoutData(new GridData(GridData.FILL_BOTH));
-						browser.setSize(800, 600);
-						browser.setUrl("http://www.google.com");
-						shell.open();
-					}
-				});
-			}
-		});
-		launchBrowserButton.setActionCommand("launchbrowser");
-
-		mainPanel.add(new JTextField(15));
-		mainPanel.add(launchBrowserButton);
-
-		frame.getContentPane().add(mainPanel, BorderLayout.CENTER);
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-
-	public static void main(String args[]) {
-		display = new Display();
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				new Snippet337();
-			}
-		});
-		while (!exit) {
-			if (!display.readAndDispatch()) display.sleep();
+			mainPanel.add(new JTextField("a JTextField"));
+			mainPanel.add(launchBrowserButton);
+			mainFrame.getContentPane().add(mainPanel, BorderLayout.CENTER);
+			mainFrame.pack();
+			mainFrame.setVisible(true);
 		}
-		display.dispose();
+	});
+	while (true) {
+		if (!display.readAndDispatch()) display.sleep();
 	}
+}
+
 }
