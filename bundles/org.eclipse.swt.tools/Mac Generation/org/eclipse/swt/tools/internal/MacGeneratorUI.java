42d93986303b778d3502e81d1a18b6952399a1d4
Silenio Quarti <silenio>
2010-03-15 22:06:53 +0000

    should not edit column zero

--- bundles/org.eclipse.swt.tools/Mac Generation/org/eclipse/swt/tools/internal/MacGeneratorUI.java
+++ bundles/org.eclipse.swt.tools/Mac Generation/org/eclipse/swt/tools/internal/MacGeneratorUI.java
@@ -104,6 +104,7 @@ public class MacGeneratorUI {
 	
 	boolean getEditable(TableItem item, int column) {
 		if (!(item.getData() instanceof Node)) return false;
+		if (column == 0) return false;
 		String attribName = item.getText();
 		return attribName.startsWith("swt_");
 	}
