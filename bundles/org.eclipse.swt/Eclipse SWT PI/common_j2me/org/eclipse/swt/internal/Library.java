194d766f4643c78ace474b6b87cb2949dd173e92
Felipe Heidrich <fheidric>
2010-03-22 20:59:37 +0000

    Update current version from v3638 to v3639

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 638;
+    static int MINOR_VERSION = 639;
 	
 	/**
 	 * SWT revision number (must be >= 0)
