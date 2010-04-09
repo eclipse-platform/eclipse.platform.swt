b83eb0c1e4cdccce4cabda49a900c4db34daef55
Silenio Quarti <silenio>
2010-04-09 18:57:42 +0000

    non predefined attributes

--- bundles/org.eclipse.swt/Eclipse SWT Accessibility/gtk/org/eclipse/swt/accessibility/AccessibleObject.java
+++ bundles/org.eclipse.swt/Eclipse SWT Accessibility/gtk/org/eclipse/swt/accessibility/AccessibleObject.java
@@ -1631,92 +1631,103 @@ class AccessibleObject {
 				OS.memmove (start_offset, new int []{event.start}, 4);
 				OS.memmove (end_offset, new int []{event.end}, 4);
 				TextStyle style = event.textStyle;
-				if (style == null) return 0;
 				int /*long*/ result = 0;
 				AtkAttribute attr = new AtkAttribute();
-				if (style.rise != 0) {
-					int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_RISE));
-					attr.value = getStringPtr (String.valueOf(style.rise));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-				}
-				if (style.underline) {
-					int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_UNDERLINE));
-					String str = "none";
-					switch (style.underlineStyle) {
-						case SWT.UNDERLINE_DOUBLE: str = "double"; break;
-						case SWT.UNDERLINE_SINGLE: str = "single"; break;
-						case SWT.UNDERLINE_ERROR: str = "error"; break;
-						case SWT.UNDERLINE_SQUIGGLE: str = "squiggle"; break;
+				if (style != null) {
+					if (style.rise != 0) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_RISE));
+						attr.value = getStringPtr (String.valueOf(style.rise));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
 					}
-					attr.value = getStringPtr (str);
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
+					if (style.underline) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_UNDERLINE));
+						String str = "none";
+						switch (style.underlineStyle) {
+							case SWT.UNDERLINE_DOUBLE: str = "double"; break;
+							case SWT.UNDERLINE_SINGLE: str = "single"; break;
+							case SWT.UNDERLINE_ERROR: str = "error"; break;
+							case SWT.UNDERLINE_SQUIGGLE: str = "squiggle"; break;
+						}
+						attr.value = getStringPtr (str);
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
+					if (style.strikeout) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STRIKETHROUGH));
+						attr.value = getStringPtr ("1");
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
+					Font font = style.font;
+					if (font != null && !font.isDisposed()) {
+						//TODO language and direction
+						int /*long*/ attrPtr;
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_FAMILY_NAME));
+						attr.value = ATK.g_strdup (OS.pango_font_description_get_family (font.handle));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+						
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_SIZE));
+						attr.value = getStringPtr (String.valueOf (OS.pango_font_description_get_size(font.handle) / OS.PANGO_SCALE));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+						
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STYLE));
+						attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_STYLE, OS.pango_font_description_get_style(font.handle)));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+						
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_VARIANT));
+						attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_VARIANT, OS.pango_font_description_get_variant(font.handle)));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+						
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STRETCH));
+						attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_STRETCH, OS.pango_font_description_get_stretch(font.handle)));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+						
+						attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_WEIGHT));
+						attr.value = getStringPtr (String.valueOf (OS.pango_font_description_get_weight(font.handle)));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
+					Color color = style.foreground;
+					if (color != null && !color.isDisposed()) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_FG_COLOR));
+						attr.value = getStringPtr ((color.handle.red & 0xFFFF) + "," + (color.handle.blue & 0xFFFF) + "," + (color.handle.blue & 0xFFFF));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
+					color = style.background;
+					if (color != null && !color.isDisposed()) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_BG_COLOR));
+						attr.value = getStringPtr ((color.handle.red & 0xFFFF) + "," + (color.handle.blue & 0xFFFF) + "," + (color.handle.blue & 0xFFFF));
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
 				}
-				if (style.strikeout) {
-					int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STRIKETHROUGH));
-					attr.value = getStringPtr ("1");
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-				}
-				Font font = style.font;
-				if (font != null && !font.isDisposed()) {
-					//TODO language and direction
-					int /*long*/ attrPtr;
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_FAMILY_NAME));
-					attr.value = ATK.g_strdup (OS.pango_font_description_get_family (font.handle));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-					
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_SIZE));
-					attr.value = getStringPtr (String.valueOf (OS.pango_font_description_get_size(font.handle) / OS.PANGO_SCALE));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-					
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STYLE));
-					attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_STYLE, OS.pango_font_description_get_style(font.handle)));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-					
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_VARIANT));
-					attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_VARIANT, OS.pango_font_description_get_variant(font.handle)));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-					
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_STRETCH));
-					attr.value = ATK.g_strdup (ATK.atk_text_attribute_get_value(ATK.ATK_TEXT_ATTR_STRETCH, OS.pango_font_description_get_stretch(font.handle)));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-					
-					attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_WEIGHT));
-					attr.value = getStringPtr (String.valueOf (OS.pango_font_description_get_weight(font.handle)));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-				}
-				Color color = style.foreground;
-				if (color != null && !color.isDisposed()) {
-					int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_FG_COLOR));
-					attr.value = getStringPtr ((color.handle.red & 0xFFFF) + "," + (color.handle.blue & 0xFFFF) + "," + (color.handle.blue & 0xFFFF));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
-				}
-				color = style.background;
-				if (color != null && !color.isDisposed()) {
-					int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
-					attr.name = ATK.g_strdup (ATK.atk_text_attribute_get_name(ATK.ATK_TEXT_ATTR_BG_COLOR));
-					attr.value = getStringPtr ((color.handle.red & 0xFFFF) + "," + (color.handle.blue & 0xFFFF) + "," + (color.handle.blue & 0xFFFF));
-					ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
-					result = OS.g_list_append(result, attrPtr);
+				if (event.attributes != null) {
+					int end = event.attributes.length / 2 * 2;
+					for (int i = 0; i < end; i+= 2) {
+						int /*long*/ attrPtr = OS.g_malloc(AtkAttribute.sizeof);
+						attr.name = getStringPtr (event.attributes[i]);
+						attr.value = getStringPtr (event.attributes[i + 1]);
+						ATK.memmove(attrPtr, attr, AtkAttribute.sizeof);
+						result = OS.g_list_append(result, attrPtr);
+					}
 				}
 				return result;
 			}
