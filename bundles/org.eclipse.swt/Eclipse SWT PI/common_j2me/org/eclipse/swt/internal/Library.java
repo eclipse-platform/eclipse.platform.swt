8120e8be87ba4f414efa53bc5991e6635e83117f
Felipe Heidrich <fheidric>
2010-04-19 22:12:00 +0000

    Update current version from v3641 to v3642

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2me/org/eclipse/swt/internal/Library.java
@@ -22,7 +22,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 641;
+    static int MINOR_VERSION = 642;
 	
 	/**
 	 * SWT revision number (must be >= 0)
