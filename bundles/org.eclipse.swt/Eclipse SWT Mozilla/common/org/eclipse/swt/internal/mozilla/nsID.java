8bf573ee7c876cf0329fbd1d4a8ab0c1b4560a42
Grant Gayed <ggayed>
2010-05-03 19:33:25 +0000

    311129 - Code refactoring of Parse method in nsID class

--- bundles/org.eclipse.swt/Eclipse SWT Mozilla/common/org/eclipse/swt/internal/mozilla/nsID.java
+++ bundles/org.eclipse.swt/Eclipse SWT Mozilla/common/org/eclipse/swt/internal/mozilla/nsID.java
@@ -54,24 +54,68 @@ public boolean Equals(nsID other) {
 	return result;
 }
 
-public void Parse (String aIDStr) {
-	if (aIDStr == null) throw new Error ();
+public void Parse(String aIDStr) {
+	if(aIDStr == null) throw new Error ();
 	int i = 0;
-	for (; i < 8; i++) m0 = (m0 << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16);
+	for (; i < 8; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m0 = (m0 << 4) + digit;
+	}
 	if (aIDStr.charAt (i++) != '-') throw new Error ();
-	for (; i < 13; i++) m1 = (short)((m1 << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
+	for (; i < 13; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m1 = (short)((m1 << 4) + digit);
+	}
 	if (aIDStr.charAt (i++) != '-') throw new Error ();
-	for (; i < 18; i++) m2 = (short)((m2 << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
+	for (; i < 18; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m2 = (short)((m2 << 4) + digit);
+	}
 	if (aIDStr.charAt (i++) != '-') throw new Error ();
-	for (; i < 21; i++) m3[0] = (byte)((m3[0] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 23; i++) m3[1] = (byte)((m3[1] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
+	for (; i < 21; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[0] = (byte)((m3[0] << 4) + digit);
+	}
+	for (; i < 23; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[1] = (byte)((m3[1] << 4) + digit);
+	}
 	if (aIDStr.charAt (i++) != '-') throw new Error ();
-	for (; i < 26; i++) m3[2] = (byte)((m3[2] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 28; i++) m3[3] = (byte)((m3[3] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 30; i++) m3[4] = (byte)((m3[4] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 32; i++) m3[5] = (byte)((m3[5] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 34; i++) m3[6] = (byte)((m3[6] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
-	for (; i < 36; i++) m3[7] = (byte)((m3[7] << 4) + Integer.parseInt (aIDStr.substring (i, i + 1), 16));
+	for (; i < 26; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[2] = (byte)((m3[2] << 4) + digit);
+	}
+	for (; i < 28; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[3] = (byte)((m3[3] << 4) + digit);
+	}
+	for (; i < 30; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[4] = (byte)((m3[4] << 4) + digit);
+	}
+	for (; i < 32; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[5] = (byte)((m3[5] << 4) + digit);
+	}
+	for (; i < 34; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[6] = (byte)((m3[6] << 4) + digit);
+	}
+	for (; i < 36; i++) {
+		int digit = Character.digit (aIDStr.charAt (i), 16);
+		if (digit == -1) throw new Error ();
+		m3[7] = (byte)((m3[7] << 4) + digit);
+	}
 }
 
 }
