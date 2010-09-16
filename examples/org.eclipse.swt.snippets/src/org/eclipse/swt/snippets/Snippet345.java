8212be79f304ef15836601af19407e5936ef3001
Silenio Quarti <silenio>
2010-09-16 15:46:11 +0000

    Wrappable Button Snippet

---
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet345.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.snippets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+
+/*
+ * Wrappable button snippet: create a wrappable button for each button type.
+ * 
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ */
+public class Snippet345 {
+	public static void main (String[] args) {
+		final Display  display = new Display ();
+		final Shell shell = new Shell ();
+		String string = "The quick brown fox jumped over the lazy brown dog";
+		Button button;
+		button = new Button(shell, SWT.PUSH | SWT.WRAP);
+		button.setText(string);
+		button = new Button(shell, SWT.RADIO | SWT.WRAP);
+		button.setText(string);
+		button = new Button(shell, SWT.TOGGLE | SWT.WRAP);
+		button.setText(string);
+		button = new Button(shell, SWT.CHECK | SWT.WRAP);
+		button.setText(string);
+		shell.addListener(SWT.Resize, new Listener() {
+			public void handleEvent(Event e) {
+				Rectangle rect = shell.getClientArea();
+				Control[] children = shell.getChildren();
+				for (int i = 0; i < children.length; i++) {
+					Control control = children[i];
+					Point size = control.computeSize(rect.width, SWT.DEFAULT);
+					control.setBounds(rect.x, rect.y, rect.width, size.y);
+					rect.y += size.y;
+				}
+//				shell.setSize(shell.getSize().x, rect.y + 30);
+			}
+		});
+		shell.setSize (400, 200);
+		shell.open ();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep ();
+			}
+		}
+		display.dispose ();
+	}
+}
