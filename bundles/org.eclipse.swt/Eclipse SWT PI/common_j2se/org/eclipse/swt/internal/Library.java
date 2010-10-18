4aa01bc8a8354ada5ae806aa7e6619b6eeec59e9
Felipe Heidrich <fheidric>
2010-10-18 21:06:49 +0000

    Update current version from v3710 to v3711

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 710;
+    static int MINOR_VERSION = 711;
 	
 	/**
 	 * SWT revision number (must be >= 0)
