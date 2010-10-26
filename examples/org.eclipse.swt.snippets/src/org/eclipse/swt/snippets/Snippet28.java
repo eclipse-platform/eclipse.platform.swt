92198efb7a50e9ad032b435072c5ae253854b682
Grant Gayed <ggayed>
2010-10-26 21:23:08 +0000

    *** empty log message ***

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet28.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet28.java
@@ -11,7 +11,7 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Shell example snippet: open a shell maximized (full screen)
+ * Shell example snippet: open a shell maximized
  *
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
