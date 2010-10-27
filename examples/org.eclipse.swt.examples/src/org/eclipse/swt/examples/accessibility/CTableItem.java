211feceb33194c03f2d7959fda2411f8cac53fa7
Felipe Heidrich <fheidric>
2010-10-27 14:08:46 +0000

    Fixed Bug 328794 - Remove unused variable in 'org.eclipse.swt.examples'

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/CTableItem.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/CTableItem.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1358,10 +1358,8 @@ boolean paint (GC gc, CTableColumn column, boolean backgroundOnly) {
 					gc.fillRectangle (startX, focusBounds.y + 1, fillWidth, focusBounds.height - 2);
 				}
 			} else {
-				int startX = column.getX ();
 				int fillWidth = column.width;
 				if (column.getOrderIndex () == 0) {
-					startX += 1;	/* space for left bound of focus rect */
 					fillWidth -= 1;
 				}
 				if (column.getOrderIndex () == parent.columns.length - 1) {
