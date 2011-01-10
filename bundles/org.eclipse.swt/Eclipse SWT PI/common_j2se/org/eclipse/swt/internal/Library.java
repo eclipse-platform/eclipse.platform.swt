e9af7093ec7caa05b79baaf56edd72b48dd43b8f
Felipe Heidrich <fheidric>
2011-01-10 23:08:58 +0000

    Update current version from v3718 to v3719

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 718;
+    static int MINOR_VERSION = 719;
 	
 	/**
 	 * SWT revision number (must be >= 0)
