3a4f3df43c8c8a6ef755f86a30a0e04a0a8b38a1
Felipe Heidrich <fheidric>
2011-01-18 23:20:07 +0000

    Update current version from v3657 to v3658

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 657;
+    static int MINOR_VERSION = 658;
 	
 	/**
 	 * SWT revision number (must be >= 0)
