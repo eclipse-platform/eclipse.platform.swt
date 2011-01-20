025a82bb0e01510f64041b6d0a683cb2a67ef4fa
Scott Kovatch <skovatch>
2011-01-20 16:46:01 +0000

    incorrect since tag added to setMenu

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -3832,7 +3832,6 @@ public void setLocation (Point location) {
  *    <li>ERROR_WIDGET_DISPOSED - if the receiver has been disposed</li>
  *    <li>ERROR_THREAD_INVALID_ACCESS - if not called from the thread that created the receiver</li>
  * </ul>
- * @since 3.7
  */
 public void setMenu (Menu menu) {
 	checkWidget();
