dcc0e2e76a96518f2bd60f127e49f8a4bd5ba452
Bogdan Gheorghe <gheorghe>
2011-01-28 15:34:56 +0000

    Touch + Gesture snippets

---
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet352.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.snippets;
+
+/*
+ * Touch example
+ *
+ * For a list of all SWT example snippets see
+ * http://www.eclipse.org/swt/snippets/
+ * 
+ * @since 3.7
+ */ 
+import org.eclipse.swt.widgets.*;
+
+public class Snippet352 {
+
+	public static void main(String [] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
+	}
+}
