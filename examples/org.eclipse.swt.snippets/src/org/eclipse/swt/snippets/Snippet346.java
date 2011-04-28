8a8981a00ea599ca69b7c1fe499433d74de3c3b8
Carolyn MacLeod <carolyn>
2011-04-28 19:02:26 +0000

    add @since 3.7

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet346.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet346.java
@@ -15,6 +15,8 @@ package org.eclipse.swt.snippets;
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
+ * 
+ * @since 3.7
  */
 import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
