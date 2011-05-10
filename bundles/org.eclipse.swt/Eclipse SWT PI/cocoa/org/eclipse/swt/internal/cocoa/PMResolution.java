71e10e5c2cf6f2a8a3d59ad11580068dfbf05d3d
Carolyn MacLeod <carolyn>
2011-05-10 18:02:06 +0000

    Bug 345279 - Do the annual copyright bash for 3.7

--- bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/PMResolution.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/PMResolution.java
@@ -1,15 +1,13 @@
-/**********************************************************************
- * Copyright (c) 2003-2004 IBM Corp.
- * Portions Copyright (c) 1983-2002, Apple Computer, Inc.
- *
- * All rights reserved.  This program and the accompanying materials
+/*******************************************************************************
+ * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
- **********************************************************************/
+ *    IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.swt.internal.cocoa;
 
 public class PMResolution {
