1b22001121b4605fda4dd7cb78a4eaa03f35e36f
Grant Gayed <ggayed>
2011-05-25 19:27:11 +0000

    javadoc

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/TouchSource.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/TouchSource.java
@@ -14,20 +14,21 @@ package org.eclipse.swt.widgets;
 import org.eclipse.swt.graphics.*;
 
 /**
- * Instances of this class represent a source of touch input. It is used to identify which input source
- * generated a <code>Touch</code> object. It also provides information about the input source, which is important
- * when deciding how to interpret the information in the <code>Touch</code> object.
+ * Instances of this class represent sources of touch input that generate <code>Touch</code> objects.
+ * They also provide information about the input source, which is important for interpreting the
+ * information in the <code>Touch</code> object.
  * <p>
  * Instances of this class can be marked as direct or indirect:
  * <ul>
- * <li>When an instance is marked as <em>direct</em> the touch source is a touch-sensitive digitizer surface such
- * as a tablet or a touch screen. There is a one-to-one mapping between a touch point and a location in a window.
+ * <li>When an instance is <em>direct</em>, the touch source is a touch-sensitive digitizer surface such
+ * as a tablet or a touch screen. There is a one-to-one mapping between a touch point and a location in
+ * a window.
  * </li><li>
- * When an instance is marked as <em>indirect</em> (or, more precisely, not direct) the touch source is a track pad 
- * or other device that normally moves the cursor, but can also interpret multiple touches on its surface. In this 
- * case, there is not a one-to-one map between the location of the touch on the device and a location on the display
- * because the user can remove their finger or stylus and touch another part of the device and resume what they were
- * doing.
+ * When an instance is <em>indirect</em> (more precisely, not direct), the touch source is a track pad or 
+ * other device that normally moves the cursor, but is also able to interpret multiple touches on its surface.
+ * In this case there is not a one-to-one mapping between the location of the touch on the device and a
+ * location on the display because the user can remove their finger or stylus and touch another part of
+ * the device and resume what they were doing.
  * </li>
  * </ul>
  *
@@ -55,19 +56,21 @@ TouchSource (int /*long*/ handle, boolean direct, Rectangle bounds) {
 }
 
 /**
- * Returns the type of touch input this source generates; true for direct, false for indirect.
- * @return boolean If true, the input source is direct. If false, the input source is indirect.
+ * Returns the type of touch input this source generates, <code>true</code> for direct or <code>false</code> for indirect.
+ *
+ * @return <code>true</code> if the input source is direct, or <code>false</code> otherwise
  */
 public boolean isDirect () {
 	return direct;
 }
 
 /**
- * Returns the bounding rectangle of the device. For a direct source, this corresponds to the bounds of the display
- * device in pixels. For an indirect source, this contains the size of the device in pixels.
+ * Returns the bounding rectangle of the device. For a direct source, this corresponds to the bounds of
+ * the display device in pixels. For an indirect source, this contains the size of the device in pixels.
  * <p>
- * Note that the x and y values may not necessarily be zero if the TouchSource is a direct source.
- * @return Rectangle The bounding rectangle of the input source.
+ * Note that the x and y values may not necessarily be 0 if the TouchSource is a direct source.
+ *
+ * @return the bounding rectangle of the input source
  */
 public Rectangle getBounds () {
 	return new Rectangle (bounds.x, bounds.y, bounds.width, bounds.height);
