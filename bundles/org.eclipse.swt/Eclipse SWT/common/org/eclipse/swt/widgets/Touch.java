6ebb804bfadcd41c49e8c4cdfd3bc566b0142e53
Grant Gayed <ggayed>
2011-05-25 19:21:17 +0000

    javadoc

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Touch.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Touch.java
@@ -11,57 +11,54 @@
 package org.eclipse.swt.widgets;
 
 /**
- * Instances of this class are created as a result of
- * a touch-based input device being touched. They are found
+ * Instances of this class are created in response to a
+ * touch-based input device being touched. They are found
  * in the <code>touches</code> field of an Event or TouchEvent.
- * <p>
- * </p>
  *
  * @see org.eclipse.swt.events.TouchEvent
- * @see Event
  * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
  * 
  * @since 3.7
  */
 public final class Touch {
-	
+
 	/**
-	 * unique identity of the touch. Use this value to track changes to a touch during
-	 * the touch's life. Two touches may have the same identity even though they came
-	 * from different sources.
+	 * The unique identity of the touch. Use this value to track changes to a touch
+	 * during the touch's life. Two touches may have the same identity even if they
+	 * come from different sources.
 	 */
 	public long id;
-	
+
 	/**
-	 * object representing the input source that generated the touch
+	 * The object representing the input source that generated the touch.
 	 */
 	public TouchSource source;
-	
+
 	/**
-	 * the state of this touch at the time it was generated. If this field is 0
-	 * the finger is still touching the device but has not otherwise moved
-	 * since the last TouchEvent was generated.
+	 * The state of this touch at the time it was generated. If this field is 0
+	 * then the finger is still touching the device but has not moved
+	 * since the last <code>TouchEvent</code> was generated.
 	 * 
 	 * @see org.eclipse.swt.SWT#TOUCHSTATE_DOWN
 	 * @see org.eclipse.swt.SWT#TOUCHSTATE_MOVE
 	 * @see org.eclipse.swt.SWT#TOUCHSTATE_UP
 	 */
 	public int state;
-	
+
 	/**
-	 * a flag indicating that the touch is the first touch from a previous state
-	 * of no touch points. Once designated as such, the touch remains the
-	 * primary touch until all fingers are removed from the device. 
+	 * A flag indicating that the touch is the first touch from a previous
+	 * state of no touch points. Once designated as such, the touch remains
+	 * the primary touch until all fingers are removed from the device. 
 	 */
 	public boolean primary;
 	
 	/** 
-	 * the X location of the touch in TouchSource coordinates
+	 * The x location of the touch in TouchSource coordinates.
 	 */
 	public int x;
 	
 	/**
-	 * the Y location of the touch in TouchSource coordinates
+	 * The y location of the touch in TouchSource coordinates.
 	 */
 	public int y;
 
