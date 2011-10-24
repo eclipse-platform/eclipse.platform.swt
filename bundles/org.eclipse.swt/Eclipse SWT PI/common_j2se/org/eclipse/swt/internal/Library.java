770424cb90ac09b444a2b31992d2c7b5853208d6
Silenio Quarti <silenio_quarti@ca.ibm.com>
2011-10-24 16:58:48 -0400

    v3809

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -24,7 +24,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 808;
+    static int MINOR_VERSION = 809;
 	
 	/**
 	 * SWT revision number (must be >= 0)
