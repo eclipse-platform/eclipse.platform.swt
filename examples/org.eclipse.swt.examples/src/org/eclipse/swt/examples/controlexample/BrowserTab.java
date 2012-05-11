92a65d008b1625e7f736b7033dfbb09553639f20
Carolyn MacLeod <Carolyn_MacLeod@ca.ibm.com>
2012-05-11 13:50:12 -0400

    Bug 375980 - ControlExample should support Browser#addAuthenticationListener(..)

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/BrowserTab.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/controlexample/BrowserTab.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -272,6 +272,13 @@ class BrowserTab extends Tab {
 	 */
 	void hookCustomListener (final String eventName) {
 		if (browser == null) return;
+		if (eventName == "AuthenticationListener") {
+			browser.addAuthenticationListener(new AuthenticationListener () {
+				public void authenticate(AuthenticationEvent event) {
+					log (eventName, event);
+				}
+			});
+		}
 		if (eventName == "CloseWindowListener") {
 			browser.addCloseWindowListener (new CloseWindowListener () {
 				public void close(WindowEvent event) {
