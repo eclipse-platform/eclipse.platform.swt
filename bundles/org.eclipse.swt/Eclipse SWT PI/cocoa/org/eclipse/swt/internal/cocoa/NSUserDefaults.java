44c31398c5f7d5a95cc03a487182c9e0b2abe8be
Silenio Quarti <silenio_quarti@ca.ibm.com>
2012-06-25 09:44:35 -0400

    Bug 377597 - Mac OS X cursor blinking dos not honor NSTextInsertionPointBlinkPeriod (missed a file)

---
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/NSUserDefaults.java
@@ -0,0 +1,37 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.internal.cocoa;
+
+public class NSUserDefaults extends NSObject {
+
+public NSUserDefaults() {
+	super();
+}
+
+public NSUserDefaults(int /*long*/ id) {
+	super(id);
+}
+
+public NSUserDefaults(id id) {
+	super(id);
+}
+
+public id objectForKey(NSString defaultName) {
+	int /*long*/ result = OS.objc_msgSend(this.id, OS.sel_objectForKey_, defaultName != null ? defaultName.id : 0);
+	return result != 0 ? new id(result) : null;
+}
+
+public static NSUserDefaults standardUserDefaults() {
+	int /*long*/ result = OS.objc_msgSend(OS.class_NSUserDefaults, OS.sel_standardUserDefaults);
+	return result != 0 ? new NSUserDefaults(result) : null;
+}
+
+}
