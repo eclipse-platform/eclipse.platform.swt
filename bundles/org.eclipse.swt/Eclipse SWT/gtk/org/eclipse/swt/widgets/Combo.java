0011536ae9806434e247156ff26d5dad88627856
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2012-08-01 17:54:10 -0400

    Bug 245388 - [consistency] Combo.pack() does not consider item lengths

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Combo.java
@@ -341,7 +341,37 @@ void clearText () {
 
 public Point computeSize (int wHint, int hHint, boolean changed) {
 	checkWidget ();
-	return computeNativeSize (handle, wHint, hHint, changed);
+	if ((style & SWT.READ_ONLY) != 0) {
+		return computeNativeSize (handle, wHint, hHint, changed);
+	}
+	if (wHint != SWT.DEFAULT && wHint < 0) wHint = 0;
+	if (hHint != SWT.DEFAULT && hHint < 0) hHint = 0;
+	int[] w = new int [1], h = new int [1];
+	OS.gtk_widget_realize (entryHandle);
+	int /*long*/ layout = OS.gtk_entry_get_layout (entryHandle);
+	OS.pango_layout_get_size (layout, w, h);
+	int xborder = Display.INNER_BORDER, yborder = Display.INNER_BORDER;
+	int /*long*/ style = OS.gtk_widget_get_style (entryHandle);
+	xborder += OS.gtk_style_get_xthickness (style);
+	yborder += OS.gtk_style_get_ythickness (style);
+	int [] property = new int [1];
+	OS.gtk_widget_style_get (entryHandle, OS.interior_focus, property, 0);
+	if (property [0] == 0) {
+		OS.gtk_widget_style_get (entryHandle, OS.focus_line_width, property, 0);
+		xborder += property [0];
+		yborder += property [0];
+	}
+	int width = OS.PANGO_PIXELS (w [0]) + xborder * 2;
+	int height = OS.PANGO_PIXELS (h [0]) + yborder * 2;
+	GtkRequisition arrowRequesition = new GtkRequisition ();
+	OS.gtk_widget_size_request (buttonHandle, arrowRequesition);
+	GtkRequisition listRequesition = new GtkRequisition ();
+	int /*long*/ listParent = OS.gtk_bin_get_child(popupHandle);
+	OS.gtk_widget_size_request (listParent, listRequesition);
+	width = Math.max (listRequesition.width, width) + arrowRequesition.width;
+	width = wHint == SWT.DEFAULT ? width : wHint;
+	height = hHint == SWT.DEFAULT ? height : hHint;
+	return new Point (width, height);
 }
 
 /**
