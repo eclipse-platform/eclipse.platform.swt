dcabec77ec8ba318c7760c28f7b3953ded5c800e
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2012-08-01 17:43:02 -0400

    Bug 368543 - Odd Display of WindowBuilder in Eclipse 3.8/4.2 on Linux

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Composite.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Composite.java
@@ -96,6 +96,9 @@ public Composite (Composite parent, int style) {
 }
 
 static int checkStyle (int style) {
+	if (OS.INIT_CAIRO) {
+		style &= ~SWT.NO_BACKGROUND;
+	}
 	style &= ~SWT.TRANSPARENT;
 	return style;
 }
