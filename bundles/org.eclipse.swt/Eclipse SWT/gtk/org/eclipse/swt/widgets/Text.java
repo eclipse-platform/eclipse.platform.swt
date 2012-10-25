b27aee77e33c1eacc56a4073be5248239be4d717
Alexander Kurtakov <akurtako@redhat.com>
2012-10-25 16:15:02 +0300

    IMContext is private in GTK3.
    There is no way to retrieve it. Note that
    gtk_*_im_context_filter_keypress should be called wherever needed.

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
@@ -1800,6 +1800,7 @@ void hookEvents () {
 }
 
 long /*int*/ imContext () {
+	if (OS.GTK_VERSION >= OS.VERSION(3, 0, 0)) return 0; 
 	if ((style & SWT.SINGLE) != 0) {
 		return OS.gtk_editable_get_editable (handle) ? OS.GTK_ENTRY_IM_CONTEXT (handle) : 0;
 	} 
