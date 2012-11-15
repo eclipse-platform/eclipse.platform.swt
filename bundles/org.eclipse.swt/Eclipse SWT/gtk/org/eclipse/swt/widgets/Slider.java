5ed9391060e1aab8e1f0348a337191397f8d0889
Anatoly Spektor <aspektor@redhat.com>
2012-11-15 16:57:31 -0500

    realize handle to solve Slider widget with SWT.DEFAULT size issue

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Slider.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Slider.java
@@ -259,6 +259,7 @@ void deregister () {
 
 public Point computeSize (int wHint, int hHint, boolean changed) {
 	checkWidget();
+	OS.gtk_widget_realize(handle);
 	if (wHint != SWT.DEFAULT && wHint < 0) wHint = 0;
 	if (hHint != SWT.DEFAULT && hHint < 0) hHint = 0;
 	Point size = computeNativeSize(handle, wHint, hHint, changed);
