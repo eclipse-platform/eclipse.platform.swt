a6ea095a16f72ca5deb1a7074e00ce6f47794bfb
Silenio Quarti <silenio_quarti@ca.ibm.com>
2013-02-27 13:51:08 -0500

    Bug 328010 - In cocoa, Help menu can not display Search menu item in chinese language.

--- bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/NSApplication.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/NSApplication.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2010 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -134,6 +134,10 @@ public void setApplicationIconImage(NSImage image) {
 
 public void setDelegate(id anObject) {
 	OS.objc_msgSend(this.id, OS.sel_setDelegate_, anObject != null ? anObject.id : 0);
+}
+
+public void setHelpMenu(NSMenu helpMenu) {
+	OS.objc_msgSend(this.id, OS.sel_setHelpMenu_, helpMenu != null ? helpMenu.id : 0);
 }
 
 public void setMainMenu(NSMenu aMenu) {
