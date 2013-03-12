c9fb1d33fdc90ff526b6516fee7d95cb9bedb0c1
Markus Keller <markus_keller@ch.ibm.com>
2013-03-12 15:28:58 +0100

    Javadoc

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/BidiSegmentListener.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/BidiSegmentListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package org.eclipse.swt.custom;
 
-import org.eclipse.swt.internal.SWTEventListener;
+import org.eclipse.swt.internal.*;
 
 /**
  * This listener interface may be implemented in order to receive
@@ -27,11 +27,10 @@ public interface BidiSegmentListener extends SWTEventListener {
  * <li>event.lineOffset line start offset (input)</li>
  * <li>event.lineText line text (input)</li>
  * <li>event.segments text segments that should be reordered separately (output)</li> 
- * <li>event.segmentsChars characters that should be inserted (output)</li>
+ * <li>event.segmentsChars characters that should be inserted (output, optional)</li>
  * </ul>
  *
  * @param event the given event
- *	separately. (output)
  * @see BidiSegmentEvent
  */
 public void lineGetSegments(BidiSegmentEvent event);
--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/SegmentListener.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/SegmentListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,7 +10,7 @@
  *******************************************************************************/
 package org.eclipse.swt.events;
 
-import org.eclipse.swt.internal.SWTEventListener;
+import org.eclipse.swt.internal.*;
 
 /**
  * This listener interface may be implemented in order to receive
@@ -25,9 +25,9 @@ public interface SegmentListener extends SWTEventListener {
  * This method is called when text content is being modified. 
  * <p>
  * The following event fields are used:<ul>
- * <li>event.text text content (input)</li>
- * <li>event.segmentsChars characters that should be inserted (output)</li>
+ * <li>event.lineText text content (input)</li>
  * <li>event.segments text offsets for segment characters (output)</li>
+ * <li>event.segmentsChars characters that should be inserted (output, optional)</li>
  * </ul>
  *
  * @param event the given event
