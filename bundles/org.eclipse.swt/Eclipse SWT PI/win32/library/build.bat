a1ee6177d65326c66d403c43ed8e9d4a4c001d59
Silenio Quarti <silenio_quarti@ca.ibm.com>
2013-05-11 22:21:07 -0400

    enable remote build for windows

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -15,6 +15,7 @@ IF EXIST C:\BUILD\swt-builddir set SWT_BUILDDIR=C:\BUILD\swt-builddir
 IF x.%SWT_BUILDDIR%==x. set SWT_BUILDDIR=S:\swt-builddir
 echo SWT build dir: %SWT_BUILDDIR%
 IF x.%MSSDK%==x. set MSSDK="%SWT_BUILDDIR%\MSSDKs\Windows Server 2003 SP1 SDK"
+IF x.%WEBKIT_HOME%==x. set WEBKIT_HOME="%SWT_BUILDDIR%\webkit"
 
 IF x.%1==x.x86 GOTO X86
 IF x.%1==x.x86_64 GOTO X86_64
@@ -24,13 +25,14 @@ IF x.%1==x.ia64 GOTO IA64
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86
 IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-java2-sdk-50-win-i386
 IF x.%BUILD_XULRUNNER%==x.true GOTO XULRUNNER
+IF x.%2%==x.make_xulrunner GOTO XULRUNNER
 call "%SWT_BUILDDIR%\MSVCs\Microsoft Visual Studio 8\Common7\Tools\vsvars32.bat"
 call %MSSDK%\setenv /XP32 /RETAIL
 IF x.%1==x.x86 shift
 GOTO MAKE
 
 :XULRUNNER
-IF x.%DEV_TOOLS%==x. set DEV_TOOLS=S:\swt-builddir
+IF x.%DEV_TOOLS%==x. set DEV_TOOLS=%SWT_BUILDDIR%
 set MSSDK="%SWT_BUILDDIR%\MSSDKs\feb2003"
 call %SWT_BUILDDIR%\MSVCs\msvc60\vc98\bin\vcvars32.bat
 call %MSSDK%\setenv /XP32 /RETAIL
@@ -40,10 +42,12 @@ GOTO MAKE
 
 :X86_64
 
+set PROCESSOR_ARCHITECTURE=AMD64
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
 IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-sdk50-x86_64
 set CFLAGS=-DJNI64
 IF x.%BUILD_XULRUNNER%==x.true GOTO XULRUNNER64
+IF x.%2%==x.make_xulrunner64 GOTO XULRUNNER64
 call %MSSDK%\setenv /X64 /RETAIL
 shift
 GOTO MAKE
@@ -58,6 +62,7 @@ GOTO MAKE
 
 :IA64
 
+set PROCESSOR_ARCHITECTURE=AMD64
 call %MSSDK%\setenv /SRV64 /RETAIL
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.ia64
 IF x.%JAVA_HOME%==x. set JAVA_HOME=%SWT_BUILDDIR%\ibm-sdk142-ia64
