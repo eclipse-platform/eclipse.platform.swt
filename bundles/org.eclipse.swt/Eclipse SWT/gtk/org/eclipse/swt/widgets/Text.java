afc0fa6e8d839b2b3830e8588568e0e0ce159e34
Arun Thondapu <arunkumar.thondapu@in.ibm.com>
2013-08-28 20:37:59 +0530

    Bug 412584 - Resource leak in swt Text control

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Text.java
@@ -1854,7 +1854,7 @@ void hookEvents () {
 long /*int*/ imContext () {
 	if (imContext != 0) return imContext; 
 	if ((style & SWT.SINGLE) != 0) {
-		return OS.gtk_editable_get_editable (handle) ? OS.GTK_ENTRY_IM_CONTEXT (handle) : 0;
+		return OS.GTK_ENTRY_IM_CONTEXT (handle);
 	} 
 	return OS.GTK_TEXTVIEW_IM_CONTEXT (handle);
 }
