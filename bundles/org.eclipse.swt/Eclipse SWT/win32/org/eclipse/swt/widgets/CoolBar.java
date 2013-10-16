d9659416ed840653e00ccf49fe47cfd2b3c387c8
Bogdan Gheorghe <gheorghe@ca.ibm.com>
2013-10-16 16:52:36 -0400

    Update Javadoc comments

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/CoolBar.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/CoolBar.java
@@ -43,7 +43,6 @@ import org.eclipse.swt.graphics.*;
  * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
  * @noextend This class is not intended to be subclassed by clients.
  */
-
 public class CoolBar extends Composite {
 	CoolItem [] items;
 	CoolItem [] originalItems;
