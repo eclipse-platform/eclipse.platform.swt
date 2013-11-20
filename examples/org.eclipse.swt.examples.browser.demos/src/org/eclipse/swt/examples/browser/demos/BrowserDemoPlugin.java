e4fc1dcfe6803dfc1d7ea1f8ada41a2682e5fd00
Alexander Kurtakov <akurtako@redhat.com>
2013-11-20 15:48:38 +0200

    Clean up browser.demo.
    * Externalize view.name.
    * Use FileLocator.resolve instead of deprecated Platform.resolve.
    
    Change-Id: I451df496ea50d74d34cb7cc421171b0391710036
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- examples/org.eclipse.swt.examples.browser.demos/src/org/eclipse/swt/examples/browser/demos/BrowserDemoPlugin.java
+++ examples/org.eclipse.swt.examples.browser.demos/src/org/eclipse/swt/examples/browser/demos/BrowserDemoPlugin.java
@@ -34,7 +34,7 @@ public class BrowserDemoPlugin extends AbstractUIPlugin {
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
-		PLUGIN_PATH = Platform.resolve(plugin.getBundle().getEntry(".")).toString();
+		PLUGIN_PATH = FileLocator.resolve(plugin.getBundle().getEntry(".")).toString();
 	}
 
 	@Override
