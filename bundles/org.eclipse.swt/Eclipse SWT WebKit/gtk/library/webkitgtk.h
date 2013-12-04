8f78fe793dfed9d45b51d1d632a27e3f120fa179
Marc-Andre Laperle <marc-andre.laperle@ericsson.com>
2013-12-04 14:00:25 -0500

    Bug 423220 - [GTK3] SWT_GTK3 variable has inconsistent effect between 4.3 and 4.4
    Check the value of the environment variable, like in the 4.4 branch but
    keep GTK2 as the default.
    
    Change-Id: I7bfb7f844cd4cfcd1b89fa12f5c0f44803fb7e3e
    Signed-off-by: Marc-Andre Laperle <marc-andre.laperle@ericsson.com>

--- bundles/org.eclipse.swt/Eclipse SWT WebKit/gtk/library/webkitgtk.h
+++ bundles/org.eclipse.swt/Eclipse SWT WebKit/gtk/library/webkitgtk.h
@@ -25,10 +25,12 @@
 	static int initialized = 0; \
 	static void *var = NULL; \
 	if (!initialized) { \
-		void* handle ; \
-		if (getenv("SWT_GTK3")) { \
+		void* handle = 0; \
+		char *gtk3 = getenv("SWT_GTK3"); \
+		if (gtk3 != NULL && strcmp(gtk3, "1") == 0) { \
 			handle = dlopen("libwebkitgtk-3.0.so.0", LOAD_FLAGS); /* webkitgtk >= 3.x lib */ \
-		} else { \
+		} \
+		if (!handle) { \
     		handle = dlopen("libwebkit-1.0.so.2", LOAD_FLAGS); /* webkitgtk 1.2.x lib */ \
 	    	if (!handle) { \
 		    	handle = dlopen("libwebkitgtk-1.0.so.0", LOAD_FLAGS); /* webkitgtk >= 1.4.x lib */ \
