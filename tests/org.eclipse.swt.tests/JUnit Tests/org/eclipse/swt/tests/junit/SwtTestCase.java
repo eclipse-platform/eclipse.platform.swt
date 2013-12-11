4eb7f8cf3e79735d171bf8ad6bbe5175223935a7
Markus Keller <markus_keller@ch.ibm.com>
2013-12-11 17:34:55 +0100

    Bug 423561: log test executions to System.out

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/SwtTestCase.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/SwtTestCase.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2007 IBM Corporation and others.
+ * Copyright (c) 2000, 2013 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -194,13 +194,7 @@ protected boolean isBidi() {
 
 @Override
 protected void setUp() {
-	try {
-		super.setUp();
-	} catch (Exception e) {
-		SWTError error = new SWTError();
-		error.throwable = e;
-		throw error;
-	}
+	System.out.println("SwtTestCase#setUp(): " + getClass().getName() + "#" + getName());
 }
 
 @Override
