b57af4c30466392e26fc65ce6cd72f327456e597
Sami Wagiaalla <swagiaal@redhat.com>
2014-02-25 15:36:06 -0500

    Bug 429086 - Remove use of gdk_colormap_get_system in GTK3
    This function is not available in GTK3. Furthermore, adding
    the gdkColor for link forground is not required in GTK3.
    
    Change-Id: I9dae6d793cfadb05d03b16c35f5a740b4832b5e3
    Signed-off-by: Sami Wagiaalla <swagiaal@redhat.com>

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Display.java
@@ -2339,8 +2339,6 @@ void initializeSystemColors () {
 	} else {
 		gdkColor.blue = (short)0xeeee;
 	}
-	long /*int*/ colormap = OS.gdk_colormap_get_system();
-	OS.gdk_colormap_alloc_color(colormap, gdkColor, true, true);
 	COLOR_LINK_FOREGROUND = gdkColor;
 
 	if (OS.GTK3) {
@@ -2397,6 +2395,9 @@ void initializeSystemColors () {
 		COLOR_TITLE_INACTIVE_BACKGROUND_GRADIENT = toGdkColor (rgba, 1.3);
 		return;
 	}
+
+	long /*int*/ colormap = OS.gdk_colormap_get_system();
+	OS.gdk_colormap_alloc_color(colormap, gdkColor, true, true);
 
 	/* Get Tooltip resources */
 	long /*int*/ tooltipStyle = OS.gtk_widget_get_style (tooltipShellHandle);
