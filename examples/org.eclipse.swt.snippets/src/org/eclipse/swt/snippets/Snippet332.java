78789f2a300b39443da61f8c0672480203e31b17
Lina Kemmel <lkemmel@il.ibm.com>
2014-03-06 18:06:36 +0200

    Add to Snippet332 a snippet that shows using of bidi segments in Combo

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet332.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet332.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2009 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,7 +11,8 @@
 package org.eclipse.swt.snippets;
 
 /*
- * SWT StyledText snippet: using BidiSegmentEvent#segmentsChars to implement custom bidi segments
+ * SWT StyledText and Combo snippet: using BidiSegmentEvent#segmentsChars and
+ * SegmentEvent#segmentsChars to implement custom bidi segments
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
@@ -22,6 +23,7 @@ import org.eclipse.swt.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.custom.*;
+import org.eclipse.swt.events.*;
 import org.eclipse.swt.graphics.*;
 
 public class Snippet332 {
@@ -29,7 +31,7 @@ public class Snippet332 {
 	public static void main(String [] args) {
 	    final Display display = new Display();
 	    Shell shell = new Shell(display);
-	    FillLayout layout = new FillLayout();
+	    GridLayout layout = new GridLayout();
 	    layout.marginHeight = layout.marginWidth = 10;
 	    shell.setLayout(layout);
 	    StyledText text = new StyledText(shell, SWT.MULTI | SWT.BORDER);
@@ -39,7 +41,8 @@ public class Snippet332 {
 	    int[] segments = {string.indexOf(segment), segment.length()};
 	    StyleRange[] ranges = {new StyleRange(0, 0, display.getSystemColor(SWT.COLOR_RED), null)};
 	    text.setStyleRanges(segments, ranges);
-	    text.setFont(new Font(display, "Tahoma", 16, 0));
+	    Font font = new Font(display, "Tahoma", 16, 0);
+	    text.setFont(font);
 	    text.addBidiSegmentListener(new BidiSegmentListener() {
 			@Override
 			public void lineGetSegments(BidiSegmentEvent event) {
@@ -49,11 +52,24 @@ public class Snippet332 {
 				event.segmentsChars = new char[] {'\u202e', '\u202C'};
 			}
 		});
-	    shell.setSize(500, 200);
+		Combo combo = new Combo(shell, SWT.SIMPLE);
+	    combo.setFont(font);
+		combo.setBackground(display.getSystemColor(SWT.COLOR_YELLOW));
+		combo.setItems(new String[] { "Option 1...", "Option 2...", "Option 3...", "Option 4..." });
+		combo.select(1);
+		combo.addSegmentListener(new SegmentListener() {
+			@Override
+			public void getSegments(SegmentEvent event) {
+				event.segments = new int [] {0, event.lineText.length()};
+				event.segmentsChars = new char [] {'\u202e', '\u202c'};
+			}
+		});
+	    shell.setSize(500, 250);
 	    shell.open();
 	    while (!shell.isDisposed()) {
 	        if (!display.readAndDispatch()) display.sleep();
 	    }
+	    font.dispose();
 	    display.dispose();
 	}
 }
