301075892ec7bfaaa1404ad7fa0e8707fc731677
Alexander Kurtakov <akurtako@redhat.com>
2014-03-26 13:59:03 +0200

    Bug 431225 - Recognize ppc64le and aarch64 as 64bit in build.sh
    If building natives on linux for ppc64le or aarch64 they need to be
    recognized to be 64 bit to get -DJNI64 added.
    
    Change-Id: Icc98a9a4160e24004cfc1dd670677b3c17b05c5c
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
@@ -421,7 +421,7 @@ esac
 
 
 # For 64-bit CPUs, we have a switch
-if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 'sparc64'  -o ${MODEL} = 's390x' ]; then
+if [ ${MODEL} = 'x86_64' -o ${MODEL} = 'ppc64' -o ${MODEL} = 'ia64' -o ${MODEL} = 'sparc64'  -o ${MODEL} = 's390x' -o ${MODEL} = 'ppc64le' -o ${MODEL} = 'aarch64' ]; then
 	SWT_PTR_CFLAGS=-DJNI64
 	if [ -d /lib64 ]; then
 		XLIB64=-L/usr/X11R6/lib64
