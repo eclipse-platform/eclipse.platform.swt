cf5354a3df99e586e64648537cfe0b3e4fe894f1
Niraj Modi <niraj.modi@in.ibm.com>
2014-04-22 17:01:28 +0530

    Bug 426354 - SWT_AWT bridge memory leak
    Change-Id: I8b018fe4b6b594d11c4fef29c03cf4175f36cbb0
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT AWT/win32/org/eclipse/swt/awt/SWT_AWT.java
+++ bundles/org.eclipse.swt/Eclipse SWT AWT/win32/org/eclipse/swt/awt/SWT_AWT.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -176,12 +176,6 @@ public static Frame new_Frame (final Composite parent) {
 					}
 				}
 				final Frame frame = (Frame) value;
-				/*
-				 * This is necessary to make lightweight components
-				 * directly added to the frame receive mouse events
-				 * properly.
-				 */
-				frame.addNotify();
 				
 				/*
 				* TEMPORARY CODE
