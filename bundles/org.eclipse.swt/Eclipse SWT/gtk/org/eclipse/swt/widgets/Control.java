2788992449f42c68294c7306e4dbf7f44afd21ac
Joshua Barkovic <joshbarkovic@gmail.com>
2014-07-09 21:40:50 -0400

    "Bug 439301 - Setting Cursor can cause crash on Wayland system"
    Revised logic in if statements.
    
    Will now prefer the gdk function if Wayland is detected.
    
    Change-Id: If917ae1d72be1fdc290d9f9115e5bfd9b618bf4b
    Signed-off-by: Joshua Barkovic <joshbarkovic@gmail.com>

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
@@ -4223,7 +4223,7 @@ void setCursor (long /*int*/ cursor) {
 	long /*int*/ window = eventWindow ();
 	if (window != 0) {
 		OS.gdk_window_set_cursor (window, cursor);
-		if (OS.GDK_WINDOWING_WAYLAND() || !OS.GDK_WINDOWING_X11 ()) {
+		if (!OS.GDK_WINDOWING_X11 () || OS.GDK_WINDOWING_WAYLAND ()) {
 			OS.gdk_flush ();
 		} else {
 			long /*int*/ xDisplay = OS.gdk_x11_display_get_xdisplay(OS.gdk_display_get_default());
--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -1823,7 +1823,7 @@ int setBounds (int x, int y, int width, int height, boolean move, boolean resize
 void setCursor (long /*int*/ cursor) {
 	if (enableWindow != 0) {
 		OS.gdk_window_set_cursor (enableWindow, cursor);
-		if (!OS.GDK_WINDOWING_X11 ()) {
+		if (!OS.GDK_WINDOWING_X11 () || OS.GDK_WINDOWING_WAYLAND()) {
 			OS.gdk_flush ();
 		} else {
 			long /*int*/ xDisplay = OS.gdk_x11_display_get_xdisplay(OS.gdk_display_get_default());
