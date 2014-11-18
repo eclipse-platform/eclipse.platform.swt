ddf37d819ff05e07a09111f1aee82664955e5aa8
Nicolaj Hoess <nicohoess@gmail.com>
2014-11-18 10:25:36 +0100

    Bug 452021 - Compile warning in official build
    Change-Id: Ie23c486fa9525f18652a97fb62dc21bf5b243e6b
    Signed-off-by: Nicolaj Hoess <nicohoess@gmail.com>

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/MenuItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/MenuItem.java
@@ -12,7 +12,6 @@ package org.eclipse.swt.widgets;
 
  
 import org.eclipse.swt.internal.win32.*;
-import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.events.*;
