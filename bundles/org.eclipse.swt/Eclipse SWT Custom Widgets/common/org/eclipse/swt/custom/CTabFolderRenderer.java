d7ac8e7ee781b836a817b78631623a4a8e64146d
Lars Vogel <Lars.Vogel@vogella.com>
2014-12-15 19:56:38 +0100

    Bug 455264 - Use for-each loop in CTabFolderRenderer.disposeSelectionHighlightGradientColors
    Change-Id: I54cd3a4061cfc67cc03a43347a6321cdf0e3504c
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolderRenderer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolderRenderer.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2014 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -546,8 +546,8 @@ public class CTabFolderRenderer {
 	void disposeSelectionHighlightGradientColors() {
 		if(selectionHighlightGradientColorsCache == null)
 			return;
-		for (int i = 0; i < selectionHighlightGradientColorsCache.length; i++) {
-			selectionHighlightGradientColorsCache[i].dispose();
+		for (Color element : selectionHighlightGradientColorsCache) {
+			element.dispose();
 		}
 		selectionHighlightGradientColorsCache = null;
 	}
