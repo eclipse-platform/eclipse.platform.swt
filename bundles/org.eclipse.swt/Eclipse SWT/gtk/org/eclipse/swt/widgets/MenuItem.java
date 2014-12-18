7581757358232d25370c9b0fe0eaa59fd24fa1f8
Markus Keller <markus_keller@ch.ibm.com>
2014-12-18 11:50:39 +0100

    Bug 454914: MenuItem.setToolTip() doesn't work for items in the shell menubar on Windows

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/MenuItem.java
@@ -14,7 +14,6 @@ package org.eclipse.swt.widgets;
 import org.eclipse.swt.*;
 import org.eclipse.swt.internal.*;
 import org.eclipse.swt.internal.gtk.*;
-import org.eclipse.swt.widgets.*;
 import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.events.*;
 
