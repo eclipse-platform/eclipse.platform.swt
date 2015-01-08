952447e8b5a05bfd4afc6654680cd9f7e4a17db4
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2015-01-08 15:33:41 +0530

    Update JAVA_HOME for s390 & s390x since the build machine has changed

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #*******************************************************************************
-# Copyright (c) 2000, 2011 IBM Corporation and others.
+# Copyright (c) 2000, 2015 IBM Corporation and others.
 # All rights reserved. This program and the accompanying materials
 # are made available under the terms of the Eclipse Public License v1.0
 # which accompanies this distribution, and is available at
@@ -370,7 +370,7 @@ case $SWT_OS.$SWT_ARCH in
 			export CC=gcc
 		fi
 		if [ "${JAVA_HOME}" = "" ]; then
-			export JAVA_HOME="/home/swtbuild/java5/ibm-java2-s390-50"
+			export JAVA_HOME="/home/swtbuild/java5/s390/ibm-java2-s390-50"
 		fi
 		if [ "${MOZILLA_SDK}" = "" ]; then
 			export MOZILLA_SDK="/home/swtbuild/mozilla-1.7.13/mozilla/dist/sdk"
@@ -399,7 +399,7 @@ case $SWT_OS.$SWT_ARCH in
 			export CC=gcc
 		fi
 		if [ "${JAVA_HOME}" = "" ]; then
-			export JAVA_HOME="/home/swtbuild/java5/ibm-java2-s390x-50"
+			export JAVA_HOME="/home/swtbuild/java5/s390/ibm-java2-s390-50"
 		fi
 		if [ "${MOZILLA_SDK}" = "" ]; then
 			export MOZILLA_SDK="/home/swtbuild/mozilla1.7.13/mozilla/dist/sdk"
