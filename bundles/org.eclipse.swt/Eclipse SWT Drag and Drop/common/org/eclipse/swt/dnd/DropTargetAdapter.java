59ef60f1d9d6a95b7cc061716223a91e50fc5b68
Leo Ufimtsev <lufimtse@redhat.com>
2015-01-12 14:19:27 -0500

    Bug 457224 - FileTransfer does not work on Ubuntu 14.04
    I added a little bit in the documentation to explain differences in DND
    behaviour on different OS's.
    
    Change-Id: I693e28fd6b5ec020efca497a519f371816b24b15
    Signed-off-by: Leo Ufimtsev <lufimtse@redhat.com>

--- bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/common/org/eclipse/swt/dnd/DropTargetAdapter.java
+++ bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/common/org/eclipse/swt/dnd/DropTargetAdapter.java
@@ -20,18 +20,29 @@ package org.eclipse.swt.dnd;
  * interested in.
  * </p>
  *
+ *<p>
+ * Please note, there are subtle difference in DND behavior on different OS's.
+ * For example during a file transfer, Windows will put out the file path
+ * as soon as the drag begins, where as Linux will make it available only
+ * on a drop operation. For correct crossplatform behavior, it is recommended
+ * to delay OS interaction until drop has occurred or verify the correctness of
+ * the operation across platforms.
+ * </p>
+ *
+ *
  * @see DropTargetListener
  * @see DropTargetEvent
  * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
+ * @see <a href="https://eclipse.org/articles/Article-SWT-DND/DND-in-SWT.html"> Eclipse corner article on DND </a>
  */
 public class DropTargetAdapter implements DropTargetListener {
 
 /**
- * This implementation of <code>dragEnter</code> permits the default 
+ * This implementation of <code>dragEnter</code> permits the default
  * operation defined in <code>event.detail</code>to be performed on the current data type
  * defined in <code>event.currentDataType</code>.
  * For additional information see <code>DropTargetListener.dragEnter</code>.
- * 
+ *
  * @param event the information associated with the drag enter event
  */
 public void dragEnter(DropTargetEvent event){}
@@ -39,27 +50,27 @@ public void dragEnter(DropTargetEvent event){}
 /**
  * This implementation of <code>dragLeave</code> does nothing.
  * For additional information see <code>DropTargetListener.dragOperationChanged</code>.
- * 
+ *
  * @param event the information associated with the drag leave event
  */
 public void dragLeave(DropTargetEvent event){}
 
 /**
- * This implementation of <code>dragOperationChanged</code> permits the default 
+ * This implementation of <code>dragOperationChanged</code> permits the default
  * operation defined in <code>event.detail</code>to be performed on the current data type
  * defined in <code>event.currentDataType</code>.
  * For additional information see <code>DropTargetListener.dragOperationChanged</code>.
- * 
+ *
  * @param event the information associated with the drag operation changed event
  */
 public void dragOperationChanged(DropTargetEvent event){}
 
 /**
- * This implementation of <code>dragOver</code> permits the default 
+ * This implementation of <code>dragOver</code> permits the default
  * operation defined in <code>event.detail</code>to be performed on the current data type
  * defined in <code>event.currentDataType</code>.
  * For additional information see <code>DropTargetListener.dragOver</code>.
- * 
+ *
  * @param event the information associated with the drag over event
  */
 public void dragOver(DropTargetEvent event){}
@@ -67,17 +78,17 @@ public void dragOver(DropTargetEvent event){}
 /**
  * This implementation of <code>drop</code> does nothing.
  * For additional information see <code>DropTargetListener.drop</code>.
- * 
+ *
  * @param event the information associated with the drop event
  */
 public void drop(DropTargetEvent event){}
 
 /**
- * This implementation of <code>dropAccept</code> permits the default 
+ * This implementation of <code>dropAccept</code> permits the default
  * operation defined in <code>event.detail</code>to be performed on the current data type
  * defined in <code>event.currentDataType</code>.
  * For additional information see <code>DropTargetListener.dropAccept</code>.
- * 
+ *
  * @param event the information associated with the drop accept event
  */
 public void dropAccept(DropTargetEvent event){}
