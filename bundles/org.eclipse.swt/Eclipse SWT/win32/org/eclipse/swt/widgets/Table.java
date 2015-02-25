f5242b26270c565338f4dcc8333627b48c9e2570
Niraj Modi <niraj.modi@in.ibm.com>
2015-02-25 12:29:47 +0530

    Bug 459680 - NPE in Table._getItem
    Change-Id: I987a6a800af62dc0913a5d8932a4ec7811357de4
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Table.java
@@ -6163,9 +6163,11 @@ LRESULT WM_LBUTTONDBLCLK (long /*int*/ wParam, long /*int*/ lParam) {
 		*/
 		if (index != -1 && pinfo.flags == OS.LVHT_ONITEMSTATEICON) {
 			TableItem item = _getItem (index);
-			item.setChecked (!item.getChecked (), true);
-			if (!OS.IsWinCE) {
-				OS.NotifyWinEvent (OS.EVENT_OBJECT_FOCUS, handle, OS.OBJID_CLIENT, index + 1);
+			if (item != null && !item.isDisposed ()) {
+				item.setChecked (!item.getChecked (), true);
+				if (!OS.IsWinCE) {
+					OS.NotifyWinEvent (OS.EVENT_OBJECT_FOCUS, handle, OS.OBJID_CLIENT, index + 1);
+				}
 			}
 		}	
 	}
@@ -6199,9 +6201,11 @@ LRESULT WM_LBUTTONDOWN (long /*int*/ wParam, long /*int*/ lParam) {
 		int index = (int)/*64*/OS.SendMessage (handle, OS.LVM_HITTEST, 0, pinfo);
 		if (index != -1 && pinfo.flags == OS.LVHT_ONITEMSTATEICON) {
 			TableItem item = _getItem (index);
-			item.setChecked (!item.getChecked (), true);
-			if (!OS.IsWinCE) {
-				OS.NotifyWinEvent (OS.EVENT_OBJECT_FOCUS, handle, OS.OBJID_CLIENT, index + 1);
+			if (item != null && !item.isDisposed ()) {
+				item.setChecked (!item.getChecked (), true);
+				if (!OS.IsWinCE) {
+					OS.NotifyWinEvent (OS.EVENT_OBJECT_FOCUS, handle, OS.OBJID_CLIENT, index + 1);
+				}
 			}
 		}	
 	}
