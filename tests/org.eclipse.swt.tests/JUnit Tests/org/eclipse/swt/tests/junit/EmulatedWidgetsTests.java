64b303517cf1df80087e0e8daead9a81d1b43829
Alexander Kurtakov <akurtako@redhat.com>
2015-03-20 10:09:41 +0200

    Bug 462631 - Adapt to JUnit 4 style tests
    Change EmulatedWidgetsTests to JUnit 4 style test suite.
    
    Change-Id: Ie3dbda2c627ef25c403ae9c346cbff7579b4f8d0
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/EmulatedWidgetsTests.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/EmulatedWidgetsTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2003 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,34 +7,33 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Red Hat Inc. - Adapt to JUnit 4.
  *******************************************************************************/
 package org.eclipse.swt.tests.junit;
 
 
-import junit.framework.*;
-import junit.textui.*;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
 /**
  * Suite for testing widgets that have been emulated on Motif.
  */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	Test_org_eclipse_swt_widgets_ColorDialog.class,
+	Test_org_eclipse_swt_widgets_FontDialog.class,	
+	Test_org_eclipse_swt_widgets_TabFolder.class,
+	Test_org_eclipse_swt_widgets_TabItem.class,
+	Test_org_eclipse_swt_widgets_Table.class,
+	Test_org_eclipse_swt_widgets_TableItem.class,
+	Test_org_eclipse_swt_widgets_TableColumn.class,	
+	Test_org_eclipse_swt_widgets_Tree.class,
+	Test_org_eclipse_swt_widgets_TreeItem.class,
+})
 public class EmulatedWidgetsTests {
 
 public static void main(String[] args) {
-	TestRunner.run (suite());
-}
-public static Test suite() {
-	TestSuite suite = new TestSuite();
-
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_ColorDialog.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_FontDialog.class);	
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_TabFolder.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_TabItem.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_Table.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_TableItem.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_TableColumn.class);	
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_Tree.class);
-	suite.addTestSuite(Test_org_eclipse_swt_widgets_TreeItem.class);
-		
-	return suite;
+	JUnitCore.main(EmulatedWidgetsTests.class.getName());
 }
 }
