10f81fb259ead3daa0dcfd8f065b90038bcc70e4
Niraj Modi <niraj.modi@in.ibm.com>
2015-04-20 22:12:43 +0530

    Bug 458917 - [Cocoa]DateTime widget turn Black with SWT.COLOR_TRANSPARENT background
    Change-Id: I99fb2b239bc72403771f5975814f733a0f39bdba
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DateTime.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DateTime.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -591,6 +591,11 @@ void sendVerticalSelection () {
 }
 
 void setBackgroundColor(NSColor nsColor) {
+	/*
+	 * Adding check to avoid DateTime background turning Black with
+	 * SWT.COLOR_TRANSPARENT, refer bug 458917
+	 */
+	if (nsColor != null && nsColor.alphaComponent () == 0) return;
 	((NSDatePicker)view).setBackgroundColor(nsColor);
 }
 
