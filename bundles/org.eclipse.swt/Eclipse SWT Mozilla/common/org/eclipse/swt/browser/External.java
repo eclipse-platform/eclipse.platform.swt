e84feaec9ce7b27fc38c8ff6438796d4fd8839e7
Niraj Modi <niraj.modi@in.ibm.com>
2015-05-12 19:30:05 +0530

    Bug 466202 - XULRunner 24 : memory leak when calling SWT BrowserFunction
    Change-Id: Ic5a649f9f99d8396fbcedc2b924c80acfbfed7f5
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT Mozilla/common/org/eclipse/swt/browser/External.java
+++ bundles/org.eclipse.swt/Eclipse SWT Mozilla/common/org/eclipse/swt/browser/External.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2008, 2013 IBM Corporation and others.
+ * Copyright (c) 2008, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -117,6 +117,7 @@ static Object convertToJava (nsIVariant variant) {
 					rc = supports.QueryInterface (id, result);
 					if (rc != XPCOM.NS_OK) Mozilla.error (rc);
 					if (result[0] == 0) Mozilla.error (XPCOM.NS_NOINTERFACE);
+					supports.Release ();
 
 					nsIVariant currentVariant = new nsIVariant (result[0]);
 					result[0] = 0;
