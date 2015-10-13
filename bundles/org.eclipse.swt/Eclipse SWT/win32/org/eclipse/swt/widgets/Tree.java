4dfb10dff46c4463cafeee75a26495b3d2ee4c93
Niraj Modi <niraj.modi@in.ibm.com>
2015-10-13 13:41:05 +0530

    Bug 439712 - NPE in Tree.WM_PAINT
    Change-Id: Ic826eeecf35059b2931c5e10f3f69ae02a806d80
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2013 IBM Corporation and others.
+ * Copyright (c) 2000, 2015 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -6983,7 +6983,7 @@ LRESULT WM_RBUTTONDOWN (long /*int*/ wParam, long /*int*/ lParam) {
 LRESULT WM_PAINT (long /*int*/ wParam, long /*int*/ lParam) {
 	if ((state & DISPOSE_SENT) != 0) return LRESULT.ZERO;
 
-	if (shrink && !ignoreShrink) {
+	if (shrink && !ignoreShrink && items != null) {
 		/* Resize the item array to fit the last item */
 		int count = items.length - 1;
 		while (count >= 0) {
