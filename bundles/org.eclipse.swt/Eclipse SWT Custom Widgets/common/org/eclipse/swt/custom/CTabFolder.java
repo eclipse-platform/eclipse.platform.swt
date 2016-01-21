585085075702b3c9665ad402cbe1a9f2889382a0
Eugen Neufeld <eneufeld@eclipsesource.com>
2016-01-21 19:32:11 +0530

    Bug 483688 - [CTabFolder] Icon of toolbar menu cropped
    Change-Id: Idbf65bdd6f5bb8ae0fad7f1a9af5dc31a103a567
    Signed-off-by: Eugen Neufeld <eneufeld@eclipsesource.com>

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -549,6 +549,9 @@ Rectangle[] computeControlBounds (Point size, boolean[][] position) {
 				rects[i].x = x;
 				rects[i].y = getControlY(size, rects, borderBottom, borderTop, i);
 				if ((alignment & (SWT.FILL | SWT.WRAP)) != 0) availableWidth -= ctrlSize.x;
+			}
+			if (tabControlSize[i].y >= tabHeight) {
+				overflow[i] = true;
 			}
 		}
 	} else {
