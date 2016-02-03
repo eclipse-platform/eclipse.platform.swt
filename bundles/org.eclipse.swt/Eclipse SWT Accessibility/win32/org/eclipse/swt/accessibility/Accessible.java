9890292f7bba6f17026ce3bc1d6ca7fb05d4ae2c
Arun Thondapu <arunkumar.thondapu@in.ibm.com>
2016-02-03 06:21:25 +0530

    Bug 447930 - Accessibility: IA2 object not instantiated until IA2 interfaces are implemented
    Change-Id: I603c285a91fe3f10d7e1b70fea8aa1b233277a18
    Signed-off-by: Arun Thondapu <arunkumar.thondapu@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT Accessibility/win32/org/eclipse/swt/accessibility/Accessible.java
+++ bundles/org.eclipse.swt/Eclipse SWT Accessibility/win32/org/eclipse/swt/accessibility/Accessible.java
@@ -1839,6 +1839,10 @@ public class Accessible {
 			return COM.E_NOINTERFACE;
 		}
 
+		/* Workaround for bug 447930 - add a temporary relation so that the IA2 object is created */
+		if (getRelationCount() == 0) {
+			addRelation(ACC.RELATION_NODE_CHILD_OF, parent);
+		}
 		int code = queryAccessible2Interfaces(guid, ppvObject);
 		if (code != COM.S_FALSE) {
 			if (DEBUG) print(this + ".QueryInterface guid=" + guidString(guid) + " returning" + hresult(code));
@@ -1997,6 +2001,10 @@ public class Accessible {
 				AddRef();
 				return COM.S_OK;
 			}
+			/* Workaround for bug 447930 - add a temporary relation so that the IA2 object is created */
+			if (getRelationCount() == 0) {
+				addRelation(ACC.RELATION_NODE_CHILD_OF, parent);
+			}
 			int code = queryAccessible2Interfaces(guid, ppvObject);
 			if (code != COM.S_FALSE) {
 				if (DEBUG) print(this + ".QueryService service=" + guidString(service) + " guid=" + guidString(guid) + " returning" + hresult(code));
@@ -2005,6 +2013,10 @@ public class Accessible {
 		}
 
 		if (COM.IsEqualGUID(service, COM.IIDIAccessible2)) {
+			/* Workaround for bug 447930 - add a temporary relation so that the IA2 object is created */
+			if (getRelationCount() == 0) {
+				addRelation(ACC.RELATION_NODE_CHILD_OF, parent);
+			}
 			int code = queryAccessible2Interfaces(guid, ppvObject);
 			if (code != COM.S_FALSE) {
 				if (DEBUG) print(this + ".*QueryService service=" + guidString(service) + " guid=" + guidString(guid) + " returning" + hresult(code));
