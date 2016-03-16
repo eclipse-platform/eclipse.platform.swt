581ec4f363156457a48fdeca3c7fd3bddf17ee48
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2016-03-16 17:28:46 +0530

    Updated Snippet to use set and get link color

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet182.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet182.java
@@ -11,7 +11,8 @@
 package org.eclipse.swt.snippets;
 
 /*
- * Link example snippet: create a link widget
+ * Link example snippet: create a link widget and 
+ * provide buttons to set the link color.
  * 
  * For a list of all SWT example snippets see
  * http://www.eclipse.org/swt/snippets/
@@ -19,7 +20,9 @@ package org.eclipse.swt.snippets;
  * @since 3.1
  */
 import org.eclipse.swt.*;
-import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
 public class Snippet182 {
@@ -27,10 +30,34 @@ public class Snippet182 {
 	public static void main(String[] args) {
 		Display display = new Display();
 		Shell shell = new Shell(display);
+		shell.setLayout(new RowLayout());
+		
 		Link link = new Link(shell, SWT.BORDER);
 		link.setText("This a very simple <A>link</A> widget.");
-		Rectangle clientArea = shell.getClientArea();
-		link.setBounds(clientArea.x, clientArea.y, 140, 40);
+		
+		Button setButton = new Button(shell, SWT.PUSH);
+		setButton.setText("Choose link color");
+		setButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("default link color " + link.getLinkForeground());
+				ColorDialog colorDialog = new ColorDialog(shell);
+				RGB color = colorDialog.open();
+				link.setLinkForeground(new Color(display, color));
+				System.out.println("user selected link color " + link.getLinkForeground());
+			}
+		});
+		
+		Button resetButton = new Button(shell, SWT.PUSH);
+		resetButton.setText("Reset link color");
+		resetButton.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				System.out.println("link color reset to system default");
+				link.setLinkForeground(null);
+			}
+		});
+		
 		shell.pack ();
 		shell.open();
 		while (!shell.isDisposed()) {
