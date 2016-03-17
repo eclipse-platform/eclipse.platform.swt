61bde33b02b214a7378d7cd72bf8492331cce40e
Niraj Modi <niraj.modi@in.ibm.com>
2016-03-17 09:53:04 +0530

    Fix compilation errors in GTK Device.java
    Change-Id: Id16a417781b267a5dc55d5a01c727763d3df1274
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/graphics/Device.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/graphics/Device.java
@@ -682,10 +682,6 @@ protected void init () {
 	shellHandle = OS.gtk_window_new(OS.GTK_WINDOW_TOPLEVEL);
 	if (shellHandle == 0) SWT.error(SWT.ERROR_NO_HANDLES);
 	OS.gtk_widget_realize(shellHandle);
-	int scaleFactor = OS.gdk_window_get_scale_factor(shellHandle);
-	System.out.println(scaleFactor);
-	scaleFactor = OS.gtk_widget_get_scale_factor(shellHandle);
-	System.out.println(scaleFactor);
 
 	/* Initialize the system font slot */
 	long /*int*/ defaultFont;
