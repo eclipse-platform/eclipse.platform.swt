e885df93e32a5ebd08ce186a9d12d5bb09ae2219
Niraj Modi <niraj.modi@in.ibm.com>
2016-07-11 15:54:39 +0530

    Bug 496939 - [win32] ctrl+backspace in preferences dialog crashed platform
    Change-Id: Iadab6b5eccd36b54922ee97c920dba10780a4ca0
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Text.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2015 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -2732,7 +2732,12 @@ LRESULT WM_CHAR (long /*int*/ wParam, long /*int*/ lParam) {
 	switch ((int)/*64*/wParam) {
 		case SWT.DEL:
 			if (OS.GetKeyState (OS.VK_CONTROL) < 0) {
-				if ((style & SWT.READ_ONLY) != 0 || (style & SWT.PASSWORD) != 0) return LRESULT.ZERO;
+				/*
+				 * 'Ctrl + BackSpace' functionality uses 'EM_REPLACESEL' native
+				 * API which is supported from Windows Vista. Adding OS version
+				 * check to avoid crash on WinXP, see more details on bug 496939
+				 */
+				if (OS.WIN32_VERSION < OS.VERSION (6, 0) || (style & SWT.READ_ONLY) != 0 || (style & SWT.PASSWORD) != 0) return LRESULT.ZERO;
 				Point selection = getSelection ();
 				int x = selection.x;
 				int y = selection.y;
