688c0e346bc63c155a78983d3bf38fb76a8c5b6c
Lev Ufimtsev <lufimtse@redhat.com>
2016-08-04 15:13:18 -0400

    Bug 430538: [GTK3][webkit] Support Custom JavaScript execution in WebKit2 mode of SWT Browser
    Re-Applying patch that was reverted because build servers were not
    updated:
    https://git.eclipse.org/r/#/c/80902/
    
    --------------------------
    
    Stage 1
    
    Patch Set 1: 2016-08Aug-04Thu (pushed2Gerrit)
    Implement return mechanism for webkit_*_run_javascript()
    - Adding a webkitgtk_custom.c file for custom function
    - modified linux_makefil for additional file 
    - cleanup of linux_makefile for webkit, (confusing target naming).
    
    Patch Set 2: 2016-08Aug-08Mon (pushed2Gerrit)
    - Implemented call to webkit
    - Implemented spinlock and while loop to wait for callback 
    	to finish. Spinlock is passed as pointer to callback.
    
    Patch Set 3: (pushed2Gerrit)
    - 2016-08Aug-08Mon - minor tweaks.
    - 2016-08Aug-09Tue - renamed custom method/function name.
    - 2016-08Aug-10Wed - fixed potential memory leak.
    - 2016-08Aug-15Mon - Implemented conditional compilation for
    	build servers without webkit2gtk4
    
    Patch Set 4:
    2016-08Aug-30Tue
    - Rewrite to call webkit functions dynamically instead of hard-linking
    - removed pkg-config dependencies to webkitgtk
    - Changed pkg-config webkit flags to use gio instead of glib.
       Note, glib is a subset of gio, and gio is a subset of Gtk2/Gtk3,
       thus this should not break anything.
    
    Patchset 14:
    - Adjusted comment in webkitgtk_custom.c
       > Warning to future developers as it's not obvious. I did not know
    until you pointed it out.
    - Removed 'ifndef' from function swt_webkit_web_view_run_javascript
    call. In hindsight, it doesn't look like it's necessary.
    
    Things I've tested:
    - Child Eclipse with webkit2 browser
    - Browser tests:  (fixes tests 3,4, 8).
    - Various browser snippets.
    
    To test yourself:
    - To enable webkit2, set:
    SWT_WEBKIT2=1
    
    - Manual inspection via SWT-SNIPPET:
    https://github.com/ericwill/SWT-snippets/blob/master/src/browser/Bug430538_JS_Set_Background.java
    If you click the 'execute' button, now background is set.
    (Note, other button 'evalute' not working yet).
    
    - Browser8.java now passes and correctly executes JS
       (enable verbose on line 30 to see before/after, now background
    changes).
    - Browser3.java now passes
    
    
    Change-Id: I1202064798ef7839af1d29aff41391d411276005
    Task-Url: https://bugs.eclipse.org/bugs/show_bug.cgi?id=430538
    Signed-off-by: Lev Ufimtsev <lufimtse@redhat.com>

---
+++ bundles/org.eclipse.swt/Eclipse SWT WebKit/gtk/library/webkitgtk_custom.h
@@ -0,0 +1,24 @@
+/*******************************************************************************
+* Copyright (c) 2016 Red Hat, Inc. All rights reserved.
+* The contents of this file are made available under the terms
+* of the GNU Lesser General Public License (LGPL) Version 2.1 that
+* accompanies this distribution (lgpl-v21.txt).  The LGPL is also
+* available at http://www.gnu.org/licenses/lgpl.html.  If the version
+* of the LGPL at http://www.gnu.org is different to the version of
+* the LGPL accompanying this distribution and there is any conflict
+* between the two license versions, the terms of the LGPL accompanying
+* this distribution shall govern.
+*
+* Contributors:
+*     Red Hat, Inc
+*******************************************************************************/
+
+#ifndef INC_webkitgtk_custom_H
+#define INC_webkitgtk_custom_H
+
+/* Manually written code */
+
+long swt_webkit_web_view_run_javascript (long webkit_handle, signed char *script);
+
+#endif /*INC_webkit_custom_H*/
+
