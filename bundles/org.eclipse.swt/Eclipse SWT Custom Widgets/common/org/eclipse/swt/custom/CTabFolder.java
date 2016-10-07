fe17d25c18954ff93d6d3c574b728ffb02e649a5
Alexander Kurtakov <akurtako@redhat.com>
2016-10-07 05:15:13 -0400

    Revert "Bug 474444 - Active part stack not visible when Theming is disabled"
    This reverts commit 35a0bb02a8079406b1a50ca48a395b464d438f8b.
    
    Change-Id: Ia63cc629de5caa8a041a0d583950eced256af864

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolder.java
@@ -211,8 +211,6 @@ public class CTabFolder extends Composite {
 	Rectangle hoverRect = new Rectangle(0,0,0,0);
 	boolean hovering;
 	boolean hoverTimerRunning;
-	boolean active;
-	static CTabFolder currentlyActiveFolder;
 
 	boolean showChevron = false;
 	Menu showMenu;
@@ -340,8 +338,6 @@ void init(int style) {
 			case SWT.Resize:           onResize(event);	break;
 			case SWT.Traverse:         onTraverse(event); break;
 			case SWT.Selection:        onSelection(event); break;
-			case SWT.Activate:         onActivate(event); break;
-			case SWT.Deactivate:       onDeactivate(event); break;
 		}
 	};
 
@@ -362,8 +358,6 @@ void init(int style) {
 		SWT.Paint,
 		SWT.Resize,
 		SWT.Traverse,
-		SWT.Activate,
-		SWT.Deactivate
 	};
 	for (int i = 0; i < folderEvents.length; i++) {
 		addListener(folderEvents[i], listener);
@@ -371,22 +365,6 @@ void init(int style) {
 
 	initAccessible();
 }
-void onDeactivate(Event event) {
-	if (currentlyActiveFolder == this) {
-		currentlyActiveFolder = null;
-	}
-	active = false;
-	redraw();
-}
-
-void onActivate(Event event) {
-	active = true;
-	if (currentlyActiveFolder == null || currentlyActiveFolder.isAncestor(this)) {
-		currentlyActiveFolder = this;
-	}
-	redraw();
-}
-
 static int checkStyle (Composite parent, int style) {
 	int mask = SWT.CLOSE | SWT.TOP | SWT.BOTTOM | SWT.FLAT | SWT.LEFT_TO_RIGHT | SWT.RIGHT_TO_LEFT | SWT.SINGLE | SWT.MULTI;
 	style = style & mask;
@@ -4070,9 +4048,5 @@ int getWrappedHeight (Point size) {
 	if (chevronVisible == visible) return;
 	chevronVisible = visible;
 	updateFolder(UPDATE_TAB_HEIGHT | REDRAW);
-}
-
-boolean shouldHighlight() {
-	return active || currentlyActiveFolder == this || isAncestor(currentlyActiveFolder) ;
 }
 }
