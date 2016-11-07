07b1ed2f4aaa8225c9adef455c2ff8a5ba479614
Lars Vogel <Lars.Vogel@vogella.com>
2016-11-07 20:57:52 +0100

    Bug 507185 - [api] Provide helper methods to use lambda expressions for KeyListener
    Formating of KeyListener does not follow project settings, so this
    change try to keep the new code formating aligned with the existing
    style.
    
    
    Change-Id: I86101c755c284cdd9e587784b974ef70d3da640f
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/KeyListener.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/events/KeyListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2011 IBM Corporation and others.
+ * Copyright (c) 2000, 2016 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,11 +7,14 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Bug 507185
  *******************************************************************************/
 package org.eclipse.swt.events;
 
 
-import org.eclipse.swt.internal.SWTEventListener;
+import java.util.function.*;
+
+import org.eclipse.swt.internal.*;
 
 /**
  * Classes which implement this interface provide methods
@@ -44,4 +47,39 @@ public void keyPressed(KeyEvent e);
  * @param e an event containing information about the key release
  */
 public void keyReleased(KeyEvent e);
+
+/**
+ * Static helper method to create a key listener for the
+ * {@link #keyPressed(KeyEvent e)}) method with a lambda expression.
+ *
+ * @param c the consumer of the event
+ * @return KeyListener
+ * @since 3.106
+ */
+public static KeyListener keyPressedAdapter(Consumer<KeyEvent> c) {
+	return new KeyAdapter() {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			c.accept(e);
+		}
+	};
+}
+
+/**
+ * Static helper method to create a key listener for the
+ * {@link #keyReleased(KeyEvent e)}) method with a lambda expression.
+ *
+ * @param c the consumer of the event
+ * @return KeyListener
+ * @since 3.106
+*/
+public static KeyListener keyReleasedAdapter(Consumer<KeyEvent> c) {
+	return new KeyAdapter() {
+		@Override
+		public void keyReleased(KeyEvent e) {
+			c.accept(e);
+		}
+	};
+}
+
 }
