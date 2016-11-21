ac57c750e227a255c0d7f2feaa5df5d934402745
Rolf Theunissen <rolf.theunissen@altran.com>
2016-11-21 09:30:12 +0100

    Bug 506030 - [GTK] clean up dynamic function calls
    Update script to mark shared gtk3 deprecated functions
    
    Change-Id: Iae5560a5cfd8752ae224a0b8d9f85729d8fd00c5
    Signed-off-by: Rolf Theunissen <rolf.theunissen@altran.com>

--- bundles/org.eclipse.swt.tools/dynamic_gtk2_vs_gtk3.py
+++ bundles/org.eclipse.swt.tools/dynamic_gtk2_vs_gtk3.py
@@ -18,12 +18,14 @@ from html.parser import HTMLParser
 API_GTK2_STABLE_URL = "https://developer.gnome.org/gtk2/stable/api-index-full.html"
 API_GTK3_0_URL = "https://developer.gnome.org/gtk3/3.0/api-index-full.html"
 API_GTK3_STABLE_URL = "https://developer.gnome.org/gtk3/stable/api-index-full.html"
+API_GTK3_DEPRECATED_URL = "https://developer.gnome.org/gtk3/stable/api-index-deprecated.html"
 
 OS_CUSTOM_FILE = "./../org.eclipse.swt/Eclipse SWT PI/gtk/library/os_custom.h"
 FUNCTION_LIST = []
 GTK2_FUNCTION_LIST = []
 GTK30_FUNCTION_LIST = []
 GTK3x_FUNCTION_LIST = []
+GTK3x_DEPR_FUNCTION_LIST = []
 
 GTK2_GTK30_SHARED_FUNCTION_LIST = []
 GTK2_GTK3x_SHARED_FUNCTION_LIST = []
@@ -51,24 +53,27 @@ def download_gtk3_0_symbols():
 def download_gtk3_stable_symbols():
     return download_symbols(API_GTK3_STABLE_URL);
 
+def download_gtk3_deprecated_symbols():
+    return download_symbols(API_GTK3_DEPRECATED_URL);
+
 def populate_function_list():
     # Read os_custom.h from file
     with open(OS_CUSTOM_FILE) as f:
         os_custom = f.read()
-    
+
     return str(os_custom);
 
-def find_gtk_version_functions(custom_str, gtk2_str, gtk30_str, gtk3x_str):
+def find_gtk_version_functions(custom_str, gtk2_str, gtk30_str, gtk3x_str, gtk3x_dpr_str):
     regex = r"((\w)+(_)+(LIB)(\s)+(LIB_)+)+"
     matchList = re.findall(regex, custom_str)
-    
+
     # Search through list of regex matches, strip the "_LIB" and populate
     # the dynamic function list.
     for match in matchList:
         location = match[0].find("_LIB")
         function_name = match[0][:location]
         FUNCTION_LIST.append(function_name)
-    
+
     # Check each dynamic function and see if it's deprecated: if so, populate
     # the deprecated function list.
     for func in FUNCTION_LIST:
@@ -79,6 +84,8 @@ def find_gtk_version_functions(custom_str, gtk2_str, gtk30_str, gtk3x_str):
             GTK30_FUNCTION_LIST.append(func)
         if func_tag in gtk3x_str:
             GTK3x_FUNCTION_LIST.append(func)
+        if func_tag in gtk3x_dpr_str:
+            GTK3x_DEPR_FUNCTION_LIST.append(func)
     return;
 
 def find_gtk2_and_gtk3_shared():
@@ -93,11 +100,13 @@ if __name__ == "__main__":
     gtk2functions = download_gtk2_stable_symbols()
     gtk30functions = download_gtk3_0_symbols()
     gtk3xfunctions = download_gtk3_stable_symbols()
+    gtk3xdepricated = download_gtk3_deprecated_symbols()
 
     os_custom_str = populate_function_list()
 
-    find_gtk_version_functions(os_custom_str, gtk2functions, gtk30functions, gtk3xfunctions)
-    
+    find_gtk_version_functions(os_custom_str, gtk2functions, gtk30functions,
+            gtk3xfunctions, gtk3xdepricated)
+
     find_gtk2_and_gtk3_shared()
 
     # If the user specified the "-f" option, the output will be written to
@@ -105,20 +114,30 @@ if __name__ == "__main__":
     if len(sys.argv) == 2 and "-f" in str(sys.argv):
         with open("dynamic_gtk2x_gtk30_shared_functions.txt","w") as f:
             for i in GTK2_GTK30_SHARED_FUNCTION_LIST:
+                if i in GTK3x_DEPR_FUNCTION_LIST:
+                    f.write ("#")
                 f.write(i + "\n")
             f.close()
         with open("dynamic_gtk2x_gtk3x_shared_functions.txt","w") as f:
             for i in GTK2_GTK3x_SHARED_FUNCTION_LIST:
+                if i in GTK3x_DEPR_FUNCTION_LIST:
+                    f.write ("#")
                 f.write(i + "\n")
             f.close()
     # If no options are specified, print the list to console
     else:
         print ("# GTK2.x - GTK3.0 SHARED")
         for i in GTK2_GTK30_SHARED_FUNCTION_LIST:
-            print(i)
+            if i in GTK3x_DEPR_FUNCTION_LIST:
+                print("#" + i)
+            else:
+                print(i)
 
-        print()            
+        print()
         print ("# GTK2.x - GTK3.x SHARED")
         for i in GTK2_GTK3x_SHARED_FUNCTION_LIST:
-            print(i)
+            if i in GTK3x_DEPR_FUNCTION_LIST:
+                print("#" + i)
+            else:
+                print(i)
     sys.exit(0)
