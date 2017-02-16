8aad90891ea4ab5762420c7424db7b01ec50c107
Arun Thondapu <arunkumar.thondapu@in.ibm.com>
2017-02-16 17:17:54 +0530

    v4741

--- bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common_j2se/org/eclipse/swt/internal/Library.java
@@ -26,7 +26,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 740;
+    static int MINOR_VERSION = 741;
 
 	/**
 	 * SWT revision number (must be >= 0)
