39b7853d7f96da18ea54bb0b2cfb7cedc605fe35
Alexander Kurtakov <akurtako@redhat.com>
2017-02-21 08:14:17 +0200

    Bug 511181 - [api] Provide helper methods to use lambda expressions for AccessibleListener methods
    Properly name the getDescription helper method to be
    getDescriptionAdapter.
    
    Change-Id: Id0cd2c9344ffe4b63a47a7412411a568f9dab399
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- bundles/org.eclipse.swt/Eclipse SWT Accessibility/common/org/eclipse/swt/accessibility/AccessibleListener.java
+++ bundles/org.eclipse.swt/Eclipse SWT Accessibility/common/org/eclipse/swt/accessibility/AccessibleListener.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2005 IBM Corporation and others.
+ * Copyright (c) 2000, 2017 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -185,7 +185,7 @@ public interface AccessibleListener extends SWTEventListener {
 	 * @return AccessibleListener
 	 * @since 3.106
 	*/
-	public static AccessibleListener getDescription(Consumer<AccessibleEvent> c) {
+	public static AccessibleListener getDescriptionAdapter(Consumer<AccessibleEvent> c) {
 		return new AccessibleAdapter() {
 			@Override
 			public void getDescription(AccessibleEvent e) {
