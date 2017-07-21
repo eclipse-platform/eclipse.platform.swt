02be325df51425fdbab5879baeb539157bb148df
Leo Ufimtsev <lufimtse@redhat.com>
2017-07-21 13:33:01 -0400

    Bug 519996 Widget is disposed exception when closing shell with ccombos
    Snippet to reproduce issue.
    
    Change-Id: I609b21126ee8d3b46b188519ba5d45df758ff5cb
    Signed-off-by: Leo Ufimtsev <lufimtse@redhat.com>

---
+++ tests/org.eclipse.swt.tests.gtk/Bug Snippets/org/eclipse/swt/tests/gtk/snippets/Bug519996_focusDisposeIssue.java
@@ -0,0 +1,40 @@
+package org.eclipse.swt.tests.gtk.snippets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CCombo;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class Bug519996_focusDisposeIssue {
+
+	public static void main(String[] args) {
+		Shell shell = new Shell();
+		shell.setLayout(new FillLayout());
+		CCombo combo = new CCombo(shell, SWT.BORDER);
+		CCombo combo1 = new CCombo(shell, SWT.BORDER);
+
+		Button button = new Button(shell, SWT.PUSH);
+		button.setText("focus and close");
+		button.addSelectionListener(new SelectionAdapter() {
+		 @Override
+		 public void widgetSelected(SelectionEvent e) {
+		  combo.setFocus();
+		  shell.close();
+		 }
+		});
+
+		shell.layout();
+		shell.open();
+
+		Display display = shell.getDisplay();
+		while (!shell.isDisposed ()) {
+			if (!display.readAndDispatch ()) display.sleep ();
+		}
+		display.dispose ();
+	}
+
+}
