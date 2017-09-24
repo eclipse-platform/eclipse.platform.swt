bebec069911a121a847f97591fa55b4431a3ad52
Till Brychcy <register.eclipse@brychcy.de>
2017-09-24 21:39:22 +0200

    Bug 522690 - Allow menus to work if DefaultApp.nib is not found
    Change-Id: I03757215acbd8eac96be55875daffad72f3a3f1d
    Signed-off-by: Till Brychcy <register.eclipse@brychcy.de>

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Display.java
@@ -945,6 +945,7 @@ void createMainMenu () {
 	NSString emptyStr = NSString.string();
 	NSMenu mainMenu = (NSMenu)new NSMenu().alloc();
 	mainMenu.initWithTitle(emptyStr);
+	mainMenu.setAutoenablesItems(false);
 
 	NSMenuItem menuItem;
 	NSMenu appleMenu;
