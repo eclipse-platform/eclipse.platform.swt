23a636dd535cf5d034a32c01f3b70238ac237533
Simeon Andreev <simeon.danailov.andreev@gmail.com>
2017-12-08 15:35:19 +0100

    Bug 528251 - view CTabFolders result in not highlighted part stack tab
    When a view contains a selected CTabFolder, selecting another view in
    the same part stack results in non-highlighted view tab. This is the
    case, since CTabFolder.onActivate/onDeactivate will tell ancestor
    CTabFolders to highlight resp. not highlight. Selecting another view
    results in deactivating the nested CTabFolder and so removing
    highlighting from the part stack tabs. This regression was introduced
    with the fix for Bug 474444.
    
    With this change, highlighting is toggled only for the CTabFolder which
    is being activated/deactivated. The change was verified both on Windows
    and on RHEL+GTK3.14. The problem from Bug 474444 is not observed.
    
    Change-Id: I6fd4bf7f049e84d95bd5e83332a6484164623576
    Signed-off-by: Simeon Andreev <simeon.danailov.andreev@gmail.com>

---
+++ tests/org.eclipse.swt.tests/BugSnippets/Bug528251_CTabFolder_nested_highlighting.java
@@ -0,0 +1,96 @@
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.custom.SashForm;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Description: Console View tab is not highlighted when selected.
+ * Steps to reproduce:
+ * <ol>
+ * <li>Open the Console View.</li>
+ * <li>Open a view with a CTabFolder in the same part stack as the Console View.</li>
+ * <li>Select a tab in the new view.</li>
+ * <li>Select the Console View.</li>
+ * </ol>
+ * Expected results: Console View tab is highlighted, indicating a selection.
+ * Actual results: Console View tab is not highlighted.
+ */
+public class Bug528251_CTabFolder_nested_highlighting {
+
+	private static final String[] REPRODUCTION_STEPS = {
+			"1. select \"Another View\" tab",
+			"2. select \"nested tab\"",
+			"3. select \"Console View\" tab",
+			"4. \"Console View\" tab must be highlighted",
+	};
+
+	private final Display display;
+	private final Shell shell;
+
+	private final CTabFolder partStackTabs;
+	private final CTabItem anotherViewTab;
+
+	public Bug528251_CTabFolder_nested_highlighting() {
+		display = new Display();
+		shell = new Shell(display);
+		shell.setSize(550, 200);
+		shell.setLayout(new FillLayout());
+		shell.setLocation(center(display, shell));
+		shell.setText("Bug 528251");
+
+		partStackTabs = new CTabFolder(shell, SWT.NONE);
+
+		// Console View
+		CTabItem consoleViewTab = new CTabItem(partStackTabs, SWT.NONE);
+		consoleViewTab.setText("Console View");
+
+		// some other view with a CTabFolder
+		SashForm anotherView = new SashForm(partStackTabs, SWT.NONE);
+		anotherViewTab = new CTabItem(partStackTabs, SWT.NONE);
+		anotherViewTab.setText("Another View");
+		anotherViewTab.setControl(anotherView);
+		CTabFolder anotherViewNestedTabs = new CTabFolder(anotherView, SWT.NONE);
+		CTabItem anotherViewNestedTab = new CTabItem(anotherViewNestedTabs, SWT.NONE);
+		anotherViewNestedTab.setText("nested tab");
+
+		// what to do to see the problem
+		StyledText text = new StyledText(shell, SWT.NONE);
+		text.setText(String.join(System.lineSeparator(), REPRODUCTION_STEPS));
+		text.setEditable(false);
+	}
+
+	void showCase() {
+		shell.open();
+		waitForClose();
+	}
+
+	private void waitForClose() {
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+		display.dispose();
+	}
+
+	private static Point center(Display display, Shell shell) {
+		Rectangle displayBounds = display.getPrimaryMonitor().getBounds();
+		Point shellBounds = shell.getSize();
+		Point center = new Point(
+				displayBounds.x + (displayBounds.width - shellBounds.x) / 2,
+				displayBounds.y + (displayBounds.height - shellBounds.y) / 2);
+
+		return center;
+	}
+
+
+	public static void main(String[] args) {
+		Bug528251_CTabFolder_nested_highlighting bug = new Bug528251_CTabFolder_nested_highlighting();
+		bug.showCase();
+	}
+}
