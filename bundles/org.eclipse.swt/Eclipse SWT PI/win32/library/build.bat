93e4378c824ab449f9ae4b4fc4fe7c9e0f13e776
Niraj Modi <niraj.modi@in.ibm.com>
2018-01-08 19:40:06 +0530

    Bug 526802 (Part2)- [Win32] Update SWT windows native build scripts to works with latest VisualStudio and Win10-SDK
    Change-Id: I45cbdb889ff9b6fe34aa4f3a46aef12c942342fc
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -11,30 +11,35 @@
 
 @echo off
 
-IF EXIST C:\BUILD\swt-builddir set "SWT_BUILDDIR=C:\BUILD\swt-builddir"
-IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=S:\swt-builddir"
+IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=W:"
 echo SWT build dir: %SWT_BUILDDIR%
 
-IF "x.%MSSDK%"=="x." set "%ProgramFiles(x86)%\Windows Kits\10"
-IF "x.%WEBKIT_DIR%"=="x." set "WEBKIT_DIR=%SWT_BUILDDIR%\webkit\WebKit-r72896"
-IF "x.%WEBKIT_SUPPORT_DIR%"=="x." set "WEBKIT_SUPPORT_DIR=%SWT_BUILDDIR%\webkit\WebKitSupportLibrary"
-IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\2017"
+IF "x.%MSSDK%"=="x." set "MSSDK=%ProgramFiles(x86)%\Windows Kits\10"
+echo Microsoft Windows10 SDK dir: %MSSDK%
+IF "x.%WEBKIT_DIR%"=="x." set "WEBKIT_DIR=%SWT_BUILDDIR%\WebKit\r72896"
+echo Webkit dir: %WEBKIT_DIR%
+IF "x.%WEBKIT_SUPPORT_DIR%"=="x." set "WEBKIT_SUPPORT_DIR=%SWT_BUILDDIR%\WebKit\SupportLibrary"
+echo Webkit SupportLibrary dir: %WEBKIT_SUPPORT_DIR%
+IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft\Visual Studio\2017\"
+IF NOT EXIST "%MSVC_HOME%" set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\2017"
+echo Microsoft Visual Studio 2017 dir: %MSVC_HOME%
 
 IF x.%1==x.x86 GOTO X86
 IF x.%1==x.x86_64 GOTO X86_64
 
 :X86
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86
-IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\ibm-java-sdk-80-win-i386\sdk"
+IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x86"
+echo SWT_JAVA_HOME x86: %SWT_JAVA_HOME%
 call "%MSVC_HOME%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-call "%MSSDK%\setenv" /XP32 /RETAIL
 IF x.%1==x.x86 shift
 GOTO MAKE
 
 :X86_64
 set PROCESSOR_ARCHITECTURE=AMD64
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
-IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\ibm-java-sdk-80-win-x86_64\sdk"
+IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x64"
+echo SWT_JAVA_HOME x64: %SWT_JAVA_HOME%
 set CFLAGS=-DJNI64
 call "%MSVC_HOME%\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 shift
