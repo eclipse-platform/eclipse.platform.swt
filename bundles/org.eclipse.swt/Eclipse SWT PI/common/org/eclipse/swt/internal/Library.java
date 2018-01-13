24d91b42cdfc39c4ad63db50a955fe0039feb757
genie.releng <releng-inbox@eclipse.org>
2018-01-12 19:04:29 -0500

    v4853

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -28,7 +28,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-    static int MINOR_VERSION = 852;
+    static int MINOR_VERSION = 853;
 
 	/**
 	 * SWT revision number (must be >= 0)
