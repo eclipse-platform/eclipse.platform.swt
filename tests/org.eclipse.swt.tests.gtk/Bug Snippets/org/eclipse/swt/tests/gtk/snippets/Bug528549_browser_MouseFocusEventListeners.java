df9d3ea9ca6954c5fbc8ccaea404e5f5120bbeca
Leo Ufimtsev <lufimtse@redhat.com>
2018-02-01 09:22:37 -0500

    Bug 530609 – Add missing license headers to bug snippets
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=530609
    Change-Id: Idacfef6e77f9b9a4a3ecb75c8ad6b1746a86f50e
    Signed-off-by: Leo Ufimtsev <lufimtse@redhat.com>

--- tests/org.eclipse.swt.tests.gtk/Bug Snippets/org/eclipse/swt/tests/gtk/snippets/Bug528549_browser_MouseFocusEventListeners.java
+++ tests/org.eclipse.swt.tests.gtk/Bug Snippets/org/eclipse/swt/tests/gtk/snippets/Bug528549_browser_MouseFocusEventListeners.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat and others. All rights reserved.
+ * The contents of this file are made available under the terms
+ * of the GNU Lesser General Public License (LGPL) Version 2.1 that
+ * accompanies this distribution (lgpl-v21.txt).  The LGPL is also
+ * available at http://www.gnu.org/licenses/lgpl.html.  If the version
+ * of the LGPL at http://www.gnu.org is different to the version of
+ * the LGPL accompanying this distribution and there is any conflict
+ * between the two license versions, the terms of the LGPL accompanying
+ * this distribution shall govern.
+ *
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
 package org.eclipse.swt.tests.gtk.snippets;
 
 import org.eclipse.swt.SWT;
@@ -8,8 +22,6 @@ import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
-import org.eclipse.swt.events.MouseMoveListener;
-import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
@@ -20,9 +32,9 @@ import org.eclipse.swt.widgets.Shell;
 /**
  * On Webkit2, mouseUp/Down were not working.
  * On Webkit1/2, Focus in/out were not working.
- * 
+ *
  * Snippet prints events to console. Run and observer.
- * 
+ *
  * On webkit1, signals were partially handled via javascript, on webkit2 only through gdk events.
  *
  */
@@ -46,7 +58,7 @@ public class Bug528549_browser_MouseFocusEventListeners {
 				System.out.println("Browser Focus gained " + e.toString());
 			}
 		});
-		
+
 		browser.addMouseListener(new MouseListener() {
 			@Override
 			public void mouseUp(MouseEvent e) {
@@ -55,14 +67,14 @@ public class Bug528549_browser_MouseFocusEventListeners {
 			@Override
 			public void mouseDown(MouseEvent e) {
 				System.out.println("Browser Mouse Down " + e.toString());
-				
+
 			}
 			@Override
 			public void mouseDoubleClick(MouseEvent e) {
 				System.out.println("Browse Mouse Double click " + e.toString());
 			}
 		});
-		
+
 
 		// Below listeners already worked before bug. But good to have around.
 		browser.addKeyListener(new KeyListener() {
@@ -76,39 +88,29 @@ public class Bug528549_browser_MouseFocusEventListeners {
 			}
 		});
 
-		browser.addMouseWheelListener(new MouseWheelListener() {
-			@Override
-			public void mouseScrolled(MouseEvent e) {
-				System.out.println("Browser scroll event " + e.toString());
-			}
-		});
+		browser.addMouseWheelListener(e -> System.out.println("Browser scroll event " + e.toString()));
 
 		// Generates a lot of events...
-		browser.addMouseMoveListener(new MouseMoveListener() {
-			@Override
-			public void mouseMove(MouseEvent e) {
-				System.out.println("Browser mouse moved " + e.toString());
-			}
-		});
-		
+		browser.addMouseMoveListener(e -> System.out.println("Browser mouse moved " + e.toString()));
+
 		Button jsOnButton = new Button(shell, SWT.PUSH);
 		jsOnButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		jsOnButton.setText("JS Off");
 		jsOnButton.addSelectionListener(SelectionListener.widgetSelectedAdapter(e -> browser.setJavascriptEnabled(false)));
-		
+
 		Button jsOffButton = new Button(shell, SWT.PUSH);
 		jsOffButton.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		jsOffButton.setText("JS On");
 		jsOffButton.addSelectionListener(SelectionListener.widgetSelectedAdapter(e -> browser.setJavascriptEnabled(true)));
-		
-		
+
+
 		Button loadNextPage = new Button(shell, SWT.PUSH);
 		loadNextPage.setLayoutData(new GridData(SWT.BEGINNING, SWT.CENTER, false, false));
 		loadNextPage.setText("Load next page");
 		loadNextPage.addSelectionListener( SelectionListener.widgetSelectedAdapter(e -> browser.setText(getNewText())));
 
 		browser.setText(getNewText());
-		
+
 		shell.open();
 		while (!shell.isDisposed()) {
 			if (!display.readAndDispatch())
@@ -116,19 +118,19 @@ public class Bug528549_browser_MouseFocusEventListeners {
 		}
 		display.dispose();
 	}
-	
+
 	static String getNewText() {
 		return "Hello world<br>"
-				+ "<input type=\"button\" value=\"Open Curtain\" onclick=\"return change(this);\" />\n" + 
-				"\n" + 
-				"<script type=\"text/javascript\">\n" + 
-				"function change( el )\n" + 
-				"{\n" + 
-				"    if ( el.value === \"Open Curtain\" )\n" + 
-				"        el.value = \"Close Curtain\";\n" + 
-				"    else\n" + 
-				"        el.value = \"Open Curtain\";\n" + 
-				"}\n" + 
+				+ "<input type=\"button\" value=\"Open Curtain\" onclick=\"return change(this);\" />\n" +
+				"\n" +
+				"<script type=\"text/javascript\">\n" +
+				"function change( el )\n" +
+				"{\n" +
+				"    if ( el.value === \"Open Curtain\" )\n" +
+				"        el.value = \"Close Curtain\";\n" +
+				"    else\n" +
+				"        el.value = \"Open Curtain\";\n" +
+				"}\n" +
 				"</script>" + loadCounter++;
 	}
 
--- tests/org.eclipse.swt.tests.gtk/native_snippets/SwtFixed_native/simple_button_app.c
+++ tests/org.eclipse.swt.tests.gtk/native_snippets/SwtFixed_native/simple_button_app.c
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat and others. All rights reserved.
+ * The contents of this file are made available under the terms
+ * of the GNU Lesser General Public License (LGPL) Version 2.1 that
+ * accompanies this distribution (lgpl-v21.txt).  The LGPL is also
+ * available at http://www.gnu.org/licenses/lgpl.html.  If the version
+ * of the LGPL at http://www.gnu.org is different to the version of
+ * the LGPL accompanying this distribution and there is any conflict
+ * between the two license versions, the terms of the LGPL accompanying
+ * this distribution shall govern.
+ *
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
+
 #include <gtk/gtk.h>
 #include "os_custom.h"
 
