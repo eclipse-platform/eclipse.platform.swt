5ea94e70056200426b2124f82323195709f99dc0
Alexander Kurtakov <akurtako@redhat.com>
2018-02-01 16:56:09 +0200

    Suppress deprecations warnings.
    For tests of deprecated method.
    
    Change-Id: I0d06befc87db71586784fd970d92efd4ce980d7f
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_StyledText.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_StyledText.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2018 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -1365,6 +1365,7 @@ public void test_getOffsetAtLineI() {
 	text.setText("");
 	assertEquals(":g:", 0, text.getOffsetAtLine(0));
 }
+@SuppressWarnings("deprecation")
 @Test
 public void test_getOffsetAtLocationLorg_eclipse_swt_graphics_Point() {
 	boolean exceptionThrown = false;
