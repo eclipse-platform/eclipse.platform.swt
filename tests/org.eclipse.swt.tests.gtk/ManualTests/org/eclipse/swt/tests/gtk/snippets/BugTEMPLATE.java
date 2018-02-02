91ec200f79b93eab3648ca4efd9706bd7537135e
Leo Ufimtsev <lufimtse@redhat.com>
2018-02-02 10:15:13 -0500

    Bug 530567 – Move BugSnippets into separate package and exclude from builds.  (rename)
    Renaming bug snippets to 'ManualTests' to better express functionality.
    
    Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=530567
    Change-Id: If0132056efaf7a952dbe9f9248180cb56c8f6c9d
    Signed-off-by: Leo Ufimtsev <lufimtse@redhat.com>

---
+++ tests/org.eclipse.swt.tests.gtk/ManualTests/org/eclipse/swt/tests/gtk/snippets/BugTEMPLATE.java
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Red Hat and others. All rights reserved.
+ * The contents of this file are made available under the terms
+ * of the GNU Lesser General Public License (LGPL) Version 2.1 that
+ * accompanies this distribution (lgpl-v21.txt).  The LGPL is also
+ * available at http://www.gnu.org/licenses/lgpl.html.  If the version
+ * of the LGPL at http://www.gnu.org is different to the version of
+ * the LGPL accompanying this distribution and there is any conflict
+ * between the two license versions, the terms of the LGPL accompanying
+ * this distribution shall govern.
+ *
+ * Contributors:
+ *     Red Hat - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.tests.gtk.snippets;
+
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+/*
+ * Title:
+ * How to run:
+ * Bug description:
+ * Expected results:
+ * GTK Version(s):
+ */
+public class BugTEMPLATE {
+
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new FillLayout());
+		shell.setSize(500, 200);
+
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
+	}
+}
