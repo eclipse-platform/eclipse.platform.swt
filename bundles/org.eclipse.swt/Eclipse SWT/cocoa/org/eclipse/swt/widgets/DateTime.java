2ef6954b2724f4b0e6be7d10562b1cdac3ce7206
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2018-07-10 11:58:34 +0530

    Bug 536862: DateTime DROPDOWN behavior on Mac is not consistent with other platforms
    checkStyle() code on Mac is now consistent with Windows code.
    DATE style should be set by default if no other (TIME or CALENDAR) style
    is set. This should be done before the check for DROP_DOWN style.
    
    Also, removed the HINT from javadoc as DROP_DOWN style is supported on
    all 3 platforms now.
    
    Change-Id: Ibf04cbfa8aa19b4e57a548016a93ac7bf14f921a

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DateTime.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/DateTime.java
@@ -32,7 +32,7 @@ import org.eclipse.swt.internal.cocoa.*;
  * <p>
  * Note: Only one of the styles DATE, TIME, or CALENDAR may be specified,
  * and only one of the styles SHORT, MEDIUM, or LONG may be specified.
- * The DROP_DOWN style is a <em>HINT</em>, and it is only valid with the DATE style.
+ * The DROP_DOWN style is only valid with the DATE style.
  * </p><p>
  * IMPORTANT: This class is <em>not</em> intended to be subclassed.
  * </p>
@@ -103,9 +103,10 @@ static int checkStyle (int style) {
 	* the SWT style.
 	*/
 	style &= ~(SWT.H_SCROLL | SWT.V_SCROLL);
+	style = checkBits (style, SWT.DATE, SWT.TIME, SWT.CALENDAR, 0, 0, 0);
 	style = checkBits (style, SWT.MEDIUM, SWT.SHORT, SWT.LONG, 0, 0, 0);
 	if ((style & SWT.DATE) == 0) style &=~ SWT.DROP_DOWN;
-	return checkBits (style, SWT.DATE, SWT.TIME, SWT.CALENDAR, 0, 0, 0);
+	return style;
 }
 
 @Override
