ab72855ede0e009d3649fd9cb7b794efbee44795
Alexander Kurtakov <akurtako@redhat.com>
2018-09-20 08:51:58 +0300

    Suppress restriction in test snippets.
    Change-Id: I410b09f8c7c2c326d1750fdc801f9c5a234d9483
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet373.java
+++ examples/org.eclipse.swt.snippets/src/org/eclipse/swt/snippets/Snippet373.java
@@ -61,6 +61,7 @@ public class Snippet373 {
 		return path;
 	};
 
+	@SuppressWarnings("restriction")
 	public static void main(String[] args) {
 		System.setProperty("swt.autoScale", "quarter");
 		Display display = new Display();
