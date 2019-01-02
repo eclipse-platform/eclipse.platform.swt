875e377ea8456db41b51de1024f73b98aa484b46
Eric Williams <ericwill@redhat.com>
2019-01-02 15:15:39 -0500

    Bug 543130: [GTK4] Children of Shell have large offsets
    Only set the box packing type on GTK3, on GTK4 this breaks children of
    the vboxHandle.
    
    Change-Id: I99b2315de86be74533aa5bcbccb84da07be7b8d3
    Signed-off-by: Eric Williams <ericwill@redhat.com>

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Shell.java
@@ -816,9 +816,8 @@ void createHandle (int index) {
 	if (vboxHandle == 0) error (SWT.ERROR_NO_HANDLES);
 	createHandle (index, false, true);
 	GTK.gtk_container_add (vboxHandle, scrolledHandle);
-	if (GTK.GTK4) {
-		GTK.gtk_box_set_child_packing(vboxHandle, scrolledHandle, GTK.GTK_PACK_END);
-	} else {
+	// Only needed on GTK3
+	if (!GTK.GTK4) {
 		GTK.gtk_box_set_child_packing (vboxHandle, scrolledHandle, true, true, 0, GTK.GTK_PACK_END);
 	}
 	group = GTK.gtk_window_group_new ();
