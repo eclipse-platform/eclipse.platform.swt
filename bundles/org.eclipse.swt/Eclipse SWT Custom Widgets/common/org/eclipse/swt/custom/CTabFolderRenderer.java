04e7674bd4a82dc50ebd04d86747241b75845fbf
Andrey Loskutov <loskutov@gmx.de>
2019-02-01 16:37:31 -0500

    Revert "Bug 501491 - Draw close button with antialiasing."
    This reverts commit b96722424ff908aba3b0ce5a2b173f5080b3c281.
    
    Change-Id: I3c3d5f4d4794f866261f62b4f458b7eadb064b54

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolderRenderer.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/CTabFolderRenderer.java
@@ -871,8 +871,6 @@ public class CTabFolderRenderer {
 		int y = closeRect.y + Math.max(1, (closeRect.height-9)/2);
 		y += parent.onBottom ? -1 : 1;
 
-		int aa = gc.getAntialias();
-		gc.setAntialias(SWT.ON);
 		Color closeBorder = display.getSystemColor(BUTTON_BORDER);
 		switch (closeImageState & (SWT.HOT | SWT.SELECTED | SWT.BACKGROUND)) {
 			case SWT.NONE: {
@@ -914,7 +912,6 @@ public class CTabFolderRenderer {
 				break;
 			}
 		}
-		gc.setAntialias(aa);
 	}
 
 	void drawChevron(GC gc, Rectangle chevronRect, int chevronImageState) {
