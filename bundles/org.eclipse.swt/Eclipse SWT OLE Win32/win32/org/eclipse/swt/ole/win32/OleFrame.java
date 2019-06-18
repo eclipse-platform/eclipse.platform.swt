b3c2ede7a8454c5ec51e6aaab69e322fe4e92bdc
Niraj Modi <niraj.modi@in.ibm.com>
2019-06-18 12:54:43 +0530

    Bug 548364 - [win32] Compiler warning reported in swt.win32.win32.x86 plugin
    Change-Id: Icf876bb7435930995224a80cc113921de7cbf767
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT OLE Win32/win32/org/eclipse/swt/ole/win32/OleFrame.java
+++ bundles/org.eclipse.swt/Eclipse SWT OLE Win32/win32/org/eclipse/swt/ole/win32/OleFrame.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -46,7 +46,6 @@ import org.eclipse.swt.widgets.*;
 final public class OleFrame extends Composite
 {
 	// Interfaces for this Ole Client Container
-	private COMObject iUnknown;
 	private COMObject iOleInPlaceFrame;
 
 	// Access to the embedded/linked Ole Object
