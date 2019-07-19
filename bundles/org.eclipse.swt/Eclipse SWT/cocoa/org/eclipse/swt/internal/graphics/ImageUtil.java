94e3dd2e453b92538d63a927db1cf02005f959e8
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2019-07-19 10:27:44 +0530

    Bug 549210 - [Regression][Cocoa] GC.drawImage draws transparent region into black once getImageData is invoked
    From Mac 10.14 onwards NSAlphaNonpremultipliedBitmapFormat does not work with images with transparency. Removed thos attribute during creation of blank image with tranparency
    
    Change-Id: I529a626ce942fb8b2dce27c08a2053d6df28fbe2
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/internal/graphics/ImageUtil.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/internal/graphics/ImageUtil.java
@@ -39,7 +39,7 @@ public class ImageUtil {
 		NSBitmapImageRep rep;
 		NSImage imgHandle= image.handle;
 		rep = (NSBitmapImageRep)new NSBitmapImageRep().alloc();
-		rep = rep.initWithBitmapDataPlanes(0, (int) targetSize.width, (int) targetSize.height, 8, 3, false, false, OS.NSDeviceRGBColorSpace, OS.NSAlphaFirstBitmapFormat | OS.NSAlphaNonpremultipliedBitmapFormat, (int) targetSize.width * 4, 32);
+		rep = rep.initWithBitmapDataPlanes(0, (int) targetSize.width, (int) targetSize.height, 8, 4, true, false, OS.NSDeviceRGBColorSpace, OS.NSAlphaFirstBitmapFormat, (int) targetSize.width * 4, 32);
 		C.memset(rep.bitmapData(), 0xFF, (int) targetSize.width * (int)targetSize.height * 4);
 		NSGraphicsContext context = NSGraphicsContext.graphicsContextWithBitmapImageRep(rep);
 		NSGraphicsContext.static_saveGraphicsState();
