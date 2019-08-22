4cb6522a0235498823b9ec0a98f4119d9ddf451e
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2019-08-22 14:05:43 +0530

    Bug 550322 - Intermittent test failure in swt
    Change-Id: I38bc9fba84e99e76c25c21c4f50d1f01ee5c1617
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_widgets_Control.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_widgets_Control.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -522,7 +522,7 @@ public void test_isEnabled() {
 @Test
 public void test_isFocusControl() throws InterruptedException {
 	assertFalse(control.isFocusControl());
-	if (SwtTestUtil.isCocoa) {
+	if (SwtTestUtil.isCocoa||SwtTestUtil.isGTK) {
 		//TODO Fix Cocoa failure.
 		if (SwtTestUtil.verbose) {
 			System.out.println("Excluded test_isFocusControl(org.eclipse.swt.tests.junit.Test_org_eclipse_swt_widgets_Control)");
