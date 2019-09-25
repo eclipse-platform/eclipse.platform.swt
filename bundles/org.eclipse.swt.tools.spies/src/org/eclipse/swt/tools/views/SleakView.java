529d8872ff5d3e86f413c4660cf2831ab890d5bd
Lars Vogel <Lars.Vogel@vogella.com>
2019-09-25 09:42:20 +0200

    Bug 437558 - Sleak not working with RCP e4
    Converts the sleak view to an e4 view. It is still available under Show
    View for the IDE and 3x RCP customer. The model element is set to not
    persist so that the view will not restored between restarts.
    
    Change-Id: I47f6b689f2f9492c8b4417051cdba8407e75e29b
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.swt.tools.spies/src/org/eclipse/swt/tools/views/SleakView.java
+++ bundles/org.eclipse.swt.tools.spies/src/org/eclipse/swt/tools/views/SleakView.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2013 IBM Corporation and others.
+ * Copyright (c) 2004, 2019 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,30 +10,33 @@
  *
  * Contributors:
  *     IBM Corporation - initial API and implementation
+ *     Lars Vogel <Lars.Vogel@vogella.com> - Converted to e4 view
  *******************************************************************************/
 package org.eclipse.swt.tools.views;
 
+import javax.annotation.*;
+
+import org.eclipse.e4.ui.di.*;
 import org.eclipse.swt.*;
 import org.eclipse.swt.tools.internal.*;
 import org.eclipse.swt.widgets.*;
-import org.eclipse.ui.part.*;
 
 /**
  * Sleak view to trace native handler allocation
  */
 
-public class SleakView extends ViewPart {
+public class SleakView {
 
 	private Composite composite;
 
-	@Override
+	@PostConstruct
 	public void createPartControl(Composite parent) {
 		composite = new Composite(parent, SWT.NONE);
 		Sleak sleak = new Sleak ();
 		sleak.create(composite);
 	}
 
-	@Override
+	@Focus
 	public void setFocus() {
 		composite.setFocus();
 	}
