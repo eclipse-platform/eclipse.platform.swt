2822ec713a601487babebbb3950746a13446ca61
Lars Vogel <Lars.Vogel@vogella.com>
2019-09-25 09:20:09 +0200

    Bug 437558 - Cleanup of Sleak view
    Includes 4.14 version updates for the plug-in Bumping the minor version
    as the next commit moves a 3x view to an 4.x view
    
    Change-Id: I07b99c989d536785d255b1a3507860914131bf2e
    Signed-off-by: Lars Vogel <Lars.Vogel@vogella.com>

--- bundles/org.eclipse.swt.tools.spies/src/org/eclipse/swt/tools/views/SleakView.java
+++ bundles/org.eclipse.swt.tools.spies/src/org/eclipse/swt/tools/views/SleakView.java
@@ -19,51 +19,23 @@ import org.eclipse.swt.widgets.*;
 import org.eclipse.ui.part.*;
 
 /**
- * This sample class demonstrates how to plug-in a new
- * workbench view. The view shows data obtained from the
- * model. The sample creates a dummy model on the fly,
- * but a real implementation would connect to the model
- * available either in this or another plug-in (e.g. the workspace).
- * The view is connected to the model using a content provider.
- * <p>
- * The view uses a label provider to define how model
- * objects should be presented in the view. Each
- * view can present the same model objects using
- * different labels and icons, if needed. Alternatively,
- * a single label provider can be shared between views
- * in order to ensure that objects of the same type are
- * presented in the same way everywhere.
- * <p>
+ * Sleak view to trace native handler allocation
  */
 
 public class SleakView extends ViewPart {
 
-	Composite parent = null;
-	Sleak sleak = null;
+	private Composite composite;
 
-	/**
-	 * The constructor.
-	 */
-	public SleakView() {
-	}
-
-	/**
-	 * This is a callback that will allow us
-	 * to create the viewer and initialize it.
-	 */
 	@Override
 	public void createPartControl(Composite parent) {
-		this.parent = new Composite(parent, SWT.NONE);
-		sleak = new Sleak ();
-		sleak.create(this.parent);
+		composite = new Composite(parent, SWT.NONE);
+		Sleak sleak = new Sleak ();
+		sleak.create(composite);
 	}
 
-	/**
-	 * Passing the focus request to the viewer's control.
-	 */
 	@Override
 	public void setFocus() {
-		parent.setFocus();
+		composite.setFocus();
 	}
 
 }
