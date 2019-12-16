eclipse-platform/eclipse.platform.swt

commit c1c3699c9190461b6b64f6c2672c5befd67c3f0c

Bug 553678: Update GTK min version to 3.14
Remove support for versions of GTK3 < 3.14.0.

Change-Id: I0c3306a5c21d30710d2d16e9fe34879674e45fc0
Signed-off-by: Eric Williams <ericwill@redhat.com>

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -1409,14 +1409,10 @@ GdkRGBA getContextBackgroundGdkRGBA () {
 
 @Override
 GdkRGBA getContextColorGdkRGBA () {
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0)) {
-		if (foreground != null) {
-			return foreground;
-		} else {
-			return display.COLOR_LIST_FOREGROUND_RGBA;
-		}
+	if (foreground != null) {
+		return foreground;
 	} else {
-		return super.getContextColorGdkRGBA ();
+		return display.COLOR_LIST_FOREGROUND_RGBA;
 	}
 }
 
@@ -2302,7 +2298,7 @@ long gtk_draw (long widget, long cairo) {
 	 * If the table was resized since the last paint, we ignore this draw request
 	 * and queue another draw request so that the pixel cache is properly invalidated.
 	 */
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0) && ownerDraw && haveBoundsChanged) {
+	if (ownerDraw && haveBoundsChanged) {
 		GTK.gtk_widget_queue_draw(handle);
 		return 0;
 	}
@@ -2974,7 +2970,7 @@ void rendererRender (long cell, long cr, long snapshot, long widget, long backgr
 	}
 	if (item != null) {
 		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) ||
-				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell) || GTK.GTK_VERSION > OS.VERSION(3, 13, 0)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
+				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
 			drawFlags = (int)flags;
 			drawState = SWT.FOREGROUND;
 			long [] ptr = new long [1];
@@ -3405,21 +3401,16 @@ void setBackgroundGdkRGBA (long context, long handle, GdkRGBA rgba) {
 		background = rgba;
 	}
 	GdkRGBA selectedBackground = display.getSystemColor(SWT.COLOR_LIST_SELECTION).handle;
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0)) {
-		String name = GTK.GTK_VERSION >= OS.VERSION(3, 20, 0) ? "treeview" : "GtkTreeView";
-		String css = name + " {background-color: " + display.gtk_rgba_to_css_string(background) + ";}\n"
-				+ name + ":selected {background-color: " + display.gtk_rgba_to_css_string(selectedBackground) + ";}";
+	String name = GTK.GTK_VERSION >= OS.VERSION(3, 20, 0) ? "treeview" : "GtkTreeView";
+	String css = name + " {background-color: " + display.gtk_rgba_to_css_string(background) + ";}\n"
+			+ name + ":selected {background-color: " + display.gtk_rgba_to_css_string(selectedBackground) + ";}";
 
 		// Cache background color
 		cssBackground = css;
 
 		// Apply background color and any foreground color
-		String finalCss = display.gtk_css_create_css_color_string (cssBackground, cssForeground, SWT.BACKGROUND);
-		gtk_css_provider_load_from_css(context, finalCss);
-	} else {
-		super.setBackgroundGdkRGBA(context, handle, rgba);
-		GTK.gtk_widget_override_background_color(handle, GTK.GTK_STATE_FLAG_SELECTED, selectedBackground);
-	}
+	String finalCss = display.gtk_css_create_css_color_string (cssBackground, cssForeground, SWT.BACKGROUND);
+	gtk_css_provider_load_from_css(context, finalCss);
 }
 
 @Override
@@ -3505,13 +3496,9 @@ void setFontDescription (long font) {
 
 @Override
 void setForegroundGdkRGBA (GdkRGBA rgba) {
-	if (GTK.GTK_VERSION >= OS.VERSION (3, 14, 0)) {
-		foreground = rgba;
-		GdkRGBA toSet = rgba == null ? display.COLOR_LIST_FOREGROUND_RGBA : rgba;
-		setForegroundGdkRGBA (handle, toSet);
-	} else {
-		super.setForegroundGdkRGBA(rgba);
-	}
+	foreground = rgba;
+	GdkRGBA toSet = rgba == null ? display.COLOR_LIST_FOREGROUND_RGBA : rgba;
+	setForegroundGdkRGBA (handle, toSet);
 }
 
 /**
@@ -3766,10 +3753,8 @@ void setParentGdkResource (Control child) {
 	} else {
 		long parentGdkWindow = eventWindow ();
 		GTK.gtk_widget_set_parent_window (child.topHandle(), parentGdkWindow);
-		if (GTK.GTK_VERSION >= OS.VERSION(3, 10, 0)) {
-			hasChildren = true;
-			connectFixedHandleDraw();
-		}
+		hasChildren = true;
+		connectFixedHandleDraw();
 	}
 }
 
@@ -4235,7 +4220,7 @@ Point resizeCalculationsGTK3 (long widget, int width, int height) {
 	 * In the error case, the SWT fixed which contains the table still resizes as expected,
 	 * and the horizontal scrollbar is only partially visible so that it doesn't overlap with table headers.
 	 */
-	if (widget == scrolledHandle && GTK.GTK_VERSION >= OS.VERSION(3, 14, 0) && getHeaderVisible()) {
+	if (widget == scrolledHandle && getHeaderVisible()) {
 		int hScrollBarHeight = hScrollBarWidth(); // this actually returns height
 		if (hScrollBarHeight > 0) {
 			sizes.y = Math.max(sizes.y, getHeaderHeight() + hScrollBarHeight + (getBorderWidth() * 2));

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -1480,14 +1480,10 @@ GdkRGBA getContextBackgroundGdkRGBA () {
 
 @Override
 GdkRGBA getContextColorGdkRGBA () {
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0)) {
-		if (foreground != null) {
-			return foreground;
-		} else {
-			return display.COLOR_LIST_FOREGROUND_RGBA;
-		}
+	if (foreground != null) {
+		return foreground;
 	} else {
-		return super.getContextColorGdkRGBA ();
+		return display.COLOR_LIST_FOREGROUND_RGBA;
 	}
 }
 
@@ -2413,7 +2409,7 @@ long gtk_draw (long widget, long cairo) {
 	 * If the tree was resized since the last paint, we ignore this draw request
 	 * and queue another draw request so that the pixel cache is properly invalidated.
 	 */
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0) && ownerDraw && haveBoundsChanged) {
+	if (ownerDraw && haveBoundsChanged) {
 		GTK.gtk_widget_queue_draw(handle);
 		return 0;
 	}
@@ -3099,7 +3095,7 @@ void rendererRender (long cell, long cr, long snapshot, long widget, long backgr
 	}
 	if (item != null) {
 		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) ||
-				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell) || GTK.GTK_VERSION > OS.VERSION(3, 13, 0)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
+				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
 			drawFlags = (int)flags;
 			drawState = SWT.FOREGROUND;
 			long [] ptr = new long [1];
@@ -3511,21 +3507,16 @@ void setBackgroundGdkRGBA (long context, long handle, GdkRGBA rgba) {
 		background = rgba;
 	}
 	GdkRGBA selectedBackground = display.getSystemColor(SWT.COLOR_LIST_SELECTION).handle;
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0)) {
-		String name = GTK.GTK_VERSION >= OS.VERSION(3, 20, 0) ? "treeview" : "GtkTreeView";
-		String css = name + " {background-color: " + display.gtk_rgba_to_css_string(background) + ";}\n"
-				+ name + ":selected {background-color: " + display.gtk_rgba_to_css_string(selectedBackground) + ";}";
+	String name = GTK.GTK_VERSION >= OS.VERSION(3, 20, 0) ? "treeview" : "GtkTreeView";
+	String css = name + " {background-color: " + display.gtk_rgba_to_css_string(background) + ";}\n"
+			+ name + ":selected {background-color: " + display.gtk_rgba_to_css_string(selectedBackground) + ";}";
 
 		// Cache background color
 		cssBackground = css;
 
 		// Apply background color and any foreground color
-		String finalCss = display.gtk_css_create_css_color_string (cssBackground, cssForeground, SWT.BACKGROUND);
-		gtk_css_provider_load_from_css(context, finalCss);
-	} else {
-		super.setBackgroundGdkRGBA(context, handle, rgba);
-		GTK.gtk_widget_override_background_color(handle, GTK.GTK_STATE_FLAG_SELECTED, selectedBackground);
-	}
+	String finalCss = display.gtk_css_create_css_color_string (cssBackground, cssForeground, SWT.BACKGROUND);
+	gtk_css_provider_load_from_css(context, finalCss);
 }
 
 @Override
@@ -3612,13 +3603,9 @@ void setFontDescription (long font) {
 
 @Override
 void setForegroundGdkRGBA (GdkRGBA rgba) {
-	if (GTK.GTK_VERSION >= OS.VERSION (3, 14, 0)) {
-		foreground = rgba;
-		GdkRGBA toSet = rgba == null ? display.COLOR_LIST_FOREGROUND_RGBA : rgba;
-		setForegroundGdkRGBA (handle, toSet);
-	} else {
-		super.setForegroundGdkRGBA(rgba);
-	}
+	foreground = rgba;
+	GdkRGBA toSet = rgba == null ? display.COLOR_LIST_FOREGROUND_RGBA : rgba;
+	setForegroundGdkRGBA (handle, toSet);
 }
 
 /**
@@ -3835,10 +3822,8 @@ void setParentGdkResource (Control child) {
 	} else {
 		long parentGdkWindow = eventWindow ();
 		GTK.gtk_widget_set_parent_window (child.topHandle(), parentGdkWindow);
-		if (GTK.GTK_VERSION >= OS.VERSION(3, 10, 0)) {
-			hasChildren = true;
-			connectFixedHandleDraw();
-		}
+		hasChildren = true;
+		connectFixedHandleDraw();
 	}
 }
 
@@ -4214,7 +4199,7 @@ Point resizeCalculationsGTK3 (long widget, int width, int height) {
 	 * In the error case, the SWT fixed which contains the tree still resizes as expected,
 	 * and the horizontal scrollbar is only partially visible so that it doesn't overlap with tree headers.
 	 */
-	if (widget == scrolledHandle && GTK.GTK_VERSION >= OS.VERSION(3, 14, 0) && getHeaderVisible()) {
+	if (widget == scrolledHandle && getHeaderVisible()) {
 		int hScrollBarHeight = hScrollBarWidth(); // this actually returns height
 		if (hScrollBarHeight > 0) {
 			sizes.y = Math.max(sizes.y, getHeaderHeight() + hScrollBarHeight + (getBorderWidth() * 2));

bundles/org.eclipse.swt/Eclipse SWT Drag and Drop/gtk/org/eclipse/swt/dnd/DragSource.java
@@ -325,7 +325,7 @@ void dragBegin(long widget, long context) {
 	 * When we recieve the signal from GTK of DragBegin, we will
 	 * notify SWT that a drag has occurred.
 	 */
-	if (GTK.GTK_VERSION >= OS.VERSION(3, 14, 0) && this.control instanceof Text) {
+	if (this.control instanceof Text) {
 		DNDEvent event = new DNDEvent();
 		Display display = Display.getCurrent();
 		Point loc = display.getCursorLocation();

bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/os.c
@@ -12563,50 +12563,6 @@ JNIEXPORT jboolean JNICALL GTK_NATIVE(_1gtk_1widget_1mnemonic_1activate)
 }
 #endif
 
-#ifndef NO__1gtk_1widget_1override_1background_1color
-JNIEXPORT void JNICALL GTK_NATIVE(_1gtk_1widget_1override_1background_1color)
-	(JNIEnv *env, jclass that, jlong arg0, jint arg1, jobject arg2)
-{
-	GdkRGBA _arg2, *lparg2=NULL;
-	GTK_NATIVE_ENTER(env, that, _1gtk_1widget_1override_1background_1color_FUNC);
-	if (arg2) if ((lparg2 = getGdkRGBAFields(env, arg2, &_arg2)) == NULL) goto fail;
-/*
-	gtk_widget_override_background_color(arg0, arg1, lparg2);
-*/
-	{
-		GTK_LOAD_FUNCTION(fp, gtk_widget_override_background_color)
-		if (fp) {
-			((void (CALLING_CONVENTION*)(jlong, jint, GdkRGBA *))fp)(arg0, arg1, lparg2);
-		}
-	}
-fail:
-	if (arg2 && lparg2) setGdkRGBAFields(env, arg2, lparg2);
-	GTK_NATIVE_EXIT(env, that, _1gtk_1widget_1override_1background_1color_FUNC);
-}
-#endif
-
-#ifndef NO__1gtk_1widget_1override_1color
-JNIEXPORT void JNICALL GTK_NATIVE(_1gtk_1widget_1override_1color)
-	(JNIEnv *env, jclass that, jlong arg0, jint arg1, jobject arg2)
-{
-	GdkRGBA _arg2, *lparg2=NULL;
-	GTK_NATIVE_ENTER(env, that, _1gtk_1widget_1override_1color_FUNC);
-	if (arg2) if ((lparg2 = getGdkRGBAFields(env, arg2, &_arg2)) == NULL) goto fail;
-/*
-	gtk_widget_override_color(arg0, arg1, lparg2);
-*/
-	{
-		GTK_LOAD_FUNCTION(fp, gtk_widget_override_color)
-		if (fp) {
-			((void (CALLING_CONVENTION*)(jlong, jint, GdkRGBA *))fp)(arg0, arg1, lparg2);
-		}
-	}
-fail:
-	if (arg2 && lparg2) setGdkRGBAFields(env, arg2, lparg2);
-	GTK_NATIVE_EXIT(env, that, _1gtk_1widget_1override_1color_FUNC);
-}
-#endif
-
 #ifndef NO__1gtk_1widget_1override_1font
 JNIEXPORT void JNICALL GTK_NATIVE(_1gtk_1widget_1override_1font)
 	(JNIEnv *env, jclass that, jlong arg0, jlong arg1)
@@ -15032,18 +14988,6 @@ JNIEXPORT jlong JNICALL OS_NATIVE(_1g_1getenv)
 fail:
 	if (arg0 && lparg0) (*env)->ReleaseByteArrayElements(env, arg0, lparg0, JNI_ABORT);
 	OS_NATIVE_EXIT(env, that, _1g_1getenv_FUNC);
-	return rc;
-}
-#endif
-
-#ifndef NO__1g_1hash_1table_1get_1values
-JNIEXPORT jlong JNICALL OS_NATIVE(_1g_1hash_1table_1get_1values)
-	(JNIEnv *env, jclass that, jlong arg0)
-{
-	jlong rc = 0;
-	OS_NATIVE_ENTER(env, that, _1g_1hash_1table_1get_1values_FUNC);
-	rc = (jlong)g_hash_table_get_values((GHashTable *)arg0);
-	OS_NATIVE_EXIT(env, that, _1g_1hash_1table_1get_1values_FUNC);
 	return rc;
 }
 #endif

bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/os_stats.c
@@ -994,8 +994,6 @@ char * GTK_nativeFunctionNames[] = {
 	"_1gtk_1widget_1map",
 	"_1gtk_1widget_1measure",
 	"_1gtk_1widget_1mnemonic_1activate",
-	"_1gtk_1widget_1override_1background_1color",
-	"_1gtk_1widget_1override_1color",
 	"_1gtk_1widget_1override_1font",
 	"_1gtk_1widget_1queue_1draw",
 	"_1gtk_1widget_1queue_1resize",
@@ -1243,7 +1241,6 @@ char * OS_nativeFunctionNames[] = {
 	"_1g_1filename_1to_1utf8",
 	"_1g_1get_1current_1time",
 	"_1g_1getenv",
-	"_1g_1hash_1table_1get_1values",
 	"_1g_1icon_1new_1for_1string",
 	"_1g_1icon_1to_1string",
 	"_1g_1idle_1add",

bundles/org.eclipse.swt/Eclipse SWT PI/gtk/library/os_stats.h
@@ -992,8 +992,6 @@ typedef enum {
 	_1gtk_1widget_1map_FUNC,
 	_1gtk_1widget_1measure_FUNC,
 	_1gtk_1widget_1mnemonic_1activate_FUNC,
-	_1gtk_1widget_1override_1background_1color_FUNC,
-	_1gtk_1widget_1override_1color_FUNC,
 	_1gtk_1widget_1override_1font_FUNC,
 	_1gtk_1widget_1queue_1draw_FUNC,
 	_1gtk_1widget_1queue_1resize_FUNC,
@@ -1217,7 +1215,6 @@ typedef enum {
 	_1g_1filename_1to_1utf8_FUNC,
 	_1g_1get_1current_1time_FUNC,
 	_1g_1getenv_FUNC,
-	_1g_1hash_1table_1get_1values_FUNC,
 	_1g_1icon_1new_1for_1string_FUNC,
 	_1g_1icon_1to_1string_FUNC,
 	_1g_1idle_1add_FUNC,

bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/GTK.java
@@ -8927,32 +8927,6 @@ public class GTK extends OS {
 	/**
 	 * @method flags=dynamic
 	 */
-	public static final native void _gtk_widget_override_color(long widget, int state, GdkRGBA color);
-	/** [GTK3; 3.16 deprecated, replaced]*/
-	public static final void gtk_widget_override_color(long widget, int state, GdkRGBA color) {
-		lock.lock();
-		try {
-			_gtk_widget_override_color(widget, state, color);
-		} finally {
-			lock.unlock();
-		}
-	}
-	/**
-	 * @method flags=dynamic
-	 */
-	public static final native void _gtk_widget_override_background_color(long widget, int state, GdkRGBA color);
-	/** [GTK3; 3.16 deprecated, replaced] */
-	public static final void gtk_widget_override_background_color(long widget, int state, GdkRGBA color) {
-		lock.lock();
-		try {
-			_gtk_widget_override_background_color(widget, state, color);
-		} finally {
-			lock.unlock();
-		}
-	}
-	/**
-	 * @method flags=dynamic
-	 */
 	public static final native void _gtk_widget_override_font(long widget, long font);
 	/** [GTK3; 3.16 deprecated] */
 	public static final void gtk_widget_override_font(long widget, long font) {

bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk/OS.java
@@ -1764,18 +1764,6 @@ public static final void g_time_val_add(long result, long microseconds) {
 	}
 }
 /**
- * @param table cast=(GHashTable *)
- */
-public static final native long _g_hash_table_get_values(long table);
-public static final long g_hash_table_get_values(long table) {
-	lock.lock();
-	try {
-		return _g_hash_table_get_values(table);
-	} finally {
-		lock.unlock();
-	}
-}
-/**
  * @param function cast=(GSourceFunc)
  * @param data cast=(gpointer)
  */

commit 9d6e19785b417802f4cf1e21fe6cc92f739d6773

Bug 558351 - [gtk][Adwaita] Most of the fields in history view are empty
Fixed regression from commit c1c3699c9190461b6b64f6c2672c5befd67c3f0c.

The original condition (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell) ||
GTK.GTK_VERSION > OS.VERSION(3, 13, 0)) should always evaluate to "true"
now with GTK >= 3.14, but it was left to be
GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell), which can be "false".

Change-Id: I07d29c161d8653dc435b7e4892554da7601b446c
Signed-off-by: Andrey Loskutov <loskutov@gmx.de>

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Table.java
@@ -2969,8 +2969,7 @@ void rendererRender (long cell, long cr, long snapshot, long widget, long backgr
 		}
 	}
 	if (item != null) {
-		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) ||
-				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
+		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) || (columnIndex != 0 || (style & SWT.CHECK) == 0)) {
 			drawFlags = (int)flags;
 			drawState = SWT.FOREGROUND;
 			long [] ptr = new long [1];

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -3094,8 +3094,7 @@ void rendererRender (long cell, long cr, long snapshot, long widget, long backgr
 		}
 	}
 	if (item != null) {
-		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) ||
-				( (GTK.GTK_IS_CELL_RENDERER_PIXBUF (cell)) && (columnIndex != 0 || (style & SWT.CHECK) == 0))) {
+		if (GTK.GTK_IS_CELL_RENDERER_TOGGLE (cell) || ( columnIndex != 0 || (style & SWT.CHECK) == 0)) {
 			drawFlags = (int)flags;
 			drawState = SWT.FOREGROUND;
 			long [] ptr = new long [1];