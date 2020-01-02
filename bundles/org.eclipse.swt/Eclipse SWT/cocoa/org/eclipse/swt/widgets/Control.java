e0d51a44f0793dd7e3e39d80ba2d67fb7fce990b
Lakshmi Shanmugam <lshanmug@in.ibm.com>
2020-01-02 15:00:17 +0530

    Bug 558678 - Control is flipped vertically when saved as image
    The image from Control.print() gets flipped on macOS 10.13 onwards,
    added a OS version check to handle this.
    
    Change-Id: I6f8c8bff021fcd79e262a77cc3a602edf97d9c8c

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Control.java
@@ -2817,14 +2817,18 @@ public boolean print (GC gc) {
 	if (gc == null) error (SWT.ERROR_NULL_ARGUMENT);
 	if (gc.isDisposed ()) error (SWT.ERROR_INVALID_ARGUMENT);
 
-	NSGraphicsContext.static_saveGraphicsState();
-	NSGraphicsContext.setCurrentContext(gc.handle);
-	NSAffineTransform transform = NSAffineTransform.transform ();
-	transform.translateXBy (0, view.bounds().height);
-	transform.scaleXBy (1, -1);
-	transform.concat ();
-	view.displayRectIgnoringOpacity(view.bounds(), gc.handle);
-	NSGraphicsContext.static_restoreGraphicsState();
+	if (OS.VERSION < OS.VERSION(10, 13, 0)) {
+		NSGraphicsContext.static_saveGraphicsState();
+		NSGraphicsContext.setCurrentContext(gc.handle);
+		NSAffineTransform transform = NSAffineTransform.transform ();
+		transform.translateXBy (0, view.bounds().height);
+		transform.scaleXBy (1, -1);
+		transform.concat ();
+		view.displayRectIgnoringOpacity(view.bounds(), gc.handle);
+		NSGraphicsContext.static_restoreGraphicsState();
+	} else {
+		view.displayRectIgnoringOpacity (view.bounds (), gc.handle);
+	}
 	return true;
 }
 
