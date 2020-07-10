79791e2f782fd2952a3116e4b0695e4fb41b32aa
Niraj Modi <niraj.modi@in.ibm.com>
2020-07-10 17:18:05 +0530

    Bug 560190 part3 - [Win32] Remove WebKit Browser backend - Fixing the SWT_BUILDDIR path pointing to local path instead of W:
    Change-Id: Idc5930b67f9f8cac85f984f76778e2cca43d6350
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -20,7 +20,7 @@ echo
 echo INFO Starting build of binaries. Detailed system setup instructions can be found in /Readme.Win32.md
 
 @rem SWT_BUILDDIR defaults to "W:" on the SWT Windows native build infrastructure machine.
-IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=C:\swt-builddir"
+IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=W:"
 echo SWT build dir: %SWT_BUILDDIR%
 
 IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft\Visual Studio\2017\"
