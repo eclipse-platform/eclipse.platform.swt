1dc73fb2effeb705a1a411c4d4b0a71de5a5230a
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2020-08-14 04:43:44 -0400

    Revert "Bug 564453 - Test failures in SWT on Mac"
    This reverts commit c732444ac6c69c3b1d9d2abbb19afa8ac9a2d758.
    
    Reason for revert: Trying out the screnshot code
    
    Change-Id: I00f3645e01f51d18d06b35f0e712087a1f46154a

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_widgets_Tree.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_widgets_Tree.java
@@ -33,6 +33,7 @@ import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.swt.widgets.TreeItem;
+import org.eclipse.test.Screenshots;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -934,9 +935,6 @@ public void test_Virtual() {
 			top[0] = item;
 			item.setText("top");
 		} else {
-			if (top[0] == null) {
-				top[0] = tree.getItem(0);
-			}
 			if (top[0] != null) {
 				int index = top[0].indexOf(item);
 				item.setText("Item " + index);
@@ -962,6 +960,12 @@ public void test_Virtual() {
 				e.printStackTrace();
 			}
 		}
+	}
+	// temp code to capture screenshot
+	if (SwtTestUtil.isCocoa) {
+		Screenshots.takeScreenshot(getClass(), testName.getMethodName());
+		// check if setData is called for root item
+		assertTrue("SetData not called for top item", top[0] != null);
 	}
 
 	// the "* 3" allows some surplus for platforms that pre-fetch items to improve scrolling performance:
