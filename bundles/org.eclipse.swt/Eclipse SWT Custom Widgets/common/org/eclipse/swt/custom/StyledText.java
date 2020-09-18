5b49b58b536eea757115f6fde58a865a06d44d0a
Andrew Obuchowicz <andrew@aobuchow.com>
2020-09-18 12:36:22 +0530

    Bug 563572 - org.eclipse.swt.custom.StyledText gets a blinking cursor when READ_ONLY
    Change-Id: Id341f9ef91d77b47167b2146d79d29cce8a3f8ec
    Signed-off-by: Andrew Obuchowicz <andrew@aobuchow.com>

--- bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
+++ bundles/org.eclipse.swt/Eclipse SWT Custom Widgets/common/org/eclipse/swt/custom/StyledText.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2018 IBM Corporation and others.
+ * Copyright (c) 2000, 2020 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10839,7 +10839,7 @@ public void showSelection() {
 void updateCaretVisibility() {
 	Caret caret = getCaret();
 	if (caret != null) {
-		if (blockSelection && blockXLocation != -1) {
+		if (blockSelection && blockXLocation != -1 || !editable) {
 			caret.setVisible(false);
 		} else {
 			Point location = caret.getLocation();
