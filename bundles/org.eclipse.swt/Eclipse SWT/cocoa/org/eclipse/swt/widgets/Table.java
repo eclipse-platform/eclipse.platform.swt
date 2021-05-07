e9b0c06b846901cf9c3fecafa3ac70997f0119d5
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-05-07 13:38:32 +0530

    Bug 568383 - [Big Sur] Tree text is shortened with Ellipsis
    Change-Id: I78f7138aaa31398d5fa5019a8398c576babd30c2
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.swt/+/180344

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Table.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Table.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2017 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -593,7 +593,7 @@ void createHandle () {
 	firstColumn.headerCell ().setTitle (str);
 	widget.addTableColumn (firstColumn);
 	dataCell = (NSTextFieldCell)new SWTImageTextCell ().alloc ().init ();
-	dataCell.setLineBreakMode(OS.NSLineBreakByTruncatingTail);
+	dataCell.setLineBreakMode(OS.NSLineBreakByClipping);
 	firstColumn.setDataCell (dataCell);
 	widget.setHighlightedTableColumn(null);
 	scrollView = scrollWidget;
