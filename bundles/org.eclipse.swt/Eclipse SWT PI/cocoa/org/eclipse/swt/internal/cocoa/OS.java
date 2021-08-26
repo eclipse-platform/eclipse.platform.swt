60b860116994ee55655ddb9a2a76b6620f3c6f08
Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
2021-08-26 19:01:09 +0530

    Fix Compilation errors
    Change-Id: Ibb9c3e96efd049124768e62dc831cab39aa8c0d0
    Signed-off-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/OS.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/cocoa/org/eclipse/swt/internal/cocoa/OS.java
@@ -209,7 +209,7 @@ public class OS extends C {
 		 * Currently Big Sur OS version matches with 10.16 and not 11.0. This may be temporary.
 		 * Creating a method, so that it can be fixed in one place if/when this changes.
 		 */
-		return OS.VERSION >= OS.VERSION(10, 16, 0);
+		return OS.VERSION >= OS.VERSION_MMB(10, 16, 0);
 	}
 
 /** JNI natives */
