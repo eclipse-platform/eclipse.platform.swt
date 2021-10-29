617f81e68501c37ebc9353b28854c0801fabd02b
Niraj Modi <niraj.modi@in.ibm.com>
2021-10-29 19:54:25 +0530

    Bug 572308 - [Win32] Update SWT Windows native build scripts to work with latest VisualStudio2019/Win10 SDK - Back-porting latest build.bat to R4_8_maintenance branch
    Change-Id: Ie536bec705108e87a087ab53724989f84b59acf8
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.swt/+/187176

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/build.bat
@@ -1,9 +1,12 @@
 @rem ***************************************************************************
-@rem Copyright (c) 2000, 2018 IBM Corporation and others.
-@rem All rights reserved. This program and the accompanying materials
-@rem are made available under the terms of the Eclipse Public License v1.0
+@rem Copyright (c) 2000, 2021 IBM Corporation and others.
+@rem
+@rem This program and the accompanying materials
+@rem are made available under the terms of the Eclipse Public License 2.0
 @rem which accompanies this distribution, and is available at
-@rem http://www.eclipse.org/legal/epl-v10.html
+@rem https://www.eclipse.org/legal/epl-2.0/
+@rem
+@rem SPDX-License-Identifier: EPL-2.0
 @rem
 @rem Contributors:
 @rem      IBM Corporation - initial API and implementation
@@ -16,70 +19,81 @@
 echo
 echo INFO Starting build of binaries. Detailed system setup instructions can be found in /Readme.Win32.md
 
-@rem builddir used for 32 bit building with webkit. Not needed on 64 bit builds.
+@rem SWT_BUILDDIR defaults to "W:" on the SWT Windows native build infrastructure machine.
 IF "x.%SWT_BUILDDIR%"=="x." set "SWT_BUILDDIR=W:"
 echo SWT build dir: %SWT_BUILDDIR%
-IF "x.%MSSDK%"=="x." set "MSSDK=%ProgramFiles(x86)%\Windows Kits\10"
-echo Microsoft Windows10 SDK dir: %MSSDK%
-IF "x.%WEBKIT_DIR%"=="x." set "WEBKIT_DIR=%SWT_BUILDDIR%\WebKit\r72896"
-echo Webkit dir: %WEBKIT_DIR%
-IF "x.%WEBKIT_SUPPORT_DIR%"=="x." set "WEBKIT_SUPPORT_DIR=%SWT_BUILDDIR%\WebKit\SupportLibrary"
-echo Webkit SupportLibrary dir: %WEBKIT_SUPPORT_DIR%
 
+@rem Specify VisualStudio Edition: 'Community', 'Enterprise', 'Professional' etc.
+IF "x.%MSVC_EDITION%"=="x." set "MSVC_EDITION=Community"
 
-IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft\Visual Studio\2017\"
-IF NOT EXIST "%MSVC_HOME%" set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\2017"
+@rem Specify VisualStudio Version: '2017' or newer '2019'
+IF "x.%MSVC_VERSION%"=="x." set "MSVC_VERSION=2019"
+
+IF "x.%MSVC_HOME%"=="x." set "MSVC_HOME=%SWT_BUILDDIR%\Microsoft\Visual Studio\%MSVC_VERSION%\"
+IF NOT EXIST "%MSVC_HOME%" set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\%MSVC_VERSION%\BuildTools"
+IF NOT EXIST "%MSVC_HOME%" set "MSVC_HOME=%ProgramFiles(x86)%\Microsoft Visual Studio\%MSVC_VERSION%\%MSVC_EDITION%"
 IF EXIST "%MSVC_HOME%" (
-	echo "Microsoft Visual Studio 2017 dir: %MSVC_HOME%"
+	echo "Microsoft Visual Studio %MSVC_VERSION% dir: %MSVC_HOME%"
 ) ELSE (
-	echo "WARNING: Microsoft Visual Studio 2017 was not found."
+	echo "WARNING: Microsoft Visual Studio %MSVC_VERSION% was not found."
+    echo "     Refer steps for SWT Windows native setup: https://www.eclipse.org/swt/swt_win_native.php"
 )
 
-IF "x.%1"=="x.x86" GOTO X86
-IF "x.%1"=="x.x86_64" GOTO X86_64
-
-:X86
-IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86
-IF "x.%SWT_JAVA_HOME%"=="x." set "SWT_JAVA_HOME=%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x86"
-IF "x.%SWT_JAVA_HOME%"=="x." (
-    echo "WARNING: x86 Java JDK not found. Please set SWT_JAVA_HOME to your JDK directory."
-) ELSE (
-    echo "SWT_JAVA_HOME x86: %SWT_JAVA_HOME%"
+IF NOT "x.%1"=="x.x86_64" (
+	ECHO 32-bit builds are no longer supported.
+	EXIT /B 1
 )
-call "%MSVC_HOME%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
-IF x.%1==x.x86 shift
-GOTO MAKE
 
-:X86_64
 set PROCESSOR_ARCHITECTURE=AMD64
 IF "x.%OUTPUT_DIR%"=="x." set OUTPUT_DIR=..\..\..\org.eclipse.swt.win32.win32.x86_64
 
-IF "x.%SWT_JAVA_HOME%"=="x." (
-    IF exist "%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x64" (
-        set "SWT_JAVA_HOME=%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x64"
-    ) ELSE (
-        IF exist "C:\Program Files\Java\jdk*" (
-            echo "Found Java in C:\Program Files\Java\jdk, automatically setting SWT_JAVA_HOME to latest java"
-            for /d %%i in ("\Program Files\Java\jdk*") do set SWT_JAVA_HOME=%%i
-        )
-    )
-)
-IF "x.%SWT_JAVA_HOME%"=="x." (
+:: Search for a usable JDK
+:: -----------------------
+IF "%SWT_JAVA_HOME%"=="" ECHO 'SWT_JAVA_HOME' was not provided, auto-searching for JDK...
+:: Bug 572733: JDK path used on Azure build machines
+IF "%SWT_JAVA_HOME%"=="" CALL :TryToUseJdk "%ProgramFiles%\AdoptOpenJDK\jdk-8.0.292.10-hotspot"
+:: Bug 526802: Probably some kind of legacy build machine path
+IF "%SWT_JAVA_HOME%"=="" CALL :TryToUseJdk "%SWT_BUILDDIR%\Java\Oracle\jdk1.8.0-latest\x64"
+:: Search for generic JDKs so that user can build with little configuration
+:: Note that first found JDK wins, so sort them by order of preference.
+IF "%SWT_JAVA_HOME%"=="" CALL :TryToUseJdk "%ProgramFiles%\Java\jdk*"
+IF "%SWT_JAVA_HOME%"=="" CALL :TryToUseJdk "%ProgramFiles%\AdoptOpenJDK\jdk*"
+IF NOT EXIST "%SWT_JAVA_HOME%" (
     echo "WARNING: x64 Java JDK not found. Please set SWT_JAVA_HOME to your JDK directory."
+    echo "     Refer steps for SWT Windows native setup: https://www.eclipse.org/swt/swt_win_native.php"
 ) ELSE (
     echo "SWT_JAVA_HOME x64: %SWT_JAVA_HOME%"
 )
 
 set CFLAGS=-DJNI64
-call "%MSVC_HOME%\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call "%MSVC_HOME%\VC\Auxiliary\Build\vcvarsall.bat" x64
 shift
-GOTO MAKE
 
-:MAKE
 @rem if call to vcvarsall.bat (which sets up environment) silently fails, then provide advice to user.
 WHERE cl
 if %ERRORLEVEL% NEQ 0 (
     echo "WARNING: cl (Microsoft C compiler) not found on path. Please install Microsoft Visual Studio."
     echo "     If already installed, try launching eclipse from the 'Developer Command Prompt for VS' "
+    echo "     Refer steps for SWT Windows native setup: https://www.eclipse.org/swt/swt_win_native.php"
 )
 nmake -f make_win32.mak %1 %2 %3 %4 %5 %6 %7 %8 %9
+GOTO :EOF
+
+:TryToUseJdk
+SET TESTED_JDK_PATH_MASK=%~1
+:: Loop over all directories matching mask.
+:: Note that directories are iterated in alphabetical order and *last* hit will
+:: be selected in hopes to select the highest available JDK version.
+FOR /D %%I IN ("%TESTED_JDK_PATH_MASK%") DO (
+	IF NOT EXIST "%%~I" (
+		ECHO -- JDK '%%~I' doesn't exist on disk
+		GOTO :EOF
+	)
+	IF NOT EXIST "%%~I\include\jni.h" (
+		ECHO -- JDK '%%~I' is bad: no jni.h
+		GOTO :EOF
+	)
+	ECHO -- JDK '%%~I' looks good, selecting it
+	SET SWT_JAVA_HOME=%%~I
+)
+GOTO :EOF
