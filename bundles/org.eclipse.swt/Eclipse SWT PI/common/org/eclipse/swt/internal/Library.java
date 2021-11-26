195b77c0504d150cbd375d39e67b91038fbabf1a
Niraj Modi <niraj.modi@in.ibm.com>
2021-11-26 13:02:51 +0530

    Bug 577439 - Configure SWT build scripts for 4.23
    Change-Id: Iba7d89732ce364b92d5c83980aaef61314a40d7d
    Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>
    Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.swt/+/188162
    Tested-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>
    Reviewed-by: Sravan Kumar Lakkimsetti <sravankumarl@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -32,12 +32,12 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 948;
+	static int MINOR_VERSION = 950;
 
 	/**
 	 * SWT revision number (must be >= 0)
 	 */
-	static int REVISION = 9;
+	static int REVISION = 1;
 
 	/**
 	 * The JAVA and SWT versions
