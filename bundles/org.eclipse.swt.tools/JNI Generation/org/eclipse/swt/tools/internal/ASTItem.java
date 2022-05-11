bb8d76a3ef89463daf08c5ae92f133fcdd980782
Александър Куртаков <akurtakov@gmail.com>
2022-05-11 16:28:31 +0300

    Update ASTItem.java

--- bundles/org.eclipse.swt.tools/JNI Generation/org/eclipse/swt/tools/internal/ASTItem.java
+++ bundles/org.eclipse.swt.tools/JNI Generation/org/eclipse/swt/tools/internal/ASTItem.java
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2013 IBM Corporation and others.
  *
- * This program and the accompanying materials
+ * This program and the accompanying materialsff
  * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-2.0/
