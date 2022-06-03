08730a5f8ee9121df44c1f646f80eaddf1243806
Niraj Modi <niraj.modi@in.ibm.com>
2022-06-03 10:58:52 +0530

    Configure SWT build scripts for 4.25 - fixes #187 Change-Id: I41998865bf26e555ee88a80af0f8e83413aa9ae4 Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -30,12 +30,12 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 952;
+	static int MINOR_VERSION = 954;
 
 	/**
 	 * SWT revision number (must be >= 0)
 	 */
-	static int REVISION = 11;
+	static int REVISION = 1;
 
 	/**
 	 * The JAVA and SWT versions
