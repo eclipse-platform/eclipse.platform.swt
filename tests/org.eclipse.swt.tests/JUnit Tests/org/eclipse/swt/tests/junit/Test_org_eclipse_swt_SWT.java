e4b45e41ddde965733ce1c7ed41b842f4a9fa6b3
Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>
2022-07-12 00:09:11 +0300

    Issue #251 - Unit test to ensure that GitHub tests currently built SWT
    Turns out, as of now, it tests published SWT instead, loading it from
    maven cache.
    
    Signed-off-by: Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_SWT.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_SWT.java
@@ -24,6 +24,13 @@ import org.eclipse.swt.SWTError;
 import org.eclipse.swt.SWTException;
 import org.junit.Test;
 
+import java.nio.file.Paths;
+import java.security.CodeSigner;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Automated Test Suite for class org.eclipse.swt.SWT
  *
@@ -115,6 +122,77 @@ public void test_getMessageLjava_lang_String() {
 		"invalid key did not return as itself",
 		"_NOT_FOUND_IN_PROPERTIES_", SWT.getMessage("_NOT_FOUND_IN_PROPERTIES_"));
 
+}
+
+private String pathFromClass(Class<?> classValue) {
+	String filePrefix = SwtTestUtil.isWindows ? "file:/" : "file:";
+
+	String url = classValue.getProtectionDomain().getCodeSource().getLocation().toString();
+	assertTrue(url.startsWith(filePrefix));
+
+	String urlPath = url.substring(filePrefix.length());
+	String path = Paths.get(urlPath).toAbsolutePath().toString();
+
+	// On Windows, use / for consistency
+	return path.replace('\\', '/');
+}
+
+private List<String> signersFromClass(Class<?> classValue) {
+	List<String> result = new ArrayList<>();
+
+	CodeSigner[] signers = classValue.getProtectionDomain().getCodeSource().getCodeSigners();
+	if (signers == null) {
+		return result;
+	}
+
+	for (CodeSigner signer : signers) {
+		Certificate cert = signer.getSignerCertPath().getCertificates().get(0);
+		if (!(cert instanceof X509Certificate)) {
+			continue;
+		}
+
+		X509Certificate x509cert = (X509Certificate)cert;
+		result.add(x509cert.getSubjectX500Principal().getName());
+	}
+
+	return result;
+}
+
+/**
+ * Ensure that SWT being tested was built together with the tests. This
+ * is a test designed for build scripts, such as the one that checks
+ * GitHub Pull Requests.
+ */
+@Test
+public void test_isLocal() {
+	// If you change default to NO, make sure that this test runs on GitHub
+	if (Boolean.getBoolean("org.eclipse.swt.tests.junit.disable.test_isLocal")) {
+		return;
+	}
+
+	String swtPath = pathFromClass(SWT.class);
+	String tstPath = pathFromClass(Test_org_eclipse_swt_SWT.class);
+	List<String> swtSigners = signersFromClass(SWT.class);
+
+	// If SWT is signed by Eclipse, that's a good sign that it wasn't built locally
+	for (String signer : swtSigners) {
+		if (signer.toLowerCase().contains("cn=eclipse.org")) {
+			fail("Tested SWT is signed by Eclipse: (SWT='" + swtPath + "' signer='" + signer + "')");
+		}
+	}
+
+	// It's hard to devise a condition that would hold for all possible
+	// locations of compiled classes. For example, my own project uses
+	// output directories different from what Eclipse does by default.
+
+	// SWT shouldn't be published in maven. Note that SWT's local build
+	// may still be cached in maven. We observed the following paths:
+	// GitHub, bad:   $HOME/.m2/repository/p2/osgi/bundle/org.eclipse.swt.gtk.linux.x86_64/3.121.0.v20220701-1002/org.eclipse.swt.gtk.linux.x86_64-3.121.0.v20220701-1002.jar
+	// Jenkins, good: $HOME/.m2/repository/org/eclipse/swt/org.eclipse.swt.gtk.linux.x86_64/3.121.0-SNAPSHOT/org.eclipse.swt.gtk.linux.x86_64-3.121.0-SNAPSHOT.jar
+	// The difference here is '/p2/osgi'.
+	if (swtPath.contains("/.m2/repository/p2/osgi/")) {
+		fail("Tested SWT path is on maven OSGI: (SWT='" + swtPath + "' Test='" + tstPath + "'");
+	}
 }
 
 @Test
