fed0767296eaad6d70d758ad533d1ea02d41ce88
Александър Куртаков <akurtakov@gmail.com>
2022-07-14 14:28:18 +0300

    Add a null check if MessageBox labels map has null values
    In case of the null value explicitly set by the user return the
    "default" button label. Implementation done for Gtk/Cocoa.
    Extra safety check for
    https://github.com/eclipse-platform/eclipse.platform.swt/issues/233

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/MessageBox.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/MessageBox.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2012 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -273,7 +273,7 @@ public int open () {
 }
 
 private NSString getLabelForButton(int buttonId, String messageId) {
-	if (labels !=null && labels.containsKey(buttonId)) {
+	if (labels !=null && labels.containsKey(buttonId) && labels.get(buttonId) != null) {
 		return NSString.stringWith(labels.get(buttonId));
 	}
 	return NSString.stringWith(SWT.getMessage(messageId));
