eclipse-platform/eclipse.platform.swt

commit 8def011fdd013543a5ea5513dacef5914c3c8662

GC#drawIcon draws *.ico(for some icons) with black background on Windows10 - Fixes #185
Change-Id: Idec2ddb0086a58c8d28c7616a628e2802d06ce56
Signed-off-by: Niraj Modi <niraj.modi@in.ibm.com>

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2020 IBM Corporation and others.
+ * Copyright (c) 2000, 2022 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -1797,8 +1797,9 @@ static long [] init(Device device, Image image, ImageData i) {
 					}
 					break;
 				case 32:
-					newDepth = 24;
-					newPalette = new PaletteData(0xFF, 0xFF00, 0xFF0000);
+					if (!(redMask == 0xFF00 && greenMask == 0xFF0000 && blueMask == 0xFF000000)) {
+						newPalette = new PaletteData(0xFF00, 0xFF0000, 0xFF000000);
+					}
 					break;
 				default:
 					SWT.error(SWT.ERROR_UNSUPPORTED_DEPTH);

tests/org.eclipse.swt.tests.win32/ManualTests/org/eclipse/swt/tests/win32/snippets/SWTIssue185.java
@@ -1,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2022 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.tests.win32.snippets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.program.Program;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+public class SWTIssue185 {
+
+	public static void main(String[] args) {
+		final Display display = new Display();
+
+		final List<Image> images = createImages(display, "zip", "jpg", "txt", "pdf", "png", "msi", "theme");
+
+		final Shell shell = new Shell(display);
+
+		shell.addListener(SWT.Paint, event -> {
+			int x = 0;
+			for (Image image : images) {
+				event.gc.drawImage(image, x, 0);
+				x += image.getImageData().width;
+				x += 10;
+			}
+		});
+
+		shell.setSize(400, 300);
+		shell.open();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+
+		for (Image image : images) {
+			image.dispose();
+		}
+
+		display.dispose();
+	}
+
+	private static List<Image> createImages(Display display, String... extensions) {
+		final List<Image> images = new ArrayList<>();
+		for (String extension : extensions) {
+			final Program program = Program.findProgram(extension);
+			if (program == null) {
+				continue;
+			}
+			final ImageData data = program.getImageData();
+			if (data == null) {
+				continue;
+			}
+			images.add(new Image(display, data));
+		}
+		return images;
+	}
+
+}

commit a62ea882f056b3df3f431796acc6f1eb6a7589c5

Issue #301 - [win32]: only allow alpha data in ImageData.data for icons
Fixes the regression introduced in PR #244 (commit 8def011fdd013543a5ea5513dacef5914c3c8662) which
also allowed alpha data in ImageData.data for bitmaps. Alpha data for bitmaps must be provided in
ImageData.alphaData. This ensures proper conversion of the image data to pre-multiplied alpha format
and aligns the behavior of Image initialization with the implementation for Linux.

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -1862,7 +1862,11 @@ static long [] init(Device device, Image image, ImageData i) {
 					}
 					break;
 				case 32:
-					if (!(redMask == 0xFF00 && greenMask == 0xFF0000 && blueMask == 0xFF000000)) {
+					if (i.getTransparencyType() != SWT.TRANSPARENCY_MASK) {
+						newDepth = 24;
+						newPalette = new PaletteData(0xFF, 0xFF00, 0xFF0000);
+					}
+					else if (!(redMask == 0xFF00 && greenMask == 0xFF0000 && blueMask == 0xFF000000)) {
 						newPalette = new PaletteData(0xFF00, 0xFF0000, 0xFF000000);
 					}
 					break;