eclipse-platform/eclipse.platform.swt

commit 17b4024759c8084afa680a06b7b2803463e84114

Bug 575787 - [win32] Optimize Tree.setItemCount
On Windows, Tree maintains its items with a single linked list. For this
reason, "find previous" operation is O(N) because it requires walking
from start of the list.

When inserting an item, it needs to be linked into the list, which
involves finding the previous item and updating its 'next' field, which
is O(N) for one item and O(N*N) for inserting N items.

The workaround is to bulk insert all items at the same position. This
means that if TreeItem had no children, inserting N items into it is
O(N) instead of O(N*N).

On top of that, this patch preallocates 'Tree.items[]' to hold exactly
the required number of items.

Resulting performance improvement on my machine:
                |      before patch     |      after patch      |
                +-----------+-----------+-----------+-----------+
                |   REGULAR |   VIRTUAL |   REGULAR |   VIRTUAL |
----------------+-----------+-----------+-----------+-----------+
   10_000 items |   0,44sec |   0,43sec |   0,07sec |   0,06sec |
  100_000 items | 113,83sec | 111,95sec |   0,70sec |   0,63sec |
1_000_000 items |  too long |  too long |   6,82sec |   6,28sec |

Change-Id: Ie9ae441948bf82f865ef63bb795709de0d89832a
Signed-off-by: Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>
Reviewed-on: https://git.eclipse.org/r/c/platform/eclipse.platform.swt/+/187659
Tested-by: Niraj Modi <niraj.modi@in.ibm.com>
Reviewed-by: Niraj Modi <niraj.modi@in.ibm.com>

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
@@ -2046,7 +2046,10 @@ void createItem (TreeColumn column, int index) {
 	}
 }
 
-// For fast bulk insert, see comments for TreeItem#TreeItem(TreeItem,int,int)
+/**
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
+ */
 void createItem (TreeItem item, long hParent, long hInsertAfter, long hItem) {
 	int id = -1;
 	if (item != null) {
@@ -2066,9 +2069,8 @@ void createItem (TreeItem item, long hParent, long hInsertAfter, long hItem) {
 				shrink = true;
 				length = Math.max (4, items.length * 3 / 2);
 			}
-			TreeItem [] newItems = new TreeItem [length];
-			System.arraycopy (items, 0, newItems, 0, items.length);
-			items = newItems;
+
+			itemsGrowArray (length);
 		}
 		lastID = id + 1;
 	}
@@ -2112,11 +2114,12 @@ void createItem (TreeItem item, long hParent, long hInsertAfter, long hItem) {
 	if (hFirstItem == hFirstIndexOf && itemCount != -1) itemCount++;
 	if (hItem == 0) {
 		/*
-		* Bug in Windows.  When a child item is added to a parent item
-		* that has no children outside of WM_NOTIFY with control code
-		* TVN_ITEMEXPANDED, the tree widget does not redraw the +/-
-		* indicator.  The fix is to detect the case when the first
-		* child is added to a visible parent item and redraw the parent.
+		* Bug in Windows.  When a child item is added to a collapsed
+		* parent item that has no children, Tree does not draw [-]
+		* indicator and the parent item continues to look as if it
+		* has no children. Reportedly this doesn't happen when item
+		* is added during WM_NOTIFY(TVN_ITEMEXPANDED). The workaround
+		* is to force redraw parent item.
 		*/
 		if (fixParent && (hParent != OS.TVI_ROOT)) {
 			if (getDrawing () && OS.IsWindowVisible (handle)) {
@@ -3851,6 +3854,22 @@ boolean isUseWsBorder () {
 	return true;
 }
 
+int itemsGetFreeCapacity() {
+	int count = 0;
+	for (TreeItem item : items) {
+		if (item == null)
+			count++;
+	}
+
+	return count;
+}
+
+void itemsGrowArray (int newCapacity) {
+	TreeItem [] newItems = new TreeItem [newCapacity];
+	System.arraycopy (items, 0, newItems, 0, items.length);
+	items = newItems;
+}
+
 void redrawSelection () {
 	if ((style & SWT.SINGLE) != 0) {
 		long hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CARET, 0);
@@ -4113,6 +4132,9 @@ public void setInsertMark (TreeItem item, boolean before) {
 
 /**
  * Sets the number of root-level items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param count the number of items
  *
@@ -4130,18 +4152,41 @@ public void setItemCount (int count) {
 }
 
 void setItemCount (int count, long hParent) {
-	long hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CHILD, hParent);
+	// Investigate existing items and decide what to do
+	long itemInsertAfter = 0;
+	int  numInserted = 0;
+	long itemDeleteFrom = 0;
+	{
+		// Iterate to position #count and find prev/next items at this position
+		int itemCount = 0;
+		long itemPrev = OS.TVI_FIRST;
+		long itemNext = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CHILD, hParent);
+		while (itemNext != 0 && itemCount < count)
+		{
+			itemPrev = itemNext;
+			itemNext = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, itemNext);
+			itemCount++;
+		}
+
+		if ((itemCount == count) && (itemNext == 0)) {
+			// Exactly 'count' items, no need to do anything.
+			return;
+		} else if (itemCount == count) {
+			// Too many items, going to delete some
+			itemDeleteFrom = itemNext;
+		} else if (itemNext == 0) {
+			// Counted all items, and there is not enough, going to insert some.
+			itemInsertAfter = itemPrev;
+			numInserted = count - itemCount;
+		}
+	}
 
 	boolean redraw = false;
 	if (OS.SendMessage (handle, OS.TVM_GETCOUNT, 0, 0) == 0) {
 		redraw = getDrawing () && OS.IsWindowVisible (handle);
 		if (redraw) OS.DefWindowProc (handle, OS.WM_SETREDRAW, 0, 0);
 	}
-	int itemCount = 0;
-	while (hItem != 0 && itemCount < count) {
-		hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, hItem);
-		itemCount++;
-	}
+
 	boolean expanded = false;
 	TVITEM tvItem = new TVITEM ();
 	tvItem.mask = OS.TVIF_HANDLE | OS.TVIF_PARAM;
@@ -4155,32 +4200,49 @@ void setItemCount (int count, long hParent) {
 		int state = (int)OS.SendMessage (handle, OS.TVM_GETITEMSTATE, hParent, OS.TVIS_EXPANDED);
 		expanded = (state & OS.TVIS_EXPANDED) != 0;
 	}
-	while (hItem != 0) {
-		tvItem.hItem = hItem;
-		OS.SendMessage (handle, OS.TVM_GETITEM, 0, tvItem);
-		hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, hItem);
-		TreeItem item = tvItem.lParam != -1 ? items [(int)tvItem.lParam] : null;
-		if (item != null && !item.isDisposed ()) {
-			item.dispose ();
-		} else {
-			releaseItem (tvItem.hItem, tvItem, false);
-			destroyItem (null, tvItem.hItem);
-		}
-	}
-	if ((style & SWT.VIRTUAL) != 0) {
-		for (int i=itemCount; i<count; i++) {
-			if (expanded) ignoreShrink = true;
-			createItem (null, hParent, OS.TVI_LAST, 0);
-			if (expanded) ignoreShrink = false;
+
+	if (itemDeleteFrom != 0) {
+		while (itemDeleteFrom != 0) {
+			tvItem.hItem = itemDeleteFrom;
+			OS.SendMessage (handle, OS.TVM_GETITEM, 0, tvItem);
+			itemDeleteFrom = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, itemDeleteFrom);
+			TreeItem item = tvItem.lParam != -1 ? items [(int)tvItem.lParam] : null;
+			if (item != null && !item.isDisposed ()) {
+				item.dispose ();
+			} else {
+				releaseItem (tvItem.hItem, tvItem, false);
+				destroyItem (null, tvItem.hItem);
+			}
 		}
 	} else {
-		shrink = true;
-		int extra = Math.max (4, (count + 3) / 4 * 4);
-		TreeItem [] newItems = new TreeItem [items.length + extra];
-		System.arraycopy (items, 0, newItems, 0, items.length);
-		items = newItems;
-		for (int i=itemCount; i<count; i++) {
-			new TreeItem (this, SWT.NONE, hParent, OS.TVI_LAST, 0);
+		// For performance reasons, reserve the necessary space in items[]
+		int freeCapacity = itemsGetFreeCapacity();
+		if (numInserted > freeCapacity)
+			itemsGrowArray (items.length + numInserted - freeCapacity);
+
+		// Note: on Windows, insert complexity is O(pos), so for performance
+		// reasons, all items are inserted at minimum possible position, that
+		// is, all at the same position.
+
+		if ((style & SWT.VIRTUAL) != 0) {
+			for (int i = 0; i < numInserted; i++) {
+				/*
+				 * Bug 206806: Windows sends 'TVN_GETDISPINFO' when item is
+				 * being inserted. This causes 'SWT.SetData' to be sent to
+				 * user code. If it happens to query 'getItemCount()', it will
+				 * get wrong number of items because we're still inserting. The
+				 * workaround is to temporarily suppress 'SWT.SetData'. Note
+				 * that the boolean flag is misleadingly used for multiple
+				 * purposes.
+				 */
+				if (expanded) ignoreShrink = true;
+				createItem (null, hParent, itemInsertAfter, 0);
+				if (expanded) ignoreShrink = false;
+			}
+		} else {
+			for (int i = 0; i < numInserted; i++) {
+				new TreeItem (this, SWT.NONE, hParent, itemInsertAfter, 0);
+			}
 		}
 	}
 	if (redraw) {

bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Tree.java
@@ -709,7 +709,10 @@ void createItem (TreeColumn column, int index) {
 	}
 }
 
-// For fast bulk insert, see comments for TreeItem#TreeItem(TreeItem,int,int)
+/**
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(org.eclipse.swt.widgets.Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
+ */
 void createItem (TreeItem item, TreeItem parentItem, int index) {
 	int count;
 	TreeItem [] items;
@@ -3054,6 +3057,9 @@ public void setHeaderVisible (boolean show) {
 
 /**
  * Sets the number of root-level items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(org.eclipse.swt.widgets.Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param count the number of items
  *

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Tree.java
@@ -988,7 +988,10 @@ void createItem (TreeColumn column, int index) {
 	updateHeaderCSS();
 }
 
-// For fast bulk insert, see comments for TreeItem#TreeItem(TreeItem,int,int)
+/**
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(org.eclipse.swt.widgets.Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
+ */
 void createItem (TreeItem item, long parentIter, int index) {
 	/*
 	 * Try to achieve maximum possible performance in bulk insert scenarios.
@@ -3496,6 +3499,9 @@ void setItemCount (long parentIter, int count) {
 
 /**
  * Sets the number of root-level items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(org.eclipse.swt.widgets.Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param count the number of items
  *

bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/TreeItem.java
@@ -67,7 +67,8 @@ public class TreeItem extends Item {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the tree.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(Tree,int,int)
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -96,6 +97,7 @@ public TreeItem (Tree parent, int style) {
  * <ol>
  * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
  * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
  * </ol>
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
@@ -124,7 +126,8 @@ public TreeItem (Tree parent, int style, int index) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the specified <code>TreeItem</code>.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(TreeItem,int,int)
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -149,12 +152,8 @@ public TreeItem (TreeItem parentItem, int style) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as <code>index</code> direct child of the specified <code>TreeItem</code>.
  * <p>
- * The fastest way to insert many items is:
- * <ol>
- * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
- * <li>Insert child items while parent item is collapsed</li>
- * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
- * </ol>
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -1397,6 +1396,12 @@ public void setImage (Image image) {
 
 /**
  * Sets the number of child items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is:
+ * <ol>
+ * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
+ * </ol>
  *
  * @param count the number of items
  *

bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/TreeItem.java
@@ -51,7 +51,8 @@ public class TreeItem extends Item {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the tree.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(Tree,int,int)
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -80,6 +81,7 @@ public TreeItem (Tree parent, int style) {
  * <ol>
  * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
  * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
  * </ol>
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
@@ -108,7 +110,8 @@ public TreeItem (Tree parent, int style, int index) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the specified <code>TreeItem</code>.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(TreeItem,int,int)
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -133,12 +136,8 @@ public TreeItem (TreeItem parentItem, int style) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as <code>index</code> direct child of the specified <code>TreeItem</code>.
  * <p>
- * The fastest way to insert many items is:
- * <ol>
- * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
- * <li>Insert child items while parent item is collapsed</li>
- * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
- * </ol>
+ * The fastest way to insert many items is documented in {@link org.eclipse.swt.widgets.TreeItem#TreeItem(Tree,int,int)}
+ * and {@link org.eclipse.swt.widgets.TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -1620,6 +1619,12 @@ public void setImage (Image [] images) {
 
 /**
  * Sets the number of child items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is:
+ * <ol>
+ * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
+ * </ol>
  *
  * @param count the number of items
  *

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/TreeItem.java
@@ -64,7 +64,8 @@ public class TreeItem extends Item {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the tree.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(Tree,int,int)
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -93,6 +94,7 @@ public TreeItem (Tree parent, int style) {
  * <ol>
  * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
  * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
  * </ol>
  *
  * @param parent a tree control which will be the parent of the new instance (cannot be null)
@@ -121,7 +123,8 @@ public TreeItem (Tree parent, int style, int index) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as last direct child of the specified <code>TreeItem</code>.
  * <p>
- * For bulk insert scenarios, see TreeItem#TreeItem(TreeItem,int,int)
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -146,12 +149,8 @@ public TreeItem (TreeItem parentItem, int style) {
  * Constructs <code>TreeItem</code> and <em>inserts</em> it into <code>Tree</code>.
  * Item is inserted as <code>index</code> direct child of the specified <code>TreeItem</code>.
  * <p>
- * The fastest way to insert many items is:
- * <ol>
- * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
- * <li>Insert child items while parent item is collapsed</li>
- * <li>Insert every item at index 0 (insert them in reverse to get the same result)</li>
- * </ol>
+ * The fastest way to insert many items is documented in {@link TreeItem#TreeItem(Tree,int,int)}
+ * and {@link TreeItem#setItemCount}
  *
  * @param parentItem a tree control which will be the parent of the new instance (cannot be null)
  * @param style no styles are currently supported, pass SWT.NONE
@@ -1698,6 +1697,12 @@ public void setImage (Image image) {
 
 /**
  * Sets the number of child items contained in the receiver.
+ * <p>
+ * The fastest way to insert many items is:
+ * <ol>
+ * <li>Use {@link Tree#setRedraw} to disable drawing during bulk insert</li>
+ * <li>Collapse the parent item before inserting (gives massive improvement on Windows)</li>
+ * </ol>
  *
  * @param count the number of items
  *

commit 2570400b0c718f754d1241ed3951db64b236d82f

Issue #333 - [win32] Revert Tree.setItemCount optimizations
Reverts code changes from commits:
* 17b40247 - Initial patch that optimizes Tree.setItemCount
* d5270df6 - followup that fixes some regressions in 17b40247

New code comments and snippets are preserved.

The revert is to let Eclipse release safely, a better optimization
attempt will be prepared later.

Signed-off-by: Alexandr Miloslavskiy <alexandr.miloslavskiy@syntevo.com>

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/Tree.java
@@ -2069,8 +2069,9 @@ void createItem (TreeItem item, long hParent, long hInsertAfter, long hItem) {
 				shrink = true;
 				length = Math.max (4, items.length * 3 / 2);
 			}
-
-			itemsGrowArray (length);
+			TreeItem [] newItems = new TreeItem [length];
+			System.arraycopy (items, 0, newItems, 0, items.length);
+			items = newItems;
 		}
 		lastID = id + 1;
 	}
@@ -3854,22 +3855,6 @@ boolean isUseWsBorder () {
 	return true;
 }
 
-int itemsGetFreeCapacity() {
-	int count = 0;
-	for (TreeItem item : items) {
-		if (item == null)
-			count++;
-	}
-
-	return count;
-}
-
-void itemsGrowArray (int newCapacity) {
-	TreeItem [] newItems = new TreeItem [newCapacity];
-	System.arraycopy (items, 0, newItems, 0, items.length);
-	items = newItems;
-}
-
 void redrawSelection () {
 	if ((style & SWT.SINGLE) != 0) {
 		long hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CARET, 0);
@@ -4152,128 +4137,69 @@ public void setItemCount (int count) {
 }
 
 void setItemCount (int count, long hParent) {
-	// Investigate existing items and decide what to do
-	long itemInsertAfter = 0;
-	int  numInserted = 0;
-	long itemDeleteFrom = 0;
-	{
-		// Iterate to position #count and find prev/next items at this position
-		int itemCount = 0;
-		long itemPrev = OS.TVI_FIRST;
-		long itemNext = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CHILD, hParent);
-		while (itemNext != 0 && itemCount < count)
-		{
-			itemPrev = itemNext;
-			itemNext = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, itemNext);
-			itemCount++;
-		}
-
-		if ((itemCount == count) && (itemNext == 0)) {
-			// Exactly 'count' items, no need to do anything.
-			return;
-		} else if (itemCount == count) {
-			// Too many items, going to delete some
-			itemDeleteFrom = itemNext;
-		} else if (itemNext == 0) {
-			// Counted all items, and there is not enough, going to insert some.
-			itemInsertAfter = itemPrev;
-			numInserted = count - itemCount;
-		}
-	}
+	long hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_CHILD, hParent);
 
 	boolean redraw = false;
 	if (OS.SendMessage (handle, OS.TVM_GETCOUNT, 0, 0) == 0) {
 		redraw = getDrawing () && OS.IsWindowVisible (handle);
 		if (redraw) OS.DefWindowProc (handle, OS.WM_SETREDRAW, 0, 0);
 	}
-
+	int itemCount = 0;
+	while (hItem != 0 && itemCount < count) {
+		hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, hItem);
+		itemCount++;
+	}
 	boolean expanded = false;
 	TVITEM tvItem = new TVITEM ();
 	tvItem.mask = OS.TVIF_HANDLE | OS.TVIF_PARAM;
-	if (!redraw && (style & SWT.VIRTUAL) != 0) {
-		if (hParent == OS.TVI_ROOT) {
-			// Trying to call TVM_GETITEMSTATE with TVI_ROOT causes a crash.
-			// Assume that root item is always expanded.
-			expanded = true;
+	if (!redraw && (style & SWT.VIRTUAL) != 0 && (hParent != OS.TVI_ROOT)) {
+		/*
+		* Bug in Windows.  Despite the fact that TVM_GETITEMSTATE claims
+		* to return only the bits specified by the stateMask, when called
+		* with TVIS_EXPANDED, the entire state is returned.  The fix is
+		* to explicitly check for the TVIS_EXPANDED bit.
+		*/
+		int state = (int)OS.SendMessage (handle, OS.TVM_GETITEMSTATE, hParent, OS.TVIS_EXPANDED);
+		expanded = (state & OS.TVIS_EXPANDED) != 0;
+	}
+	while (hItem != 0) {
+		tvItem.hItem = hItem;
+		OS.SendMessage (handle, OS.TVM_GETITEM, 0, tvItem);
+		hItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, hItem);
+		TreeItem item = tvItem.lParam != -1 ? items [(int)tvItem.lParam] : null;
+		if (item != null && !item.isDisposed ()) {
+			item.dispose ();
 		} else {
-			/*
-			 * Bug in Windows.  Despite the fact that TVM_GETITEMSTATE claims
-			 * to return only the bits specified by the stateMask, when called
-			 * with TVIS_EXPANDED, the entire state is returned.  The fix is
-			 * to explicitly check for the TVIS_EXPANDED bit.
-			 */
-			int state = (int)OS.SendMessage (handle, OS.TVM_GETITEMSTATE, hParent, OS.TVIS_EXPANDED);
-			expanded = (state & OS.TVIS_EXPANDED) != 0;
+			releaseItem (tvItem.hItem, tvItem, false);
+			destroyItem (null, tvItem.hItem);
 		}
 	}
-
-	if (itemDeleteFrom != 0) {
-		while (itemDeleteFrom != 0) {
-			tvItem.hItem = itemDeleteFrom;
-			OS.SendMessage (handle, OS.TVM_GETITEM, 0, tvItem);
-			itemDeleteFrom = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_NEXT, itemDeleteFrom);
-			TreeItem item = tvItem.lParam != -1 ? items [(int)tvItem.lParam] : null;
-			if (item != null && !item.isDisposed ()) {
-				item.dispose ();
-			} else {
-				releaseItem (tvItem.hItem, tvItem, false);
-				destroyItem (null, tvItem.hItem);
-			}
+	if ((style & SWT.VIRTUAL) != 0) {
+		for (int i=itemCount; i<count; i++) {
+			/*
+			 * Bug 206806: Windows sends 'TVN_GETDISPINFO' when item is
+			 * being inserted. This causes 'SWT.SetData' to be sent to
+			 * user code, but user code will likely be confused by
+			 * inconsistent Tree state (because we're still inserting):
+			 * - 'getItemCount()' will be wrong
+			 * - 'Event.index' will be wrong
+			 * The workaround is to temporarily suppress 'SWT.SetData'. Note
+			 * that the boolean flag is misleadingly used for multiple
+			 * purposes. What really happens is that 'TVN_GETDISPINFO' will
+			 * queue a repaint for item and early return.
+			 */
+			if (expanded) ignoreShrink = true;
+			createItem (null, hParent, OS.TVI_LAST, 0);
+			if (expanded) ignoreShrink = false;
 		}
 	} else {
-		// For performance reasons, reserve the necessary space in items[]
-		int freeCapacity = itemsGetFreeCapacity();
-		if (numInserted > freeCapacity)
-			itemsGrowArray (items.length + numInserted - freeCapacity);
-
-		// Note: on Windows, insert complexity is O(pos), so for performance
-		// reasons, all items are inserted at minimum possible position, that
-		// is, all at the same position.
-		long rootItem = OS.SendMessage (handle, OS.TVM_GETNEXTITEM, OS.TVGN_ROOT, 0);
-		int indexInsertAfter = (rootItem == 0) ? 0 : findIndex (rootItem, itemInsertAfter);
-
-		if ((style & SWT.VIRTUAL) != 0) {
-			for (int i = 0; i < numInserted; i++) {
-				// By inserting item in the middle, the relation of
-				// 'lastIndexOf' to 'hLastIndexOf' becomes invalid if
-				// 'hLastIndexOf' points after insertion point (because
-				// inserting means that all subsequent indices change).
-				// The solution is to adjust variables to unaffected values.
-				//
-				// Note that 'Tree.indexOf()' also gets called inside
-				// 'createItem()' below (via 'TVN_GETDISPINFO'). Therefore,
-				// merely invalidating variables would be a O(N*N) performance
-				// hit because it will need to walk entire list for every item
-				// inserted. On top of that, it will return wrong values via
-				// 'TVN_GETDISPINFO'.
-				//
-				// Also note that 'Tree.indexOf()' is optimized to look for an
-				// item just after cached one before resorting to walking
-				// entire list. This means that caching item just before the
-				// inserted one is efficient.
-				lastIndexOf = indexInsertAfter;
-				hLastIndexOf = itemInsertAfter;
-
-				/*
-				 * Bug 206806: Windows sends 'TVN_GETDISPINFO' when item is
-				 * being inserted. This causes 'SWT.SetData' to be sent to
-				 * user code, but user code will likely be confused by
-				 * inconsistent Tree state (because we're still inserting):
-				 * - 'getItemCount()' will be wrong
-				 * - 'Event.index' will be wrong
-				 * The workaround is to temporarily suppress 'SWT.SetData'. Note
-				 * that the boolean flag is misleadingly used for multiple
-				 * purposes. What really happens is that 'TVN_GETDISPINFO' will
-				 * queue a repaint for item and early return.
-				 */
-				if (expanded) ignoreShrink = true;
-				createItem (null, hParent, itemInsertAfter, 0);
-				if (expanded) ignoreShrink = false;
-			}
-		} else {
-			for (int i = 0; i < numInserted; i++) {
-				new TreeItem (this, SWT.NONE, hParent, itemInsertAfter, 0);
-			}
+		shrink = true;
+		int extra = Math.max (4, (count + 3) / 4 * 4);
+		TreeItem [] newItems = new TreeItem [items.length + extra];
+		System.arraycopy (items, 0, newItems, 0, items.length);
+		items = newItems;
+		for (int i=itemCount; i<count; i++) {
+			new TreeItem (this, SWT.NONE, hParent, OS.TVI_LAST, 0);
 		}
 	}
 	if (redraw) {