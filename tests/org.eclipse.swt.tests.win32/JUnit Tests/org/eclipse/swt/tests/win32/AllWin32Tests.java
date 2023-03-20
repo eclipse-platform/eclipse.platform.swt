e6001701cce4df8e6f587b3c20de3e25df6bff8d
Mat Booth <mat.booth@gmail.com>
2023-03-20 11:19:24 +0000

    Issue #600: [win32] Fix failing test_isXMouseActive test
    This test is executed twice by the github actions verification, but only
    fails the second time.
    
    Without explicit configuration, the tycho-surefire plugin just globs all
    the tests in the bundle, which in the presence of an explicit test suite,
    will result in the duplicate execution of tests.
    
    This change explicitly configures tycho to execute the test suite, and
    adds the Test_org_eclipse_swt_events_KeyEvent test to the test suite,
    from which it was missing.
    
    Signed-off-by: Mat Booth <mat.booth@gmail.com>

--- tests/org.eclipse.swt.tests.win32/JUnit Tests/org/eclipse/swt/tests/win32/AllWin32Tests.java
+++ tests/org.eclipse.swt.tests.win32/JUnit Tests/org/eclipse/swt/tests/win32/AllWin32Tests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2018 Red Hat and others. All rights reserved.
+ * Copyright (c) 2018, 2023 Red Hat and others. All rights reserved.
  * The contents of this file are made available under the terms
  * of the GNU Lesser General Public License (LGPL) Version 2.1 that
  * accompanies this distribution (lgpl-v21.txt).  The LGPL is also
@@ -24,6 +24,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
 	Test_org_eclipse_swt_dnd_DND.class,
+	Test_org_eclipse_swt_events_KeyEvent.class,
 	Test_org_eclipse_swt_widgets_Display.class,
 	TestTreeColumn.class
 })
