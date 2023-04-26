f5677a882150eb93e87331b931dee434f87d033a
Александър Куртаков <akurtakov@gmail.com>
2023-04-26 16:01:27 +0300

    Remove unused import in tests

--- tests/org.eclipse.swt.tests/ManualTests/org/eclipse/swt/tests/manual/Issue0400_WaylandDifferentDndMouseEvents.java
+++ tests/org.eclipse.swt.tests/ManualTests/org/eclipse/swt/tests/manual/Issue0400_WaylandDifferentDndMouseEvents.java
@@ -16,7 +16,6 @@ package org.eclipse.swt.tests.manual;
 
 import org.eclipse.swt.*;
 import org.eclipse.swt.dnd.*;
-import org.eclipse.swt.graphics.*;
 import org.eclipse.swt.layout.*;
 import org.eclipse.swt.widgets.*;
 
