2e45bb20f544911f68af00850b28bed45ccf7383
Deepika Udayagiri <deepika.udayagiri@in.ibm.com>
2023-09-05 17:11:15 +0530

    Configure SWT build scripts for 4.30
    Fixes https://github.com/eclipse-platform/eclipse.platform.swt/pull/796

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -30,7 +30,7 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 964;
+	static int MINOR_VERSION = 963;
 
 	/**
 	 * SWT revision number (must be >= 0)
