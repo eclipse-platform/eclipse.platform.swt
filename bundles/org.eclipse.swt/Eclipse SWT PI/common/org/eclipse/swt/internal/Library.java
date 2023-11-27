6a83d0c57888b88bf53eef47b00ed30b13eea5c5
Deepika Udayagiri <deepika.udayagiri@in.ibm.com>
2023-11-27 12:50:06 +0530

    Fixes https://github.com/eclipse-platform/eclipse.platform.releng.aggregator/issues/1557

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -35,12 +35,12 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 963;
+	static int MINOR_VERSION = 964;
 
 	/**
 	 * SWT revision number (must be >= 0)
 	 */
-	static int REVISION = 5;
+	static int REVISION = 1;
 
 	/**
 	 * The JAVA and SWT versions
