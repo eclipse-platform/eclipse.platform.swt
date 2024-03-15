1656982b91a850f8847439aa486ebdea211178a9
Christian Kohlschütter <christian@kohlschutter.com>
2024-03-15 13:56:12 +0100

    cocoa: Enforce native item height for Table, Tree, List if necessary
    Newer versions of macOS introduced a new style of table rows with a
    padding that is larger than previously seen. This broke item height
    computations (#677). An attempt to fix this had the unexpected side
    effect that now some UI elements in Eclipse IDE (and other apps with
    "-Dorg.eclipse.swt.internal.carbon.smallFonts" enabled),
    eclise.platform.ui #1674, and it also broke compatibility with older
    versions of macOS that did not have the additional padding.
    
    This change reverts the previous fix, and adds logic to enforce the
    native item height by default, without making assumption about the exact
    height (which varies with "smallFonts", for example).
    
    The setting can be controlled by a new System property,
    org.eclipse.swt.internal.cocoa.useNativeItemHeight, that, if set to
    false, overrules the default. This allows apps to bring brack the old
    macOS behavior.
    
    Lastly, update two tests in Test_org_eclipse_swt_widgets_List that made
    assumptions about the item height being different for two small font
    sizes -- this is no longer true when the native item height is enforced.
    We now increase the font size of the second font such that this corner
    case no longer matters.
    
    Fixes https://github.com/eclipse-platform/eclipse.platform.ui/issues/1674
    Fixes https://github.com/eclipse-platform/eclipse.platform.swt/issues/677

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Tree.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Tree.java
@@ -97,6 +97,8 @@ public class Tree extends Composite {
 	/* Used to control drop feedback when DND.FEEDBACK_EXPAND and DND.FEEDBACK_SCROLL is set/not set */
 	boolean shouldExpand = true, shouldScroll = true;
 
+	final int nativeItemHeight;
+
 	static int NEXT_ID;
 
 	/*
@@ -108,9 +110,6 @@ public class Tree extends Composite {
 	static final int IMAGE_GAP = 3;
 	static final int TEXT_GAP = 2;
 	static final int CELL_GAP = 1;
-
-	/* Vertical cell padding for tree item */
-	static final int VERTICAL_CELL_PADDING= 8;
 
 /**
  * Constructs a new instance of this class given its parent
@@ -147,6 +146,9 @@ public class Tree extends Composite {
  */
 public Tree (Composite parent, int style) {
 	super (parent, checkStyle (style));
+
+	this.nativeItemHeight = (int)((NSTableView)view).rowHeight();
+	setItemHeight(null, null, true);
 }
 
 @Override
@@ -3165,7 +3167,10 @@ void setItemHeight (Image image, NSFont font, boolean set) {
 	if (font == null) font = getFont ().handle;
 	double ascent = font.ascender ();
 	double descent = -font.descender () + font.leading ();
-	int height = (int)Math.ceil (ascent + descent) + VERTICAL_CELL_PADDING;
+	int height = (int)Math.ceil (ascent + descent) + 1;
+	if (display.useNativeItemHeight) {
+		height = Math.max (height, nativeItemHeight);
+	}
 	Rectangle bounds = image != null ? image.getBounds () : imageBounds;
 	if (bounds != null) {
 		imageBounds = bounds;
