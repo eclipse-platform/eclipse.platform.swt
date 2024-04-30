0c56201adf9f181accca9d5fca9d6a59ba39d154
Heiko Klare <heiko.klare@vector.com>
2024-04-30 16:00:15 +0200

    Reduce Win32 FontRegistries visibility and move tests inside fragment
    The classes DefaultSWTFontRegistry and ScalingSWTFontRegistry are
    currently public only to make them testable from a test bundle. There is
    currently no pre-configured way to implement unit tests for classes that
    are not directly accessible via public API.
    
    With this change, the SWT Win32 fragments are extended by a
    configuration for providing fragment-internal unit tests executed with
    plain Maven surefire, which can access the classes under test even with
    reduced visibility. Due to current limitation in Tycho, an according
    temporary extension to the Maven configuration for the binaries projects
    is made. This configuration is applied to the tests for the
    DefaultSWTFontRegistry and ScalingSWTFontRegistry, such that their
    visibilities can properly be reduced to package visibility.

--- tests/org.eclipse.swt.tests.win32/JUnit Tests/org/eclipse/swt/tests/win32/AllWin32Tests.java
+++ tests/org.eclipse.swt.tests.win32/JUnit Tests/org/eclipse/swt/tests/win32/AllWin32Tests.java
@@ -15,20 +15,15 @@
 package org.eclipse.swt.tests.win32;
 
 import org.eclipse.swt.graphics.ImageWin32Tests;
-import org.eclipse.swt.internal.DefaultSWTFontRegistryTests;
-import org.eclipse.swt.internal.ScalingSWTFontRegistryTests;
 import org.eclipse.swt.tests.win32.widgets.TestTreeColumn;
 import org.eclipse.swt.tests.win32.widgets.Test_org_eclipse_swt_widgets_Display;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-	DefaultSWTFontRegistryTests.class,
 	ImageWin32Tests.class,
-	ScalingSWTFontRegistryTests.class,
 	Test_org_eclipse_swt_dnd_DND.class,
 	Test_org_eclipse_swt_events_KeyEvent.class,
 	Test_org_eclipse_swt_widgets_Display.class,
