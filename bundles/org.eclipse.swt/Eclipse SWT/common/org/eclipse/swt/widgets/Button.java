34ed02ef594925056fb71309aecf349819436d4a
fedejeanne <Federico.Jeanne@vector.com>
2024-08-06 16:33:52 +0200

    Let Button extend Canvas instead of Scrollable
    That way it will be rendered in Linux (as a black square so it still
    doesn't really work)

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/widgets/Button.java
@@ -47,7 +47,7 @@ import org.eclipse.swt.internal.*;
  * @see <a href="http://www.eclipse.org/swt/">Sample code and further information</a>
  * @noextend This class is not intended to be subclassed by clients.
  */
-public class Button extends Scrollable {
+public class Button extends Canvas {
 	String text = "", message = "";
 	Image image, disabledImage;
 	ImageList imageList;
