668d981f55e6b43e87d514703e92eaa1793b8fab
Deepika Udayagiri <deepika.udayagiri@in.ibm.com>
2024-09-02 12:54:50 +0530

    Configure SWT build scripts for 4.34
    Fixes eclipse-platform/eclipse.platform.releng.aggregator#2287
    
    @elsazac : Can you review and merge this pr please.

--- bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/common/org/eclipse/swt/internal/Library.java
@@ -35,12 +35,12 @@ public class Library {
 	/**
 	 * SWT Minor version number (must be in the range 0..999)
 	 */
-	static int MINOR_VERSION = 966;
+	static int MINOR_VERSION = 967;
 
 	/**
 	 * SWT revision number (must be >= 0)
 	 */
-	static int REVISION = 5;
+	static int REVISION = 0;
 
 	/**
 	 * The JAVA and SWT versions
