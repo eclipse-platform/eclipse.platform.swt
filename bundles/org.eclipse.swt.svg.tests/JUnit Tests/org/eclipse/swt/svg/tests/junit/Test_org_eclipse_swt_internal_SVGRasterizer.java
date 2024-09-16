dceecc6469335d9f5b4e6769bb73e53374376079
Michael Bangas <michael.bangas@vector.com>
2024-09-16 17:55:31 +0200

    Introduce SVG Rasterization for Icons
    Feature Proposal: Rasterization of SVGs at Runtime for Eclipse Icons
    Fixes https://github.com/eclipse-platform/eclipse.platform.swt/issues/1438
    
    Eclipse currently loads icons exclusively as raster graphics (e.g., `.png`), without support for vector formats like `.svg`. A major drawback of raster graphics is their inability to scale without degrading image quality. Additionally, generating icons of different sizes requires manually rasterizing SVGs outside Eclipse, leading to unnecessary effort and many icon files.
    
    This PR introduces support for vector graphics in Eclipse, enabling SVGs to be used for icons. Existing PNG icons will continue to be loaded alongside SVGs, allowing the use of the new functionality without the need to replace all PNG files at once.
    
    ---
    - **How It Works**:
      - To use SVG icons, simply place the SVG file in the bundle and reference it in the `plugin.xml` and other necessary locations, as is done for PNGs. No additional configuration is required.
      - At runtime, Eclipse uses the library JSVG to rasterize the SVG into a raster image of the desired size, eliminating the need for scaling. My analysis shows that JSVG is the most suitable Java library for this purpose.
      - You need to write the flag `-Dswt.autoScale=quarter` into your `eclipse.ini` file or into the run arguments of a new configuration.

---
+++ bundles/org.eclipse.swt.svg.tests/JUnit Tests/org/eclipse/swt/svg/tests/junit/Test_org_eclipse_swt_internal_SVGRasterizer.java
@@ -0,0 +1,102 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Vector Informatik GmbH and others.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse
+ * Public License 2.0 which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors: Michael Bangas (Vector Informatik GmbH) - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.svg.tests.junit;
+
+import static org.eclipse.swt.tests.junit.SwtTestUtil.assertSWTProblem;
+import static org.junit.Assert.fail;
+
+import java.io.File;
+import java.nio.file.Path;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.SWTException;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.ImageDataProvider;
+import org.eclipse.swt.graphics.ImageFileNameProvider;
+import org.eclipse.swt.tests.junit.SwtTestUtil;
+import org.eclipse.swt.widgets.Display;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Test_org_eclipse_swt_internal_SVGRasterizer {
+
+	Display display;
+
+	ImageFileNameProvider imageFileNameProvider = zoom -> {
+		String fileName = "collapseall.svg";
+		return getPath(fileName);
+	};
+
+	ImageDataProvider imageDataProvider = zoom -> {
+		String fileName = "collapseall.svg";
+		return new ImageData(getPath(fileName), zoom);
+	};
+
+	@Before
+	public void setUp() {
+		display = Display.getDefault();
+	}
+
+	String getPath(String fileName) {
+		String urlPath = "";
+		String pluginPath = System.getProperty("PLUGIN_PATH");
+		if (pluginPath == null) {
+			urlPath = Path.of("data/" + fileName).toAbsolutePath().toString();
+		} else {
+			urlPath = pluginPath + "/data/" + fileName;
+		}
+		if (File.separatorChar != '/')
+			urlPath = urlPath.replace('/', File.separatorChar);
+		if (SwtTestUtil.isWindows && urlPath.indexOf(File.separatorChar) == 0)
+			urlPath = urlPath.substring(1);
+		urlPath = urlPath.replaceAll("%20", " ");
+		return urlPath;
+	}
+
+	@Test
+	public void test_ConstructorLorg_eclipse_swt_graphics_Device_ImageFileNameProvider() {
+		// Valid provider
+		Image image = new Image(display, imageFileNameProvider);
+		image.dispose();
+		// Corrupt Image provider
+		ImageFileNameProvider provider = zoom -> {
+			String fileName = "corrupt.svg";
+			return getPath(fileName);
+		};
+		try {
+			image = new Image(display, provider);
+			image.dispose();
+			fail("No exception thrown for corrupt image file.");
+		} catch (SWTException e) {
+			assertSWTProblem("Incorrect exception thrown for provider with corrupt images", SWT.ERROR_INVALID_IMAGE, e);
+		}
+	}
+
+	@Test
+	public void test_ConstructorLorg_eclipse_swt_graphics_Device_ImageDataProvider() {
+		// Valid provider
+		Image image = new Image(display, imageDataProvider);
+		image.dispose();
+		// Corrupt Image provider
+		ImageDataProvider provider = zoom -> {
+			String fileName = "corrupt.svg";
+			return new ImageData(getPath(fileName), zoom);
+		};
+		try {
+			image = new Image(display, provider);
+			image.dispose();
+			fail("No exception thrown for corrupt image file.");
+		} catch (SWTException e) {
+			assertSWTProblem("Incorrect exception thrown for provider with corrupt images", SWT.ERROR_INVALID_IMAGE, e);
+		}
+	}
+}
