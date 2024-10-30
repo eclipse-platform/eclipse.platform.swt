0eb74329419b0a9ee662ba50e5822b9ad0059369
ShadelessFox <yanuari.channel@gmail.com>
2024-10-30 16:49:01 +0100

    Win32: Fix compatibility with Windows Server 2012, 2016, Windows 8.1

--- bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/os_custom.h
+++ bundles/org.eclipse.swt/Eclipse SWT PI/win32/library/os_custom.h
@@ -20,3 +20,8 @@
 /* Libraries for dynamic loaded functions */
 #define GetDpiForMonitor_LIB "shcore.dll"
 #define RtlGetVersion_LIB "ntdll.dll"
+#define OpenThemeDataForDpi_LIB "uxtheme.dll"
+#define GetSystemMetricsForDpi_LIB "user32.dll"
+#define GetThreadDpiAwarenessContext_LIB "user32.dll"
+#define SetThreadDpiAwarenessContext_LIB "user32.dll"
+#define SystemParametersInfoForDpi_LIB "user32.dll"
