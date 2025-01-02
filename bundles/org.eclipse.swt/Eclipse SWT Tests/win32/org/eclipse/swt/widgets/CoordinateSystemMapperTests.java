7403c743ac530c089d3853ed8ea424d8ce4b7c4d
Amartya Parijat <amartya.parijat@yatta.de>
2025-01-02 12:11:43 +0100

    Refactor out CoordinateSystemMapper from Display and add tests
    This commit refactors the Display to separate single zoom and multi zoom
    coordinate system mappers in separate classes. Additionally, it adds
    some tests for validating the multi-zoom coordinate system mapper. A few
    tests are disabled because of the current state of multi-zoom coordinate
    system limitations.
    
    Contributes to #62 and #127

---
+++ bundles/org.eclipse.swt/Eclipse SWT Tests/win32/org/eclipse/swt/widgets/CoordinateSystemMapperTests.java
@@ -0,0 +1,160 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Yatta Solutions and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yatta Solutions - initial tests
+ *******************************************************************************/
+package org.eclipse.swt.widgets;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.*;
+import java.util.stream.*;
+
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.internal.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class CoordinateSystemMapperTests {
+
+	Supplier<Monitor[]> getMonitorConsumer;
+	Monitor[] monitors;
+
+	private Monitor createMonitor(CoordinateSystemMapper mapper, Rectangle boundsInPixels, int nativeZoom) {
+		Monitor monitor = new Monitor();
+		Rectangle bounds = mapper.mapMonitorBounds(boundsInPixels, DPIUtil.getZoomForAutoscaleProperty(nativeZoom));
+		monitor.setBounds(bounds);
+		monitor.setClientArea(bounds);
+		monitor.zoom = nativeZoom;
+		return monitor;
+	}
+
+	void setupMonitors(CoordinateSystemMapper mapper) {
+		Rectangle boundsInPixelsForLeftMonitor = new Rectangle(0, 0, 2000, 2000);
+		Rectangle boundsInPixelsForRightMonitor = new Rectangle(2000, 0, 2000, 2000);
+		monitors = new Monitor[] { createMonitor(mapper, boundsInPixelsForLeftMonitor, 200),
+				createMonitor(mapper, boundsInPixelsForRightMonitor, 100) };
+	}
+
+	Stream<CoordinateSystemMapper> provideCoordinateSystemMappers() {
+		return Stream.of(new MultiZoomCoordinateSystemMapper(null, () -> monitors),
+				new SingleZoomCoordinateSystemMapper(null));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	void translatePointInNoMonitorBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Point pt = new Point(5000, -400);
+		Point px = mapper.translateToDisplayCoordinates(pt, monitors[0].getZoom());
+		assertEquals(pt, mapper.translateFromDisplayCoordinates(px, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	@Disabled("Disabled due to current limitations of MultiZoomCoordinateSystemMapper")
+	void translatePointInGapBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Point pt = new Point(1900, 400);
+		Point px = mapper.translateToDisplayCoordinates(pt, monitors[0].getZoom());
+		assertEquals(pt, mapper.translateFromDisplayCoordinates(px, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	void translateRectangleInNoMonitorBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPts = new Rectangle(5000, -400, 200, 200);
+		Rectangle rectInPxs = mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom());
+		assertEquals(rectInPts, mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	@Disabled("Disabled due to current limitations of MultiZoomCoordinateSystemMapper")
+	void translateRectangleInGapBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPts = new Rectangle(1800, 400, 100, 100);
+		Rectangle rectInPxs = mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom());
+		assertEquals(rectInPts, mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	@Disabled("Disabled due to current limitations of MultiZoomCoordinateSystemMapper")
+	void translateRectangleInGapPartiallyInRightBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPts = new Rectangle(1950, 400, 100, 100);
+		Rectangle rectInPxs = mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom());
+		assertEquals(rectInPts, mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	void translateRectangleInGapPartiallyInLeftBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPts = new Rectangle(750, 400, 100, 100);
+		Rectangle rectInPxs = mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom());
+		assertEquals(rectInPts, mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	void translateRectangleInPointsInBothMonitorsPartiallyBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPts = new Rectangle(950, 400, 1500, 100);
+		Rectangle rectInPxs = mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom());
+		assertEquals(rectInPts, mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom()));
+	}
+
+	@Test
+	@Disabled("Disabled due to current limitations of MultiZoomCoordinateSystemMapper")
+	void moveRectangleInPixelsInRightMonitorsPartiallyBackAndForthShouldBeTheSame() {
+		CoordinateSystemMapper mapper = provideCoordinateSystemMappers().findFirst().get();
+		setupMonitors(mapper);
+		Rectangle rectInPxs = new Rectangle(1990, -10, 2000, 2000);
+		Rectangle expectedSmallRectInPxs = new Rectangle(0, 0, 0, monitors[0].getZoom());
+		expectedSmallRectInPxs.x = rectInPxs.x + (rectInPxs.width / 2) - 200;
+		expectedSmallRectInPxs.y = rectInPxs.y + (rectInPxs.height / 2) - 200;
+		expectedSmallRectInPxs.width = 400;
+		expectedSmallRectInPxs.height = 400;
+		Rectangle rectInPts = mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom());
+		Rectangle smallRectInPts = new Rectangle(0, 0, 0, monitors[0].getZoom());
+		smallRectInPts.x = rectInPts.x + (rectInPts.width / 2) - 200;
+		smallRectInPts.y = rectInPts.y + (rectInPts.height / 2) - 200;
+		smallRectInPts.width = 400;
+		smallRectInPts.height = 400;
+		Rectangle smallRectInPxs = mapper.translateToDisplayCoordinates(smallRectInPts, monitors[0].getZoom());
+		assertEquals(expectedSmallRectInPxs, smallRectInPxs);
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	@Disabled("Disabled due to current limitations of MultiZoomCoordinateSystemMapper")
+	void translateRectangleInPixelsOutisdeMonitorsBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPxs = new Rectangle(4400, 400, 1000, 1000);
+		Rectangle rectInPts = mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom());
+		assertEquals(rectInPxs, mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom()));
+	}
+
+	@ParameterizedTest
+	@MethodSource("provideCoordinateSystemMappers")
+	void translateRectangleInPixelsInBothMonitorsBackAndForthShouldBeTheSame(CoordinateSystemMapper mapper) {
+		setupMonitors(mapper);
+		Rectangle rectInPxs = new Rectangle(1500, 400, 502, 500);
+		Rectangle rectInPts = mapper.translateFromDisplayCoordinates(rectInPxs, monitors[0].getZoom());
+		assertEquals(rectInPxs, mapper.translateToDisplayCoordinates(rectInPts, monitors[0].getZoom()));
+	}
+
+}
---
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/widgets/SingleZoomCoordinateSystemMapper.java
@@ -0,0 +1,108 @@
+/*******************************************************************************
+ * Copyright (c) 2025 Yatta Solutions and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     Yatta Solutions - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.swt.widgets;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.internal.*;
+
+class SingleZoomCoordinateSystemMapper implements CoordinateSystemMapper {
+
+	private final Display display;
+
+	SingleZoomCoordinateSystemMapper(Display display) {
+		this.display = display;
+	}
+
+	private int getZoomLevelForMapping(Control from, Control to) {
+		if (from != null && from.isDisposed()) {
+			display.error(SWT.ERROR_INVALID_ARGUMENT);
+		}
+		if (to != null && to.isDisposed()) {
+			display.error(SWT.ERROR_INVALID_ARGUMENT);
+		}
+		if (to != null) {
+			return to.getZoom();
+		}
+		return from.getZoom();
+	}
+
+	@Override
+	public Point map(Control from, Control to, Point point) {
+		int zoom = getZoomLevelForMapping(from, to);
+		point = DPIUtil.scaleUp(point, zoom);
+		return DPIUtil.scaleDown(display.mapInPixels(from, to, point), zoom);
+	}
+
+	@Override
+	public Rectangle map(Control from, Control to, Rectangle rectangle) {
+		int zoom = getZoomLevelForMapping(from, to);
+		rectangle = DPIUtil.scaleUp(rectangle, zoom);
+		return DPIUtil.scaleDown(display.mapInPixels(from, to, rectangle), zoom);
+	}
+
+	@Override
+	public Point map(Control from, Control to, int x, int y) {
+		int zoom = getZoomLevelForMapping(from, to);
+		x = DPIUtil.scaleUp(x, zoom);
+		y = DPIUtil.scaleUp(y, zoom);
+		return DPIUtil.scaleDown(display.mapInPixels(from, to, x, y), zoom);
+	}
+
+	@Override
+	public Rectangle map(Control from, Control to, int x, int y, int width, int height) {
+		int zoom = getZoomLevelForMapping(from, to);
+		x = DPIUtil.scaleUp(x, zoom);
+		y = DPIUtil.scaleUp(y, zoom);
+		width = DPIUtil.scaleUp(width, zoom);
+		height = DPIUtil.scaleUp(height, zoom);
+		return DPIUtil.scaleDown(display.mapInPixels(from, to, x, y, width, height), zoom);
+	}
+
+	@Override
+	public Rectangle mapMonitorBounds(Rectangle rect, int zoom) {
+		return DPIUtil.autoScaleDown(rect);
+	}
+
+	@Override
+	public Point translateFromDisplayCoordinates(Point point, int zoom) {
+		return DPIUtil.scaleDown(point, zoom);
+	}
+
+	@Override
+	public Point translateToDisplayCoordinates(Point point, int zoom) {
+		return DPIUtil.scaleUp(point, zoom);
+	}
+
+	@Override
+	public Rectangle translateFromDisplayCoordinates(Rectangle rect, int zoom) {
+		return DPIUtil.scaleDown(rect, zoom);
+	}
+
+	@Override
+	public Rectangle translateToDisplayCoordinates(Rectangle rect, int zoom) {
+		return DPIUtil.scaleUp(rect, zoom);
+	}
+
+	@Override
+	public Point getCursorLocation() {
+		Point cursorLocationInPixels = display.getCursorLocationInPixels();
+		return DPIUtil.autoScaleDown(cursorLocationInPixels);
+	}
+
+	@Override
+	public void setCursorLocation(int x, int y) {
+		display.setCursorLocationInPixels(DPIUtil.autoScaleUp(x), DPIUtil.autoScaleUp(y));
+	}
+}
