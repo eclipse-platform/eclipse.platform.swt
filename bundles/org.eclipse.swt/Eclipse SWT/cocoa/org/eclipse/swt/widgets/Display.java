22eece1314eb92da05f0848c30fdf3b5a615a7a1
Heiko Klare <heiko.klare@vector.com>
2025-01-11 12:39:03 +0100

    Deprecate insufficient Display#setRescalingAtRuntime() method
    The method Display#setRescalingAtRuntime() allows to activate the
    monitor-specific scaling mechanism on Windows. This must, however,
    actually be activated before the Display is instantiated, like it is
    done during the Display constructor execution if the system property for
    global activation of monitor-specific scaling is specified. In
    consequence, calling #setRescalingAtRuntime() does not take the full
    intended effect.
    
    This change deprecates the method so that the system property needs to
    be set for proper initialization of the monitor-specific scaling
    functionality. It also adapts the according tests to the deprecation of
    the API and alternatives to be used.

--- bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Display.java
+++ bundles/org.eclipse.swt/Eclipse SWT/cocoa/org/eclipse/swt/widgets/Display.java
@@ -6858,7 +6858,10 @@ public boolean isRescalingAtRuntime() {
  * @param activate whether rescaling shall be activated or deactivated
  * @return whether activating or deactivating the rescaling was successful
  * @since 3.127
+ * @deprecated this method should not be used as it needs to be called already
+ *             during instantiation to take proper effect
  */
+@Deprecated(since = "2025-03", forRemoval = true)
 public boolean setRescalingAtRuntime(boolean activate) {
 	// not implemented for Cocoa
 	return false;
