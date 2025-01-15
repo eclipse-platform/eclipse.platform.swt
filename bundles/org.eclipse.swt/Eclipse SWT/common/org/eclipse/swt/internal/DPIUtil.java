ffa10849c52645026eb96daca7a8194d48017abb
Heiko Klare <heiko.klare@vector.com>
2025-01-15 10:41:21 +0100

    Use AutoScaleMethod "smooth" by default for fractional scaling
    Currently, AutoScaleMethod "nearest" is used by default, leading to
    jagged image scaling results. The method "smooth" is only used on GTK
    with a fractional scaling value not being a multiple of 100.
    The reason for not using "smooth" scaling on all platform was a bug
    regarding icon transparency that has long been resolved.
    
    With this change, AutoScaleMethod "smooth" is used by default in
    fractional scaling scenarios (i.e., with deviceZoom not being an integer
    multiple of 100) and when using monitor-specific scaling on Windows. To
    still use "nearest" in these scenarios, it can be activated via system
    property.

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/DPIUtil.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/internal/DPIUtil.java
@@ -79,10 +79,9 @@ public class DPIUtil {
 	 * <li>"nearest": nearest-neighbor interpolation, may look jagged</li>
 	 * <li>"smooth": smooth edges, may look blurry</li>
 	 * </ul>
-	 * The current default is to use "nearest", except on
-	 * GTK when the deviceZoom is not an integer multiple of 100%.
-	 * The smooth strategy currently doesn't work on Win32 and Cocoa, see
-	 * <a href="https://bugs.eclipse.org/493455">bug 493455</a>.
+	 * The current default is to use "smooth" on GTK when deviceZoom is an integer
+	 * multiple of 100% and on Windows if monitor-specific scaling is enabled, and
+	 * "nearest" otherwise..
 	 */
 	private static final String SWT_AUTOSCALE_METHOD = "swt.autoScale.method";
 
@@ -605,12 +604,20 @@ public static void setDeviceZoom (int nativeDeviceZoom) {
 	DPIUtil.deviceZoom = deviceZoom;
 	System.setProperty("org.eclipse.swt.internal.deviceZoom", Integer.toString(deviceZoom));
 	if (deviceZoom != 100 && autoScaleMethodSetting == AutoScaleMethod.AUTO) {
-		if (deviceZoom / 100 * 100 == deviceZoom || !"gtk".equals(SWT.getPlatform())) {
-			autoScaleMethod = AutoScaleMethod.NEAREST;
-		} else {
+		if (sholdUseSmoothScaling()) {
 			autoScaleMethod = AutoScaleMethod.SMOOTH;
+		} else {
+			autoScaleMethod = AutoScaleMethod.NEAREST;
 		}
 	}
+}
+
+private static boolean sholdUseSmoothScaling() {
+	return switch (SWT.getPlatform()) {
+	case "gtk" -> deviceZoom / 100 * 100 != deviceZoom;
+	case "win32" -> isMonitorSpecificScalingActive();
+	default -> false;
+	};
 }
 
 public static void setUseCairoAutoScale (boolean cairoAutoScale) {
