e5cd4e39974b125416cb860bf5b5b4e895a11f6c
Thomas Singer <ts-swt@syntevo.com>
2025-01-31 15:05:14 +0100

    SkijaGC: fix fontSize
    The windows GC obviously scales the font independent from the SWT
    autoscaling feature. This change restores this behavior for Skija, too.

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/SkijaGC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/graphics/SkijaGC.java
@@ -620,7 +620,7 @@ public class SkijaGC extends GCHandle {
 			style = FontStyle.ITALIC;
 		}
 		this.font = new Font(Typeface.makeFromName(fontData.getName(), style));
-		int fontSize = DPIUtil.autoScaleUp(fontData.getHeight());
+		int fontSize = DPIUtil.scaleUp(fontData.getHeight(), DPIUtil.getNativeDeviceZoom());
 		if (SWT.getPlatform().equals("win32")) {
 			fontSize *= this.font.getSize() / innerGC.getDevice().getSystemFont().getFontData()[0].getHeight();
 		}
