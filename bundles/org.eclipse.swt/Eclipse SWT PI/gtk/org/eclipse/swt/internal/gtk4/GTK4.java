d9df7665c17a77fd73ff7e166694b30cf140747d
Patrick Ziegler <ziegler.patrick@hotmail.de>
2025-02-03 20:37:43 +0100

    [GTK4] Migrate deprecated ColorChooser to ColorDialog
    The Color Chooser has been moved to the GTK3 class and all references
    inside the GTK4 blocks have been updated to use the new API.
    
    Note that the GTK4 API doesn't provide the means to set a custom
    palette, meaning that the colors specified with setRGBS(RGB[]) are
    effectively ignored.

--- bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk4/GTK4.java
+++ bundles/org.eclipse.swt/Eclipse SWT PI/gtk/org/eclipse/swt/internal/gtk4/GTK4.java
@@ -318,6 +318,54 @@ public class GTK4 {
 	 */
 	public static final native long gtk_font_dialog_set_title(long self, byte[] title);
 
+	/* GtkColorDialog */
+	/**
+	 * @method flags=dynamic
+	 */
+	public static final native long gtk_color_dialog_new();
+	/**
+	 * @method flags=dynamic
+	 *
+	 * @param title cast=(char *)
+	 */
+	public static final native long gtk_color_dialog_set_title(long self, byte[] title);
+	/**
+	 * @method flags=dynamic
+	 *
+	 * @param with_alpha cast=(gboolean)
+	 */
+	public static final native void gtk_color_dialog_set_with_alpha(long self, boolean with_alpha);
+	/**
+	 * @method flags=dynamic
+	 */
+	public static final native boolean gtk_color_dialog_get_with_alpha(long self);
+	/**
+	 * @method flags=dynamic
+	 *
+	 * @param modal cast=(gboolean)
+	 */
+	public static final native void gtk_color_dialog_set_modal(long self, boolean modal);
+	/**
+	 * @method flags=dynamic
+	 */
+	public static final native boolean gtk_color_dialog_get_modal(long self);
+	/**
+	 * @method flags=dynamic
+	 *
+	 * @param parent cast=(GtkWindow *)
+	 * @param initial_color cast=(GdkRGBA *)
+	 * @param cancellable cast=(GCancellable *)
+	 * @param callback cast=(GAsyncReadyCallback)
+	 * @param user_data cast=(gpointer)
+	 */
+	public static final native void gtk_color_dialog_choose_rgba(long self, long parent, GdkRGBA initial_color, long cancellable, long callback, long user_data);
+	/**
+	 * @method flags=dynamic
+	 *
+	 * @param result cast=(GAsyncResult *)
+	 * @param error cast=(GError **)
+	 */
+	public static final native long gtk_color_dialog_choose_rgba_finish(long self, long result, long[] error);
 
 	/* GtkScrolledWindow */
 	public static final native long gtk_scrolled_window_new();
