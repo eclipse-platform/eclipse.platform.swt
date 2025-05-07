eclipse-platform/eclipse.platform.swt

commit f10721abc3c13600d5c13ebf031e84c4b99408db

[win32] Improve GC init for ImageGcDrawer
This commits wraps the GC initialization for the ImageGcDrawer to directly
pass the zoom from the wrapper when the GC is initialized. In all other
cases the GC will be initialized with 100 zoom.

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -1719,6 +1719,10 @@ private ImageHandle init(ImageData i, int zoom) {
  */
 @Override
 public long internal_new_GC (GCData data) {
+	return configureGC(data, 100);
+}
+
+private long configureGC(GCData data, int zoom) {
 	if (isDisposed()) SWT.error(SWT.ERROR_GRAPHIC_DISPOSED);
 	/*
 	* Create a new GC that can draw into the image.
@@ -1743,9 +1747,9 @@ public long internal_new_GC (GCData data) {
 			data.style |= SWT.LEFT_TO_RIGHT;
 		}
 		data.device = device;
-		data.nativeZoom = initialNativeZoom;
+		data.nativeZoom = zoom;
 		data.image = this;
-		data.font = SWTFontProvider.getSystemFont(device, initialNativeZoom);
+		data.font = SWTFontProvider.getSystemFont(device, zoom);
 	}
 	return imageDC;
 }
@@ -2530,7 +2534,7 @@ private class ImageGcDrawerWrapper extends DynamicImageProviderWrapper {
 		} else {
 			image = new Image(device, width, height, zoom);
 		}
-		GC gc = new GC(image, gcStyle);
+		GC gc = new GC(new DrawableWrapper(image, zoom), gcStyle);
 		try {
 			gc.data.nativeZoom = zoom;
 			drawer.drawOn(gc, width, height);
@@ -2541,6 +2545,26 @@ private class ImageGcDrawerWrapper extends DynamicImageProviderWrapper {
 		} finally {
 			gc.dispose();
 			image.dispose();
+		}
+	}
+
+	private class DrawableWrapper implements Drawable {
+		private final Image image;
+		private final int zoom;
+
+		public DrawableWrapper(Image image, int zoom) {
+			this.image = image;
+			this.zoom = zoom;
+		}
+
+		@Override
+		public long internal_new_GC(GCData data) {
+			return this.image.configureGC(data, zoom);
+		}
+
+		@Override
+		public void internal_dispose_GC(long handle, GCData data) {
+			this.image.internal_dispose_GC(handle, data);
 		}
 	}
 

commit 5826d3e659b447d5f39ce1aee2eb17254c831f30

[win32] Fix double buffer paint with ImageGcDrawer
This commit fixes a regression introduced with f10721a that did not
properly retain the currently drawn zoom level when an ImageGcDrawer
is used. That leads to issues with zoom != 100.

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Image.java
@@ -1691,7 +1691,7 @@ private ImageHandle init(ImageData i, int zoom) {
  */
 @Override
 public long internal_new_GC (GCData data) {
-	return configureGC(data, 100);
+	return this.imageProvider.configureGCData(data);
 }
 
 private long configureGC(GCData data, int zoom) {
@@ -1853,6 +1853,10 @@ public static Image win32_new(Device device, int type, long handle, int nativeZo
 private abstract class AbstractImageProviderWrapper {
 
 	protected abstract Rectangle getBounds(int zoom);
+
+	protected long configureGCData(GCData data) {
+		return configureGC(data, 100);
+	}
 
 	public Collection<Integer> getPreservedZoomLevels() {
 		return Collections.emptySet();
@@ -2480,6 +2484,7 @@ private class ImageGcDrawerWrapper extends DynamicImageProviderWrapper {
 	private ImageGcDrawer drawer;
 	private int width;
 	private int height;
+	private int currentZoom = 100;
 
 	ImageGcDrawerWrapper(ImageGcDrawer imageGcDrawer, int width, int height) {
 		checkProvider(imageGcDrawer, ImageGcDrawer.class);
@@ -2495,12 +2500,18 @@ private class ImageGcDrawerWrapper extends DynamicImageProviderWrapper {
 	}
 
 	@Override
+	protected long configureGCData(GCData data) {
+		return configureGC(data, currentZoom);
+	}
+
+	@Override
 	ImageData newImageData(int zoom) {
 		return getImageMetadata(zoom).getImageData();
 	}
 
 	@Override
 	protected ImageHandle newImageHandle(int zoom) {
+		currentZoom = zoom;
 		int gcStyle = drawer.getGcStyle();
 		Image image;
 		if ((gcStyle & SWT.TRANSPARENT) != 0) {