fc1f0779b495477c655ce7428f09deb64fbca63d
Thomas Singer <ts-swt@syntevo.com>
2025-06-05 18:22:31 +0200

    Control.gtk_event_after: simplify
    no array is needed

--- bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
+++ bundles/org.eclipse.swt/Eclipse SWT/gtk/org/eclipse/swt/widgets/Control.java
@@ -3731,10 +3731,9 @@ long gtk_event_after (long widget, long gdkEvent) {
 		}
 		case GDK.GDK_FOCUS_CHANGE: {
 			if (!isFocusHandle (widget)) break;
-			boolean [] focusIn = new boolean [1];
 			GdkEventFocus gdkEventFocus = new GdkEventFocus ();
 			GTK3.memmove (gdkEventFocus, gdkEvent, GdkEventFocus.sizeof);
-			focusIn[0] = gdkEventFocus.in != 0;
+			boolean focusIn = gdkEventFocus.in != 0;
 
 			/*
 			 * Feature in GTK. The GTK combo box popup under some window managers
@@ -3746,7 +3745,7 @@ long gtk_event_after (long widget, long gdkEvent) {
 			 * NOTE: This code runs for all menus.
 			 */
 			Display display = this.display;
-			if (focusIn[0]) {
+			if (focusIn) {
 				if (display.ignoreFocus) {
 					display.ignoreFocus = false;
 					break;
@@ -3761,7 +3760,7 @@ long gtk_event_after (long widget, long gdkEvent) {
 					}
 				}
 			}
-			sendFocusEvent (focusIn[0] ? SWT.FocusIn : SWT.FocusOut);
+			sendFocusEvent (focusIn ? SWT.FocusIn : SWT.FocusOut);
 			break;
 		}
 	}
