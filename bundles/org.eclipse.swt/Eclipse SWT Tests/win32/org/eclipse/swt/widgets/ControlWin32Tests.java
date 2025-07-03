e15f5491f3a513a433b90c9aacecdb9f23301a34
Amartya Parijat <amartya.parijat@yatta.de>
2025-07-03 15:05:02 +0200

    Reintroduce scaling methods for Geometries
    This commit reintroduces scaling methods for plain Rectangle & Point to provide
    backwards compatibility to the consumers which explicitly create a
    Rectangle & Point and want to call scaleBounds or scaleUp/scaleDown since they
    have different behaviour and Rectangle.OfFloat unifies this.

--- bundles/org.eclipse.swt/Eclipse SWT Tests/win32/org/eclipse/swt/widgets/ControlWin32Tests.java
+++ bundles/org.eclipse.swt/Eclipse SWT Tests/win32/org/eclipse/swt/widgets/ControlWin32Tests.java
@@ -100,11 +100,11 @@ class ControlWin32Tests {
 
 		button.setBounds(new Rectangle(0, 47, 200, 47));
 		assertEquals("Control::setBounds(Rectangle) doesn't scale up correctly",
-				new Rectangle(0, 82, 350, 82), button.getBoundsInPixels());
+				new Rectangle(0, 82, 350, 83), button.getBoundsInPixels());
 
 		button.setBounds(0, 47, 200, 47);
 		assertEquals("Control::setBounds(int, int, int, int) doesn't scale up correctly",
-				new Rectangle(0, 82, 350, 82), button.getBoundsInPixels());
+				new Rectangle(0, 82, 350, 83), button.getBoundsInPixels());
 	}
 
 	record FontComparison(int originalFontHeight, int currentFontHeight) {
