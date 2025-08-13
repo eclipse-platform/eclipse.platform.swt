eclipse-platform/eclipse.platform.swt

commit e8aadbb8fa15ab03cfe7b972a5567eb2b36394ee

[Win32] Refactor the Cursor constructors
Extracting out the OS handle creation logic into separate methods.
Intention is to make them reusable if someone want to create only a OS
handle within a Cursor class.

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Cursor.java
@@ -135,6 +135,11 @@ Cursor(Device device) {
  */
 public Cursor(Device device, int style) {
 	this(device);
+	this.handle = setupCursorFromStyle(style);
+	init();
+}
+
+private static long setupCursorFromStyle(int style) {
 	long lpCursorName = 0;
 	switch (style) {
 		case SWT.CURSOR_HAND: 		lpCursorName = OS.IDC_HAND; break;
@@ -162,9 +167,9 @@ public Cursor(Device device, int style) {
 		default:
 			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
 	}
-	handle = OS.LoadCursor(0, lpCursorName);
+	long handle = OS.LoadCursor(0, lpCursorName);
 	if (handle == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-	init();
+	return handle;
 }
 
 /**
@@ -207,6 +212,12 @@ public Cursor(Device device, ImageData source, ImageData mask, int hotspotX, int
 	this.hotspotX = hotspotX;
 	this.hotspotY = hotspotY;
 	this.imageDataProvider = null;
+	this.handle = setupCursorFromImageData(source, mask, hotspotX, hotspotY);
+	init();
+	this.device.registerResourceWithZoomSupport(this);
+}
+
+private static long setupCursorFromImageData(ImageData source, ImageData mask, int hotspotX, int hotspotY) {
 	if (source == null) SWT.error(SWT.ERROR_NULL_ARGUMENT);
 	if (mask == null) {
 		if (source.getTransparencyType() != SWT.TRANSPARENCY_MASK) {
@@ -233,10 +244,9 @@ public Cursor(Device device, ImageData source, ImageData mask, int hotspotX, int
 
 	/* Create the cursor */
 	long hInst = OS.GetModuleHandle(null);
-	handle = OS.CreateCursor(hInst, hotspotX, hotspotY, source.width, source.height, sourceData, maskData);
+	long handle = OS.CreateCursor(hInst, hotspotX, hotspotY, source.width, source.height, sourceData, maskData);
 	if (handle == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-	init();
-	this.device.registerResourceWithZoomSupport(this);
+	return handle;
 }
 
 /**
@@ -275,10 +285,13 @@ public Cursor(Device device, ImageData source, int hotspotX, int hotspotY) {
 	this.hotspotX = hotspotX;
 	this.hotspotY = hotspotY;
 	this.imageDataProvider = null;
-	setupCursorFromImageData(source);
+	this.handle = setupCursorFromImageData(device, source, hotspotX, hotspotY);
+	isIcon = true;
+	init();
+	this.device.registerResourceWithZoomSupport(this);
 }
 
-private void setupCursorFromImageData(ImageData source) {
+private static long setupCursorFromImageData(Device device, ImageData source, int hotspotX, int hotspotY) {
 	if (source == null) SWT.error(SWT.ERROR_NULL_ARGUMENT);
 	/* Check the hotspots */
 	if (hotspotX >= source.width || hotspotX < 0 ||
@@ -333,7 +346,7 @@ private void setupCursorFromImageData(ImageData source) {
 		if (hMask == 0) SWT.error(SWT.ERROR_NO_HANDLES);
 	} else {
 		ImageData mask = source.getTransparencyMask();
-		long [] result = Image.initIcon(this.device, source, mask);
+		long [] result = Image.initIcon(device, source, mask);
 		hBitmap = result[0];
 		hMask = result[1];
 	}
@@ -344,13 +357,12 @@ private void setupCursorFromImageData(ImageData source) {
 	info.hbmMask = hMask;
 	info.xHotspot = hotspotX;
 	info.yHotspot = hotspotY;
-	handle = OS.CreateIconIndirect(info);
+	long handle = OS.CreateIconIndirect(info);
 	OS.DeleteObject(hBitmap);
 	OS.DeleteObject(hMask);
 	if (handle == 0) SWT.error(SWT.ERROR_NO_HANDLES);
-	isIcon = true;
-	init();
-	this.device.registerResourceWithZoomSupport(this);
+
+	return handle;
 }
 
 /**
@@ -390,7 +402,10 @@ public Cursor(Device device, ImageDataProvider imageDataProvider, int hotspotX, 
 	this.mask = null;
 	this.hotspotX = hotspotX;
 	this.hotspotY = hotspotY;
-	setupCursorFromImageData(this.source);
+	this.handle = setupCursorFromImageData(device, this.source, hotspotX, hotspotY);
+	isIcon = true;
+	init();
+	this.device.registerResourceWithZoomSupport(this);
 }
 
 /**

commit b16c084024db4dc21b0e83e7af117822b50f0de6

Fix NullPointerException when creating cursor with null device #2414
This is a follow-up to e8aadbb8fa15ab03cfe7b972a5567eb2b36394ee which
introduced a regression when working with null devices.

If null is passed as argument, the value of Device.getDevice() is used
as a fallback when initializing this field. This field must be used for
all other operations in the constructor, rather than the constructor
argument.

Closes https://github.com/eclipse-platform/eclipse.platform.swt/issues/2414

bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/Cursor.java
@@ -119,7 +119,7 @@ public final class Cursor extends Resource {
 public Cursor(Device device, int style) {
 	super(device);
 	this.cursorHandleProvider = new StyleCursorHandleProvider(style);
-	this.handle = this.cursorHandleProvider.createHandle(device, DEFAULT_ZOOM).getHandle();
+	this.handle = this.cursorHandleProvider.createHandle(this.device, DEFAULT_ZOOM).getHandle();
 	init();
 	this.device.registerResourceWithZoomSupport(this);
 }
@@ -160,7 +160,7 @@ public Cursor(Device device, int style) {
 public Cursor(Device device, ImageData source, ImageData mask, int hotspotX, int hotspotY) {
 	super(device);
 	this.cursorHandleProvider = new ImageDataWithMaskCursorHandleProvider(source, mask, hotspotX, hotspotY);
-	this.handle = this.cursorHandleProvider.createHandle(device, DEFAULT_ZOOM).getHandle();
+	this.handle = this.cursorHandleProvider.createHandle(this.device, DEFAULT_ZOOM).getHandle();
 	init();
 	this.device.registerResourceWithZoomSupport(this);
 }
@@ -229,7 +229,7 @@ private static CursorHandle setupCursorFromImageData(ImageData source, ImageData
 public Cursor(Device device, ImageData source, int hotspotX, int hotspotY) {
 	super(device);
 	this.cursorHandleProvider = new ImageDataCursorHandleProvider(source, hotspotX, hotspotY);
-	this.handle = this.cursorHandleProvider.createHandle(device, DEFAULT_ZOOM).getHandle();
+	this.handle = this.cursorHandleProvider.createHandle(this.device, DEFAULT_ZOOM).getHandle();
 	init();
 	this.device.registerResourceWithZoomSupport(this);
 }
@@ -341,7 +341,7 @@ public Cursor(Device device, ImageDataProvider imageDataProvider, int hotspotX, 
 	super(device);
 	if (imageDataProvider == null) SWT.error(SWT.ERROR_NULL_ARGUMENT);
 	this.cursorHandleProvider = new ImageDataProviderCursorHandleProvider(imageDataProvider, hotspotX, hotspotY);
-	this.handle = this.cursorHandleProvider.createHandle(device, DEFAULT_ZOOM).getHandle();
+	this.handle = this.cursorHandleProvider.createHandle(this.device, DEFAULT_ZOOM).getHandle();
 	init();
 	this.device.registerResourceWithZoomSupport(this);
 }

tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_graphics_Cursor.java
@@ -154,6 +154,8 @@ public void test_ConstructorWithImageDataProvider() {
 	Image sourceImage = new Image(display, 10, 10);
 	Cursor cursor = new Cursor(display, sourceImage::getImageData, 0, 0);
 	cursor.dispose();
+	cursor = new Cursor(null, sourceImage::getImageData, 0, 0);
+	cursor.dispose();
 	sourceImage.dispose();
 
 	assertThrows("No exception thrown when ImageDataProvider is null",