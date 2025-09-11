8444ec7a85304cf50d35c121b79df01559534c6b
arunjose696 <arun.jose@yatta.de>
2025-09-11 08:21:36 +0200

    Add internal API in SVGrasterizer to rasterize images at given height and width
    Adding internal API to SVGRasterizer and simple tests to see if the svgs
    are loaded at right sizes by the rasterizer

--- bundles/org.eclipse.swt.svg/src/org/eclipse/swt/svg/JSVGRasterizer.java
+++ bundles/org.eclipse.swt.svg/src/org/eclipse/swt/svg/JSVGRasterizer.java
@@ -74,32 +74,45 @@ public class JSVGRasterizer implements SVGRasterizer {
 		if (zoom < 0) {
 			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
 		}
-		SVGDocument svgDocument = loadSVG(inputStream);
-		if (svgDocument == null) {
-			SWT.error(SWT.ERROR_INVALID_IMAGE);
-		}
+		SVGDocument svgDocument = loadAndValidateSVG(inputStream);
 		BufferedImage rasterizedImage = renderSVG(svgDocument, zoom);
 		return convertToSWTImageData(rasterizedImage);
 	}
 
-	private SVGDocument loadSVG(InputStream inputStream) {
-		return SVG_LOADER.load(inputStream, null, LoaderContext.createDefault());
+	@Override
+	public ImageData rasterizeSVG(InputStream inputStream, int width, int height) {
+		SVGDocument svgDocument = loadAndValidateSVG(inputStream);
+		BufferedImage rasterizedImage = renderSVG(svgDocument, width, height);
+		return convertToSWTImageData(rasterizedImage);
+	}
+	
+	private SVGDocument loadAndValidateSVG(InputStream inputStream) {
+		SVGDocument svgDocument = SVG_LOADER.load(inputStream, null, LoaderContext.createDefault());
+		if (svgDocument == null) {
+			SWT.error(SWT.ERROR_INVALID_IMAGE);
+		}
+		return svgDocument;
 	}
 
 	private BufferedImage renderSVG(SVGDocument svgDocument, int zoom) {
+		FloatSize sourceImageSize = svgDocument.size();
 		float scalingFactor = zoom / 100.0f;
-		BufferedImage image = createImageBase(svgDocument, scalingFactor);
-		Graphics2D g = configureRenderingOptions(scalingFactor, image);
+		int targetImageWidth = calculateTargetWidth(scalingFactor, sourceImageSize);
+		int targetImageHeight = calculateTargetHeight(scalingFactor, sourceImageSize);
+		return renderSVG(svgDocument, targetImageWidth, targetImageHeight);
+	}
+	
+	private BufferedImage renderSVG(SVGDocument svgDocument, int width, int height) {
+		if (width <= 0 || height <= 0) {
+			SWT.error(SWT.ERROR_INVALID_ARGUMENT);
+		}
+		BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+		float widthScalingFactor = width / svgDocument.size().width;
+		float heightScalingFactor = height / svgDocument.size().height;
+		Graphics2D g = configureRenderingOptions(widthScalingFactor, heightScalingFactor, image);
 		svgDocument.render(null, g);
 		g.dispose();
 		return image;
-	}
-
-	private BufferedImage createImageBase(SVGDocument svgDocument, float scalingFactor) {
-		FloatSize sourceImageSize = svgDocument.size();
-		int targetImageWidth = calculateTargetWidth(scalingFactor, sourceImageSize);
-		int targetImageHeight = calculateTargetHeight(scalingFactor, sourceImageSize);
-		return new BufferedImage(targetImageWidth, targetImageHeight, BufferedImage.TYPE_INT_ARGB);
 	}
 
 	private int calculateTargetWidth(float scalingFactor, FloatSize sourceImageSize) {
@@ -112,10 +125,11 @@ public class JSVGRasterizer implements SVGRasterizer {
 		return (int) Math.round(sourceImageHeight * scalingFactor);
 	}
 
-	private Graphics2D configureRenderingOptions(float scalingFactor, BufferedImage image) {
+	private Graphics2D configureRenderingOptions(float widthScalingFactor, float heightScalingFactor,
+			BufferedImage image) {
 		Graphics2D g = image.createGraphics();
 		g.setRenderingHints(RENDERING_HINTS);
-		g.scale(scalingFactor, scalingFactor);
+		g.scale(widthScalingFactor, heightScalingFactor);
 		return g;
 	}
 
