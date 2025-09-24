f7223a882ab6c565e4ae9054bfab902e39aa242e
Jonah Graham <jonah@kichwacoders.com>
2025-09-24 15:49:49 -0400

    Sort all test names passed to @SelectClasses
    Also formats them so that they can be automatically formatted easily
    in the future with JDT Formatter by forcing line breaks with `//`
    
    The [AnyEdit Tools](https://marketplace.eclipse.org/content/anyedit-tools)
    can be used to sort the list in the future.
    
    This is being done to make it easier to identify if tests are missing
    from the lists.

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllNonBrowserTests.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllNonBrowserTests.java
@@ -27,27 +27,47 @@ import org.junit.platform.suite.api.Suite;
  * Suite for running most SWT test cases (all except for browser tests).
  */
 @Suite
-@SelectClasses({ Test_org_eclipse_swt_SWT.class, Test_org_eclipse_swt_SWTException.class,
-		Test_org_eclipse_swt_SWTError.class, Test_org_eclipse_swt_widgets_Display.class, AllGraphicsTests.class,
-		AllWidgetTests.class, Test_org_eclipse_swt_layout_GridData.class,
-		Test_org_eclipse_swt_events_ControlEvent.class, Test_org_eclipse_swt_events_ModifyEvent.class,
-		Test_org_eclipse_swt_events_ArmEvent.class, Test_org_eclipse_swt_events_ShellEvent.class,
-		Test_org_eclipse_swt_events_TypedEvent.class, Test_org_eclipse_swt_events_PaintEvent.class,
-		Test_org_eclipse_swt_events_VerifyEvent.class, Test_org_eclipse_swt_events_KeyEvent.class,
-		Test_org_eclipse_swt_events_TraverseEvent.class, Test_org_eclipse_swt_events_DisposeEvent.class,
-		Test_org_eclipse_swt_events_SelectionEvent.class, Test_org_eclipse_swt_events_HelpEvent.class,
-		Test_org_eclipse_swt_events_FocusEvent.class, Test_org_eclipse_swt_events_MouseEvent.class,
-		Test_org_eclipse_swt_events_MenuEvent.class, Test_org_eclipse_swt_events_TreeEvent.class,
-		Test_org_eclipse_swt_layout_FormAttachment.class, Test_org_eclipse_swt_layout_BorderLayout.class,
-		Test_org_eclipse_swt_printing_PrintDialog.class, Test_org_eclipse_swt_printing_PrinterData.class,
-		Test_org_eclipse_swt_printing_Printer.class, Test_org_eclipse_swt_program_Program.class,
-		Test_org_eclipse_swt_accessibility_Accessible.class,
-		Test_org_eclipse_swt_accessibility_AccessibleControlEvent.class,
-		Test_org_eclipse_swt_accessibility_AccessibleEvent.class,
-		Test_org_eclipse_swt_accessibility_AccessibleTextEvent.class,
-		Test_org_eclipse_swt_internal_SVGRasterizer.class,
-		DPIUtilTests.class,
-		JSVGRasterizerTest.class})
+@SelectClasses({ //
+		// Basic tests
+		Test_org_eclipse_swt_SWT.class, //
+		Test_org_eclipse_swt_SWTException.class, //
+		Test_org_eclipse_swt_SWTError.class, //
+		Test_org_eclipse_swt_widgets_Display.class, //
+		// Groups of tests
+		AllGraphicsTests.class, //
+		AllWidgetTests.class, //
+		// Rest of tests alphabetically
+		DPIUtilTests.class, //
+		JSVGRasterizerTest.class, //
+		Test_org_eclipse_swt_accessibility_Accessible.class, //
+		Test_org_eclipse_swt_accessibility_AccessibleControlEvent.class, //
+		Test_org_eclipse_swt_accessibility_AccessibleEvent.class, //
+		Test_org_eclipse_swt_accessibility_AccessibleTextEvent.class, //
+		Test_org_eclipse_swt_events_ArmEvent.class, //
+		Test_org_eclipse_swt_events_ControlEvent.class, //
+		Test_org_eclipse_swt_events_DisposeEvent.class, //
+		Test_org_eclipse_swt_events_FocusEvent.class, //
+		Test_org_eclipse_swt_events_HelpEvent.class, //
+		Test_org_eclipse_swt_events_KeyEvent.class, //
+		Test_org_eclipse_swt_events_MenuEvent.class, //
+		Test_org_eclipse_swt_events_ModifyEvent.class, //
+		Test_org_eclipse_swt_events_MouseEvent.class, //
+		Test_org_eclipse_swt_events_PaintEvent.class, //
+		Test_org_eclipse_swt_events_SelectionEvent.class, //
+		Test_org_eclipse_swt_events_ShellEvent.class, //
+		Test_org_eclipse_swt_events_TraverseEvent.class, //
+		Test_org_eclipse_swt_events_TreeEvent.class, //
+		Test_org_eclipse_swt_events_TypedEvent.class, //
+		Test_org_eclipse_swt_events_VerifyEvent.class, //
+		Test_org_eclipse_swt_internal_SVGRasterizer.class, //
+		Test_org_eclipse_swt_layout_BorderLayout.class, //
+		Test_org_eclipse_swt_layout_FormAttachment.class, //
+		Test_org_eclipse_swt_layout_GridData.class, //
+		Test_org_eclipse_swt_printing_PrintDialog.class, //
+		Test_org_eclipse_swt_printing_Printer.class, //
+		Test_org_eclipse_swt_printing_PrinterData.class, //
+		Test_org_eclipse_swt_program_Program.class, //
+})
 public class AllNonBrowserTests {
 	private static List<Error> leakedResources;
 
--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllWidgetTests.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/AllWidgetTests.java
@@ -21,35 +21,57 @@ import org.junit.platform.suite.api.Suite;
  * Suite for testing all of the widget test cases.
  */
 @Suite
-@SelectClasses({ Test_org_eclipse_swt_widgets_Shell.class,
-		Test_org_eclipse_swt_widgets_ExpandItem.class, Test_org_eclipse_swt_widgets_MenuItem.class,
-		Test_org_eclipse_swt_widgets_ToolItem.class, Test_org_eclipse_swt_widgets_TabItem.class,
-		Test_org_eclipse_swt_widgets_TableItem.class, Test_org_eclipse_swt_widgets_TableColumn.class,
-		Test_org_eclipse_swt_widgets_TreeItem.class, Test_org_eclipse_swt_widgets_Caret.class,
-		Test_org_eclipse_swt_widgets_Event.class, Test_org_eclipse_swt_widgets_Menu.class,
-		Test_org_eclipse_swt_widgets_Label.class,
-		Test_org_eclipse_swt_widgets_Button.class, Test_org_eclipse_swt_widgets_ExpandBar.class,
-		Test_org_eclipse_swt_widgets_List.class, Test_org_eclipse_swt_widgets_Text.class,
-		Test_org_eclipse_swt_widgets_ScrollBar.class, Test_org_eclipse_swt_widgets_Sash.class,
-		Test_org_eclipse_swt_widgets_Tree.class, Test_org_eclipse_swt_widgets_TabFolder.class,
-		Test_org_eclipse_swt_widgets_Combo.class, Test_org_eclipse_swt_widgets_Group.class,
-		Test_org_eclipse_swt_widgets_ToolBar.class, Test_org_eclipse_swt_widgets_Table.class,
-		Test_org_eclipse_swt_widgets_Canvas.class, Test_org_eclipse_swt_widgets_Scale.class,
-		Test_org_eclipse_swt_widgets_Slider.class, Test_org_eclipse_swt_widgets_ProgressBar.class,
-		Test_org_eclipse_swt_widgets_Composite.class, Test_org_eclipse_swt_widgets_Link.class,
-		Test_org_eclipse_swt_widgets_DateTime.class, Test_org_eclipse_swt_widgets_ColorDialog.class,
-		Test_org_eclipse_swt_widgets_FileDialog.class, Test_org_eclipse_swt_widgets_DirectoryDialog.class,
-		Test_org_eclipse_swt_widgets_FontDialog.class, Test_org_eclipse_swt_widgets_MessageBox.class,
-		Test_org_eclipse_swt_widgets_Monitor.class,
-		Test_org_eclipse_swt_custom_StyleRange.class, Test_org_eclipse_swt_custom_CCombo.class,
-		Test_org_eclipse_swt_custom_CLabel.class,
-		Test_org_eclipse_swt_custom_CTabItem.class,
-		Test_org_eclipse_swt_custom_StyledText.class,
-		Test_org_eclipse_swt_custom_StyledText_VariableLineHeight.class,
-		Test_org_eclipse_swt_custom_StyledText_multiCaretsSelections.class,
-		Test_org_eclipse_swt_custom_StyledTextLineSpacingProvider.class,
-		Test_org_eclipse_swt_custom_CTabFolder.class, Test_org_eclipse_swt_widgets_Spinner.class,
-		Test_org_eclipse_swt_widgets_ScrolledComposite.class,
-		Test_org_eclipse_swt_custom_BusyIndicator.class})
+@SelectClasses({ //
+		// Sorted list of tests
+		Test_org_eclipse_swt_custom_BusyIndicator.class, //
+		Test_org_eclipse_swt_custom_CCombo.class, //
+		Test_org_eclipse_swt_custom_CLabel.class, //
+		Test_org_eclipse_swt_custom_CTabFolder.class, //
+		Test_org_eclipse_swt_custom_CTabItem.class, //
+		Test_org_eclipse_swt_custom_StyleRange.class, //
+		Test_org_eclipse_swt_custom_StyledText.class, //
+		Test_org_eclipse_swt_custom_StyledTextLineSpacingProvider.class, //
+		Test_org_eclipse_swt_custom_StyledText_VariableLineHeight.class, //
+		Test_org_eclipse_swt_custom_StyledText_multiCaretsSelections.class, //
+		Test_org_eclipse_swt_widgets_Button.class, //
+		Test_org_eclipse_swt_widgets_Canvas.class, //
+		Test_org_eclipse_swt_widgets_Caret.class, //
+		Test_org_eclipse_swt_widgets_ColorDialog.class, //
+		Test_org_eclipse_swt_widgets_Combo.class, //
+		Test_org_eclipse_swt_widgets_Composite.class, //
+		Test_org_eclipse_swt_widgets_DateTime.class, //
+		Test_org_eclipse_swt_widgets_DirectoryDialog.class, //
+		Test_org_eclipse_swt_widgets_Event.class, //
+		Test_org_eclipse_swt_widgets_ExpandBar.class, //
+		Test_org_eclipse_swt_widgets_ExpandItem.class, //
+		Test_org_eclipse_swt_widgets_FileDialog.class, //
+		Test_org_eclipse_swt_widgets_FontDialog.class, //
+		Test_org_eclipse_swt_widgets_Group.class, //
+		Test_org_eclipse_swt_widgets_Label.class, //
+		Test_org_eclipse_swt_widgets_Link.class, //
+		Test_org_eclipse_swt_widgets_List.class, //
+		Test_org_eclipse_swt_widgets_Menu.class, //
+		Test_org_eclipse_swt_widgets_MenuItem.class, //
+		Test_org_eclipse_swt_widgets_MessageBox.class, //
+		Test_org_eclipse_swt_widgets_Monitor.class, //
+		Test_org_eclipse_swt_widgets_ProgressBar.class, //
+		Test_org_eclipse_swt_widgets_Sash.class, //
+		Test_org_eclipse_swt_widgets_Scale.class, //
+		Test_org_eclipse_swt_widgets_ScrollBar.class, //
+		Test_org_eclipse_swt_widgets_ScrolledComposite.class, //
+		Test_org_eclipse_swt_widgets_Shell.class, //
+		Test_org_eclipse_swt_widgets_Slider.class, //
+		Test_org_eclipse_swt_widgets_Spinner.class, //
+		Test_org_eclipse_swt_widgets_TabFolder.class, //
+		Test_org_eclipse_swt_widgets_TabItem.class, //
+		Test_org_eclipse_swt_widgets_Table.class, //
+		Test_org_eclipse_swt_widgets_TableColumn.class, //
+		Test_org_eclipse_swt_widgets_TableItem.class, //
+		Test_org_eclipse_swt_widgets_Text.class, //
+		Test_org_eclipse_swt_widgets_ToolBar.class, //
+		Test_org_eclipse_swt_widgets_ToolItem.class, //
+		Test_org_eclipse_swt_widgets_Tree.class, //
+		Test_org_eclipse_swt_widgets_TreeItem.class, //
+})
 public class AllWidgetTests {
 }
