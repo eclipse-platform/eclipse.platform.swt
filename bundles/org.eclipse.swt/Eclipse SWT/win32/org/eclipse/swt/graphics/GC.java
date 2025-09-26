c5bfc29abef4b601a8a153f46afa08539e7698cd
arunjose696 <arun.jose@yatta.de>
2025-09-26 16:46:29 +0200

    Fix null pointer when drawable of a GC is null
    The drawable on a GC can be null this would result in a null pointer
    error in GC#calculateZoomForImage as we check if the drawable is not
    autoScalable.

--- bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/GC.java
+++ bundles/org.eclipse.swt/Eclipse SWT/win32/org/eclipse/swt/graphics/GC.java
@@ -1148,7 +1148,7 @@ private class DrawScalingImageToImageOperation extends ImageOperation {
 			// unscaled images can use the GC zoom
 			return gcZoom;
 		}
-		if (!drawable.isAutoScalable()) {
+		if (drawable != null && !drawable.isAutoScalable()) {
 			return gcZoom;
 		}
 
