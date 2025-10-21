69ccb90b0fa8e076f574c5c059073c7197ab71ed
Jonah Graham <jonah@kichwacoders.com>
2025-10-21 14:51:59 -0400

    Spin event loop in widget tests that access Clipboard
    This carries on earlier work, such as PR
    https://github.com/eclipse-platform/eclipse.platform.swt/pull/2489
    
    Part of https://github.com/eclipse-platform/eclipse.platform.swt/issues/2598

--- tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_CCombo.java
+++ tests/org.eclipse.swt.tests/JUnit Tests/org/eclipse/swt/tests/junit/Test_org_eclipse_swt_custom_CCombo.java
@@ -78,7 +78,7 @@ public void test_ConstructorLorg_eclipse_swt_widgets_CompositeI() {
 
 @Tag("clipboard")
 @Test
-public void test_copy() {
+public void test_copy() throws InterruptedException {
 	if (SwtTestUtil.isCocoa) {
 		// TODO Fix Cocoa failure.
 		if (SwtTestUtil.verbose) {
@@ -92,6 +92,7 @@ public void test_copy() {
 	ccombo.copy();
 	ccombo.setSelection(new Point(0,0));
 	ccombo.paste();
+	SwtTestUtil.processEvents(1000, () -> "23123456".equals(ccombo.getText()));
 	assertEquals("23123456", ccombo.getText());
 }
 
@@ -135,7 +136,7 @@ public void test_isFocusControl() {
 
 @Tag("clipboard")
 @Test
-public void test_paste() {
+public void test_paste() throws InterruptedException {
 	if (SwtTestUtil.isCocoa) {
 		// TODO Fix Cocoa failure.
 		if (SwtTestUtil.verbose) {
@@ -147,8 +148,10 @@ public void test_paste() {
 	ccombo.setText("123456");
 	ccombo.setSelection(new Point(1,3));
 	ccombo.cut();
+	SwtTestUtil.processEvents(1000, () -> "1456".equals(ccombo.getText()));
 	assertEquals("1456", ccombo.getText());
 	ccombo.paste();
+	SwtTestUtil.processEvents(1000, () -> "123456".equals(ccombo.getText()));
 	assertEquals("123456", ccombo.getText());
 }
 
