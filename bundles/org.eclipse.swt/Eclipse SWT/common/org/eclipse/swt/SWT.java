950fd8c85f7bf6bd349faa4ea2e11641d1a34abb
Александър Куртаков <akurtakov@gmail.com>
2026-03-07 17:51:51 +0200

    Remove SWT.CHROMIUM constant
    It is not doing anything, causes confusion in the API and deprecation
    period passed.
    Minor version bumped for the removal.

--- bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWT.java
+++ bundles/org.eclipse.swt/Eclipse SWT/common/org/eclipse/swt/SWT.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2000, 2021 IBM Corporation and others.
+ * Copyright (c) 2000, 2026 IBM Corporation and others.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -2563,22 +2563,6 @@ public class SWT {
 	 * @since 3.7
 	 */
 	public static final int WEBKIT = 1 << 16;
-
-	/**
-	 * Style constant specifying that a Browser should use Chromium Embedded Framework
-	 * for rendering its content (value is 1&lt;&lt;17).
-	 * <p><b>Note:</b> No longer supported and ignored.</p>
-	 * <p><b>Used By:</b></p>
-	 * <ul>
-	 * <li><code>Browser</code></li>
-	 * </ul>
-	 *
-	 * @since 3.115
-	 * @deprecated Support for Chromium was limited to ancient and full of CVEs version of Chromium.
-	 * See <a href="https://bugs.eclipse.org/bugs/show_bug.cgi?id=572010">bug report</a> for details
-	 */
-	@Deprecated(forRemoval = true, since = "2024-03")
-	public static final int CHROMIUM = 1 << 17;
 
 	/**
 	 * Style constant specifying that a Browser should use Edge (WebView2)
