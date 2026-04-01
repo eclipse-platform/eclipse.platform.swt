2eae123a9ecd938f03e3832c1404e0c30544178f
Lars Vogel <Lars.Vogel@vogella.com>
2026-04-01 09:53:41 +0200

    Migrate Ripple effect to Graphics Example
    See https://github.com/lcaron/SWT-OldSchoolEffect/issues/3 or
    https://github.com/lcaron/SWT-OldSchoolEffect for all SWT examples

--- examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/graphics/GraphicsExample.java
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/graphics/GraphicsExample.java
@@ -470,6 +470,7 @@ GraphicsTab[] createTabs() {
 		new LineJoinTab(this),
 		new RegionClippingTab(this),
 		new StarfieldTab(this),
+		new RippleTab(this),
 		new CustomAlphaTab(this),
 		new TextAntialiasTab(this),
 		new GraphicAntialiasTab(this),
