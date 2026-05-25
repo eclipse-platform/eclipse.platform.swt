823aeba299457f36f3c3f63fefe099b8ca9d9e25
Александър Куртаков <akurtakov@gmail.com>
2026-05-25 19:41:53 +0300

    Simplify Gtk development tools
    * Remove unneeded and outdated scripts.
    * Update install_sysdeps.sh for current Fedora
    * Improve gtk-dev-guide for the facts that there is only one repo,
    .classpath files don't have to be edited, gtk4 is built by default and
    etc.

--- bundles/org.eclipse.swt.tools/gtk/install_sysdeps.sh
+++ bundles/org.eclipse.swt.tools/gtk/install_sysdeps.sh
@@ -8,56 +8,45 @@
 # 2) Add a case to the case statement below.
 # 3) Add a function to handle your distribution.
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
 
 func_echo_plus () {
 	# Echo function that prints output in green to distinguish it from sub-shell output.
-	GREEN='\033[0;32m'
-	NC='\033[0m' # No Color
 	echo -e "${GREEN}${@}${NC}"
 }
 
 func_echo_error () {
 	# As above, but in red. Also pre-appends '***' to output.
-	RED='\033[0;31m'
-	NC='\033[0m' # No Color
-echo -e "${RED}*** ${@}${NC}"
+	echo -e "${RED}*** ${@}${NC}"
 }
 
 func_configure_fedora () {
-	FEDORA_VERSION=$(cat /etc/system-release | cut -f3 -d" ")
-	if [ "$FEDORA_VERSION" -lt "21" ]; then
-		INSTALL_CMD="yum"
-	else
-		INSTALL_CMD="dnf"
-	fi
-
 
 	func_echo_plus "Installing C Development Tools"
-	set -x
-	sudo $INSTALL_CMD -y groups install "C Development Tools and Libraries"
+	sudo dnf -y groups install "C Development Tools and Libraries"
 
-	func_echo_plus "Installing Java 11 development packages that include jni.h for JNI bindings."
-	sudo $INSTALL_CMD -y install java-11-openjdk-devel.x86_64
+	func_echo_plus "Installing Java development packages that include jni.h for JNI bindings."
+	sudo dnf -y install java-25-openjdk-devel
 
-	func_echo_plus "Installing Gtk3 development packages"
-	sudo $INSTALL_CMD -y install gtk3-devel
-
-	func_echo_plus "Installing X11 Development libraries. Someday when wayland takes over these will not be needed..."
-	# Deals with error: "#include <X11/Intrinsic.h>, #include <X11/extensions/XTest.h>" build errors)
-	sudo $INSTALL_CMD -y install libXt-devel
+	func_echo_plus "Installing Gtk development packages"
+	sudo dnf -y install gtk3-devel gtk4-devel
 
 	func_echo_plus "Install Mesa (OpenGL headers)"
 	# Deals with error: "/usr/bin/ld: cannot find -lGLU collect2: error: ld returned 1 exit status"
-	sudo $INSTALL_CMD -y install mesa-libGLU-devel
+	sudo dnf -y install mesa-libGLU-devel
 
 	func_echo_plus "Done"
 }
 
 
-LINUX_DISTRO=$(cat /etc/system-release | cut -f1 -d" ")
-if [ "$LINUX_DISTRO" = "" ]; then
-	func_echo_error "Error, could not identify your distribution"
-	exit
+if [ -f /etc/os-release ]; then
+    . /etc/os-release
+    LINUX_DISTRO="$ID"
+else
+    echo "Error: cannot identify distribution"
+    exit 1
 fi
 
 DISTRO_NOT_KNOWN_MSG="
@@ -66,14 +55,13 @@ Consider updating this script for your distribution.
 In general, You should install the following packages:
  - C Development tools (usually comes in a 'group install')
  - java-*-openjdk-devel  (depending on current version of java)
- - gtk3-devel
- - libXt-devel
+ - gtk3-devel and gtk4-devel
  - mesa-libGLU-devel
 "
 
 
 case "$LINUX_DISTRO" in
-"Fedora")
+"fedora")
 	func_echo_plus "Fedora found. Installing packages..."
 	func_configure_fedora
 	;;
@@ -85,10 +73,3 @@ case "$LINUX_DISTRO" in
 	;;
 esac
 
-
-# check if .classpath exists in swt project.
-if [ -a "../org.eclipse.swt/.classpath" ]; then 
-	func_echo_plus ".classpath found, you are good to go";
-else 
-	func_echo_error "Warning: ../org.eclipse.swt/.classpath not found. Normally you rename ../org.eclipse.swt/.classpath_gtk to ../*/.classpath manually"
-fi
